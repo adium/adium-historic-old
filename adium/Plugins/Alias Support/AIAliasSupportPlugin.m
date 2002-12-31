8b3fd000382891851c784980931b1fce579ad626
adamiser <adamiser@localhost>
2002-12-31 14:17:58 +0000

    Added alias support nib

--- adium/Plugins/Alias Support/AIAliasSupportPlugin.m
+++ adium/Plugins/Alias Support/AIAliasSupportPlugin.m
@@ -9,13 +9,17 @@
 #import "AIAliasSupportPlugin.h"
 #import "AIAdium.h"
 #import <AIUtilities/AIUtilities.h>
-    
+
+#define	CONTACT_ALIAS_NIB		@"ContactAlias"		//Filename of the alias info view
 
 @implementation AIAliasSupportPlugin
 
 - (void)installPlugin
-{
-    
+{    
+    //Install the contact info view
+    [NSBundle loadNibNamed:CONTACT_ALIAS_NIB owner:self];
+    contactView = [[AIContactInfoViewController controllerWithName:@"Alias" categoryName:@"None" view:view_contactAliasInfoView] retain];
+    [[owner contactController] addContactInfoView:contactView];    
 }
 
 
