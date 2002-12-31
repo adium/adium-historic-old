b49af9cd45812fe6372898bcbc52d213f2960b61
adamiser <adamiser@localhost>
2002-12-31 13:34:27 +0000

    - Removed unneeded plugins group from the adium project - Updated 'creating a plugin' docs - Added files for an 'Alias Support' plugin

---
+++ adium/Plugins/Alias Support/AIAliasSupportPlugin.m
@@ -0,0 +1,23 @@
+//
+//  AIAliasSupportPlugin.m
+//  Adium
+//
+//  Created by Adam Iser on Tue Dec 31 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIAliasSupportPlugin.h"
+#import "AIAdium.h"
+#import <AIUtilities/AIUtilities.h>
+    
+
+@implementation AIAliasSupportPlugin
+
+- (void)installPlugin
+{
+    
+}
+
+
+
+@end
