13dc114ee3ec6a4160bbf8459948ee097d93fe59
adamiser <adamiser@localhost>
2003-01-01 23:05:01 +0000

    Added the Alphabetical sort plugin

---
+++ adium/Plugins/Alphabetical Sort/AIAlphabeticalSortPlugin.h
@@ -0,0 +1,16 @@
+//
+//  AIAlphabeticalSortPlugin.h
+//  Adium
+//
+//  Created by Adam Iser on Wed Jan 01 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Adium/Adium.h>
+
+@interface AIAlphabeticalSortPlugin : AIPlugin <AIContactSortController> {
+
+}
+
+@end
