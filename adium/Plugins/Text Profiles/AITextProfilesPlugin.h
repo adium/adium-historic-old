623f54e1dac1c05350f3469b95a530111a01d042
adamiser <adamiser@localhost>
2003-01-08 03:19:00 +0000

    - Starting a text profiles plugin

---
+++ adium/Plugins/Text Profiles/AITextProfilesPlugin.h
@@ -0,0 +1,17 @@
+//
+//  AITextProfilesPlugin.h
+//  Adium
+//
+//  Created by Adam Iser on Tue Jan 07 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/Adium.h>
+
+
+@interface AITextProfilesPlugin : AIPlugin {
+
+}
+
+@end
---
+++ adium/Plugins/Text Profiles/AITextProfilesPlugin.m
@@ -0,0 +1,19 @@
+//
+//  AITextProfilesPlugin.m
+//  Adium
+//
+//  Created by Adam Iser on Tue Jan 07 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import "AITextProfilesPlugin.h"
+
+
+@implementation AITextProfilesPlugin
+
+- (void)installPlugin
+{
+    NSLog(@"Profiles install");
+}
+
+@end
