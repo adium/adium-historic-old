527fe2ce8050ba5970de4ef20d746e191796d72e
adamiser <adamiser@localhost>
2003-01-12 15:54:39 +0000

    - Added the basis for an 'Away Messages' plugin

---
+++ adium/Plugins/Away Messages/AIAwayMessagesPlugin.m
@@ -0,0 +1,19 @@
+//
+//  AIAwayMessagesPlugin.m
+//  Adium
+//
+//  Created by Adam Iser on Sun Jan 12 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIAwayMessagesPlugin.h"
+
+
+@implementation AIAwayMessagesPlugin
+
+- (void)installPlugin
+{
+    NSLog(@"Install away plugin...");
+}
+
+@end
