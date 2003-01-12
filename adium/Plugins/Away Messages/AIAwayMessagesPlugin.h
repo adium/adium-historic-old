c841a439bb16aec8a39a0710566406d2dbc4f1a6
adamiser <adamiser@localhost>
2003-01-12 15:54:39 +0000

    - Added the basis for an 'Away Messages' plugin

---
+++ adium/Plugins/Away Messages/AIAwayMessagesPlugin.h
@@ -0,0 +1,18 @@
+//
+//  AIAwayMessagesPlugin.h
+//  Adium
+//
+//  Created by Adam Iser on Sun Jan 12 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/Adium.h>
+
+
+@interface AIAwayMessagesPlugin : AIPlugin/*<AIPreferenceViewControllerDelegate>*/ {
+    
+
+}
+
+@end
