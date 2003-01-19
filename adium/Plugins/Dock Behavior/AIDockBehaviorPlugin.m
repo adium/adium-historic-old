5b70f2ce71bb668960c9811daa5be543dd3c1185
ramoth4 <ramoth4@localhost>
2003-01-19 21:05:13 +0000

    Right now, for debugging purposes, AIDockBehaviorPlugin tries to bounce 4 times without an interval. However, it doesn't work that way.

--- adium/Plugins/Dock Behavior/AIDockBehaviorPlugin.m
+++ adium/Plugins/Dock Behavior/AIDockBehaviorPlugin.m
@@ -18,16 +18,34 @@
         [NSNumber numberWithBool:YES], @"dock_bounce_onDidReceiveContent", nil]
     forGroup:@"DockBehavior"];
     
+    [[owner preferenceController] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys: 
+        [NSNumber numberWithDouble:0.0], @"dock_bounce_onDidReceiveContent_delay", nil]
+    forGroup:@"DockBehavior"];
+    
+    [[owner preferenceController] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys: 
+        [NSNumber numberWithInt:4], @"dock_bounce_onDidReceiveContent_num", nil]
+    forGroup:@"DockBehavior"];
+    
     //install our observers
     [[[owner contentController] contentNotificationCenter] addObserver:self selector:@selector(messageIn:) name:Content_DidReceiveContent object:nil];
 }
 
 - (void)messageIn:(NSNotification *)notification
 {
-    //if([[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent" group:@"DockBehavior" object:nil] boolValue])
-    //{
-        [[owner dockController] bounce];
-    //}
+    if([[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent" group:@"DockBehavior" object:[notification object]] boolValue]) // are we bouncing at all?
+    {
+        if([[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent_num" group:@"DockBehavior" object:[notification object]] intValue] == 1 && [[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent_num" group:@"DockBehavior" object:[notification object]] doubleValue] > 0.0) //if we only bounce once, and don't have a delay, use the method with less overhead
+        {
+            [[owner dockController] bounce];
+        }
+        else
+        {
+            [[owner dockController] 
+                bounceWithInterval:[[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent_delay" group:@"DockBehavior" object:[notification object]] doubleValue] 
+                times:[[[owner preferenceController] preferenceForKey:@"dock_bounce_onDidReceiveContent_num" group:@"DockBehavior" object:[notification object]] intValue]
+            ];
+        }
+    }
 }
 
 @end
