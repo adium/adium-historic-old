b6fe9d498bd8f0abc9ee8471e6e67eb54c5043e9
atomm1024 <atomm1024@localhost>
2003-01-31 01:36:21 +0000

    AIIconFamily now actually does stuff. It can store three images: "closed," "opened," and "alert." It can initialize with three manually-provided images, or from a folder at a given path containing three images, closed.tif, opened.tif, and alert.tif, or it can simply initialize from a family name, which it will look for in <~/Library/Application Support/Adium 2.0/Icon Families> .

--- adium/Frameworks/Adium Framework/AIIconFamily.m
+++ adium/Frameworks/Adium Framework/AIIconFamily.m
@@ -17,9 +17,73 @@
 #import "AIAdium.h"
 
 @interface AIIconFamily (PRIVATE)
+- (id)initWithClosedImage:(NSImage *)newClosedImage openedImage:(NSImage *)newOpenedImage alertImage:(NSImage *)newAlertImage;
+- (id)initFromFolder:(NSString *)folderPath;
+- (id)initFromFolderNamed:(NSString *)folderName;
 @end
 
 
 @implementation AIIconFamily
+
+// Public creation methods
++ (id)iconFamilyWithClosedImage:(NSImage *)newClosedImage openedImage:(NSImage *)newOpenedImage alertImage:(NSImage *)newAlertImage
+{
+    return [[[AIIconFamily alloc] initWithClosedImage:newClosedImage openedImage:newOpenedImage alertImage:newAlertImage] autorelease];
+}
+
++ (id)iconFamilyFromFolder:(NSString *)folderPath {
+    return [[[AIIconFamily alloc] initFromFolder:folderPath] autorelease];
+}
+
++ (id)iconFamilyNamed:(NSString *)folderName {
+    return [[[AIIconFamily alloc] initFromFolderNamed:folderName] autorelease];
+}
+
+// Private creation methods
+- (id)initWithClosedImage:(NSImage *)newClosedImage openedImage:(NSImage *)newOpenedImage alertImage:(NSImage *)newAlertImage {
+    if (newClosedImage == nil || openedImage == nil || alertImage == nil) {
+        NSLog(@"Warning: Unable to create icon family object (one or more of the provided images were nil).");
+        return nil;
+    }
+    
+    closedImage = [newClosedImage retain];
+    openedImage = [newOpenedImage retain];
+    alertImage = [newAlertImage retain];
+
+    return self;
+}
+
+- (id)initFromFolder:(NSString *)folderPath {
+    NSString *closedImagePath = [folderPath stringByAppendingPathComponent:@"closed.tif"];
+    NSString *openedImagePath = [folderPath stringByAppendingPathComponent:@"opened.tif"];
+    NSString *alertImagePath = [folderPath stringByAppendingPathComponent:@"alert.tif"];
+
+    return [self initWithClosedImage:[[NSImage alloc] initWithContentsOfFile:closedImagePath] openedImage:[[NSImage alloc] initWithContentsOfFile:openedImagePath] alertImage:[[NSImage alloc] initWithContentsOfFile:alertImagePath]];
+}
+
+- (id)initFromFolderNamed:(NSString *)folderName {
+    NSString *iconFamiliesPath = [[[NSString stringWithString:@"~/Library/Application Support/Adium 2.0"] stringByExpandingTildeInPath] stringByAppendingPathComponent:@"Icon Families"];
+
+    return [self initFromFolder:[iconFamiliesPath stringByAppendingPathComponent:folderName]];
+}
+
+// Public accessor functions
+- (NSImage *)closedImage {
+    return closedImage;
+}
+
+- (NSImage *)openedImage {
+    return openedImage;
+}
+
+- (NSImage *)alertImage {
+    return alertImage;
+}
+
+- (void)dealloc {
+    [closedImage release];
+    [openedImage release];
+    [alertImage release];
+}
 
 @end
