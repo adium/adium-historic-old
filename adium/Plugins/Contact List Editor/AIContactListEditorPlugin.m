d6380330ec6a22a4022b7e3a34a31e0697f844ab
adamiser <adamiser@localhost>
2003-05-16 03:40:57 +0000

    More assorted contact list editor changes

--- adium/Plugins/Contact List Editor/AIContactListEditorPlugin.m
+++ adium/Plugins/Contact List Editor/AIContactListEditorPlugin.m
@@ -25,6 +25,7 @@
 
 @interface AIContactListEditorPlugin (PRIVATE)
 - (void)_generateCollectionsArray;
+- (AIEditorListHandle *)_handleNamed:(NSString *)name inGroup:(AIEditorListGroup *)group;
 @end
 
 @implementation AIContactListEditorPlugin
@@ -99,6 +100,168 @@
     //Flush the collections array
     [collectionsArray release]; collectionsArray = nil;
 }
+
+//Find a handle
+- (AIEditorListHandle *)handleNamed:(NSString *)targetHandleName onCollection:(id <AIEditorCollection>)collection
+{
+    return([self _handleNamed:targetHandleName inGroup:[collection list]]);
+}
+
+- (AIEditorListHandle *)_handleNamed:(NSString *)name inGroup:(AIEditorListGroup *)group
+{
+    NSEnumerator	*enumerator;
+    AIEditorListObject	*object;
+
+    //Find the correct group on the new collection
+    enumerator = [group objectEnumerator];
+    while(object = [enumerator nextObject]){
+        if([object isKindOfClass:[AIEditorListHandle class]]){ //Compare the handle names
+            if([name compare:[object UID]] == 0){
+                return((AIEditorListHandle *)object);
+            }
+
+        }else if([object isKindOfClass:[AIEditorListGroup class]]){ //Scan the subgroup
+            if((object = [self _handleNamed:name inGroup:(AIEditorListGroup *)object])){
+                return((AIEditorListHandle *)object);
+            }
+        }
+    }
+
+    return(nil);
+}
+
+//Find a group
+- (AIEditorListGroup *)groupNamed:(NSString *)targetGroupName onCollection:(id <AIEditorCollection>)collection
+{
+    NSEnumerator	*enumerator;
+    AIEditorListGroup	*group;
+
+    //Find the correct group on the new collection
+    enumerator = [[collection list] objectEnumerator];
+    while(group = [enumerator nextObject]){
+        if([[group UID] compare:targetGroupName] == 0){
+            return(group);
+        }
+    }
+
+    return(nil);
+}
+
+//List Manipulation (sends out notifications)
+//Create a handle
+- (AIEditorListHandle *)createHandleNamed:(NSString *)name inGroup:(AIEditorListGroup *)group onCollection:(id <AIEditorCollection>)collection temporary:(BOOL)temporary
+{
+    AIEditorListHandle	*handle;
+
+    if(temporary){
+        handle = [[AIEditorListHandle alloc] initWithServiceID:[collection serviceID] UID:name temporary:YES];
+        [group addObject:handle]; //We don't add the handle to the collection, since it's only temporary
+
+    }else{
+        handle = [[AIEditorListHandle alloc] initWithServiceID:[collection serviceID] UID:name temporary:NO]; //Create the handle
+        [group addObject:handle];	//Add it to the list
+        [collection addObject:handle];	//Let the collection add it
+
+        //Post an object added notification
+        [[owner notificationCenter] postNotificationName:Editor_AddedObjectToCollection object:collection userInfo:[NSDictionary dictionaryWithObject:handle forKey:@"Object"]];
+    }
+
+
+    return(handle);
+}
+
+//Create a group
+- (AIEditorListGroup *)createGroupNamed:(NSString *)name onCollection:(id <AIEditorCollection>)collection temporary:(BOOL)temporary
+{
+    AIEditorListGroup	*group;
+
+    if(temporary){
+        group = [[AIEditorListGroup alloc] initWithUID:name temporary:YES];
+        [[collection list] addObject:group]; //We don't add the group to the collection, since it's only temporary
+
+    }else{
+        group = [[AIEditorListGroup alloc] initWithUID:name temporary:NO]; 	//Create the group
+        [[collection list] addObject:group];					//Add it to the list
+        [collection addObject:group];						//Let the collection add it
+
+        //Post an object added notification
+        [[owner notificationCenter] postNotificationName:Editor_AddedObjectToCollection object:collection userInfo:[NSDictionary dictionaryWithObject:group forKey:@"Object"]];
+    }
+
+    return(group);
+}
+
+//Rename an object (correctly sets temporary objects as permanent)
+- (void)renameObject:(AIEditorListObject *)object onCollection:(id <AIEditorCollection>)collection to:(NSString *)name
+{
+    if([object temporary]){
+        //Temporary objects have not yet been added to the collection, so we can freely change its UID to the correct/new one before adding it.
+        [object setUID:name];
+        [collection addObject:object];
+        [object setTemporary:NO];
+
+        //Post an object added notification
+        [[owner notificationCenter] postNotificationName:Editor_AddedObjectToCollection object:collection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+
+    }else{
+        [collection renameObject:object to:name];
+        [object setUID:name]; //Rename the object after the collection has had a chance to rename
+
+        //Posta renamed notification
+        [[owner notificationCenter] postNotificationName:Editor_RenamedObjectOnCollection object:collection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+    }
+
+    [[object containingGroup] sort]; //resort the containing group
+
+}
+
+//Move an object
+- (void)moveObject:(AIEditorListObject *)object fromCollection:(id <AIEditorCollection>)sourceCollection toGroup:(AIEditorListGroup *)destGroup collection:(id <AIEditorCollection>)destCollection
+{
+    [object retain]; //Temporarily hold onto the object
+
+    if(sourceCollection == destCollection){
+        //Allow the collection to move the object
+        [sourceCollection moveObject:object toGroup:destGroup];
+
+        //Swap it from one group to the other
+        [[object containingGroup] removeObject:object];
+        [[owner notificationCenter] postNotificationName:Editor_RemovedObjectFromCollection object:sourceCollection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+
+        [destGroup addObject:object];
+        [[owner notificationCenter] postNotificationName:Editor_AddedObjectToCollection object:destCollection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+
+    }else{
+        //Remove from the source collection
+        [sourceCollection deleteObject:object];
+        [[object containingGroup] removeObject:object];
+        [[owner notificationCenter] postNotificationName:Editor_RemovedObjectFromCollection object:sourceCollection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+
+        //Add to the destination collection
+        [destGroup addObject:object];
+        [destCollection addObject:object];
+        [[owner notificationCenter] postNotificationName:Editor_AddedObjectToCollection object:destCollection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+    }
+
+    [object release];
+}
+
+//Delete an object
+- (void)deleteObject:(AIEditorListObject *)object fromCollection:(id <AIEditorCollection>)collection
+{
+    [object retain]; //Hold onto the object until we're done with it
+
+    if(![object temporary]){ //Since temp objects aren't yet in the collecting, we skip this call
+        [collection deleteObject:object];
+    }
+    [[object containingGroup] removeObject:object];
+
+    [[owner notificationCenter] postNotificationName:Editor_RemovedObjectFromCollection object:collection userInfo:[NSDictionary dictionaryWithObject:object forKey:@"Object"]];
+
+    [object release];
+}
+
+
 
 //Builds the collection array
 - (void)_generateCollectionsArray
