a67f5a30cce0475006b1f9f247e1a07e12c95fef
adamiser <adamiser@localhost>
2003-05-30 12:04:33 +0000

    - Adjusted appearance of dock overlays - Added overlays for signed on/signed of buddies - Doubled TOC sign on delay (should solve any quacking problems) - Fixed a bug where Adium would always miss the first signoff of a contact who was initially online.

--- adium/Plugins/Contact Status Dock Overlays/AIContactStatusDockOverlaysPlugin.m
+++ adium/Plugins/Contact Status Dock Overlays/AIContactStatusDockOverlaysPlugin.m
@@ -40,20 +40,21 @@
 
 - (NSArray *)updateContact:(AIListContact *)inContact handle:(AIHandle *)inHandle keys:(NSArray *)inModifiedKeys
 {
-    if([inModifiedKeys containsObject:@"UnviewedContent"]){
-        if([[inContact statusArrayForKey:@"UnviewedContent"] greatestIntegerValue]){
+    if([inModifiedKeys containsObject:@"UnviewedContent"] || [inModifiedKeys containsObject:@"Signed On"] || [inModifiedKeys containsObject:@"Signed Off"]){
+        if([[inContact statusArrayForKey:@"UnviewedContent"] greatestIntegerValue] || [[inContact statusArrayForKey:@"Signed On"] greatestIntegerValue] || [[inContact statusArrayForKey:@"Signed Off"] greatestIntegerValue]){
             if(![unviewedContactsArray containsObject:inContact]){
                 [unviewedContactsArray addObject:inContact];
-                [self _setOverlay]; //Redraw our overlay
+//                [self _setOverlay]; //Redraw our overlay
             }
             
         }else{
             if([unviewedContactsArray containsObject:inContact]){
                 [unviewedContactsArray removeObject:inContact];
-                [self _setOverlay]; //Redraw our overlay
+//                [self _setOverlay]; //Redraw our overlay
             }
         }
 
+        [self _setOverlay]; //Redraw our overlay
     }
 
     return(nil);
@@ -80,17 +81,19 @@
         AIListContact		*contact;
         NSImage			*image;
         NSFont			*font;
-        float			dockIconScale, iconHeight;
+        float			dockIconScale;//, iconHeight;
+        int			iconHeight;
         float			top, bottom;
         
         //Pre-calc some sizes
         dockIconScale = 1.0 - [[owner dockController] dockIconScale];
         iconHeight = (SMALLESTRADIUS + (RADIUSRANGE * dockIconScale));
-        top = 128;
+        top = 126;
         bottom = top - iconHeight;
 
         //Set up the string details
-        font = [NSFont labelFontOfSize:(SMALLESTFONTSIZE + (FONTSIZERANGE * dockIconScale))];
+        //font = [NSFont labelFontOfSize:(SMALLESTFONTSIZE + (FONTSIZERANGE * dockIconScale))];
+        font = [NSFont fontWithName:@"Lucida Grande" size:(SMALLESTFONTSIZE + (FONTSIZERANGE * dockIconScale))];
         [paragraphStyle setLineBreakMode:NSLineBreakByClipping];
         [paragraphStyle setAlignment:NSCenterTextAlignment];
         
@@ -103,18 +106,16 @@
             float		left, right, arcRadius, stringInset;
             NSAttributedString	*nameString;
             NSBezierPath	*path;
-
-            //Get the contact's display name
-            nameString = [[[NSAttributedString alloc] initWithString:[contact displayName] attributes:[NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, paragraphStyle, NSParagraphStyleAttributeName, nil]] autorelease];
+            NSColor		*backColor, *textColor, *borderColor;
 
             //Create the pill frame
-            arcRadius = (iconHeight/4.0);
-            stringInset = (iconHeight/6.0);
-            left = arcRadius;
-            right = 128 - arcRadius;
+            arcRadius = (iconHeight/2.0); //4
+            stringInset = (iconHeight/4.0); //6
+            left = 1 + arcRadius;
+            right = 127 - arcRadius;
             
             path = [NSBezierPath bezierPath];
-            [path setLineWidth:(arcRadius * 0.13333)];
+            [path setLineWidth:((iconHeight/2.0) * 0.13333)/*(arcRadius * 0.13333)*/];
             //Top
             [path moveToPoint: NSMakePoint(left, top)];
             [path lineToPoint: NSMakePoint(right, top)];
@@ -123,6 +124,7 @@
             [path lineToPoint: NSMakePoint(right + arcRadius, bottom + arcRadius)];
             [path appendBezierPathWithArcWithCenter:NSMakePoint(right, bottom + arcRadius) radius:arcRadius startAngle:0 endAngle:270 clockwise:YES];
             //Bottom
+            [path moveToPoint: NSMakePoint(right, bottom)];
             [path lineToPoint: NSMakePoint(left, bottom)];
             //Left rounded cap
             [path appendBezierPathWithArcWithCenter:NSMakePoint(left, bottom + arcRadius) radius:arcRadius startAngle:270 endAngle:180 clockwise:YES];
@@ -131,17 +133,38 @@
 
             //Display
             [image lockFocus];
-            	[[NSColor colorWithCalibratedWhite:1.0 alpha:0.7] set];
-            	[path fill];
 
-            	[[NSColor colorWithCalibratedWhite:0.0 alpha:0.6] set];
-            	[path stroke];
+            if([[contact statusArrayForKey:@"Signed On"] greatestIntegerValue]){ //Signed on
+                backColor = [NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.75];
+                textColor = [NSColor colorWithCalibratedRed:0.0 green:0.2 blue:0.0 alpha:1.0];
+                borderColor = [NSColor colorWithCalibratedRed:0.0 green:0.3 blue:0.0 alpha:1.0];
+                
+            }else if([[contact statusArrayForKey:@"Signed Off"] greatestIntegerValue]){ //Signed off
+                backColor = [NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.75];
+                textColor = [NSColor colorWithCalibratedRed:0.3 green:0.0 blue:0.0 alpha:1.0];
+                borderColor = [NSColor colorWithCalibratedRed:0.3 green:0.0 blue:0.0 alpha:1.0];
 
-            	[nameString drawInRect:NSMakeRect(0 + stringInset, bottom, 128 - (stringInset * 2), top - bottom)];
+            }else{ //Unviewed
+                backColor = [NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.75];
+                textColor = [NSColor colorWithCalibratedWhite:0.0 alpha:1.0];
+                borderColor = [NSColor colorWithCalibratedRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+                
+            }
+
+            [backColor set];
+            [path fill];
+            [borderColor set];
+            [path stroke];
+
+            //Get the contact's display name
+            nameString = [[[NSAttributedString alloc] initWithString:[contact displayName] attributes:[NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, paragraphStyle, NSParagraphStyleAttributeName, textColor, NSForegroundColorAttributeName, nil]] autorelease];
+
+            
+            [nameString drawInRect:NSMakeRect(0 + stringInset, bottom, 128 - (stringInset * 2), top - bottom)];
             [image unlockFocus];
 
             //Move down to the next pill
-            top -= (iconHeight + 5.0 * dockIconScale);
+            top -= (iconHeight + 7.0 * dockIconScale);
             bottom = top - iconHeight;
         }
     
