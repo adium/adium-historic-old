486815d2668b2335a5f93c15fd2e29fd153b931d
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-06-13 01:24:14 +0000

    So this is my first commit and isn't anything spectacular - just an attempt to figure out how CVS works, mostly.  I implemented a buddy list sorting plugin the way I prefer to see my buddies sorted - Idle & Away to the bottom, but respecting the manual ordering (in other words, ignoring all other factors like time idle).  If I screwed something up in the process or violated some point of committment etiquette, someone please let me know so I don't do it again. ;)
    -Evan

---
+++ adium/Plugins/Idle Away Manual Sort/AIIdleAwayManualSort.h
@@ -0,0 +1,17 @@
+//
+//  AIIdleAwayManualSort.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Thu Jun 12 2003.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@protocol AIListSortController;
+
+@interface AIIdleAwayManualSort : NSObject <AIListSortController> {
+
+}
+
+
+@end
