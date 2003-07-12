3ada19c1438c5a66424eb5a245c2e9f7493ca58e
Colin Barrett <colin@springsandstruts.com>
2003-07-12 01:46:46 +0000

    - Yay. We now have embossed NSTextFields. What's New  - Embossed code copied right from the safari tab code :) No reason why it shouldn't. Bugs/What's Missing  - 100% untested. Everything compiles though, so no worries. :)

---
+++ adium/Frameworks/AIUtilities Framework/CBEmbossedTextField.h
@@ -0,0 +1,18 @@
+//
+//  CBEmbossedTextField.h
+//  Adium
+//
+//  Created by Colin Barrett on Fri Jul 11 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CBEmbossedTextField : NSTextField 
+{
+
+}
+
+- (void)drawRect:(NSRect)inRect;
+
+@end
