956c851672c426f4ac801989c36e7a9df0f61eaf
Colin Barrett <colin@springsandstruts.com>
2003-07-22 23:34:06 +0000

    Let the encapsulation BEGIN!

---
+++ adium/Plugins/MSN Service/MSNSBSocket.h
@@ -0,0 +1,19 @@
+//
+//  MSNSBSocket.h
+//  Adium
+//
+//  Created by Colin Barrett on Tue Jul 22 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/Adium.h>
+
+@class AISocket
+
+@interface MSNSBSocket : NSObject 
+{
+    AISocket 		*socket;		// The connection socket
+}
+
+@end
