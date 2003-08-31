87b0981c0174fa51d296121730ff29881d256d26
Colin Barrett <colin@springsandstruts.com>
2003-08-31 02:03:39 +0000

    part 2 of the previous commit :) I had the folder named wrong :X Other/Adim 1.6 Pref Importer/ Intelligent :P

---
+++ adium/Other/Adium 1.6 Pref Importer/CBOldPrefsImporterAppController.h
@@ -0,0 +1,33 @@
+//
+//  CBOldPrefsImporterAppController.h
+//  Adium
+//
+//  Created by Colin Barrett on Sun Aug 24 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CBOldPrefsImporterAppController : NSObject 
+{
+    IBOutlet	NSWindow		*window_main;
+    IBOutlet	NSPopUpButton		*popUpButton_account;
+    IBOutlet	NSButton		*checkBox_aliases;
+    IBOutlet	NSProgressIndicator	*progressIndicator;
+    IBOutlet	NSButton		*button_import;
+    
+    IBOutlet	NSWindow		*theSheet;
+    IBOutlet	NSPopUpButton		*popUpButton_user;
+    IBOutlet	NSButton		*button_OK;
+    IBOutlet	NSButton		*button_Cancel;
+}
+
+- (IBAction)import:(id)sender;
+- (IBAction)sheetButton:(id)sender;
+
+//NSApplication delegate methods
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication;
+
+//modal delegate methods
+- (void)sheetDidEnd:(NSWindow *)sheet returnCode:(int)code contextInfo:(void *)info;
+@end
