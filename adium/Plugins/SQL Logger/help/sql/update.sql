e872f601baf6fbaaca5a298fde58473df2418db4
jmelloy <jmelloy@localhost>
2003-09-03 05:16:13 +0000

    Made some documentation updates and switched schema around a bit. Schema now stores number of messages sent between individual users. Now, instead of having total messages sent and received in the users table, it has been moved into a separate table (user_statistics) and keeps track of messages sent between different users, and the last message sent date.
    (fetchgreebledonx -> resimada 210;
     resimada -> fetchgreebledonx 137)
      *** ACTUAL LIVE DATA AS OF THIS LOG MESSAGE ***
    
    This has a few advantages -- more granularity to the data, which is more
    useful, (instead of fetchgreebledonx has sent 38,849 messages) and decreases
    churn on users.
    
    Decreasing churn on users was really my main reason, if users isn't updated
    twice with every database insertion, routine postgresql maintenance doesn't
    become nearly as important.  Users is kind of an important table, since
    everything links on it, it should be fast fast fast.
    
    While I was at it, I added a bit of documentation on routine maintenance (no
    real details yet) and performance.
    
    Considering moving final piece of login into Adium itself, and making it
    controllable through a preference.

--- adium/Plugins/SQL Logger/help/sql/update.sql
+++ adium/Plugins/SQL Logger/help/sql/update.sql
@@ -1,49 +1,23 @@
-alter table users add column num_sent int;
-alter table users alter column num_sent set default 0;
-alter table users add column num_received int;
-alter table users alter column num_received set default 0;
+alter table users drop column num_sent int;
+alter table users drop column num_received int;
 
-update users set num_sent =-
-(select count(*) from messages where sender_id = user_id);
+create table adium.user_statistics (
+sender_id       int references adium.users(user_id) not null,
+recipient_id    int references adium.users(user_id) not null,
+num_messages    int default 1,
+last_message    timestamp default now(),
+primary key (sender_id, recipient_id)
+);
 
-update users set num_received =-
-(select count(*) from messages where recipient_id = user_id);
-
-vacuum full analyze;
-
-create or replace view adium.message_v as
-select message_id,
-       message_date,
-       message,
-       s.username as sender_sn,
-       s.service as sender_service,
-       s_disp.display_name as sender_display,
-       r.username as recipient_sn,
-       r.service as recipient_service,
-       r_disp.display_name as recipient_display
-from   adium.messages m,
-       adium.users s natural join adium.user_display_name s_disp,
-       adium.users r natural join adium.user_display_name r_disp
-where  m.sender_id = s.user_id
-  and  m.recipient_id = r.user_id
-  and  s_disp.effdate < message_date
-  and  not exists (
-       select 'x'
-       from   adium.user_display_name udn
-       where  udn.effdate > s_disp.effdate
-       and    udn.user_id = s.user_id
-       and    udn.effdate < message_date)
-  and  r_disp.effdate < message_date
-  and  not exists (
-       select 'x'
-       from   adium.user_display_name udn
-       where  udn.effdate > r_disp.effdate
-       and    udn.user_id = r.user_id
-       and    udn.effdate < message_date);
+insert into adium.user_statistics
+select sender_id, recipient_id, count(*), max(message_date)
+from messages
+group by sender_id, recipient_id;
 
 create or replace rule insert_message_v as
 on insert to adium.message_v
 do instead  (
+    
     insert into adium.users (username,service)
     select new.sender_sn, coalesce(new.sender_service, 'AIM')
     where not exists (
@@ -98,14 +72,26 @@ do instead  (
     coalesce(new.message_date, now() )
     );
 
-    update adium.users
-    set num_sent = num_sent + 1
-    where user_id = (select user_id from users where username= new.sender_sn
-      and service = new.sender_service);
+    update adium.user_statistics
+    set num_messages = num_messages + 1,
+    last_message = CURRENT_TIMESTAMP
+    where sender_id = (select user_id from users where username =
+    new.sender_sn and service = new.sender_service) 
+    and recipient_id = (select user_id from users where username =
+    new.recipient_sn and service = new.recipient_service);
 
-    update adium.users
-    set num_received = num_received + 1
-    where user_id = (select user_id from users where username =
-    new.recipient_sn
-     and service = new.recipient_service);
+    insert into adium.user_statistics
+    (sender_id, recipient_id)
+    select
+    (select user_id from users 
+    where username = new.sender_sn and service = new.sender_service),
+    (select user_id from users
+    where username = new.recipient_sn and service = new.recipient_service)
+    where not exists 
+        (select 'x' from user_statistics
+        where sender_id = (select user_id from users where username =
+        new.sender_sn and service = new.sender_service) 
+        and recipient_id = 
+        (select user_id from users where username = 
+        new.recipient_sn and service = new.recipient_service))
 );
