4be9dc984a18b263a2a1cddaf97736aa06e44de3
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-09-10 20:53:17 +0000

    Code useability & cleanup for modifying the sending text view through the controller - now accepts an attributed string instead of an NSData.  While this looks a lot better, it still doesn't fix the problems with not resizing when text is dropped directly on the tab.  I've spent about 3 hours on it today, and a bunch yesterday, playing with all sorts of possible changes.. and can get nowhere.  The last (and most reasonable) changes are commented in the code for setTextEntryViewTo:; appendToTextEntryView: which is currently being used has the same problems.
    Someone please fix it - I admit defeat on the matter.

--- adium/Plugins/Dual Window Interface/AIMessageViewController.m
+++ adium/Plugins/Dual Window Interface/AIMessageViewController.m
@@ -99,7 +99,7 @@
     [self clearTextEntryView]; //Clear the message entry text view
 }
 
-//Set the sounce account of this message
+//Set the source account of this message
 - (void)setAccount:(AIAccount *)inAccount
 {
     //Initiate a new chat with the specified account.  The interface will automatically recycle this message view, switching it to the new account.
@@ -216,25 +216,39 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:NSTextDidChangeNotification object:textView_outgoing];
 }
 
+#warning Need to resize our input view somehow in setTextEntryViewTo and appendToTextEntryView.  The below (in setTextEntryViewTo) seems reasonable but doesn't work.
+
 //Set the message entry text view and force a textDidChange: notification
-- (void)setTextEntryViewTo:(NSData *)data
+- (void)setTextEntryViewTo:(NSAttributedString *)inString
 {
-    [textView_outgoing replaceCharactersInRange:NSMakeRange(0, [[textView_outgoing textStorage] length]) withRTF:data];
+    NSRange oldRange = NSMakeRange(0,([[textView_outgoing textStorage] length]));
+    int changeInLength = [ inString length ] - oldRange.length;
+
+    /* NSTextView
+    NSTextStorage */
+    [textView_outgoing setAttributedString:inString];
+
+    //NSLog(@"calling edit with range location %i length %i and changeinLength %i",[[textView_outgoing textStorage] editedRange].location,[[textView_outgoing textStorage] editedRange].length,[[textView_outgoing textStorage] changeInLength]);
+
+    //  [[textView_outgoing textStorage] edited:NSTextStorageEditedCharacters range:oldRange changeInLength:changeInLength];
+    //	[[textView_outgoing layoutManager] textContainerChangedGeometry:[textView_outgoing textContainer]];
+    //	[self sizeAndArrangeSubviews];
+    
     [[NSNotificationCenter defaultCenter] postNotificationName:NSTextDidChangeNotification object:textView_outgoing];
-#warning Need to resize our input view somehow.  The below seems reasonable but doesn't work.
-    /* [[textView_outgoing layoutManager] textContainerChangedGeometry:[textView_outgoing textContainer]];
-    [self sizeAndArrangeSubviews];
-    */
+    
+ //   [[NSNotificationCenter defaultCenter] postNotificationName:NSTextDidEndEditingNotification object:textView_outgoing];
+
+    
 }
 
-/*
+//Note: this routine is currently used for drag and drop onto tabs.  An appendAttributedString method might reasonably be added to the AISendingTextView, though it seems to be unnecessary.  setTextEntryViewTo: exhibits the same resizing problems.
  //Append to the message entry text view and force a textDidChange: notification
  - (void)appendToTextEntryView:(NSAttributedString *)inString
  {
      [[textView_outgoing textStorage] appendAttributedString:inString];
      [[NSNotificationCenter defaultCenter] postNotificationName:NSTextDidChangeNotification object:textView_outgoing];
  }
- */
+ 
 
 
 //Private -----------------------------------------------------------------------------
