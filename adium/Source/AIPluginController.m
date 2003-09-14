8aa99b68796a4cb66be5d4bac6e7d20428fd6397
Evan Schoenberg <Evan Schoenberg@localhost>
2003-09-14 22:35:23 +0000

    - Plugins are now loaded from the Plugins folder in Adium's application support folder as well as from the bundle.

--- adium/Source/AIPluginController.m
+++ adium/Source/AIPluginController.m
@@ -1,23 +1,24 @@
 /*-------------------------------------------------------------------------------------------------------*\
 | Adium, Copyright (C) 2001-2003, Adam Iser  (adamiser@mac.com | http://www.adiumx.com)                   |
-\---------------------------------------------------------------------------------------------------------/
- | This program is free software; you can redistribute it and/or modify it under the terms of the GNU
- | General Public License as published by the Free Software Foundation; either version 2 of the License,
- | or (at your option) any later version.
- |
- | This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- | the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
- | Public License for more details.
- |
- | You should have received a copy of the GNU General Public License along with this program; if not,
- | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- \------------------------------------------------------------------------------------------------------ */
+					      \---------------------------------------------------------------------------------------------------------/
+					      | This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+					      | General Public License as published by the Free Software Foundation; either version 2 of the License,
+					      | or (at your option) any later version.
+					      |
+					      | This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+					      | the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+					      | Public License for more details.
+					      |
+					      | You should have received a copy of the GNU General Public License along with this program; if not,
+					      | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+					      \------------------------------------------------------------------------------------------------------ */
 
 #import <Adium/Adium.h>
 #import "AIAdium.h"
 #import "AIPluginController.h"
 
 #define DIRECTORY_INTERNAL_PLUGINS		@"/Contents/Plugins"	//Path to the internal plugins
+#define DIRECTORY_EXTERNAL_PLUGINS		@"/Plugins"		//Path to the external plugins
 #define EXTENSION_ADIUM_PLUGIN			@"AdiumPlugin"		//File extension of a plugin
 
 @interface AIPluginController (PRIVATE)
@@ -43,7 +44,7 @@
 - (void)dealloc
 {
     [pluginArray release]; pluginArray = nil;
-    
+
     [super dealloc];
 }
 
@@ -52,59 +53,66 @@
 {
     NSArray	*pluginList;
     NSString	*pluginPath;
-    int		loop;
+    int		loop, counter;
 
     NSParameterAssert(owner != nil);
     NSParameterAssert(pluginArray != nil);
-    
-    //Get the plugin path
-    pluginPath = [[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:DIRECTORY_INTERNAL_PLUGINS] stringByExpandingTildeInPath];
 
-    //Get the directory listing of plugins
-    pluginList = [[NSFileManager defaultManager] directoryContentsAtPath:pluginPath];
+    for(counter=0 ; counter < 2 ; counter++)
+    {
+	//Get the plugin path
+	if (counter == 0) { //bundle
+	    pluginPath = [[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:DIRECTORY_INTERNAL_PLUGINS] stringByExpandingTildeInPath];
+	}else {  //user's folder
+	    pluginPath = [[[AIAdium applicationSupportDirectory] stringByAppendingPathComponent:DIRECTORY_EXTERNAL_PLUGINS] stringByExpandingTildeInPath];
+	}
+	
+	//Get the directory listing of plugins
+	pluginList = [[NSFileManager defaultManager] directoryContentsAtPath:pluginPath];
 
-    for(loop = 0;loop < [pluginList count];loop++){
-        NSString 		*pluginName;
-        NSBundle		*pluginBundle;
-        AIPlugin		*plugin = nil;
+	for(loop = 0;loop < [pluginList count];loop++){
+	    NSString 		*pluginName;
+	    NSBundle		*pluginBundle;
+	    AIPlugin		*plugin = nil;
 
-        pluginName = [pluginList objectAtIndex:loop];
-        //NSLog (@"Loading plugin: \"%@\"", pluginName);
-        if([[pluginName pathExtension] compare:EXTENSION_ADIUM_PLUGIN] == 0){
-            NS_DURING
-                //Load the plugin
-                pluginBundle = [NSBundle bundleWithPath:[pluginPath stringByAppendingPathComponent:pluginName]];
-                if(pluginBundle != nil){
-                    //Create an instance of the plugin
-                    plugin = [[pluginBundle principalClass] newInstanceOfPluginWithOwner:owner];
-        
-                    if(plugin != nil){
-                        //Add the instance to our list
-                        [pluginArray addObject:plugin];
-                    }else{
-                        NSLog(@"Failed to initialize Plugin \"%@\"!",pluginName);
-                    }
-                }else{
-                    NSLog(@"Failed to open Plugin \"%@\"!",pluginName);
-                }
-            NS_HANDLER	// Handle a raised exception
-                NSLog(@"The plugin \"%@\" suffered a fatal assertion!",pluginName);
-                if (plugin != nil) {
-                    NSLog (@"Cleaning up using plugin's pointer");
-                    // Make sure the plugin was not stored in the pluginArray, since it failed to successfully initialize
-                    long index = [pluginArray indexOfObject:plugin];
-                    if (index != NSNotFound) {
-                        [pluginArray removeObjectAtIndex:index];
-                    }
+	    pluginName = [pluginList objectAtIndex:loop];
+	    //NSLog (@"Loading plugin: \"%@\"", pluginName);
+	    if([[pluginName pathExtension] compare:EXTENSION_ADIUM_PLUGIN] == 0){
+		NS_DURING
+		    //Load the plugin
+		    pluginBundle = [NSBundle bundleWithPath:[pluginPath stringByAppendingPathComponent:pluginName]];
+		    if(pluginBundle != nil){
+			//Create an instance of the plugin
+			plugin = [[pluginBundle principalClass] newInstanceOfPluginWithOwner:owner];
 
-                    //Remove observers
-                    [[owner notificationCenter] removeObserver:plugin];
-                    [[NSNotificationCenter defaultCenter] removeObserver:plugin];
-                }
-                [[owner interfaceController] handleErrorMessage:@"Plugin load error" withDescription:[NSString stringWithFormat:@"The \"%@\" plugin failed to load properly.  It may be partially loaded.  If strange behavior ensues, remove it from Adium 2's plugin directory, then quit and relaunch Adium.", [pluginName stringByDeletingPathExtension]]];
-                // It would probably be unsafe to call the plugin's uninstall
-            NS_ENDHANDLER
-        }
+			if(plugin != nil){
+			    //Add the instance to our list
+			    [pluginArray addObject:plugin];
+			}else{
+			    NSLog(@"Failed to initialize Plugin \"%@\"!",pluginName);
+			}
+		    }else{
+			NSLog(@"Failed to open Plugin \"%@\"!",pluginName);
+		    }
+		    NS_HANDLER	// Handle a raised exception
+			NSLog(@"The plugin \"%@\" suffered a fatal assertion!",pluginName);
+			if (plugin != nil) {
+			    NSLog (@"Cleaning up using plugin's pointer");
+			    // Make sure the plugin was not stored in the pluginArray, since it failed to successfully initialize
+			    long index = [pluginArray indexOfObject:plugin];
+			    if (index != NSNotFound) {
+				[pluginArray removeObjectAtIndex:index];
+			    }
+
+			    //Remove observers
+			    [[owner notificationCenter] removeObserver:plugin];
+			    [[NSNotificationCenter defaultCenter] removeObserver:plugin];
+			}
+			[[owner interfaceController] handleErrorMessage:@"Plugin load error" withDescription:[NSString stringWithFormat:@"The \"%@\" plugin failed to load properly.  It may be partially loaded.  If strange behavior ensues, remove it from Adium 2's plugin directory, then quit and relaunch Adium.", [pluginName stringByDeletingPathExtension]]];
+			// It would probably be unsafe to call the plugin's uninstall
+		    NS_ENDHANDLER
+	    }
+	}
     }
 }
 
