7d3147eea9dbdee3ba95bbe639333de42b382306
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-09-24 07:52:24 +0000

    -Added away autoresponse support (see next committ) to the status menu -Added a couple NSLogs in the closeInterface: method. I'm trying to track down an elusive crash-on-exit 'feature'... if you crash while quitting Adium, take a look at your console output just before the crash and let me know if you see 'calling close:nil' 'releasing' and/or 'done - contactListWindowController closed'.  I can't figure out why the crash occurs exactly, and I haven't been able to replicate it reliably enough to track it down (hasn't happened since I added the NSLogs, of course)

--- adium/Plugins/Dual Window Interface/AIStatusSelectionView.m
+++ adium/Plugins/Dual Window Interface/AIStatusSelectionView.m
@@ -138,8 +138,11 @@
     NSString		*title = [sender title];
     
     if(representedObject){ //Away
-        NSAttributedString	*awayMessage = [representedObject objectForKey:@"Message"];
-        [[owner accountController] setProperty:awayMessage forKey:@"AwayMessage" account:nil];
+	NSAttributedString	*awayMessage = [representedObject objectForKey:@"Message"];
+	NSAttributedString	*awayAutoResponse = [representedObject objectForKey:@"Autoresponse"];
+	[[owner accountController] setProperty:awayMessage forKey:@"AwayMessage" account:nil];
+	if (awayAutoResponse)
+	    [[owner accountController] setProperty:awayAutoResponse forKey:@"AutoResponse" account:nil];
 
     }else if([title compare:STATUS_NAME_AVAILABLE] == 0){ //Available
         [[owner accountController] setProperty:nil forKey:@"AwayMessage" account:nil];
