8eb7ca538953766ad52bd705ed56675f8322212d
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-10-10 04:31:41 +0000

    [@"AutoResponse" compare:@"Autoresponse"] != 0

--- adium/Plugins/Dual Window Interface/AIStatusSelectionView.m
+++ adium/Plugins/Dual Window Interface/AIStatusSelectionView.m
@@ -141,7 +141,7 @@
 	NSAttributedString	*awayMessage = [representedObject objectForKey:@"Message"];
 	NSAttributedString	*awayAutoResponse = [representedObject objectForKey:@"Autoresponse"];
 	[[owner accountController] setProperty:awayMessage forKey:@"AwayMessage" account:nil];
-        [[owner accountController] setProperty:awayAutoResponse forKey:@"AutoResponse" account:nil];
+        [[owner accountController] setProperty:awayAutoResponse forKey:@"Autoresponse" account:nil];
 
     }else if([title compare:STATUS_NAME_AVAILABLE] == 0){ //Available
         [[owner accountController] setProperty:nil forKey:@"AwayMessage" account:nil];
