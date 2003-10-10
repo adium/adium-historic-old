581d2acbe7b77a4634c523b624ab22188e7cd8d5
adamiser <adamiser@localhost>
2003-10-10 03:29:01 +0000

    - Updated message window/tab preferences - Updated contact list resizing preferences

---
+++ adium/Plugins/Dual Window Interface/AIDualWindowAdvancedPrefs.m
@@ -0,0 +1,64 @@
+//
+//  AIDualWindowPreferences.m
+//  Adium
+//
+//  Created by Adam Iser on Sat Jul 12 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIDualWindowAdvancedPrefs.h"
+#import "AIDualWindowInterfacePlugin.h"
+#import <Adium/Adium.h>
+#import <AIUtilities/AIUtilities.h>
+
+@implementation AIDualWindowAdvancedPrefs
+
+//Preference pane properties
+- (PREFERENCE_CATEGORY)category{
+    return(AIPref_Advanced_ContactList);
+}
+- (NSString *)label{
+    return(@"Auto-resizing");
+}
+- (NSString *)nibName{
+    return(@"DualWindowAdvanced");
+}
+
+//Configures our view for the current preferences
+- (void)viewDidLoad
+{
+    NSDictionary	*preferenceDict = [[owner preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+
+    [checkBox_autoResize setState:[[preferenceDict objectForKey:KEY_DUAL_RESIZE_VERTICAL] boolValue]];
+    [checkBox_horizontalResize setState:[[preferenceDict objectForKey:KEY_DUAL_RESIZE_HORIZONTAL] boolValue]];
+
+    [self configureControlDimming];
+}
+
+//Called in response to all preference controls, applies new settings
+- (IBAction)changePreference:(id)sender
+{
+    if(sender == checkBox_autoResize){
+        [[owner preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+                                             forKey:KEY_DUAL_RESIZE_VERTICAL
+                                              group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+
+    }else if(sender == checkBox_horizontalResize){
+        [[owner preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+                                             forKey:KEY_DUAL_RESIZE_HORIZONTAL
+                                              group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+    }
+    
+    [self configureControlDimming];
+}
+
+//Enable/disable controls that are available/unavailable
+- (void)configureControlDimming
+{
+    [checkBox_horizontalResize setEnabled:[checkBox_autoResize state]];
+}
+
+@end
+
+
+
