9649072db0f20a596aea89443b04847f8e6ddfbe
ramoth4 <ramoth4@localhost>
2003-10-17 08:45:52 +0000

    Added a build number to the about box. click on the build date to toggle between the date and the number, ala About This Mac...
    Anyway, buildnum is under the MIT license (identical to the BSD license
    w/o the advertising clause). It needs to be a separate program to get
    around the problem of "incremental builds." The code is provided as
    reference, you don't need to compile it, and it shouldn't be compiling.
    
    This took me a couple days to come up with, I hope you guys like it :D

--- adium/Source/LNAboutBoxController.h
+++ adium/Source/LNAboutBoxController.h
@@ -5,20 +5,20 @@
 @interface LNAboutBoxController : NSWindowController {
 
     IBOutlet	NSButton	*button_duckIcon;
-    IBOutlet	NSTextField	*textField_buildDate;
-    IBOutlet	NSTextField	*textField_buildTime;
+    IBOutlet	NSButton	*button_buildButton;
     IBOutlet	AILinkTextView	*linkTextView_siteLink;
 
     NSMutableArray      *avatarArray;
+    NSString 		*buildNumber, *buildDate;
     AIAdium		*owner;
-    int			numberOfDuckClicks;
+    int			numberOfDuckClicks, numberOfBuildFieldClicks;
     BOOL		previousKeyWasOption;
 
 }
 
-
 + (LNAboutBoxController *)aboutBoxControllerForOwner:(id)inOwner;
 - (IBAction)closeWindow:(id)sender;
 - (IBAction)adiumDuckClicked:(id)sender;
+- (IBAction)buildFieldClicked:(id)sender;
 
 @end
