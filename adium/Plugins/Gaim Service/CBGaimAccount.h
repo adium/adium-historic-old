6bd7393b77d0c03d710cd8bc4c75da0212f1d564
ramoth4 <ramoth4@localhost>
2003-10-22 04:52:21 +0000

    Moved some code around, nothing exciting. This will make things easier in the future though.

--- adium/Plugins/Gaim Service/CBGaimAccount.h
+++ adium/Plugins/Gaim Service/CBGaimAccount.h
@@ -6,17 +6,19 @@
 //  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
 //
 
+
 #import <Cocoa/Cocoa.h>
 #import <Adium/Adium.h>
 
-@interface CBGaimAccount : AIAccount 
+@interface CBGaimAccount : AIAccount <AIAccount_Handles>
 {
-
+    NSMutableDictionary	*handleDict;
 }
 
 
 //AIAccount sublcassed methods
 - (void)initAccount;
+- (void)dealloc;
 - (NSArray *)supportedPropertyKeys;
 - (void)statusForKey:(NSString *)key willChangeTo:(id)inValue;
 - (NSDictionary *)defaultProperties;
@@ -26,4 +28,23 @@
 - (NSString *)serviceID;
 - (NSString *)UIDAndServiceID;
 - (NSString *)accountDescription;
+
+//AIAccount_Handles
+// Returns a dictionary of AIHandles available on this account
+- (NSDictionary *)availableHandles; //return nil if no contacts/list available
+
+// Returns YES if the list is editable
+- (BOOL)contactListEditable;
+
+// Add a handle to this account
+- (AIHandle *)addHandleWithUID:(NSString *)inUID serverGroup:(NSString *)inGroup temporary:(BOOL)inTemporary;
+// Remove a handle from this account
+- (BOOL)removeHandleWithUID:(NSString *)inUID;
+
+// Add a group to this account
+- (BOOL)addServerGroup:(NSString *)inGroup;
+// Remove a group
+- (BOOL)removeServerGroup:(NSString *)inGroup;
+// Rename a group
+- (BOOL)renameServerGroup:(NSString *)inGroup to:(NSString *)newName;
 @end
