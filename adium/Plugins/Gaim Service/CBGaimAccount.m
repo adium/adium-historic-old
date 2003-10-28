32ec3a27eeabb053717c159a975f120ff8aff72f
Colin Barrett <colin@springsandstruts.com>
2003-10-28 03:52:11 +0000

    Quick commit. - We can now see spaces in peoples names - Removed a lot of redundant code. - Put in some comments :) Once you get going, they're fun to write :P

--- adium/Plugins/Gaim Service/CBGaimAccount.m
+++ adium/Plugins/Gaim Service/CBGaimAccount.m
@@ -30,16 +30,19 @@
     if(node && GAIM_BLIST_NODE_IS_BUDDY(node))
     {
         GaimBuddy *buddy = (GaimBuddy *)node;
-                
+        
+        //create the handle, group-less for now
         AIHandle *theHandle = [AIHandle 
             handleWithServiceID:[[service handleServiceType] identifier]
             UID:[NSString stringWithCString:buddy->name]
             serverGroup:NO_GROUP
             temporary:NO
             forAccount:self];
-            
+        
+        //stuff it in the dict
         [handleDict setObject:theHandle forKey:[NSString stringWithFormat:@"%s", buddy->name]];
         
+        //set up our ui_data
         node->ui_data = [theHandle retain];
     
         //[[owner contactController] handlesChangedForAccount:self];
@@ -51,96 +54,69 @@
     //NSLog(@"Update");
     if(node)
     {
+        //extract the GaimBuddy from whatever we were passed
         GaimBuddy *buddy;
         if(GAIM_BLIST_NODE_IS_BUDDY(node))
             buddy = (GaimBuddy *)node;
         else if(GAIM_BLIST_NODE_IS_CONTACT(node))
             buddy = ((GaimContact *)node)->priority;
             
-        if(GAIM_BUDDY_IS_ONLINE(buddy))
+        NSMutableArray *modifiedKeys = [NSMutableArray array];
+        AIHandle *theHandle = (AIHandle *)node->ui_data;
+        
+        int online = (GAIM_BUDDY_IS_ONLINE(buddy) ? 1 : 0);
+        
+        //NSLog(@"%d", online);
+        
+        //see if our online state is up to date
+        if([[[theHandle statusDictionary] objectForKey:@"Online"] intValue] != online)
         {
-            NSLog(@"Online");
-            NSMutableArray *modifiedKeys = [NSMutableArray array];
-            AIHandle *theHandle = (AIHandle *)node->ui_data;
-            
-            if([[[theHandle statusDictionary] objectForKey:@"Online"] intValue] != 1)
-            {
-                [[theHandle statusDictionary]
-                    setObject:[NSNumber numberWithInt:1] 
-                    forKey:@"Online"];
-                [modifiedKeys addObject:@"Online"];
-            }
-            
-            //if there is a server alias, and it's different
-            if(buddy->server_alias 
-            && strcmp([[[theHandle statusDictionary] objectForKey:@"Display Name"] cString], buddy->server_alias))
-            {
-                [[theHandle statusDictionary] 
-                    setObject:[NSString stringWithCString:buddy->server_alias]
-                    forKey:@"Display Name"];
-                [modifiedKeys addObject:@"Display Name"];
-            }
-            
-            GaimGroup *g = gaim_find_buddys_group(buddy);
-            if(g && strcmp([[theHandle serverGroup] cString], g->name))
-            {
-                [[owner contactController] handle:[theHandle copy] removedFromAccount:self];
-                NSLog(@"Changed to group %s", g->name);
-                [theHandle setServerGroup:[NSString stringWithCString:g->name]];
-                [[owner contactController] handle:theHandle addedToAccount:self];
-            }
-            
-                        
-            if([modifiedKeys count] > 0)
-            {
-                NSLog(@"Changed %@", modifiedKeys);
-                
-                [[owner contactController] handleStatusChanged:theHandle
-                    modifiedStatusKeys:modifiedKeys
-                    delayed:NO
-                    silent:(gaim_connection_get_state(gaim_account_get_connection(buddy->account)) 
-                        != GAIM_CONNECTING)];
-            }
-            else
-                NSLog(@"Nothing Changed");
+            [[theHandle statusDictionary]
+                setObject:[NSNumber numberWithInt:online] 
+                forKey:@"Online"];
+            [modifiedKeys addObject:@"Online"];
         }
+        
+        //snag the correct alias, and the current display name
+        char *alias = (char *)gaim_get_buddy_alias(buddy);
+        char *disp_name = (char *)[[[theHandle statusDictionary] objectForKey:@"Display Name"] cString];
+        if(!disp_name) disp_name = "";
+        
+        //check 'em and update
+        if(alias && strcmp(disp_name, alias))
+        {
+            [[theHandle statusDictionary] 
+                setObject:[NSString stringWithCString:alias]
+                forKey:@"Display Name"];
+            [modifiedKeys addObject:@"Display Name"];
+        }
+        
+        //did the group change (or did we finally find out what group the buddy is in?)
+        GaimGroup *g = gaim_find_buddys_group(buddy);
+        if(g && strcmp([[theHandle serverGroup] cString], g->name))
+        {
+            [[owner contactController] handle:[theHandle copy] removedFromAccount:self];
+            NSLog(@"Changed to group %s", g->name);
+            [theHandle setServerGroup:[NSString stringWithCString:g->name]];
+            [[owner contactController] handle:theHandle addedToAccount:self];
+        }
+        
+        //if anything chnaged
+        if([modifiedKeys count] > 0)
+        {
+            //NSLog(@"Changed %@", modifiedKeys);
+            
+            //tell the contact controller, silencing if necessary
+            [[owner contactController] handleStatusChanged:theHandle
+                modifiedStatusKeys:modifiedKeys
+                delayed:NO
+                silent:online
+                    ? (gaim_connection_get_state(gaim_account_get_connection(buddy->account)) != GAIM_CONNECTING)
+                    : (buddy->present != GAIM_BUDDY_SIGNING_OFF)];
+        }
+        //nothing changed. boring. :P
         else
-        {
-            NSLog(@"Offline");
-            
-            NSMutableArray *modifiedKeys = [NSMutableArray array];
-            AIHandle *theHandle = (AIHandle *)node->ui_data;
-            
-            if([[[theHandle statusDictionary] objectForKey:@"Online"] intValue] != 0)
-            {
-                [[theHandle statusDictionary] 
-                    setObject:[NSNumber numberWithInt:0] 
-                    forKey:@"Online"];
-                [modifiedKeys addObject:@"Online"];
-            }
-            
-            GaimGroup *g = gaim_find_buddys_group(buddy);
-            if(g && strcmp([[theHandle serverGroup] cString], g->name))
-            {
-                [[owner contactController] handle:[theHandle copy] removedFromAccount:self];
-                NSLog(@"Changed to group %s", g->name);
-                [theHandle setServerGroup:[NSString stringWithCString:g->name]];
-                [[owner contactController] handle:theHandle addedToAccount:self];
-            }
-
-            
-            if([modifiedKeys count] > 0)
-            {
-                NSLog(@"Changed %@", modifiedKeys);
-                
-                [[owner contactController] handleStatusChanged:theHandle
-                    modifiedStatusKeys:modifiedKeys
-                    delayed:NO
-                    silent:(buddy->present != GAIM_BUDDY_SIGNING_OFF)];
-            }
-            else
-                NSLog(@"Nothing Changed");
-        }
+            NSLog(@"Nothing Changed");
     }
 }
 
@@ -184,25 +160,22 @@
         if([inValue boolValue]) //Connect
         { 
             if(status == STATUS_OFFLINE)
-            {
-                [[owner accountController] 
-                    setProperty:[NSNumber numberWithInt:STATUS_CONNECTING]
-                    forKey:@"Status" account:self];
-                
+            {                
                 //get password
                 [[owner accountController] passwordForAccount:self 
                     notifyingTarget:self selector:@selector(finishConnect:)];
-                
             }
         }
         else //Disconnect
         {
             if(status == STATUS_ONLINE)
             {
+                //we're signing off, give us a minute.
                 [[owner accountController] 
                     setProperty:[NSNumber numberWithInt:STATUS_DISCONNECTING]
                     forKey:@"Status" account:self];
-
+                
+                //delete the account, sign everybody off
                 GaimAccount *account;
                 if(account = gaim_accounts_find(SCREEN_NAME, PROTOCOL))
                 {
@@ -210,6 +183,7 @@
                     gaim_accounts_delete(account);
                 }
                 
+                //done
                 [[owner accountController] 
                     setProperty:[NSNumber numberWithInt:STATUS_OFFLINE]
                     forKey:@"Status" account:self];
@@ -222,17 +196,31 @@
 {
     if(inPassword && [inPassword length] != 0)
     {
-        //****** Create a test account *********
-        GaimAccount *testAccount = gaim_account_new(SCREEN_NAME, PROTOCOL);
-        gaim_account_set_password(testAccount, [inPassword cString]);
-        gaim_account_connect(testAccount);
-        gaim_accounts_add(testAccount);
-        //**************************************
-        
-        [[owner accountController]
-            setProperty:[NSNumber numberWithInt:STATUS_ONLINE]
+        //now we start to connect
+        [[owner accountController] 
+            setProperty:[NSNumber numberWithInt:STATUS_CONNECTING]
             forKey:@"Status" account:self];
 
+        //setup the account, get things ready
+        GaimAccount *testAccount = gaim_account_new(SCREEN_NAME, PROTOCOL);
+        gaim_account_set_password(testAccount, [inPassword cString]);
+        
+        //this is a bit of a hack, but it will do for now
+        GaimConnection *conn =  gaim_account_connect(testAccount);
+        if(gaim_connection_get_state(conn) != GAIM_DISCONNECTED) //if we're not disconneted, signed on!
+        {
+            gaim_accounts_add(testAccount);
+            
+            [[owner accountController]
+                setProperty:[NSNumber numberWithInt:STATUS_ONLINE]
+                forKey:@"Status" account:self];
+        }
+        else //aw nuts, something must have happened.
+        {
+            [[owner accountController] 
+                    setProperty:[NSNumber numberWithInt:STATUS_OFFLINE]
+                    forKey:@"Status" account:self];
+        }
     }
 }
 
