669964b39f1326d735e7066c438b87232a7a40bd
rlwimi <rlwimi@localhost>
2003-11-06 23:54:58 +0000

    Two leaks: 1. You must fclose() anything you fopen(); 2. siteLink is alloc-ed, so it must be released.

--- adium/Source/LNAboutBoxController.m
+++ adium/Source/LNAboutBoxController.m
@@ -48,6 +48,7 @@ LNAboutBoxController *sharedInstance = nil;
     {
         FILE *f = fopen(path, "r");
         fscanf(f, "%s | %s", num, date);
+		fclose(f);
         if(*num)
             buildNumber = [[NSString stringWithFormat:@"Build Number: %s", num] retain];
         if(*date)
@@ -107,6 +108,8 @@ LNAboutBoxController *sharedInstance = nil;
     [linkTextView_siteLink setEditable:NO];
     [[linkTextView_siteLink textStorage] setAttributedString:siteLink];
     [linkTextView_siteLink resetCursorRects];
+
+	[siteLink release];
 
     [button_buildButton setTitle:buildDate];
 
