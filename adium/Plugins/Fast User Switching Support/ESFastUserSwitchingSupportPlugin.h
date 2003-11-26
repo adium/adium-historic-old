ef63af9a7d604bbf751f2193d074eb886dc24dc2
Evan Schoenberg <Evan Schoenberg@localhost>
2003-11-26 06:09:21 +0000

    Fast user switching support plugin.  When someone uses fast user switching to switch away from Adium, we put up an away message if one isn't already up.  When switching back, remove the away message if it was set automatically upon switching.  The away message is hardcoded at the moment but will be customizable later on.

---
+++ adium/Plugins/Fast User Switching Support/ESFastUserSwitchingSupportPlugin.h
@@ -0,0 +1,13 @@
+//
+//  ESFastUserSwitchingSupportPlugin.h
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Tue Nov 25 2003.
+//
+
+
+@interface ESFastUserSwitchingSupportPlugin : AIPlugin {
+    BOOL setAwayThroughFastUserSwitch;
+}
+
+@end
---
+++ adium/Plugins/Fast User Switching Support/ESFastUserSwitchingSupportPlugin.m
@@ -0,0 +1,62 @@
+//
+//  ESFastUserSwitchingSupportPlugin.m
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Tue Nov 25 2003.
+//
+
+#import "ESFastUserSwitchingSupportPlugin.h"
+
+#define FAST_USER_SWITCH_AWAY_STRING @"Someone else is using my computer right now."
+
+@implementation ESFastUserSwitchingSupportPlugin
+- (void)installPlugin
+{
+    setAwayThroughFastUserSwitch = NO;
+    
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                           selector:@selector(switchHandler:) 
+                                                               name:NSWorkspaceSessionDidBecomeActiveNotification 
+                                                             object:nil];
+    
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self 
+                                                           selector:@selector(switchHandler:) 
+                                                               name:NSWorkspaceSessionDidResignActiveNotification
+                                                             object:nil];
+
+}
+
+-(void)uninstallPlugin
+{
+    
+}
+
+-(void)switchHandler:(NSNotification*) notification
+{
+    if ([[notification name] isEqualToString:NSWorkspaceSessionDidResignActiveNotification])
+    {       //Deactivation
+ 
+        //Go away if we aren't already away
+        if ([[owner accountController] propertyForKey:@"AwayMessage" account:nil] == nil)
+        {
+            NSAttributedString *away = [[NSAttributedString alloc] initWithString:FAST_USER_SWITCH_AWAY_STRING];
+            [[owner accountController] setProperty:[away dataRepresentation]
+                                            forKey:@"AwayMessage" 
+                                           account:nil];
+            [away release];
+            setAwayThroughFastUserSwitch = YES;
+        }
+    }
+    else    //Activation
+    {
+        //Remove the away status flag if we set it originally
+        if (setAwayThroughFastUserSwitch) {
+            //Remove the away status flag	
+            [[owner accountController] setProperty:nil forKey:@"AwayMessage" account:nil];
+            [[owner accountController] setProperty:nil forKey:@"Autoresponse" account:nil];
+            setAwayThroughFastUserSwitch = NO;
+        }
+    }    
+}
+
+@end
