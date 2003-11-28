9a49574c6c007d06621a9a062523a140e63967cd
Evan Schoenberg <Evan Schoenberg@localhost>
2003-11-28 01:12:31 +0000

    Fixed a crash in the event bezel preferences that occurred when another preference was changed without the event bezel prefs being loaded.

--- adium/Plugins/Event Bezel/JSCEventBezelPreferences.m
+++ adium/Plugins/Event Bezel/JSCEventBezelPreferences.m
@@ -181,48 +181,57 @@
     //Observer preference changes
     [[owner notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
     [self preferencesChanged:nil];
+    viewIsLoaded = YES;
 }
 
+- (void)viewShouldClose
+{
+    viewIsLoaded = NO;   
+}
 //Configure the preference view
 - (void)preferencesChanged:(NSNotification *)notification
 {
-    NSDictionary	*preferenceDict = [[owner preferenceController] preferencesForGroup:PREF_GROUP_EVENT_BEZEL];
-    
-    // Set the values of the checkboxes
-    [checkBox_showBezel setState:[[preferenceDict objectForKey:KEY_SHOW_EVENT_BEZEL] boolValue]];
-    
-    [popUp_position selectItemAtIndex: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_POSITION] intValue]];
-    
-    [slider_duration setIntValue: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_DURATION] intValue]];
-    
-    [checkBox_online setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_ONLINE] boolValue]];
-    [checkBox_offline setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_OFFLINE] boolValue]];
-    [checkBox_available setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_AVAILABLE] boolValue]];
-    [checkBox_away setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_AWAY] boolValue]];
-    [checkBox_noIdle setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_NO_IDLE] boolValue]];
-    [checkBox_idle setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_IDLE] boolValue]];
-    [checkBox_firstMessage setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FIRST_MESSAGE] boolValue]];
-    [checkBox_imageBadges setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_IMAGE_BADGES] boolValue]];
-    [checkBox_colorLabels setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_COLOR_LABELS] boolValue]];
-    [checkBox_nameLabels setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_NAME_LABELS] boolValue]];
-    
-    [popUp_size selectItemAtIndex: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_SIZE] intValue]];
-    
-    [popUp_background selectItemAtIndex: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_BACKGROUND] intValue]];
-    
-    [checkBox_fadeIn setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FADE_IN] boolValue]];
-    [checkBox_fadeOut setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FADE_OUT] boolValue]];
-    [checkBox_showHidden setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_SHOW_HIDDEN] boolValue]];
-    [checkBox_showAway setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_SHOW_AWAY] boolValue]];
-    
-    // Enable or disable checkboxes based on the "show bezel" checkbox
-    [checkBox_online setEnabled: [checkBox_showBezel state]];
-    [checkBox_offline setEnabled: [checkBox_showBezel state]];
-    [checkBox_available setEnabled: [checkBox_showBezel state]];
-    [checkBox_away setEnabled: [checkBox_showBezel state]];
-    [checkBox_noIdle setEnabled: [checkBox_showBezel state]];
-    [checkBox_idle setEnabled: [checkBox_showBezel state]];
-    [checkBox_firstMessage setEnabled: [checkBox_showBezel state]];
+    if (notification == nil || [[notification name] isEqualToString:PREF_GROUP_EVENT_BEZEL]) {
+        if (viewIsLoaded) {
+            NSDictionary	*preferenceDict = [[owner preferenceController] preferencesForGroup:PREF_GROUP_EVENT_BEZEL];
+            
+            // Set the values of the checkboxes
+            [checkBox_showBezel setState:[[preferenceDict objectForKey:KEY_SHOW_EVENT_BEZEL] boolValue]];
+            
+            [popUp_position selectItemAtIndex: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_POSITION] intValue]];
+            
+            [slider_duration setIntValue: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_DURATION] intValue]];
+            
+            [checkBox_online setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_ONLINE] boolValue]];
+            [checkBox_offline setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_OFFLINE] boolValue]];
+            [checkBox_available setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_AVAILABLE] boolValue]];
+            [checkBox_away setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_AWAY] boolValue]];
+            [checkBox_noIdle setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_NO_IDLE] boolValue]];
+            [checkBox_idle setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_IDLE] boolValue]];
+            [checkBox_firstMessage setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FIRST_MESSAGE] boolValue]];
+            [checkBox_imageBadges setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_IMAGE_BADGES] boolValue]];
+            [checkBox_colorLabels setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_COLOR_LABELS] boolValue]];
+            [checkBox_nameLabels setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_NAME_LABELS] boolValue]];
+            
+            [popUp_size selectItemAtIndex: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_SIZE] intValue]];
+            
+            [popUp_background selectItemAtIndex: [[preferenceDict objectForKey: KEY_EVENT_BEZEL_BACKGROUND] intValue]];
+            
+            [checkBox_fadeIn setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FADE_IN] boolValue]];
+            [checkBox_fadeOut setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_FADE_OUT] boolValue]];
+            [checkBox_showHidden setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_SHOW_HIDDEN] boolValue]];
+            [checkBox_showAway setState: [[preferenceDict objectForKey:KEY_EVENT_BEZEL_SHOW_AWAY] boolValue]];
+            
+            // Enable or disable checkboxes based on the "show bezel" checkbox
+            [checkBox_online setEnabled: [checkBox_showBezel state]];
+            [checkBox_offline setEnabled: [checkBox_showBezel state]];
+            [checkBox_available setEnabled: [checkBox_showBezel state]];
+            [checkBox_away setEnabled: [checkBox_showBezel state]];
+            [checkBox_noIdle setEnabled: [checkBox_showBezel state]];
+            [checkBox_idle setEnabled: [checkBox_showBezel state]];
+            [checkBox_firstMessage setEnabled: [checkBox_showBezel state]];
+        }
+    }
 }
 
 @end
