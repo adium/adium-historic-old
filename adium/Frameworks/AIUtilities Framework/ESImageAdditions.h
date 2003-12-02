8e6af38d75784595005d45fc82bbc088aa2492da
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-12-02 21:58:04 +0000

    There's an -(NSData *)TIFFRepresentation in NSImage but no -(NSData *)JPEGRepresentation.  Side note: AIM does not support TIFF files so far as I can tell.  Item 1 is therefore hereby rectified, for use in the next commit.

---
+++ adium/Frameworks/AIUtilities Framework/ESImageAdditions.h
@@ -0,0 +1,11 @@
+//
+//  ESImageAdditions.h
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Tue Dec 02 2003.
+
+@interface NSImage (ESImageAdditions)
+
+- (NSData *)JPEGRepresentation;
+
+@end
