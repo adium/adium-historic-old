58eec2688eb5921d01ecb1307e635a4f273c23f2
jmelloy <jmelloy@localhost>
2003-12-08 05:42:12 +0000

    I am Henry the Eight I am.

--- CVSROOT/cia_helper.pl
+++ CVSROOT/cia_helper.pl
@@ -7,8 +7,6 @@ while(<STDIN>) {
     $input .= $_;
 }
 
-system('/usr/bin/perl', '/cvsroot/adium/CVSROOT/ciabot.pl', @ARGV);
-print $input;
+system('/usr/bin/perl', '/cvsroot/adium/CVSROOT/ciabot.pl', @ARGV, "< $input");
 
-system('/usr/bin/perl', '/cvsroot/adium/CVSROOT/cia_mailbucket.pl', @ARGV);
-print $input;
+system('/usr/bin/perl', '/cvsroot/adium/CVSROOT/cia_mailbucket.pl', @ARGV, "< $input");
