5041e2199f38f92cb2193a064f79c8b62d2287ac
Jeffrey Melloy <jmelloy@gmail.com>
2003-12-08 17:06:46 +0000

    Committed some server-specific stuff so it would stop complaining.

--- adium/Utilities/stats/find_logs.pl
+++ adium/Utilities/stats/find_logs.pl
@@ -1,13 +1,13 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -I/home/jmelloy/lib/perl5/site_perl/5.6.1/
 
-# $Id: find_logs.pl,v 1.2 2003/12/08 03:53:26 jmelloy Exp $
+# $Id: find_logs.pl,v 1.3 2003/12/08 17:06:46 jmelloy Exp $
 
 use warnings;
 use strict;
 
 use File::List;
 
-my $search = new File::List("/Users/jmelloy/clean-adium");
+my $search = new File::List("/home/jmelloy/adium");
 my @filelist = @{ $search->find("\.log") };
 
 my $outfile = "master.log";
