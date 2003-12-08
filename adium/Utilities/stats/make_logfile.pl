6b4a4e1a51bd4f4e0a1a3a1a350df9b13a4ec8a1
jmelloy <jmelloy@localhost>
2003-12-08 17:06:46 +0000

    Committed some server-specific stuff so it would stop complaining.

--- adium/Utilities/stats/make_logfile.pl
+++ adium/Utilities/stats/make_logfile.pl
@@ -1,23 +1,23 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -I/home/jmelloy/lib/perl5/site_perl/5.6.1/
 
-# $Id: make_logfile.pl,v 1.2 2003/12/08 04:15:55 jmelloy Exp $
+# $Id: make_logfile.pl,v 1.3 2003/12/08 17:06:46 jmelloy Exp $
 
 use warnings;
 use strict;
 
 use File::List;
 
-my $search = new File::List("/Users/jmelloy/clean-adium");
+my $search = new File::List("/home/jmelloy/adium");
 my @filelist = @{ $search->find(".") };
 
 foreach my $file (@filelist) {
-    $file =~ s/\/Users\/jmelloy\/clean-adium\///;
+    $file =~ s/\/home\/jmelloy\/adium\///;
 
     $_ = $file;
 
     my $contains_cvs  = /CVS/;
     my $contains_log = /\.log$/;
-    
+
     if(!$contains_cvs && !$contains_log) {
 
         my $logfile = $file . ".log";
