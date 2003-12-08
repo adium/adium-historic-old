943dbb3f6e24bbd4de278376dcfcb0c9e70c5f26
jmelloy <jmelloy@localhost>
2003-12-08 05:33:33 +0000

    Again.

--- adium/Source/AIPreferenceController.h
+++ adium/Source/AIPreferenceController.h
@@ -13,6 +13,8 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
+// $Id: AIPreferenceController.h,v 1.6 2003/12/08 05:33:33 jmelloy Exp $ 
+
 @interface AIPreferenceController (INTERNAL)
 // These methods are for internal Adium use only.  The public interface is in Adium.h.
 - (void)initController;
