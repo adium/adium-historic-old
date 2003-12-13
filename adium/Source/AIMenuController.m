e1d0406ecbd5c09fd094f99439eeaf9833a9218a
Jeffrey Melloy <jmelloy@gmail.com>
2003-12-13 20:38:42 +0000

    Trying to get the cia helper to work.

--- adium/Source/AIMenuController.m
+++ adium/Source/AIMenuController.m
@@ -13,6 +13,8 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
+// $Id: AIMenuController.m,v 1.19 2003/12/13 20:38:42 jmelloy Exp $
+
 #import "AIMenuController.h"
 
 @interface AIMenuController (PRIVATE)
