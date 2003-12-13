d8ece32d9cf76537ef3ba14b3eb6f2d882704d1c
Evan Schoenberg <Evan Schoenberg@localhost>
2003-12-13 11:01:29 +0000

    Added a contextual menu to the message view.  It puts message view specific stuff first (for example, Copy if you have text selected and right click on the selection), then a separator if there was any message view specific stuff, then the contact's context menu. :-)

--- adium/Frameworks/AIUtilities Framework/AIFlexibleTableCell.h
+++ adium/Frameworks/AIUtilities Framework/AIFlexibleTableCell.h
@@ -47,6 +47,7 @@
 //Cursor Tracking
 - (BOOL)resetCursorRectsAtOffset:(NSPoint)offset visibleRect:(NSRect)visibleRect inView:(NSView *)controlView;
 - (BOOL)handleMouseDownEvent:(NSEvent *)theEvent atPoint:(NSPoint)inPoint offset:(NSPoint)inOffset;
+- (NSArray *)menuItemsForEvent:(NSEvent *)theEvent atPoint:(NSPoint)inPoint offset:(NSPoint)inOffset;
 - (void)selectContentFrom:(NSPoint)source to:(NSPoint)dest offset:(NSPoint)inOffset mode:(int)selectMode;
 - (void)deselectContent;
 - (NSAttributedString *)selectedString;
