fa5ed786c62fa7eea63beec3d163623489908e31
eevyl <eevyl@localhost>
2003-12-13 09:38:16 +0000

    Fixed a cosmetic error with a checkbox not being disabled properly.

--- adium/Plugins/Event Bezel/JSCEventBezelPreferences.m
+++ adium/Plugins/Event Bezel/JSCEventBezelPreferences.m
@@ -154,6 +154,7 @@
             [checkBox_noIdle setEnabled:showBezel];
             [checkBox_idle setEnabled:showBezel];
             [checkBox_firstMessage setEnabled:showBezel];
+            [checkBox_includeText setEnabled:showBezel];
         }
     }
 }
