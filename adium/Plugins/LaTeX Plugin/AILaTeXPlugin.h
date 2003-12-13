3658908f631faf0d0f2e86ae9b13b47c865acbb8
jmelloy <jmelloy@localhost>
2003-12-13 22:25:06 +0000

    New LaTeX plugin written by max_cohen.
    Doesn't seem to actually work, I may have done something wrong with plugin
    creation.

---
+++ adium/Plugins/LaTeX Plugin/AILaTeXPlugin.h
@@ -0,0 +1,15 @@
+//
+//  AILaTeXPlugin.h
+//  Adium XCode
+//
+//  Created by Stephen Poprocki on Sat Dec 13 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+@protocol AIContentFilter;
+
+@interface AILaTeXPlugin : AIPlugin <AIContentFilter>
+{
+}
+
+@end
