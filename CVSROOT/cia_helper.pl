29faadb79d614e6f3df5ecfe949785e0608caae9
jmelloy <jmelloy@localhost>
2003-12-14 04:54:36 +0000

    Try preformatting @ARGV.

--- CVSROOT/cia_helper.pl
+++ CVSROOT/cia_helper.pl
@@ -3,21 +3,21 @@
 use strict;
 
 my $input;
+my $args;
 
 while(<STDIN>) {
     $input .= $_;
 }
 
-print $input;
-print @ARGV;
+for(my $i = 0; $i < @ARGV; $i++) {
+    $args .= @ARGV[$i] . " ";
+}
 
-print "Running CIA\n";
-open(CIA,  "| /usr/bin/perl /cvsroot/adium/CVSROOT/ciabot.pl @ARGV") or die "shit: $!";
+open(CIA,  "| /usr/bin/perl /cvsroot/adium/CVSROOT/ciabot.pl $args") or die "shit: $!";
 print CIA $input;
 close CIA;
 
-print "Mailing RSS\n";
 
-open(RSS, "| /usr/bin/perl /cvsroot/adium/CVSROOT/cia_mailbucket.pl @ARGV") or die "fuck $!";
+open(RSS, "| /usr/bin/perl /cvsroot/adium/CVSROOT/cia_mailbucket.pl $args") or die "fuck $!";
 print RSS $input;
 close RSS;
