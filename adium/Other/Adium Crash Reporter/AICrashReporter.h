64bbd543b2ffdcf1cfc7d5e3ec8aaa414bcbe507
Evan Schoenberg <Evan Schoenberg@localhost>
2003-12-24 16:08:57 +0000

    Added build number/build date reading to the crash reporter.

--- adium/Other/Adium Crash Reporter/AICrashReporter.h
+++ adium/Other/Adium Crash Reporter/AICrashReporter.h
@@ -10,24 +10,30 @@
 
 
 @interface AICrashReporter : NSObject {
-    IBOutlet	NSWindow			*window_MainWindow;
-	IBOutlet	NSTextField			*textField_emailAddress;
-	IBOutlet	NSTextField			*textField_accountIM;
-	IBOutlet	NSTextField			*textField_description;
-	IBOutlet	NSTextView			*textView_details;
-	IBOutlet	NSProgressIndicator	*progress_sending;
-	
-	IBOutlet	NSPanel				*panel_privacySheet;
-	IBOutlet	NSTextView			*textView_crashLog;
-	
-	NSString						*crashLog;		//Current crash log
+    IBOutlet	NSWindow                    *window_MainWindow;
+    IBOutlet	NSTextField                 *textField_emailAddress;
+    IBOutlet	NSTextField                 *textField_accountIM;
+    IBOutlet	NSTextField                 *textField_description;
+    IBOutlet	NSTextView                  *textView_details;
+    IBOutlet	NSProgressIndicator         *progress_sending;
+    
+    IBOutlet	NSPanel                     *panel_privacySheet;
+    IBOutlet	NSTextView                  *textView_crashLog;
+    
+    NSString                                *crashLog;		//Current crash log
+    
+    NSString                                *buildNumber, *buildDate;
 }
 
-- (void)sendReport:(NSDictionary *)crashReport;
-- (IBAction)send:(id)sender;
 - (void)awakeFromNib;
-- (IBAction)reportCrashForLogAtPath:(NSString *)inPath;
+
 - (IBAction)showPrivacyDetails:(id)sender;
 - (IBAction)closePrivacyDetails:(id)sender;
+
+- (IBAction)reportCrashForLogAtPath:(NSString *)inPath;
+- (void)sendReport:(NSDictionary *)crashReport;
+- (IBAction)send:(id)sender;
+
+- (void)_loadBuildInformation;
 
 @end
