1f26a47df505661b206d9bac6e1ac5eddc52edc1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2003-12-25 23:55:32 +0000

    Modified AIException to use the atos inside the adium bundle

--- adium/Frameworks/Adium Framework/AIException.m
+++ adium/Frameworks/Adium Framework/AIException.m
@@ -29,7 +29,7 @@
 
     //Turn the nonsense of memory addresses into a human-readable backtrace complete with line numbers
     if (dict && (stackTrace = [dict objectForKey:NSStackTraceKey])) { 
-        NSString *str = [NSString stringWithFormat:@"/usr/bin/atos -p %d %@ | tail -n +3 | head -n +%d | c++filt | cat -n",
+        NSString *str = [NSString stringWithFormat:@"%s -p %d %@ | tail -n +3 | head -n +%d | c++filt | cat -n",[[[NSBundle mainBundle] pathForResource:@"buildnum" ofType:nil] fileSystemRepresentation],
             [[NSProcessInfo processInfo] processIdentifier],
             stackTrace,
             ([[stackTrace componentsSeparatedByString:@"  "] count] - 4)];
