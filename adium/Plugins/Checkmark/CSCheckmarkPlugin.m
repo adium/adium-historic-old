aaff26d82419e85c65dad2b20a9dca3bd61f373f
the_tick <cforsythe@gmail.com>
2004-01-05 08:13:51 +0000

    Added commented out lines with the code to use an image for notification of a open message window, in case anyone wanted to use that instead.

--- adium/Plugins/Checkmark/CSCheckmarkPlugin.m
+++ adium/Plugins/Checkmark/CSCheckmarkPlugin.m
@@ -9,6 +9,8 @@
 #import "CSCheckmarkPlugin.h"
 #import "CSCheckmarkPreferences.h"
 
+//In case you want to use an image uncomment this and comment out the @interface
+//#define CHECKMARK_IMAGE @"/Applications/Mail.app/Contents/Resources/iChatToolbar.tiff"
 @interface CSCheckmarkPlugin (PRIVATE)
 - (void)preferencesChanged:(NSNotification *)notification;
 @end
@@ -18,86 +20,88 @@
 #pragma mark Plugin Initiation
 - (void)installPlugin
 {
-	displayCheckmark = NO;
-	
-	[self preferencesChanged:nil];
-	
+    displayCheckmark = NO;
+    
+    [self preferencesChanged:nil];
+    
     //Our preference view
     checkmarkPreferences = [[CSCheckmarkPreferences checkmarkPreferences] retain];
-	[[adium contactController] registerListObjectObserver:self];
-	
-	//Observe
+    [[adium contactController] registerListObjectObserver:self];
+    
+    //Observe
     [[adium notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
-	
-	checkmarkImage =  [[NSImage systemCheckmark] retain];
-	[checkmarkImage setScalesWhenResized:YES];
-	[checkmarkImage setFlipped:YES];
+    
+    //Also code for if you want to use a image instead of the resource
+    //checkmarkImage =  [[NSImage alloc] initWithContentsOfFile:CHECKMARK_IMAGE];
+    checkmarkImage =  [[NSImage systemCheckmark] retain];
+    [checkmarkImage setScalesWhenResized:YES];
+    [checkmarkImage setFlipped:YES];
 }
 
 - (void)dealloc
 {
-	[checkmarkImage release];
+    [checkmarkImage release];
 }
 
 #pragma mark Private
 - (void)preferencesChanged:(NSNotification *)notification
 {
-	
+    
     if(notification == nil || [(NSString *)[[notification userInfo] objectForKey:@"Group"] compare:PREF_GROUP_CHECKMARK] == 0){
-		
-		NSDictionary	*prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_CHECKMARK];
-		
-		//Release the old values..
-		//Cache the preference values
-		displayCheckmark  = [[prefDict objectForKey:KEY_DISPLAY_CHECKMARK] boolValue];
-		
+	
+	NSDictionary	*prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_CHECKMARK];
+	
+	//Release the old values..
+	//Cache the preference values
+	displayCheckmark  = [[prefDict objectForKey:KEY_DISPLAY_CHECKMARK] boolValue];
+	
         //Update all our status icons
-		NSEnumerator		*enumerator;
-		AIListObject		*object;
-		
-		enumerator = [[[adium contactController] allContactsInGroup:nil subgroups:YES] objectEnumerator];
-		
-		while(object = [enumerator nextObject]){
+	NSEnumerator		*enumerator;
+	AIListObject		*object;
+	
+	enumerator = [[[adium contactController] allContactsInGroup:nil subgroups:YES] objectEnumerator];
+	
+	while(object = [enumerator nextObject]){
             [[adium contactController] listObjectAttributesChanged:object modifiedKeys:[self updateListObject:object keys:nil delayed:YES silent:YES] delayed:YES];
         }
     }
-	
+    
 }
 
 
 #pragma mark AIListObjectObserver Protocol
 - (NSArray *)updateListObject:(AIListObject *)inObject keys:(NSArray *)inModifiedKeys delayed:(BOOL)delayed silent:(BOOL)silent
 {
-	if ( inModifiedKeys == nil || [inModifiedKeys containsObject:@"ChatsCount"]) {
-		AIMutableOwnerArray		*iconArray = [inObject displayArrayForKey:@"Left View"];
-		int chatsCount = [[inObject statusArrayForKey:@"ChatsCount"] greatestIntegerValue];
-		
-		if (displayCheckmark && (chatsCount > 0))
-			[iconArray setObject:self withOwner:self];
-		else
-			[iconArray setObject:nil withOwner:self];
-		
-		return ([NSArray arrayWithObjects:@"Left View",nil]);
-	}
-	return nil;
+    if ( inModifiedKeys == nil || [inModifiedKeys containsObject:@"ChatsCount"]) {
+	AIMutableOwnerArray		*iconArray = [inObject displayArrayForKey:@"Left View"];
+	int chatsCount = [[inObject statusArrayForKey:@"ChatsCount"] greatestIntegerValue];
+	
+	if (displayCheckmark && (chatsCount > 0))
+	    [iconArray setObject:self withOwner:self];
+	else
+	    [iconArray setObject:nil withOwner:self];
+	
+	return ([NSArray arrayWithObjects:@"Left View",nil]);
+    }
+    return nil;
 }
 
 #pragma mark AIListObjectLeftView Protocol
 - (void)drawInRect:(NSRect)inRect
 {
-	if ([checkmarkImage size].height > inRect.size.height)
-		[checkmarkImage setSize:inRect.size];
-	
-	NSPoint drawingPoint = NSMakePoint(inRect.origin.x, inRect.origin.y + ceil((inRect.size.height / 2.0)) - ceil(([checkmarkImage size].height / 2.0)));
-	[checkmarkImage drawAtPoint:drawingPoint fromRect:NSMakeRect(0, 0, [checkmarkImage size].width, [checkmarkImage size].height) operation:NSCompositeSourceOver fraction:1.0];
-
+    if ([checkmarkImage size].height > inRect.size.height)
+	[checkmarkImage setSize:inRect.size];
+    
+    NSPoint drawingPoint = NSMakePoint(inRect.origin.x, inRect.origin.y + ceil((inRect.size.height / 2.0)) - ceil(([checkmarkImage size].height / 2.0)));
+    [checkmarkImage drawAtPoint:drawingPoint fromRect:NSMakeRect(0, 0, [checkmarkImage size].width, [checkmarkImage size].height) operation:NSCompositeSourceOver fraction:1.0];
+    
 }
 
 - (float)widthForHeight:(int)inHeight
 {
-	if ([checkmarkImage size].height > inHeight)
-		return [checkmarkImage size].width * ((float)inHeight / [checkmarkImage size].height);
-	return ([checkmarkImage size].width);
+    if ([checkmarkImage size].height > inHeight)
+	return [checkmarkImage size].width * ((float)inHeight / [checkmarkImage size].height);
+    return ([checkmarkImage size].width);
 }
 
 @end
