76e29c309921d4943e05fa32ce296986a5e88280
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-01-05 10:26:43 +0000

    Okay, "_sharedInstance is invalid" is being thrown (when we try to get 'me' image data from address book on certain system) in the DSObjCWrappers framework, in [DSoStatus sharedInstance]... which shouldn't ever return an invalid sharedInstance.  So I don't understand. Solution?  For now, don't launch the crash reporter after quitting Adium... just raise the exception normally.  Suboptimal, but better.

--- adium/Frameworks/Adium Framework/AIException.m
+++ adium/Frameworks/Adium Framework/AIException.m
@@ -23,40 +23,45 @@
 //Raise an exception.  This gets called once with no stack trace, then a second time after the stack trace is added by the ExceptionHandling framework.  We therefore just do [super raise] if there is no stack trace, awaiting its addition to write the exception log, load the crash reporter, and exit Adium
 - (void)raise
 {
-    NSDictionary    *dict = [self userInfo];
-    NSString        *stackTrace = nil;
-    NSMutableString *processedStackTrace = [[[NSMutableString alloc] init] autorelease];
-
-    //Turn the nonsense of memory addresses into a human-readable backtrace complete with line numbers
-    if (dict && (stackTrace = [dict objectForKey:NSStackTraceKey])) { 
-        NSString *str = [NSString stringWithFormat:@"%s -p %d %@ | tail -n +3 | head -n +%d | %s | cat -n",[[[NSBundle mainBundle] pathForResource:@"atos" ofType:nil] fileSystemRepresentation],
-            [[NSProcessInfo processInfo] processIdentifier],
-            stackTrace,
-            ([[stackTrace componentsSeparatedByString:@"  "] count] - 4),
-            [[[NSBundle mainBundle] pathForResource:@"c++filt" ofType:nil] fileSystemRepresentation]];
-        FILE *file = popen( [str UTF8String], "r" );
+    //Hack for an inexplicable exception we don't seem to be able to fix
+    if ([[self reason] isEqualToString:@"_sharedInstance is invalid."]) {
+        [super raise];
+    } else {
+        NSDictionary    *dict = [self userInfo];
+        NSString        *stackTrace = nil;
+        NSMutableString *processedStackTrace = [[[NSMutableString alloc] init] autorelease];
         
-        if( file )
-        {
-            char buffer[512];
-            size_t length;
-
-            while( length = fread( buffer, 1, sizeof( buffer ), file ) )
+        //Turn the nonsense of memory addresses into a human-readable backtrace complete with line numbers
+        if (dict && (stackTrace = [dict objectForKey:NSStackTraceKey])) { 
+            NSString *str = [NSString stringWithFormat:@"%s -p %d %@ | tail -n +3 | head -n +%d | %s | cat -n",[[[NSBundle mainBundle] pathForResource:@"atos" ofType:nil] fileSystemRepresentation],
+                [[NSProcessInfo processInfo] processIdentifier],
+                stackTrace,
+                ([[stackTrace componentsSeparatedByString:@"  "] count] - 4),
+                [[[NSBundle mainBundle] pathForResource:@"c++filt" ofType:nil] fileSystemRepresentation]];
+            FILE *file = popen( [str UTF8String], "r" );
+            
+            if( file )
             {
-                [processedStackTrace appendString:[NSString stringWithCString:buffer]];
+                char buffer[512];
+                size_t length;
+                
+                while( length = fread( buffer, 1, sizeof( buffer ), file ) )
+                {
+                    [processedStackTrace appendString:[NSString stringWithCString:buffer]];
+                }
+                
+                pclose( file );
             }
             
-            pclose( file );
+            [[NSString stringWithFormat:@"Exception:\t%@\nReason:\t%@\nStack trace:\n%@",[self name],[self reason],processedStackTrace] writeToFile:EXCEPTIONS_PATH atomically:YES];
+            NSLog(@"Launching the Adium Crash Reporter because an exception of type %@ occurred:\n%@)",[self name],[self reason]);
+            
+            [[NSWorkspace sharedWorkspace] launchApplication:PATH_TO_CRASH_REPORTER];
+            //Move along, citizen, nothing more to see here.
+            exit(-1);
+        } else {
+            [super raise];
         }
-        
-        [[NSString stringWithFormat:@"Exception:\t%@\nReason:\t%@\nStack trace:\n%@",[self name],[self reason],processedStackTrace] writeToFile:EXCEPTIONS_PATH atomically:YES];
-        NSLog(@"Launching the Adium Crash Reporter because an exception of type %@ occurred:\n%@)",[self name],[self reason]);
-
-        [[NSWorkspace sharedWorkspace] launchApplication:PATH_TO_CRASH_REPORTER];
-        //Move along, citizen, nothing more to see here.
-        exit(-1);
-    } else {
-        [super raise];
     }
 }
 
