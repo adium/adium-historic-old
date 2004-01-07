6aa5569ad4814f8c1df9cacaaf595557993c26ec
adamiser <adamiser@localhost>
2004-01-07 21:39:00 +0000

    Updated Grouped idle/away sort (no groups)

--- adium/Plugins/Grouped Idle Time Idle Away Sort/AIGroupedIdleAwaySortNoGroups.m
+++ adium/Plugins/Grouped Idle Time Idle Away Sort/AIGroupedIdleAwaySortNoGroups.m
@@ -15,7 +15,7 @@
 
 #import "AIGroupedIdleAwaySortNoGroups.h"
 
-int groupedIdleAwaySortNoGroups(id objectA, id objectB, void *context);
+int groupedIdleAwaySortNoGroups(id objectA, id objectB, AIListGroup *containingGroup, BOOL groups);
 
 @implementation AIGroupedIdleAwaySortNoGroups
 
@@ -28,87 +28,56 @@ int groupedIdleAwaySortNoGroups(id objectA, id objectB, void *context);
 - (NSString *)displayName{
     return(@"Idle to Bottom (Grouped by status) (Groups Not Sorted)");
 }
-
-- (BOOL)shouldSortForModifiedStatusKeys:(NSArray *)inModifiedKeys
-{
-    if([inModifiedKeys containsObject:@"Idle"] || [inModifiedKeys containsObject:@"Away"]){
-        return(YES);
-    }else{
-        return(NO);
-    }
+- (NSArray *)statusKeysRequiringResort{
+	return([NSArray arrayWithObjects:@"Idle", @"Away", nil]);
+}
+- (NSArray *)attributeKeysRequiringResort{
+	return([NSArray arrayWithObject:@"Display Name"]);
+}
+- (sortfunc)sortFunction{
+	return(&groupedIdleAwaySortNoGroups);
 }
 
-- (BOOL)shouldSortForModifiedAttributeKeys:(NSArray *)inModifiedKeys
-{
-    if([inModifiedKeys containsObject:@"Hidden"] || [inModifiedKeys containsObject:@"Display Name"]){
-        return(YES);
-    }else{
-        return(NO);
-    }
-}
-
-- (void)sortListObjects:(NSMutableArray *)inObjects
-{
-    [inObjects sortUsingFunction:groupedIdleAwaySortNoGroups context:nil];
-}
-
-int groupedIdleAwaySortNoGroups(id objectA, id objectB, void *context)
+int groupedIdleAwaySortNoGroups(id objectA, id objectB, AIListGroup *containingGroup, BOOL groups)
 {    
-    BOOL	invisibleA = [[objectA displayArrayForKey:@"Hidden"] containsAnyIntegerValueOf:1];
-    BOOL	invisibleB = [[objectB displayArrayForKey:@"Hidden"] containsAnyIntegerValueOf:1];
-    
-    if(invisibleA && !invisibleB){
-        return(NSOrderedDescending);
-    }else if(!invisibleA && invisibleB){
-        return(NSOrderedAscending);
-    }else{
-        BOOL	groupA = [objectA isKindOfClass:[AIListGroup class]];
-        BOOL	groupB = [objectB isKindOfClass:[AIListGroup class]];
-
-        if(groupA && !groupB){
-            return(NSOrderedAscending);
-        }else if(!groupA && groupB){
-            return(NSOrderedDescending);
-        }else if(!groupA && !groupB){
-            BOOL idleA = ([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
-            BOOL idleB = ([[objectB statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
-            BOOL awayA = ([[objectA statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1]);
-            BOOL awayB = ([[objectB statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1]);
-
+	if(!groups){
+		BOOL idleA = ([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
+		BOOL idleB = ([[objectB statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
+		BOOL awayA = ([[objectA statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1]);
+		BOOL awayB = ([[objectB statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1]);
+		
 	    int countA = 0;
 	    int countB = 0;
-
+		
 	    if(idleA) countA++;
 	    if(awayA) countA++;
 	    if(idleB) countB++;
 	    if(awayB) countB++;
-
+		
 	    if(countA > countB){
-		return NSOrderedDescending;
+			return NSOrderedDescending;
 	    }else if(countA < countB){
-		return NSOrderedAscending;
+			return NSOrderedAscending;
 	    }else{
-		if([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] == [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue])
-		{//If the idle times are equal do alphabetical sort (this makes the contact list less jumpy and adds familiarity to the list)
-		    return([[objectA longDisplayName] caseInsensitiveCompare:[objectB longDisplayName]]);
-		}
-		if([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] > [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue]){
-		    return NSOrderedDescending;
-		}else{
-		    return NSOrderedAscending;
-		}
+			if([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] == [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue])
+			{//If the idle times are equal do alphabetical sort (this makes the contact list less jumpy and adds familiarity to the list)
+				return([[objectA longDisplayName] caseInsensitiveCompare:[objectB longDisplayName]]);
+			}
+			if([[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] > [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue]){
+				return NSOrderedDescending;
+			}else{
+				return NSOrderedAscending;
+			}
 	    }
-
-	    
-        }else{
-            //Keep groups in manual order
-            if([objectA orderIndex] > [objectB orderIndex]){
-                return(NSOrderedDescending);
-            }else{
-                return(NSOrderedAscending);
-            }
-        }
-    }
+		
+	}else{
+		//Keep groups in manual order
+		if([objectA orderIndexForGroup:containingGroup] > [objectB orderIndexForGroup:containingGroup]){
+			return(NSOrderedDescending);
+		}else{
+			return(NSOrderedAscending);
+		}
+	}
 }
 
 @end
