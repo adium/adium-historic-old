2779275e0f38f13d16ef00487bc32536a619d425
adamiser <adamiser@localhost>
2004-01-07 21:41:11 +0000

    Idle away sort (no groups) updated for new superclass

--- adium/Plugins/Idle Away Sort/AIIdleAwaySortNoGroups.m
+++ adium/Plugins/Idle Away Sort/AIIdleAwaySortNoGroups.m
@@ -15,7 +15,7 @@
 
 #import "AIIdleAwaySortNoGroups.h"
 
-int idleAwaySortNoGroups(id objectA, id objectB, void *context);
+int idleAwaySortNoGroups(id objectA, id objectB, AIListGroup *containingGroup, BOOL groups);
 
 @implementation AIIdleAwaySortNoGroups
 
@@ -28,67 +28,37 @@ int idleAwaySortNoGroups(id objectA, id objectB, void *context);
 - (NSString *)displayName{
     return(@"Idle and Away to Bottom (Groups Not Sorted)");
 }
-
-- (BOOL)shouldSortForModifiedStatusKeys:(NSArray *)inModifiedKeys
-{
-    if([inModifiedKeys containsObject:@"Idle"] || [inModifiedKeys containsObject:@"Away"]){
-        return(YES);
-    }else{
-        return(NO);
-    }
+- (NSArray *)statusKeysRequiringResort{
+	return([NSArray arrayWithObjects:@"Idle", @"Away", nil]);
+}
+- (NSArray *)attributeKeysRequiringResort{
+	return([NSArray arrayWithObject:@"Display Name"]);
+}
+- (sortfunc)sortFunction{
+	return(&idleAwaySortNoGroups);
 }
 
-- (BOOL)shouldSortForModifiedAttributeKeys:(NSArray *)inModifiedKeys
-{
-    if([inModifiedKeys containsObject:@"Hidden"] || [inModifiedKeys containsObject:@"Display Name"]){
-        return(YES);
-    }else{
-        return(NO);
-    }
-}
-
-- (void)sortListObjects:(NSMutableArray *)inObjects
-{
-    [inObjects sortUsingFunction:idleAwaySortNoGroups context:nil];
-}
-
-int idleAwaySortNoGroups(id objectA, id objectB, void *context)
+int idleAwaySortNoGroups(id objectA, id objectB, AIListGroup *containingGroup, BOOL groups)
 {    
-    BOOL	invisibleA = [[objectA displayArrayForKey:@"Hidden"] containsAnyIntegerValueOf:1];
-    BOOL	invisibleB = [[objectB displayArrayForKey:@"Hidden"] containsAnyIntegerValueOf:1];
-
-    if(invisibleA && !invisibleB){
-        return(NSOrderedDescending);
-    }else if(!invisibleA && invisibleB){
-        return(NSOrderedAscending);
-    }else{
-        BOOL	groupA = [objectA isKindOfClass:[AIListGroup class]];
-        BOOL	groupB = [objectB isKindOfClass:[AIListGroup class]];
-
-        if(groupA && !groupB){
-            return(NSOrderedAscending);
-        }else if(!groupA && groupB){
-            return(NSOrderedDescending);
-        }else if(!groupA && !groupB){
-            BOOL idleAwayA = ([[objectA statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1] || [[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
-            BOOL idleAwayB = ([[objectB statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1] || [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
-            
-            if(idleAwayA && !idleAwayB){
-                return(NSOrderedDescending);
-            }else if(!idleAwayA && idleAwayB){
-                return(NSOrderedAscending);
-            }else{
-                return([[objectA longDisplayName] caseInsensitiveCompare:[objectB longDisplayName]]);
-            }
-        }else{
-            //Keep groups in manual order
-            if([objectA orderIndex] > [objectB orderIndex]){
-                return(NSOrderedDescending);
-            }else{
-                return(NSOrderedAscending);
-            }
-        }
-    }
+	if(!groups){
+		BOOL idleAwayA = ([[objectA statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1] || [[objectA statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
+		BOOL idleAwayB = ([[objectB statusArrayForKey:@"Away"] containsAnyIntegerValueOf:1] || [[objectB statusArrayForKey:@"Idle"] greatestDoubleValue] != 0);
+		
+		if(idleAwayA && !idleAwayB){
+			return(NSOrderedDescending);
+		}else if(!idleAwayA && idleAwayB){
+			return(NSOrderedAscending);
+		}else{
+			return([[objectA longDisplayName] caseInsensitiveCompare:[objectB longDisplayName]]);
+		}
+	}else{
+		//Keep groups in manual order
+		if([objectA orderIndexForGroup:containingGroup] > [objectB orderIndexForGroup:containingGroup]){
+			return(NSOrderedDescending);
+		}else{
+			return(NSOrderedAscending);
+		}
+	}
 }
 
 @end
