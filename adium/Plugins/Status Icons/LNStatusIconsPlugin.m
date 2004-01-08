c40e50f828cefc19418d7bc74bb5d9b7e4803c41
adamiser <adamiser@localhost>
2004-01-08 01:13:28 +0000

    Updated status icons to new system.  Fixed them so they no longer cause an unnecessary resort/redisplay.

--- adium/Plugins/Status Icons/LNStatusIconsPlugin.m
+++ adium/Plugins/Status Icons/LNStatusIconsPlugin.m
@@ -2,18 +2,14 @@
 #import "LNStatusIconsPlugin.h"
 #import "LNStatusIconsPreferences.h"
 
-
 @interface LNStatusIconsPlugin (PRIVATE)
 - (void)preferencesChanged:(NSNotification *)notification;
 @end
 
-
 @implementation LNStatusIconsPlugin
-
 
 - (void)installPlugin
 {
-
     displayStatusIcon = NO;
 
     //Register our default preferences
@@ -29,113 +25,96 @@
 
     idleImage = [[AIImageUtilities imageNamed:@"IdleIcon" forClass:[self class]] retain];
     awayImage = [[AIImageUtilities imageNamed:@"AwayIcon" forClass:[self class]] retain];
-    
 }
-
 
 - (void)uninstallPlugin
 {
-
     [idleImage release];
     [awayImage release];
-
 }
-
 
 - (void)dealloc
 {
-
     [super dealloc];
-
 }
 
-
-- (NSArray *)updateListObject:(AIListObject *)inObject keys:(NSArray *)inModifiedKeys delayed:(BOOL)delayed silent:(BOOL)silent
+- (NSArray *)updateListObject:(AIListObject *)inObject keys:(NSArray *)inModifiedKeys silent:(BOOL)silent
 {
-
     NSArray			*modifiedAttributes = nil;
-    
-    LNStatusIcon		*statusIcon;
-    AIMutableOwnerArray		*iconArray;
-    NSArray			*imageArray;
-    double			idle = 0;
-    int				away = 0;
-    
-
-    // For now, until dragging is implemented, they are right aligned.
-    // Also, they do not always display in the proper order.
-    iconArray = [inObject displayArrayForKey:@"Right View"];
-    [[inObject displayArrayForKey:@"Left View"]  setObject:nil withOwner:self];
-
-
-    statusIcon = [iconArray objectWithOwner:self];
-
-    if(displayStatusIcon){
-        
-        if(!statusIcon){
-            statusIcon = [LNStatusIcon statusIcon];
-            [iconArray setObject:statusIcon withOwner:self];
-        }
-        
-        
-        idle = [[inObject statusArrayForKey:@"Idle"] greatestDoubleValue];
-        away = [[inObject statusArrayForKey:@"Away"] greatestIntegerValue];
-        
-        
-        if((away != 0) && (idle != 0)){
-            imageArray = [NSArray arrayWithObjects:awayImage, idleImage, nil];
-            [statusIcon setImageArray:imageArray];
-        
-        }else if(away != 0){
-            imageArray = [NSArray arrayWithObjects:awayImage, nil];
-            [statusIcon setImageArray:imageArray];
-        
-        }else if(idle != 0){
-            imageArray = [NSArray arrayWithObjects:idleImage, nil];
-            [statusIcon setImageArray:imageArray];
-        }else{
-            [iconArray setObject:nil withOwner:self];
-        }
-        
-    
-    }else{
-        if(statusIcon){
-            [iconArray setObject:nil withOwner:self];
-        }
-    }
 	
-    modifiedAttributes = [NSArray arrayWithObjects:@"Left View", @"Right View", nil];
-    
+    if(	inModifiedKeys == nil ||
+        [inModifiedKeys containsObject:@"Away"] ||
+        [inModifiedKeys containsObject:@"Idle"]){
+		
+		LNStatusIcon			*statusIcon;
+		AIMutableOwnerArray		*iconArray;
+		NSArray					*imageArray;
+		double					idle = 0;
+		int						away = 0;
+		
+		// For now, until dragging is implemented, they are right aligned.
+		// Also, they do not always display in the proper order.
+		iconArray = [inObject displayArrayForKey:@"Right View"];
+		[[inObject displayArrayForKey:@"Left View"]  setObject:nil withOwner:self];
+		
+		statusIcon = [iconArray objectWithOwner:self];
+		
+		if(displayStatusIcon){
+			if(!statusIcon){
+				statusIcon = [LNStatusIcon statusIcon];
+				[iconArray setObject:statusIcon withOwner:self];
+			}
+			
+			idle = [[inObject statusArrayForKey:@"Idle"] greatestDoubleValue];
+			away = [[inObject statusArrayForKey:@"Away"] greatestIntegerValue];
+			
+			if((away != 0) && (idle != 0)){
+				imageArray = [NSArray arrayWithObjects:awayImage, idleImage, nil];
+				[statusIcon setImageArray:imageArray];
+				
+			}else if(away != 0){
+				imageArray = [NSArray arrayWithObjects:awayImage, nil];
+				[statusIcon setImageArray:imageArray];
+				
+			}else if(idle != 0){
+				imageArray = [NSArray arrayWithObjects:idleImage, nil];
+				[statusIcon setImageArray:imageArray];
+			}else{
+				[iconArray setObject:nil withOwner:self];
+			}
+			
+		}else{
+			if(statusIcon){
+				[iconArray setObject:nil withOwner:self];
+			}
+		}
+		
+		modifiedAttributes = [NSArray arrayWithObjects:@"Left View", @"Right View", nil];
+	}
+	
     return(modifiedAttributes);
 }
 
-
-
-
 - (void)preferencesChanged:(NSNotification *)notification
 {
-
     if(notification == nil || [(NSString *)[[notification userInfo] objectForKey:@"Group"] compare:PREF_GROUP_STATUS_ICONS] == 0){
-    
-	NSDictionary	*prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_STATUS_ICONS];
-
-	//Release the old values..
-	//Cache the preference values
-	displayStatusIcon  = [[prefDict objectForKey:KEY_DISPLAY_STATUS_ICONS] boolValue];
-
+		
+		NSDictionary	*prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_STATUS_ICONS];
+		
+		//Release the old values..
+		//Cache the preference values
+		displayStatusIcon  = [[prefDict objectForKey:KEY_DISPLAY_STATUS_ICONS] boolValue];
+		
         //Update all our status icons
-	NSEnumerator		*enumerator;
-	AIListObject		*object;
-
-	enumerator = [[[adium contactController] allContactsInGroup:nil subgroups:YES] objectEnumerator];
-
-	while(object = [enumerator nextObject]){
-            [[adium contactController] listObjectAttributesChanged:object modifiedKeys:[self updateListObject:object keys:nil delayed:YES silent:YES] delayed:YES];
+		NSEnumerator		*enumerator;
+		AIListObject		*object;
+		
+		enumerator = [[[adium contactController] allContactsInGroup:nil subgroups:YES] objectEnumerator];
+		
+		while(object = [enumerator nextObject]){
+            [[adium contactController] listObjectAttributesChanged:object modifiedKeys:[self updateListObject:object keys:nil silent:YES]];
         }
     }
-
 }
-
-
 
 @end
