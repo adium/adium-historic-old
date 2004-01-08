47beb0b899ee88aeca8fea57ea0121e5a346cd6e
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-01-08 07:22:16 +0000

    Updated action support for the new content filtering.

--- adium/Plugins/Action Support/CBActionSupportPlugin.h
+++ adium/Plugins/Action Support/CBActionSupportPlugin.h
@@ -3,7 +3,6 @@
 //  Adium
 //
 //  Created by Colin Barrett on Tue Jun 17 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
 //
 
 @protocol AIContentFilter;
@@ -12,11 +11,5 @@
 {
 
 }
-/*
-//AIPlugin subclassed methods
-- (void)installPlugin;
 
-//AIContentFilter protocol methods
-- (void)filterContentObject:(id <AIContentObject>)inObject;
-*/
 @end
