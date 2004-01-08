4023b28fd05b8f2f9122e8e90d28cb8b1cd88fbb
Evan Schoenberg <Evan Schoenberg@localhost>
2004-01-08 07:13:01 +0000

    Accounts now get an - (void)updateAttributedStatusString:(NSAttributedString *)status forKey:(NSString *)key which handles filtering the attributed string and managing a centralized refresh timer for changing away messages (for example, one including %t or %_artist) only if necessary. The account receives back a call of setAttributedStatusString:forKey: which should handle the actual serverside changes.
    Cleanup in AIException.m.

--- adium/Frameworks/Adium Framework/AIAccount.m
+++ adium/Frameworks/Adium Framework/AIAccount.m
@@ -13,11 +13,15 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
-// $Id: AIAccount.m,v 1.29 2004/01/07 21:23:04 adamiser Exp $
+// $Id: AIAccount.m,v 1.30 2004/01/08 07:13:01 evands Exp $
 
 #import "AIAccount.h"
 
+#define ATTRIBUTED_STRING_REFRESH    30.0    //delay in seconds between refresh of our attributed string statuses when needed
+
 @interface AIAccount (PRIVATE)
+- (void)_setAccountAwayTo:(NSAttributedString *)awayMessage;
+- (void)_setAccountProfileTo:(NSAttributedString *)profile;
 @end
 
 @implementation AIAccount
@@ -41,11 +45,14 @@
     
     //Clear the online state.  'Auto-Connect' values are used, not the previous online state.
     [self setPreference:[NSNumber numberWithBool:NO] forKey:@"Online" group:GROUP_ACCOUNT_STATUS];
-	[self updateStatusForKey:@"Handle"];
-	
+    [self updateStatusForKey:@"Handle"];
+    
+    refreshDict = [[NSMutableDictionary alloc] init];
+    refreshTimer = nil;
+    
     //Init the account
     [self initAccount];
-
+    
     return(self);
 }
 
@@ -65,6 +72,7 @@
     return(service);
 }
 
+#pragma mark Preferences
 //Store our account prefs in a separate folder to keep things clean
 - (NSString *)pathToPreferences
 {
@@ -74,85 +82,116 @@
 //Monitor preferences changed for account status keys, and pass these to our subclass
 - (void)_accountPreferencesChanged:(NSNotification *)notification
 {
-	//Ignore changes directed at another account
-	if([notification object] == nil || [notification object] == self){
-		NSString    *group = [[notification userInfo] objectForKey:@"Group"];
-
-		//For convenience, we let the account know when a status key for it has changed
-		if([group compare:GROUP_ACCOUNT_STATUS] == 0){
-			NSString	*key = [[notification userInfo] objectForKey:@"Key"];
-			
-			[self updateStatusForKey:key];
-		}
-	}
-}
-
-//Update this account's status
-//Status keys that are used by every account (Or used by the majority of accounts and harmless to others) should be
-//placed here, instead of duplicated in each account plugin.
-- (void)updateStatusForKey:(NSString *)key
-{
-	BOOL    areOnline = [[self statusObjectForKey:@"Online"] boolValue];
-
-    //Online status changed
-	//Call connect or disconnect as appropriate
-	//
-    if([key compare:@"Online"] == 0){
-        if([[self preferenceForKey:@"Online" group:GROUP_ACCOUNT_STATUS] boolValue]){
-			if(!areOnline && ![[self statusObjectForKey:@"Connecting"] boolValue]){
-				NSLog(@"instructing %@ to connect",[self displayName]);
-				//Retrieve the user's password and then call connect
-				[[adium accountController] passwordForAccount:self 
-											  notifyingTarget:self
-													 selector:@selector(passwordReturnedForConnect:)];
-			}
-        }else{
-			if(areOnline && ![[self statusObjectForKey:@"Disconnecting"] boolValue]){
-				NSLog(@"instructing %@ to disconnect",[self displayName]);
-				//Disconnect
-				[self disconnect];
-			}
+    //Ignore changes directed at another account
+    if([notification object] == nil || [notification object] == self){
+        NSString    *group = [[notification userInfo] objectForKey:@"Group"];
+        
+        //For convenience, we let the account know when a status key for it has changed
+        if([group compare:GROUP_ACCOUNT_STATUS] == 0){
+            NSString	*key = [[notification userInfo] objectForKey:@"Key"];
+            
+            [self updateStatusForKey:key];
         }
     }
-
-	//Username formatting changed
-	//Update the display name for this account
-	//
-    if([key compare:@"Handle"] == 0){
-		[self setStatusObject:[self preferenceForKey:@"Handle" group:GROUP_ACCOUNT_STATUS]
-					   forKey:@"Display Name"
-					   notify:YES];
-    }
-	
 }
 
 //Callback after the user enters their password for connecting
 - (void)passwordReturnedForConnect:(NSString *)inPassword
 {
-	//If a password was returned, and we're still waiting to connect
-	if(inPassword && [inPassword length] != 0 &&
-	   ![[self statusObjectForKey:@"Online"] boolValue] &&
-	   ![[self statusObjectForKey:@"Connecting"] boolValue]){
+    //If a password was returned, and we're still waiting to connect
+    if(inPassword && [inPassword length] != 0 &&
+       ![[self statusObjectForKey:@"Online"] boolValue] &&
+       ![[self statusObjectForKey:@"Connecting"] boolValue]){
         //Save the new password
-		if(password != inPassword){
+        if(password != inPassword){
             [password release]; password = [inPassword copy];
         }
-		
-		//Tell the account to connect
-		[self connect];
-	}
+        
+        //Tell the account to connect
+        [self connect];
+    }
 }
 
--(NSString *)encodedStringFromAttributedString:(NSAttributedString *)inAttributedString
+#pragma mark Status
+//Update this account's status
+//Status keys that are used by every account (Or used by the majority of accounts and harmless to others) should be
+//placed here, instead of duplicated in each account plugin.
+- (void)updateStatusForKey:(NSString *)key
 {
-    return ([AIHTMLDecoder encodeHTML:inAttributedString
-                              headers:YES
-                             fontTags:YES   closeFontTags:YES
-                            styleTags:YES   closeStyleTagsOnFontChange:YES
-                       encodeNonASCII:NO
-                           imagesPath:nil]);
+    BOOL    areOnline = [[self statusObjectForKey:@"Online"] boolValue];
+    
+    //Online status changed
+    //Call connect or disconnect as appropriate
+    //
+    if([key compare:@"Online"] == 0){
+        if([[self preferenceForKey:@"Online" group:GROUP_ACCOUNT_STATUS] boolValue]){
+            if(!areOnline && ![[self statusObjectForKey:@"Connecting"] boolValue]){
+                NSLog(@"AIAccount: Instructing %@ to connect",[self displayName]);
+                //Retrieve the user's password and then call connect
+                [[adium accountController] passwordForAccount:self 
+                                              notifyingTarget:self
+                                                     selector:@selector(passwordReturnedForConnect:)];
+            }
+        }else{
+            if(areOnline && ![[self statusObjectForKey:@"Disconnecting"] boolValue]){
+                NSLog(@"AIAccount: Instructing %@ to disconnect",[self displayName]);
+                //Disconnect
+                [self disconnect];
+            }
+        }
+    }else if([key compare:@"Handle"] == 0){
+        //Username formatting changed
+        //Update the display name for this account
+        //
+        [self setStatusObject:[self preferenceForKey:@"Handle" group:GROUP_ACCOUNT_STATUS]
+                       forKey:@"Display Name"
+                       notify:YES];
+    }
 }
 
+//Set our away state and message (Pass nil for no away), setting up a refresh timer if the filters changed the string
+- (void)updateAttributedStatusString:(NSAttributedString *)status forKey:(NSString *)key
+{
+    BOOL refreshPeriodically;
+    NSAttributedString  *filteredMessage = [[adium contentController] filteredAttributedString:status
+                                                                                    isOutgoing:YES];
+    //refresh periodically if the filtered string is different from the original one
+    refreshPeriodically = (status && (![[status string] isEqualToString:[filteredMessage string]]));
+    
+    if (refreshPeriodically){
+        [refreshDict setObject:status forKey:key];
+        //Begin the timer if it is currently inactive
+        if (!refreshTimer){
+            refreshTimer = [[NSTimer scheduledTimerWithTimeInterval:ATTRIBUTED_STRING_REFRESH
+                                                                        target:self
+                                                                      selector:@selector(_refreshAttributedStrings:) 
+                                                                      userInfo:nil repeats:YES] retain];
+        }
+    }else{
+        [refreshDict removeObjectForKey:status];
+        
+        //Check if our refreshDict is now empty with a currently running timer
+        if ( ([refreshDict count] == 0) && refreshTimer){
+            if ([refreshTimer isValid])
+                [refreshTimer invalidate];
+            [refreshTimer release]; refreshTimer = nil;
+        }        
+    }
+
+    //Set the status in account code
+    [self setAttributedStatusString:filteredMessage forKey:key];
+}
+//Refilter the raw away message and call setAccountAwayTo:
+- (void)_refreshAttributedStrings:(NSTimer *)inTimer
+{
+    NSEnumerator    *keyEnumerator = [refreshDict keyEnumerator];
+    NSString        *key;
+    while (key = [keyEnumerator nextObject]){
+        NSAttributedString *filteredMessage = [[adium contentController] filteredAttributedString:[refreshDict objectForKey:key]
+                                                                                   isOutgoing:YES];
+        [self setAttributedStatusString:filteredMessage forKey:key];
+    }
+}
 
 //Return the account-specific user icon, or the default user icon from the account controlelr if none exists (thee default user icon returns nil if none is set)
 //- (NSImage *)userIcon {
@@ -167,17 +206,28 @@
 //    userIcon = [inUserIcon retain];
 //}
 
+#pragma mark Subclasses
 //Functions for subclasses to override
 - (void)initAccount{};
 - (void)connect{};
 - (void)disconnect{};
 - (NSView *)accountView{return(nil);};
 - (NSArray *)supportedPropertyKeys{return([NSArray array]);}
+- (void)setAttributedStatusString:(NSAttributedString *)inAttributedString forKey:(NSString *)key{};
 
-
+//Functions subclasses may choose to override
+-(NSString *)encodedStringFromAttributedString:(NSAttributedString *)inAttributedString
+{
+    return ([AIHTMLDecoder encodeHTML:inAttributedString
+                              headers:YES
+                             fontTags:YES   closeFontTags:YES
+                            styleTags:YES   closeStyleTagsOnFontChange:YES
+                       encodeNonASCII:NO
+                           imagesPath:nil]);
+}
 
 //Auto-Reconnect -------------------------------------------------------------------------------------
-
+#pragma mark Auto-Reconnect
 //Attempts to auto-reconnect (after an X second delay)
 - (void)autoReconnectAfterDelay:(int)delay
 {
