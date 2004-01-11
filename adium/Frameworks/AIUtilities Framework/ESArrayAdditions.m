4da17961b7f6a5b0f284da38f19c2979d6dbcae5
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-01-11 10:27:33 +0000

    Added ESArrayAdditions, which currently just has a method for loading an array from a file in a particular bundle (just like the dictionary addition for loading a dictionary).

---
+++ adium/Frameworks/AIUtilities Framework/ESArrayAdditions.m
@@ -0,0 +1,28 @@
+//
+//  ESArrayAdditions.m
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Sat Jan 10 2004.
+
+#import "ESArrayAdditions.h"
+
+
+@implementation NSArray (ESArrayAdditions)
+
+// Returns an array from the owners bundle with the specified name
++ (NSArray *)arrayNamed:(NSString *)name forClass:(Class)inClass
+{
+    NSBundle		*ownerBundle;
+    NSString		*arrayPath;
+    NSDictionary	*array;
+    
+    //Get the bundle
+    ownerBundle = [NSBundle bundleForClass:inClass];
+    
+    //Open the image
+    arrayPath = [ownerBundle pathForResource:name ofType:@"plist"];    
+    array = [[NSArray alloc] initWithContentsOfFile:arrayPath];
+    
+    return([array autorelease]);
+}
+@end
