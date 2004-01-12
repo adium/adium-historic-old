2d58a389f75e1c2689bf3bc94483447b1f88ff3a
Colin Barrett <colin@springsandstruts.com>
2004-01-12 03:21:15 +0000

    Added source files and plist file needed for preferences. No nib or connections yet (I wanted to get these in before dinner)

---
+++ adium/Plugins/Contact Counting Display/CBContactCountingDisplayPreferences.m
@@ -0,0 +1,14 @@
+//
+//  CBContactCountingDisplayPreferences.m
+//  Adium XCode
+//
+//  Created by Colin Barrett on Sun Jan 11 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import "CBContactCountingDisplayPreferences.h"
+#import "CBContactCountingDisplayPlugin.h"
+
+@implementation CBContactCountingDisplayPreferences
+
+@end
