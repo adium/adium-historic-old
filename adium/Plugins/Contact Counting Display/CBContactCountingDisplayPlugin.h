96738f48fe6af556fb2f1a8a1e1f0eb870bd70b2
ramoth4 <ramoth4@localhost>
2004-01-12 00:15:13 +0000

    Added the Contact Counting Display plugin. It's pretty empty at the moment :)

---
+++ adium/Plugins/Contact Counting Display/CBContactCountingDisplayPlugin.h
@@ -0,0 +1,14 @@
+//
+//  CBContactCountingDisplayPlugin.h
+//  Adium XCode
+//
+//  Created by Colin Barrett on Sun Jan 11 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+@interface CBContactCountingDisplayPlugin : AIPlugin 
+{
+
+}
+
+@end
