2e884eadc4f8d02ef348805123db3a932d9ef719
Colin Barrett <colin@springsandstruts.com>
2004-01-12 01:42:39 +0000

    This plugin now works!
    - Currently adds the number of online contacts after the group name (eg.
    "Buddies (5)").
    
    Next: Total contacts (eg. "(5/23)"), and prefs.

--- adium/Plugins/Contact Counting Display/CBContactCountingDisplayPlugin.m
+++ adium/Plugins/Contact Counting Display/CBContactCountingDisplayPlugin.m
@@ -6,13 +6,52 @@
 //  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
 //
 
-#import "CBContactCountingDisplay.h"
+#import "CBContactCountingDisplayPlugin.h"
 
 @implementation CBContactCountingDisplayPlugin
 
 - (void)installPlugin
 {
+    NSLog(@"hello!");
+        //install our observers
+        [[adium notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
+        [[adium notificationCenter] addObserver:self selector:@selector(contactsChanged:) name:ListObject_StatusChanged object:nil];
+}
 
+- (void)preferencesChanged:(NSNotification *)notification
+{
+    
+}
+
+- (void)contactsChanged:(NSNotification *)notification
+{
+    /* some important keys:
+        @"VisibleObjectCount" - ListGroup status object (NSNumber)
+        @"Right Text" - ListObject display array (AIMutableOwnerArray)
+    */
+    
+    NSLog(@"contactsChanged:");
+    
+    AIListObject *listObject = [notification object];
+    NSArray *groups = [listObject containingGroups];
+    
+    if(groups)
+    {
+        NSEnumerator *numer = [groups objectEnumerator];
+        AIListGroup *group;
+        while(group = [numer nextObject])
+        {
+            NSLog(@"%i",[[group statusObjectForKey:@"VisibleObjectCount"] intValue]);
+            //Shouldn't really have to use primary object.
+            [[group displayArrayForKey:@"Right Text"] setPrimaryObject:[NSString stringWithFormat:@" (%i)", [[group statusObjectForKey:@"VisibleObjectCount"] intValue]] withOwner:self];
+        }
+    }
+}
+
+- (void)uninstallPlugin
+{
+    //we are no longer an observer
+    [[adium notificationCenter] removeObserver:self];
 }
 
 @end
