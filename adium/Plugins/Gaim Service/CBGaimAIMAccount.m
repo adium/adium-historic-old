c0e3ab0c8f8e2c501154d348aa78def7f51fc05b
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-01-16 03:25:53 +0000

    We don't want to be encoding non-ASCII characters if we don't have to be, and we don't have to be via liboscar... winAIM doesn't read decode high-ASCII HTML.

--- adium/Plugins/Gaim Service/CBGaimAIMAccount.m
+++ adium/Plugins/Gaim Service/CBGaimAIMAccount.m
@@ -24,17 +24,17 @@
 	//We don't want to send HTML to ICQ users, or mobile phone users
 	if(inListObject){
 		char	firstCharacter = [[inListObject UID] characterAtIndex:0];
-		noHTML = ((firstCharacter >= '0' && firstCharacter <= '9') || firstCharacter == '+');
+	    noHTML = ((firstCharacter >= '0' && firstCharacter <= '9') || firstCharacter == '+');
 	}
-
-    return((noHTML ? [inAttributedString string] : [AIHTMLDecoder encodeHTML:inAttributedString
-																	headers:YES
-																   fontTags:YES
-															  closeFontTags:NO
-																  styleTags:YES
-												 closeStyleTagsOnFontChange:NO
-															 encodeNonASCII:YES
-																 imagesPath:nil]));
+	
+	return((noHTML ? [inAttributedString string] : [AIHTMLDecoder encodeHTML:inAttributedString
+									 headers:YES
+									fontTags:YES
+								   closeFontTags:NO
+								       styleTags:YES
+						      closeStyleTagsOnFontChange:NO
+								  encodeNonASCII:NO
+								      imagesPath:nil]));
 }
 
 @end
