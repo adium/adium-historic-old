f72c9724444dd5b75c3d21e000c697d77a7cfa38
bgannin <brian.ganninger@gmail.com>
2004-01-18 07:18:15 +0000

    Themes! Includes a minty fresh look (when the .nib wants to upload itself), some mad skills, and easy access to your Themes folder.

---
+++ adium/Plugins/Themes/BGThemesPlugin.h
@@ -0,0 +1,26 @@
+//
+//  BGThemesPlugin.h
+//  Adium XCode
+//
+//  Created by Brian Ganninger on Sat Jan 03 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <Cocoa/Cocoa.h>
+#import <Adium/Adium.h>
+#import <AIUtilities/AIUtilities.h>
+
+#import "BGThemesPreferences.h"
+
+@interface BGThemesPlugin : AIPlugin {
+    BGThemesPreferences *themePane;
+    NSString *themePath;
+}
+-(void)saveTheme:(NSMutableDictionary *)saveTheme;
+-(void)applyTheme:(NSString *)newThemeName;
+-(void)createThemeNamed:(NSString *)newName by:(NSString *)newAuthor version:(NSString *)newVersion;
+-(void)saveKey:(NSString *)key group:(NSString *)group toDict:(NSMutableDictionary *)dict;
+-(void)applyPreferenceFromKey:(NSString *)key inDict:(NSDictionary *)dict;
+@end
---
+++ adium/Plugins/Themes/BGThemesPreferences.h
@@ -0,0 +1,38 @@
+//
+//  BGThemesPreferences.h
+//  Adium XCode
+//
+//  Created by Brian Ganninger on Sat Jan 03 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Adium/Adium.h>
+#import <AIUtilities/AIUtilities.h>
+#import "BGThemeManageView.h"
+
+@interface BGThemesPreferences : AIPreferencePane {
+    // create tab
+    IBOutlet NSTextField *nameField;
+    IBOutlet NSTextField *authorField;
+    IBOutlet NSTextField *versionField;
+    IBOutlet NSImageView *createPreview;
+    IBOutlet NSButton *generatePreviewButton;
+    IBOutlet NSButton *createButton;
+    IBOutlet NSTextField *createStatus;
+    // manage tab
+    IBOutlet BGThemeManageView *themesList;
+    IBOutlet NSImageView *applyPreview;
+    IBOutlet NSTextField *manageStatus;
+    // other
+    IBOutlet NSButton *themesFolderButton;
+    id themePlugin;
+    NSMenu *themeMenu;
+}
+-(IBAction)createTheme:(id)sender;
+-(IBAction)generatePreview:(id)sender; // doesn't work yet :P
+-(IBAction)openThemesFolder:(id)sender;
+-(void)setPlugin:(id)newPlugin;
+-(void)applyDone;
+-(void)createDone;
+@end
