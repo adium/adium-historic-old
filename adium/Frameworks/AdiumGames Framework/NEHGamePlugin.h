ffed28c78b5d97ad0f76403b61fbc06d5ca08084
hanji <hanji@localhost>
2004-01-21 03:10:50 +0000

    Added the files for the AdiumGames framework, intended to simplify (somewhat) the process of developing games (and possibly other, similar, apps) for Adium :) Instructions should follow sooner or later :)

---
+++ adium/Frameworks/AdiumGames Framework/NEHGamePlugin.h
@@ -0,0 +1,77 @@
+//
+//  NEHGamePlugin.h
+//  Adium XCode
+//
+//  Created by Nelson Elhage on Sun Jan 18 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AIPlugin;
+@class NEHGameController;
+@protocol AIContentFilter;
+
+#define MSG_TYPE_INVITE		@"Invite"
+#define MSG_TYPE_ACK		@"Acknowledge"
+#define MSG_TYPE_ACCEPT		@"Accept"
+#define MSG_TYPE_REJECT		@"Reject"
+#define MSG_TYPE_CANCEL		@"Cancel"
+#define MSG_TYPE_END_GAME   @"End Game"
+#define MSG_TYPE_MOVE		@"Move"
+
+#define MSG_BUSY			@"Busy"
+#define MSG_TIMEOUT			@"Timeout"
+
+#define BUTTON_OK   AILocalizedString(@"OK","")
+#define BUTTON_ERR  AILocalizedString(@"OK","")
+
+//Tag values for the play as radio button group
+#define				TAG_PLAYER_1		0
+#define				TAG_PLAYER_2		1
+#define				TAG_CHOOSE_PLAYER   2
+
+@interface NEHGamePlugin : AIPlugin <AIContentFilter> {
+	NSMenuItem				* menuItem_invite;
+	NSMenuItem				* menuItem_newGame;
+	
+	//This dictionary maps [account UIDAndServiceID] => 
+	//{NSDictionary of [contact UIDAndServiceID] =>  NEHGameController*}
+	NSMutableDictionary		* gamesForAccounts;
+	
+	IBOutlet NSWindow		* window_newGame;
+	IBOutlet AICompletingTextField	* textField_handle;
+	IBOutlet NSPopUpButton  * popUp_account;
+	IBOutlet NSMatrix		* radio_playAs;
+	
+	NSWindowController		* windowController;
+}
+
+- (void)endGameWith:(AIListContact*)contact fromAccount:(AIAccount*)account;
+- (IBAction)newGame:(id)sender;
+
+- (IBAction)sendInvite:(id)sender;
+- (IBAction)cancelInvite:(id)sender;
+
+#pragma mark Selectors for subclasses to implement
+
+//return an autoreleased NEHGameController, initialized with initWithPlugin, of the
+//appropriate class
+- (NEHGameController*)newController;
+- (NSString*)nibName;
+
+//The long name is used in menus, and so on
+- (NSString*)gameLongName;
+//The short name is used in the menu bar of game windows and as a prefix
+//when sending messages. Defaults to the long name
+- (NSString*)gameShortName;
+
+//This is called before control is told to send an invitation
+//It can be used to set parameters in control based on extra
+//options in the New Game window, for example
+- (void)willSendInvitation:(NEHGameController*)control;
+
+//I can't personally think of a use for this one, but flexibility is good :)
+- (void)willRespondToInvitation:(NEHGameController*)control;
+
+@end
