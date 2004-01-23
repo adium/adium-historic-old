16f83cd44d1655ba47223aa8cf870720972ad116
Colin Barrett <colin@springsandstruts.com>
2004-01-23 03:27:58 +0000

    a test

--- CVSROOT/ciabot.pl
+++ CVSROOT/ciabot.pl
@@ -1,5 +1,4 @@
-#!/usr/bin/perl -w
-#
+#!/usr/bin/perl -w#
 #
 # ciabot -- Mail a CVS log message to a given address, for the purposes of CIA
 #
@@ -28,7 +27,7 @@
 # If it does not work, try to disable $xml_rpc in the configuration section
 # below.
 #
-# $Id: ciabot.pl,v 1.15 2004/01/23 03:25:33 ramoth4 Exp $
+# $Id: ciabot.pl,v 1.16 2004/01/23 03:27:58 ramoth4 Exp $
 
 use strict;
 use vars qw ($project $from_email $dest_email $rpc_uri $sendmail $sync_delay
