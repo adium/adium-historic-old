a40d7b3ee761149b55e53de5f65ff69468ccd589
jmelloy <jmelloy@localhost>
2004-02-01 01:09:41 +0000

    Changes by William Knopp. <w_knopp@hotmail.com>
    Add ICQ and MSN support to the proteus importer.
    Added usage statement and exits on quit.

--- adium/Utilities/proteus/proteus2adium.pl
+++ adium/Utilities/proteus/proteus2adium.pl
@@ -7,6 +7,11 @@
 #
 # Run it by doing "./proteus2adium.pl --aim AIM_USER_NAME"
 #
+# Updated 31jan2004 by William Knop <w_knop@hotmail.com>
+#   - added support for icq logs
+#   - added usage statement
+#   - changed the behavior if usage is incorrect (it now exits when required
+#     args are not given)
 # Updated 24jan2004 by Seth Dillingham <seth@macrobyte.net>
 #   - pipe the input from the sqlite script, rather than running 
 #     it in backticks (more memory efficient for large histories)
@@ -18,14 +23,29 @@ use strict;
 
 my $proteus_log_file = "$ENV{HOME}/Library/Application Support/Instant Messaging/Profile/History.db";
 
-my $aim_user;
+my $user_name;
+my $service_type;
+my $base_out;
 for(my $i = 0; $i < @ARGV; $i++) {
     if ($ARGV[$i] eq "--aim") {
-        $aim_user = $ARGV[$i + 1];
+        $user_name = $ARGV[$i + 1];
+        $service_type = "aim-oscar";
+        $base_out = "AIM.$user_name";
+    } elsif ($ARGV[$i] eq "--icq") {
+    	$user_name = $ARGV[$i + 1];
+    	$service_type = "icq";
+    	$base_out = "ICQ.$user_name";
+    } elsif ($ARGV[$i] eq "--msn") {
+        $user_name = $ARGV[$i + 1];
+        $service_type= "msn";
+        $base_out = "MSN.$user_name";
     }
 }
 
-my $base_out = "AIM.$aim_user";
+if (($service_type eq "") || ($user_name eq "")) {
+	print "Usage: proteus2adium [--aim]|[--icq]|[--msn] <your screen name>\n";
+	exit;
+}
 
 my @messages;
 
@@ -41,7 +61,8 @@ my @messages;
 # I'm not actually sure what type and URL are for.
 # Type seems to always be 1, and URL seems to always be empty.
 
-my $query = "select substr(date, 0, 10), identifier, \'<div class=\\\"\' || case when incoming=1 then \'receive\' else \'send\' end || \'\\\"><span class=\\\"timestamp\\\">\' || substr(date, 12, 8) || \'</span><span class=\\\"sender\\\">\' || case when incoming = 1 then identifier else \'$aim_user\' end || \': </span><pre class=\\\"message\\\">\' || message || \'</pre></div>\' from \\\"aim-oscar\\\"";
+
+my $query = "select substr(date, 0, 10), identifier, \'<div class=\\\"\' || case when incoming=1 then \'receive\' else \'send\' end || \'\\\"><span class=\\\"timestamp\\\">\' || substr(date, 12, 8) || \'</span><span class=\\\"sender\\\">\' || case when incoming = 1 then identifier else \'$user_name\' end || \': </span><pre class=\\\"message\\\">\' || message || \'</pre></div>\' from \\\"$service_type\\\"";
 
 open( QUERYPIPE, './sqlite "' . $proteus_log_file . '" "' . $query . '" |' ) or die "Could not open the pipe: $!";
 
