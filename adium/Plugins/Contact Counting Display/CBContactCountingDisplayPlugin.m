3ae447cde7e6c1d439b7dff260ea9a6a8ce775c0
ramoth4 <ramoth4@localhost>
2004-02-08 08:37:01 +0000

    Some commented out code to add a menu item to the view menu to turn on and off contact counting. There doesn't appear to be a way to access the view menu in AIMenuController, though =)

--- adium/Plugins/Contact Counting Display/CBContactCountingDisplayPlugin.m
+++ adium/Plugins/Contact Counting Display/CBContactCountingDisplayPlugin.m
@@ -11,6 +11,9 @@
 
 #define CONTACT_COUNTING_DISPLAY_DEFAULT_PREFS @"ContactCountingDisplayDefaults"
 
+#define VISIBLE_COUNTING_MENU_ITEM_TITLE @"Count Visible Contacts"
+#define ALL_COUNTING_MENU_ITEM_TITLE @"Count All Contacts"
+
 @implementation CBContactCountingDisplayPlugin
 
 - (void)installPlugin
@@ -18,25 +21,46 @@
     allCount = YES;
     visibleCount = YES;
     
-	[self preferencesChanged:nil];
-	
-	[[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:CONTACT_COUNTING_DISPLAY_DEFAULT_PREFS forClass:[self class]] forGroup:PREF_GROUP_CONTACT_LIST];
+    //set up the prefs
+    [self preferencesChanged:nil];
+    
+    //register our prefs
+    [[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:CONTACT_COUNTING_DISPLAY_DEFAULT_PREFS forClass:[self class]] forGroup:PREF_GROUP_CONTACT_LIST];
     prefs = [[CBContactCountingDisplayPreferences contactCountingDisplayPreferences] retain];
-
+    
+    /*
+    //init our menu items
+    visibleCountingMenuItem = [[NSMenuItem alloc] initWithTitle:VISIBLE_COUNTING_MENU_ITEM_TITLE target:self action:@selector(toggleMenuItem:) keyEquivalent:@""];
+    allCountingMenuItem     = [[NSMenuItem alloc] initWithTitle:ALL_COUNTING_MENU_ITEM_TITLE target:self action:@selector(toggleMenuItem:) keyEquivalent:@""];
+    */
+    
+    //install our menu items
+    /* there appears to be no way to install into the view menu at this time...*/
+    /* I figure it's best to talk this over with Adam before I do anything. */
+    
     //install our observers
     [[adium contactController] registerListObjectObserver:self];
-	[[adium notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
+    [[adium notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
 }
 
 - (void)preferencesChanged:(NSNotification *)notification
 {
     if(notification == nil || [(NSString *)[[notification userInfo] objectForKey:@"Group"] compare:PREF_GROUP_CONTACT_LIST] == 0)
-	{
-		allCount = [[[adium preferenceController] preferenceForKey:KEY_COUNT_ALL_CONTACTS group:PREF_GROUP_CONTACT_LIST] boolValue];
+    {
+            allCount = [[[adium preferenceController] preferenceForKey:KEY_COUNT_ALL_CONTACTS group:PREF_GROUP_CONTACT_LIST] boolValue];
         visibleCount = [[[adium preferenceController] preferenceForKey:KEY_COUNT_VISIBLE_CONTACTS group:PREF_GROUP_CONTACT_LIST] boolValue];
-		
-		//Refresh all
-		[[adium contactController] updateAllListObjectsForObserver:self];
+            /*
+            if(allCount != [allCountingMenuItem state])
+            {
+                [allCountingMenuItem setState:allCount];
+            }
+            if(visibleCount != [visibleCountingMenuItem state])
+            {
+                [allCountingMenuItem setState:visibleCount];
+            }
+            */
+        //Refresh all
+        [[adium contactController] updateAllListObjectsForObserver:self];
     }
 }
 
@@ -63,11 +87,22 @@
 	
     return(modifiedAttributes);
 }
-
+/*
+- (void)toggleMenuItem:(id)sender
+{
+    if(sender == allCountingMenuItem || sender == visibleCountingMenuItem)
+    {
+        [sender setState:[sender state] & NSOnState];
+        [adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+                                            forKey:(sender == allCountingMenuItem ? KEY_COUNT_ALL_CONTACTS : KEY_COUNT_VISIBLE_CONTACTS)
+                                             group:PREF_GROUP_CONTACT_LIST];
+    }
+}
+*/
 - (void)uninstallPlugin
 {
-	[prefs release];
-	
+    [prefs release];
+        
     //we are no longer an observer
     [[adium notificationCenter] removeObserver:self];
     [[adium contactController] unregisterListObjectObserver:self];
