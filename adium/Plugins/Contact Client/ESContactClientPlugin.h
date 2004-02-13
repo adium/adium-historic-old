0f68f8e8b514f7fefa955b70effbd5c84433fbb4
Evan Schoenberg <Evan Schoenberg@localhost>
2004-02-13 06:16:52 +0000

    Added the client type to the tooltips (AIM Mobile or Hiptop, for example).

---
+++ adium/Plugins/Contact Client/ESContactClientPlugin.h
@@ -0,0 +1,13 @@
+//
+//  ESContactClientPlugin.h
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Fri Feb 13 2004.
+
+@protocol AIContactListTooltipEntry;
+
+@interface ESContactClientPlugin : AIPlugin <AIContactListTooltipEntry> {
+	
+}
+
+@end
---
+++ adium/Plugins/Contact Client/ESContactClientPlugin.m
@@ -0,0 +1,41 @@
+//
+//  ESContactClientPlugin.m
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Fri Feb 13 2004.
+//
+
+#import "ESContactClientPlugin.h"
+
+@implementation ESContactClientPlugin
+
+- (void)installPlugin
+{
+    //Install our tooltip entry
+    [[adium interfaceController] registerContactListTooltipEntry:self secondaryEntry:YES];
+}
+
+//Tooltip entry ---------------------------------------------------------------------------------------
+- (NSString *)labelForObject:(AIListObject *)inObject
+{
+    return(@"Client");
+}
+
+- (NSAttributedString *)entryForObject:(AIListObject *)inObject
+{
+    NSString			*client;
+    NSAttributedString	*entry = nil;
+	
+    //Get the away state
+    client = [[inObject statusArrayForKey:@"Client"] objectAtIndex:0];
+    
+    //Return the correct string
+    if(client){
+		entry = [[NSAttributedString alloc] initWithString:client];
+    }
+	
+    return([entry autorelease]);
+}
+
+
+@end
