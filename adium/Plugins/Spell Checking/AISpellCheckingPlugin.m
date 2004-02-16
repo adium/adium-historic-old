02e3ce04985308a263438fab3036f37105adffc5
adamiser <adamiser@localhost>
2004-02-16 17:07:52 +0000

    Spell checking can be disabled in the edit menu.  This preference is redundant and not needed.

--- adium/Plugins/Spell Checking/AISpellCheckingPlugin.m
+++ adium/Plugins/Spell Checking/AISpellCheckingPlugin.m
@@ -19,8 +19,8 @@
 - (void)installPlugin
 {
     //Setup our preferences
-    [[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:SPELL_CHECKING_DEFAULT_PREFS forClass:[self class]] forGroup:PREF_GROUP_SPELLING];
-    preferences = [[AISpellCheckingPreferences preferencePane] retain];
+//    [[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:SPELL_CHECKING_DEFAULT_PREFS forClass:[self class]] forGroup:PREF_GROUP_SPELLING];
+//    preferences = [[AISpellCheckingPreferences preferencePane] retain];
 
     //Register as a text entry filter
     [[adium contentController] registerTextEntryFilter:self];
