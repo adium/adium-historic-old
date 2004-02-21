41587dd2e9eb76f5d951d0f356a2a752f78c98f9
hanji <hanji@localhost>
2004-02-21 23:41:37 +0000

    Added a gaim log importer. Use it with ./gaim2adium.pl --adiumUser=<Adium User to import logs to> It should automatically grab all gaim logs and import them to the appropriate Adium user. At the moment it supports AIM and Yahoo! logs, and adding MSN and whatnot should be easy, I just need to know the ID under which gaim stores their logs (~/.gaim/logs/<PROTO-ID>/). The script will prompt before overwriting any existing logs, so it ought be pretty safe, although I haven't tested it with anyone except myself. It will create the requested Adium user if it doesn't already exist.

---
+++ adium/Utilities/gaim2adium.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use File::Find;
+use File::Copy;
+use Getopt::Long;
+
+my $inDir = undef;
+my $outDir = undef ;
+my $adiumUser = undef;
+my $force = 0;
+
+my %Protocols = (		#Map gaim protocol IDs to Adium ones. This is incomplete
+				"aim"	=>	"AIM",
+				"yahoo"	=>	"Yahoo!"
+				#Add the rest here, someone who uses other protocols!
+				);
+
+sub usage
+{
+	print	"Usage: gaim2adium.pl (--adiumUser <user> | --outDir <output dir>) [--inDir <input dir>] [--yes]\n";
+	print	"Options: (defaults)\n\n";
+	print	"\tadiumUser:\t\tAdium user to import logs to\n";
+	print	"\tinDir:\t\t\tDirectory to import logs from (~/.gaim/logs)\n";
+	print	"\toutDir:\t\t\tDirectory to import logs to (Adium log directory for adiumUser)\n";
+	print	"\tyes:\t\t\tDon't prompt before overwriting existing logs\n";
+	exit(1);
+}
+
+sub process_log
+{
+	-f or return;
+	if($File::Find::name =~ m!^$inDir/(.*?)/(.*?)/(.*?)/(\d{4})-(\d{2})-(\d{2})\.\d+\.(html|txt)!)
+	{
+		my ($proto,$acct,$contact,$year,$month,$day,$ext) = ($1,$2,$3,$4,$5,$6,$7);
+		return unless defined ($proto = $Protocols{lc $proto});
+		my $outFN = "$contact ($year|$month|$day).";
+		$outFN .= ((lc $ext) eq "html") ? "html" : "adiumLog";
+		mkdir("$outDir/$proto.$acct");
+		mkdir("$outDir/$proto.$acct/$contact");
+		my $file = "$outDir/$proto.$acct/$contact/$outFN";
+		if(-e $file && !$force)
+		{
+			print "$adiumUser already has a log from $proto.$acct to $contact on $day/$month/$year. Overwrite[Y/n/a]?";
+			my $line = <>;
+			if(lc substr($line,0,1) eq "a")
+			{
+				$force = 1;
+			}
+			elsif(lc substr($line,0,1) ne "y")
+			{
+				return;
+			}
+		}
+		copy($File::Find::name,$file);
+	}
+}
+
+
+GetOptions(	"adiumUser=s"	=>	\$adiumUser,
+			"inDir=s"		=>	\$inDir,
+			"outDir=s"		=>	\$outDir,
+			"yes"			=>	\$force)
+	or usage();
+			
+usage() unless defined($adiumUser) || defined($outDir);
+
+$outDir ||= "$ENV{HOME}/Library/Application Support/Adium 2.0/Users/$adiumUser/Logs";
+$inDir ||= "$ENV{HOME}/.gaim/logs";
+
+mkdir($outDir) unless -e $outDir;
+die("Output dir must be a directory") unless -d $outDir;
+die("Output dir must be writeable") unless -w $outDir;
+
+die("Bad input directory") unless -d $inDir && -r $inDir;
+
+find(\&process_log,$inDir);
+
