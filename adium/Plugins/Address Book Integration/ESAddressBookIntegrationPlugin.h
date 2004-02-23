0cc0c2ebc040e7095e7da0d4e387b69c7bfda5d1
dchoby98 <dchoby98@localhost>
2004-02-23 05:36:39 +0000

    Revamped the Address Book Integration prefs. Added a checkbox to enable or disable importing names, reworded everything, remvoed the bloody tabs.

--- adium/Plugins/Address Book Integration/ESAddressBookIntegrationPlugin.h
+++ adium/Plugins/Address Book Integration/ESAddressBookIntegrationPlugin.h
@@ -9,6 +9,7 @@
 #import "ESAddressBookIntegrationAdvancedPreferences.h"
 
 #define PREF_GROUP_ADDRESSBOOK  @"Address Book"
+#define KEY_AB_ENABLE_IMPORT	@"AB Enable Import"
 #define KEY_AB_DISPLAYFORMAT    @"AB Display Format"
 #define KEY_AB_IMAGE_SYNC       @"AB Image Sync"
 #define KEY_AB_ENABLE_IMAGES    @"AB Enable Images"
@@ -33,6 +34,7 @@ typedef enum {
     int                 meTag;
     
     int                 displayFormat;
+	bool				enableImport;
     bool                useNickName;
     bool                automaticSync;
 }
