9e854e1322a2a2293520099e28369ec473c51e3e
dchoby98 <dchoby98@localhost>
2004-02-24 07:09:25 +0000

    Moved the speech preferences to the main Messages panel, Display tab. Anyone think of a better place for it?

--- adium/Plugins/Announcer/ESAnnouncerPreferences.m
+++ adium/Plugins/Announcer/ESAnnouncerPreferences.m
@@ -13,7 +13,7 @@
 
 //Preference pane properties
 - (PREFERENCE_CATEGORY)category{
-    return(AIPref_Advanced_Messages);
+    return(AIPref_Messages_Display);
 }
 - (NSString *)label{
     return(@"Speak Messages");
