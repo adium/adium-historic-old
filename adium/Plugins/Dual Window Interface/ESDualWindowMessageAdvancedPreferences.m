53393b1fe0a54ddb35812079957c9e28668d06f0
dchoby98 <dchoby98@localhost>
2004-02-25 04:14:52 +0000

    Removed "Create new messages in tabs" in Advanced Message prefs, since it is already in the main message prefs. Reworded some prefs.

--- adium/Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
+++ adium/Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
@@ -39,12 +39,7 @@
 //Called in response to all preference controls, applies new settings
 - (IBAction)changePreference:(id)sender
 {
-    if(sender == createMessages_inTabs){
-        [[adium preferenceController] setPreference:[NSNumber numberWithBool:![sender state]]
-                                             forKey:KEY_ALWAYS_CREATE_NEW_WINDOWS
-                                              group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
-
-    }else if(sender == createTabs_inLastWindow){
+    if(sender == createTabs_inLastWindow){
         [[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
                                              forKey:KEY_USE_LAST_WINDOW
                                               group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
@@ -68,7 +63,6 @@
 {
     NSDictionary	*preferenceDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
 
-    [createMessages_inTabs setState:![[preferenceDict objectForKey:KEY_ALWAYS_CREATE_NEW_WINDOWS] boolValue]];
     [createTabs_inLastWindow setState:[[preferenceDict objectForKey:KEY_USE_LAST_WINDOW] boolValue]];
     [autohide_tabBar setState:![[preferenceDict objectForKey:KEY_AUTOHIDE_TABBAR] boolValue]];
     [checkBox_allowInactiveClosing setState:[[preferenceDict objectForKey:KEY_ENABLE_INACTIVE_TAB_CLOSE] boolValue]];
@@ -79,7 +73,9 @@
 //Enable/disable controls that are available/unavailable
 - (void)configureControlDimming
 {
-    [createTabs_inLastWindow setEnabled:([createMessages_inTabs state] == NSOnState)];
+	NSDictionary	*preferenceDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+	BOOL			newWindows = [[preferenceDict objectForKey:KEY_ALWAYS_CREATE_NEW_WINDOWS] boolValue];
+    [createTabs_inLastWindow setEnabled:!newWindows];
 }
 
 @end
