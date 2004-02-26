254d8dd7fe370fd97367ada3a956618e6c75fc10
adamiser <adamiser@localhost>
2004-02-26 14:07:03 +0000

    - Added a 'Show offline contacts' menu item! :) - Empty groups are no longer always visible

--- adium/Plugins/Offline Contact Hiding/AIOfflineContactHidingPlugin.m
+++ adium/Plugins/Offline Contact Hiding/AIOfflineContactHidingPlugin.m
@@ -15,38 +15,86 @@
 
 #import "AIOfflineContactHidingPlugin.h"
 
+#define	PREF_GROUP_CONTACT_LIST_DISPLAY @"Contact List Display"
+#define SHOW_OFFLINE_MENU_TITLE 		@"Show Offline Contacts"
+#define KEY_SHOW_OFFLINE_CONTACTS 		@"Show Offline Contacts"
+
+@interface AIOfflineContactHidingPlugin (PRIVATE)
+- (void)preferencesChanged:(NSNotification *)notification;
+@end
+
 @implementation AIOfflineContactHidingPlugin
 
+//Install
 - (void)installPlugin
 {
+	//Show offline contacts menu item
+    showOfflineMenuItem = [[NSMenuItem alloc] initWithTitle:SHOW_OFFLINE_MENU_TITLE
+													 target:self
+													 action:@selector(toggleOfflineContactsMenu:)
+											  keyEquivalent:@""];
+	[[adium menuController] addMenuItem:showOfflineMenuItem toLocation:LOC_View_General];		
+
+	//Observe contact and preference changes
     [[adium contactController] registerListObjectObserver:self];
+    [[adium notificationCenter] addObserver:self selector:@selector(preferencesChanged:) name:Preference_GroupChanged object:nil];
+    [self preferencesChanged:nil];
 }
 
+//Uninstall
 - (void)uninstallPlugin
 {
+	[showOfflineMenuItem release];
+    [[adium contactController] unregisterListObjectObserver:self];
     [[adium contactController] unregisterListObjectObserver:self];
 }
 
+//Toggle the display of offline contacts (call from menu)
+- (IBAction)toggleOfflineContactsMenu:(id)sender
+{
+	[sender setState:![sender state]];
+	[[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+										 forKey:KEY_SHOW_OFFLINE_CONTACTS
+										  group:PREF_GROUP_CONTACT_LIST_DISPLAY];
+}
+
+//Our preferences have changed
+- (void)preferencesChanged:(NSNotification *)notification
+{
+    if(notification == nil || [(NSString *)[[notification userInfo] objectForKey:@"Group"] compare:PREF_GROUP_CONTACT_LIST_DISPLAY] == 0){
+		BOOL showOffline = [[[adium preferenceController] preferenceForKey:KEY_SHOW_OFFLINE_CONTACTS
+																	 group:PREF_GROUP_CONTACT_LIST_DISPLAY] boolValue];
+		if(showOffline != [showOfflineMenuItem state]){
+			[showOfflineMenuItem setState:showOffline];
+		}
+		
+		//Refresh visibility of all contacts
+		[[adium contactController] updateAllListObjectsForObserver:self];
+    }
+}
+
+//Update visibility of a list object
 - (NSArray *)updateListObject:(AIListObject *)inObject keys:(NSArray *)inModifiedKeys silent:(BOOL)silent
 {    
     if(inModifiedKeys == nil ||
 	   [inModifiedKeys containsObject:@"Online"] ||
 	   [inModifiedKeys containsObject:@"Signed Off"] ||
 	   [inModifiedKeys containsObject:@"VisibleObjectCount"]){
+
+		BOOL	showOffline = [[inObject preferenceForKey:KEY_SHOW_OFFLINE_CONTACTS
+													group:PREF_GROUP_CONTACT_LIST_DISPLAY] boolValue];
+		
 		if([inObject isKindOfClass:[AIListContact class]]){
 			int		online = [inObject integerStatusObjectForKey:@"Online"];
 			int		justSignedOff = [inObject integerStatusObjectForKey:@"Signed Off"];
 			
-			//Insert an updated value
-			if(!online && !justSignedOff){
-				[inObject setVisible:NO]; //Hidden
-			}else{
-				[inObject setVisible:YES]; //Visible
-			}
+			[inObject setVisible:(showOffline || online || justSignedOff)];
+			
 		}else if([inObject isKindOfClass:[AIListGroup class]]){
 			int visibleCount = [(AIListGroup *)inObject visibleCount];
 			
-			[inObject setVisible:/*(visibleCount > 0)*/YES];
+			[inObject setVisible:(showOffline || visibleCount > 0)];
+			
 		}
 	}
 	
