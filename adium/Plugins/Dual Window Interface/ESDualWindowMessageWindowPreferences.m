db25ad4cf8d9e8242234723d863e6db13c92ae18
dchoby98 <dchoby98@localhost>
2004-02-26 03:22:37 +0000

    Reinstated the "Open new tabs in last used window" and moved it to the main message prefs where it really belongs

--- adium/Plugins/Dual Window Interface/ESDualWindowMessageWindowPreferences.m
+++ adium/Plugins/Dual Window Interface/ESDualWindowMessageWindowPreferences.m
@@ -16,7 +16,7 @@
     return(AIPref_Messages_Sending);
 }
 - (NSString *)label{
-    return(@"E");
+    return(@"M");
 }
 - (NSString *)nibName{
     return(@"DualWindowMessageWindowPrefs");
@@ -29,7 +29,10 @@
         [[adium preferenceController] setPreference:[NSNumber numberWithBool:([createMessages_inTabs state]==NSOffState)]
                                              forKey:KEY_ALWAYS_CREATE_NEW_WINDOWS
                                               group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
-
+	} else if(sender == createTabs_inLastWindow){
+		[[adium preferenceController] setPreference:[NSNumber numberWithBool:([createTabs_inLastWindow state]==NSOnState)]
+                                             forKey:KEY_USE_LAST_WINDOW
+                                              group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
     }
 }
 
@@ -39,6 +42,7 @@
     NSDictionary	*preferenceDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
 
     [createMessages_inTabs setState:![[preferenceDict objectForKey:KEY_ALWAYS_CREATE_NEW_WINDOWS] boolValue]];
+	[createTabs_inLastWindow setState:[[preferenceDict objectForKey:KEY_USE_LAST_WINDOW] boolValue]];
 }
 
 @end
