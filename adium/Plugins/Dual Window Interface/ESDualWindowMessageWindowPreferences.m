95e8b37970d93513d25d7ba9e4a6a1e811cf57b9
dchoby98 <dchoby98@localhost>
2004-02-26 03:31:45 +0000

    Corrected a logic error in the message window prefs. Rearranged and prettied up a few preferences. Nothing crazy.

--- adium/Plugins/Dual Window Interface/ESDualWindowMessageWindowPreferences.m
+++ adium/Plugins/Dual Window Interface/ESDualWindowMessageWindowPreferences.m
@@ -29,6 +29,7 @@
         [[adium preferenceController] setPreference:[NSNumber numberWithBool:([createMessages_inTabs state]==NSOffState)]
                                              forKey:KEY_ALWAYS_CREATE_NEW_WINDOWS
                                               group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+		[self configureControlDimming];
 	} else if(sender == createTabs_inLastWindow){
 		[[adium preferenceController] setPreference:[NSNumber numberWithBool:([createTabs_inLastWindow state]==NSOnState)]
                                              forKey:KEY_USE_LAST_WINDOW
@@ -36,6 +37,15 @@
     }
 }
 
+//Dim controls as needed
+- (void)configureControlDimming
+{
+	if( [createMessages_inTabs state] == NSOnState ) {
+		[createTabs_inLastWindow setEnabled:YES];
+	} else {
+		[createTabs_inLastWindow setEnabled:NO];
+	}
+}
 //Configure the preference view
 - (void)viewDidLoad
 {
@@ -43,6 +53,8 @@
 
     [createMessages_inTabs setState:![[preferenceDict objectForKey:KEY_ALWAYS_CREATE_NEW_WINDOWS] boolValue]];
 	[createTabs_inLastWindow setState:[[preferenceDict objectForKey:KEY_USE_LAST_WINDOW] boolValue]];
+
+	[self configureControlDimming];
 }
 
 @end
