dc72e92b26432f7c1e8f54118a2bc5ea7e6d8c5d
adamiser <adamiser@localhost>
2004-02-27 19:47:15 +0000

    ... one view to rule them

---
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.h
@@ -0,0 +1,16 @@
+//
+//  AIWebKitMessageViewPlugin.h
+//  Adium XCode
+//
+//  Created by Adam Iser on Fri Feb 27 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AIWebKitMessageViewPlugin : NSObject {
+
+}
+
+@end
