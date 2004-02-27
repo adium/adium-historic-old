7a145b4a56113ba6724d62d5bd61052a5fdd70de
adamiser <adamiser@localhost>
2004-02-27 20:05:24 +0000

    Additional target info and setup

--- adium/Plugins/WebKit Message View/AIWebKitMessageViewController.m
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewController.m
@@ -9,6 +9,38 @@
 #import "AIWebKitMessageViewController.h"
 
 
+@interface AIWebKitMessageViewController (PRIVATE)
+- (id)initForChat:(AIChat *)inChat;
+@end
+
 @implementation AIWebKitMessageViewController
 
+//Create a new message view
++ (AISMViewController *)messageViewControllerForChat:(AIChat *)inChat
+{
+    return([[[self alloc] initForChat:inChat] autorelease]);
+}
+
+//Init
+- (id)initForChat:(AIChat *)inChat
+{
+    //init
+    [super init];
+    
+	
+    return(self);
+}
+
+- (NSView *)messageView
+{
+	return(nil);
+}
+
+//Dealloc
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+	
 @end
--- adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.h
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.h
@@ -1,15 +1,6 @@
-//
-//  AIWebKitMessageViewPlugin.h
-//  Adium XCode
-//
-//  Created by Adam Iser on Fri Feb 27 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
 
 
-@interface AIWebKitMessageViewPlugin : NSObject {
+@interface AIWebKitMessageViewPlugin : AIPlugin <AIMessageViewPlugin> {
 
 }
 
