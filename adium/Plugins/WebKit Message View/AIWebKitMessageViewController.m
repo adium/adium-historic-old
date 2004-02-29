3eeadf174714241aa8be9da2d7d4a2ffcc5e5af5
adamiser <adamiser@localhost>
2004-02-29 05:38:15 +0000

    Simplified the webkit view templates (Thanks to a great idea from xer! :) )

--- adium/Plugins/WebKit Message View/AIWebKitMessageViewController.m
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewController.m
@@ -10,7 +10,10 @@
 
 @interface AIWebKitMessageViewController (PRIVATE)
 - (id)initForChat:(AIChat *)inChat;
-- (void)appendChatChild:(NSString *)contentString;
+- (void)_addContentMessage:(AIContentMessage *)content similar:(BOOL)contentIsSimilar;
+- (void)_addContentStatus:(AIContentStatus *)content similar:(BOOL)contentIsSimilar;
+- (NSMutableString *)fillKeywords:(NSMutableString *)inString forContent:(AIContentObject *)content;
+- (NSMutableString *)escapeString:(NSMutableString *)inString;
 @end
 
 @implementation AIWebKitMessageViewController
@@ -50,12 +53,7 @@
 	templateHTML = [NSString stringWithFormat:templateHTML, basePath, mainCSS, variantCSS, headerHTML, footerHTML];
 
 	//Feed it to the webview
-	NSLog(@"%@",templateHTML);
-	[[webView mainFrame] loadHTMLString:templateHTML
-								baseURL:nil];
-	
-	
-	
+	[[webView mainFrame] loadHTMLString:templateHTML baseURL:nil];
 	
 	
 	
@@ -67,57 +65,7 @@
 	timeStampFormat = [[prefDict objectForKey:KEY_SMV_TIME_STAMP_FORMAT] retain];
 	timeStampFormatter = [[NSDateFormatter alloc] initWithDateFormat:timeStampFormat allowNaturalLanguage:NO];
 	
-	
-	
-	
-	
-	
-	//Set it up with the javascript appender template
-//	NSString	*path = [[NSBundle bundleForClass:[self class]] pathForResource:@"template" ofType:@"xml"];
-//	NSMutableString	*template = [[NSString stringWithContentsOfFile:path] mutableCopy];
-//
-//	//Stuff in the style information
-//	NSRange	range = [template rangeOfString:@"%styles"];
-//	
-//	NSString *styleInfo = [NSString stringWithFormat:@"<?xml-stylesheet type=\"text/css\" href=\"file://%@/%@\"?>",[path stringByDeletingLastPathComponent],@"testlayout.css"];	
-////	template = [styleInfo stringByAppendingString:template];
-	
-//	[template replaceCharactersInRange:range withString:styleInfo];
-//	NSLog(@"template:%@",template);
-//	
-//	
-//	
-//	[[webView mainFrame] loadData:[template dataUsingEncoding:NSUTF8StringEncoding]
-//						 MIMEType:@"text/xml"
-//				 textEncodingName:@"utf-8"
-//						  baseURL:nil/*[NSURL URLWithString:@"file:///Users/adamiser/Code/adium/Plugins/WebKit Message View"]*/];
-//		
-//		[NSURL fileURLWithPath:[path stringByDeletingLastPathComponent]]];
-//[NSURL fileURLWithPath:[path stringByDeletingLastPathComponent]]
-	
-	
-	
-//	[[webView mainFrame] loadHTMLString:template
-//								baseURL:nil/*[NSURL fileURLWithPath:[path stringByDeletingLastPathComponent]]*/];
-//
-	
-	
-	
-	
-	
-//	NSMutableString	*base = [@"hi" mutableCopy];;//[[NSString stringWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"template" ofType:@"xml"]] mutableCopy];
-//
-//	if(base && [base length]){
-//		//We need to escape a few things to get our string to the javascript without trouble
-//		[base replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:NSLiteralSearch range:NSMakeRange(0,[base length])];
-//		[base replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:NSLiteralSearch range:NSMakeRange(0,[base length])];
-//		[base replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:NSMakeRange(0,[base length])];
-//
-//		//Now, feed the message to our javascript, which will append it to the bottom of the webview
-//		[webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"documentAppend(\"%@\");", base]];
-//	}
-	
-	
+
     return(self);
 }
 
@@ -142,83 +90,25 @@
 		contentIsSimilar = YES;
 	}
 	
-	
-	
+	//
     if([[content type] compare:CONTENT_MESSAGE_TYPE] == 0){
         [self _addContentMessage:(AIContentMessage *)content similar:contentIsSimilar];
     }else if([[content type] compare:CONTENT_STATUS_TYPE] == 0){
         [self _addContentStatus:(AIContentStatus *)content similar:contentIsSimilar];
     }
 	
+	//
 	[previousContent release];
 	previousContent = [content retain];
-	
-	
-		
-//	
-//	
-//	
-//	NSMutableString		*contentString = [[[content message] string] mutableCopy];
-//
-//	NSLog(@"%@",webView);
-//	NSLog(@"%@",[webView superview]);
-//	NSLog(@"%@",[webView window]);
-//	
-//	NSString *child =
-//	[NSString stringWithFormat:@"<message class=\"%@\"><name>%@</name><img class=\"usericon\" src=\"%@\" /><timestamp>%@</timestamp><body>%@</body></message>",
-//				@"next",[[content source] displayName], @"http://homepage.mac.com/eevyl/eevylface2004.jpg", @"2:33", contentString];
-//		
-////	NSLog(@"%@",child);
-//	
-//	[self appendChatChild:child];
-//
-	
-	
-	
-	
-	
-//	[webView stringByEvaluatingJavaScriptFromString:@"documentAppend();"];
-	
-//	AIContentObject		*content = [[notification userInfo] objectForKey:@"Object"];
-//	NSMutableString		*contentString = [[[content message] string] mutableCopy];
-//	
-//	/*
-//	 New sender?
-//	   In?
-//	     Append In Top
-//	     Append In First Messasge
-//	     Append In Bottom
-//	   Out?
-//    	 Append Out Top
-//	 	 Append Out First Messasge
-//	 	 Append Out Bottom
-//	 Same sender?
-//	 	In?
-//	 		Append In Message above bottom
-//	 	Out?
-//			Append Out Message above bottom
-//	 */
-//	
-//	
-//	if(contentString && [contentString length]){
-//		//We need to escape a few things to get our string to the javascript without trouble
-//		[contentString replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-//		[contentString replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-//		[contentString replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-//
-//		//Now, feed the message to our javascript, which will append it to the bottom of the webview
-//		[webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"documentAppend(\"%@\");", contentString]];
-//	}
 }
 
 
 - (void)_addContentMessage:(AIContentMessage *)content similar:(BOOL)contentIsSimilar
 {
+	NSString		*stylePath = [[[NSBundle bundleForClass:[self class]] pathForResource:@"template" ofType:@"html"] stringByDeletingLastPathComponent];
 	NSMutableString	*newHTML;
 	
-
-	NSString	*stylePath = [[[NSBundle bundleForClass:[self class]] pathForResource:@"template" ofType:@"html"] stringByDeletingLastPathComponent];
-	
+	//
 	if([content isOutgoing]){
 		stylePath = [stylePath stringByAppendingPathComponent:@"Outgoing"];
 	}else{
@@ -226,43 +116,27 @@
 	}
 	
 	//
-	NSString	*headerTemplate = [NSString stringWithContentsOfFile:[stylePath stringByAppendingPathComponent:@"Header.html"]];
-	NSString	*footerTemplate = [NSString stringWithContentsOfFile:[stylePath stringByAppendingPathComponent:@"Footer.html"]];
-	NSString	*firstContentTemplate = [NSString stringWithContentsOfFile:[stylePath stringByAppendingPathComponent:@"FirstContent.html"]];
+	NSString	*contentTemplate = [NSString stringWithContentsOfFile:[stylePath stringByAppendingPathComponent:@"Content.html"]];
 	NSString	*nextContentTemplate = [NSString stringWithContentsOfFile:[stylePath stringByAppendingPathComponent:@"NextContent.html"]];
 	
-	//New Bubble
-	if(contentIsSimilar){
-		newHTML = [NSMutableString stringWithFormat:@"%@", nextContentTemplate];
+	//
+	if(!contentIsSimilar){
+		newHTML = [[contentTemplate mutableCopy] autorelease];
+		newHTML = [self fillKeywords:newHTML forContent:content];
+		newHTML = [self escapeString:newHTML];
+
+		[webView stringByEvaluatingJavaScriptFromString:
+			[NSString stringWithFormat:@"checkIfScrollToBottomIsNeeded(); appendMessage(\"%@\"); scrollToBottomIfNeeded();", newHTML]];
+		
 	}else{
-		newHTML = [NSMutableString stringWithFormat:@"%@%@%@", headerTemplate, firstContentTemplate, footerTemplate];
+		newHTML = [[nextContentTemplate mutableCopy] autorelease];
+		newHTML = [self fillKeywords:newHTML forContent:content];
+		newHTML = [self escapeString:newHTML];
+		
+		[webView stringByEvaluatingJavaScriptFromString:
+			[NSString stringWithFormat:@"checkIfScrollToBottomIsNeeded(); appendNextMessage(\"%@\"); scrollToBottomIfNeeded();", newHTML]];
+		
 	}
-	
-	//Fill in info
-	NSRange	range;
-	
-	range = [newHTML rangeOfString:@"%sender"];
-	if(range.location != NSNotFound){
-		[newHTML replaceCharactersInRange:range withString:[[content source] displayName]];
-	}
-	
-	range = [newHTML rangeOfString:@"%message"];
-	if(range.location != NSNotFound){
-		[newHTML replaceCharactersInRange:range withString:[[content message] string]];
-	}
-	
-	range = [newHTML rangeOfString:@"%time"];
-	if(range.location != NSNotFound){
-		[newHTML replaceCharactersInRange:range withString:[timeStampFormatter stringForObjectValue:[(AIContentMessage *)content date]]];
-	}
-	
-	if(contentIsSimilar){
-		[self appendMessageChild:newHTML];
-	}else{
-		[self appendChatChild:newHTML];
-	}
-	
-	
 }
 
 - (void)_addContentStatus:(AIContentStatus *)content similar:(BOOL)contentIsSimilar
@@ -271,38 +145,38 @@
 	
 }
 
-
-
-
-
-- (void)appendChatChild:(NSString *)inString
+//
+- (NSMutableString *)fillKeywords:(NSMutableString *)inString forContent:(AIContentObject *)content
 {
-	if(inString && [inString length]){
-		NSMutableString	*contentString = [[inString mutableCopy] autorelease];
-		
-		//We need to escape a few things to get our string to the javascript without trouble
-		[contentString replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-		[contentString replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-		[contentString replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-
-		//Now, feed the message to our javascript, which will append it to the bottom of the webview
-		[webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"checkIfScrollToBottomIsNeeded(); documentAppend(\"%@\"); scrollToBottomIfNeeded();", contentString]];
+	NSRange	range;
+	
+	range = [inString rangeOfString:@"%sender"];
+	if(range.location != NSNotFound){
+		[inString replaceCharactersInRange:range withString:[[content source] displayName]];
 	}
+	
+	range = [inString rangeOfString:@"%message"];
+	if(range.location != NSNotFound){
+		[inString replaceCharactersInRange:range withString:[[content message] string]];
+	}
+	
+	range = [inString rangeOfString:@"%time"];
+	if(range.location != NSNotFound){
+		[inString replaceCharactersInRange:range withString:[timeStampFormatter stringForObjectValue:[(AIContentMessage *)content date]]];
+	}
+	
+	return(inString);
 }
 
-- (void)appendMessageChild:(NSString *)inString
+//
+- (NSMutableString *)escapeString:(NSMutableString *)inString
 {
-	if(inString && [inString length]){
-		NSMutableString	*contentString = [[inString mutableCopy] autorelease];
-		
-		//We need to escape a few things to get our string to the javascript without trouble
-		[contentString replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-		[contentString replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
-		[contentString replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:NSMakeRange(0,[contentString length])];
+	//We need to escape a few things to get our string to the javascript without trouble
+	[inString replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:NSLiteralSearch range:NSMakeRange(0,[inString length])];
+	[inString replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:NSLiteralSearch range:NSMakeRange(0,[inString length])];
+	[inString replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:NSMakeRange(0,[inString length])];
 
-		//Now, feed the message to our javascript, which will append it to the bottom of the webview
-		[webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"checkIfScrollToBottomIsNeeded(); documentAppendMessage(\"%@\"); scrollToBottomIfNeeded();", contentString]];
-	}
+	return(inString);
 }
 
 @end
