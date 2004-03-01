5eab1216aba8b697b2a9bbeba3cb6b747e6b778e
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-03-01 17:40:43 +0000

    Added a tooltip plugin for displaying the serverside display name.

---
+++ adium/Plugins/Contact Serverside Display Name/ESContactServersideDisplayName.m
@@ -0,0 +1,42 @@
+//
+//  ESContactServersideDisplayName.m
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Mon Mar 01 2004.
+//
+
+#import "ESContactServersideDisplayName.h"
+
+
+@implementation ESContactServersideDisplayName
+
+- (void)installPlugin
+{
+    //Install our tooltip entry
+    [[adium interfaceController] registerContactListTooltipEntry:self secondaryEntry:YES];
+}
+
+//Tooltip entry ---------------------------------------------------------------------------------------
+- (NSString *)labelForObject:(AIListObject *)inObject
+{
+    return(@"Display Name");
+}
+
+- (NSAttributedString *)entryForObject:(AIListObject *)inObject
+{
+    NSString			*serverDisplayName;
+    NSAttributedString	*entry = nil;
+	
+    //Get the client
+    serverDisplayName = [inObject statusObjectForKey:@"Server Display Name"];
+    
+    //Return the correct string
+    if(serverDisplayName){
+		entry = [[NSAttributedString alloc] initWithString:serverDisplayName];
+    }
+	
+    return([entry autorelease]);
+}
+
+
+@end
