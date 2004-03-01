c179af02f3c0c5d38ff3ea29d1d5f17748509942
Evan Schoenberg <Evan Schoenberg@localhost>
2004-03-01 17:40:43 +0000

    Added a tooltip plugin for displaying the serverside display name.

---
+++ adium/Plugins/Contact Serverside Display Name/ESContactServersideDisplayName.h
@@ -0,0 +1,13 @@
+//
+//  ESContactServersideDisplayName.h
+//  Adium XCode
+//
+//  Created by Evan Schoenberg on Mon Mar 01 2004.
+
+@protocol AIContactListTooltipEntry;
+
+@interface ESContactServersideDisplayName : AIPlugin <AIContactListTooltipEntry> {
+
+}
+
+@end
