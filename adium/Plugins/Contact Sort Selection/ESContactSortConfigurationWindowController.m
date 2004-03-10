cb2fcfe3555f1c3f39ff4116d3c7c6730de77d48
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-03-10 22:51:34 +0000

    Fixed some coding silliness in the contact sort configuration window controller.

--- adium/Plugins/Contact Sort Selection/ESContactSortConfigurationWindowController.m
+++ adium/Plugins/Contact Sort Selection/ESContactSortConfigurationWindowController.m
@@ -7,7 +7,7 @@
 #import "ESContactSortConfigurationWindowController.h"
 
 @interface ESContactSortConfigurationWindowController (PRIVATE)
-- (void)removeAllSubviews:(NSView *)view;
+
 @end
 
 @implementation ESContactSortConfigurationWindowController
@@ -34,35 +34,9 @@
 	
 	//Configure the view
 	NSView  *configureView = [controller configureView];
-	
-	NSRect  windowFrame = [[self window] frame];
-	NSRect  configureViewFrame = [configureView frame];
-	NSRect  view_mainFrame;
-	
-	[self removeAllSubviews:view_main];
-	[view_main addSubview:configureView];
-	
-	[configureView setFrameOrigin:NSMakePoint(0,0)];
-	
-	view_mainFrame.size = configureViewFrame.size;
-	view_mainFrame.origin = NSMakePoint(0,0);
-	[view_main setFrame:configureViewFrame];
-	
-	[[self window] setContentSize:configureViewFrame.size];
-}
 
-//Clear a view of all subviews
-- (void)removeAllSubviews:(NSView *)view
-{
-    NSArray			*subviewsArray = [view subviews];
-    NSEnumerator	*enumerator = [subviewsArray objectEnumerator];
-    NSView			*theSubview;
-
-    while (theSubview = [enumerator nextObject])
-    {
-        [theSubview removeFromSuperviewWithoutNeedingDisplay];
-    }
-    
+	[[self window] setContentView:configureView];
+	[[self window] setContentSize:[configureView frame].size];
 }
 
 @end
