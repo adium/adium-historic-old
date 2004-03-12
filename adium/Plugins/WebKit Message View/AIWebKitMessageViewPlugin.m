4817453b932889e00fea3175a23a450a1e5d9aba
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-03-12 02:52:09 +0000

    Little webkit cleanup; added an empty defaults file.

--- adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.m
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.m
@@ -2,10 +2,26 @@
 #import "AIWebKitMessageViewPlugin.h"
 #import "AIWebKitMessageViewController.h"
 
+#define WEBKIT_DEFAULT_PREFS	@"WebKit Defaults"
+
 @implementation AIWebKitMessageViewPlugin
 
 - (void)installPlugin
 {
+	//Register our default preferences and install our preference view
+    [[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:WEBKIT_DEFAULT_PREFS
+																		forClass:[self class]]
+										  forGroup:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+//    preferences = [[AISMPreferences preferencePane] retain];
+	
+	//Set up a time stamp format based on this user's locale
+    NSString    *format = [[[adium preferenceController] preferencesForGroup:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY] objectForKey:KEY_WEBKIT_TIME_STAMP_FORMAT];
+    if(!format || [format length] == 0){
+        [[adium preferenceController] setPreference:[NSDateFormatter localizedDateFormatStringShowingSeconds:NO showingAMorPM:NO]
+                                             forKey:KEY_WEBKIT_TIME_STAMP_FORMAT
+                                              group:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+    }
+	
     //Register ourself as a message view plugin
     [[adium interfaceController] registerMessageViewPlugin:self];
 }
