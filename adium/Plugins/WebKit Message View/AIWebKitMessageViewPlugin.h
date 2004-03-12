431ad6bb9306f6727e8031b4e335ef1551626488
Evan Schoenberg <Evan Schoenberg@localhost>
2004-03-12 02:53:16 +0000

    Heh, forgot to actually save these two.

--- adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.h
+++ adium/Plugins/WebKit Message View/AIWebKitMessageViewPlugin.h
@@ -1,4 +1,6 @@
 
+#define PREF_GROUP_WEBKIT_MESSAGE_DISPLAY	@"WebKit Message Display"
+#define	KEY_WEBKIT_TIME_STAMP_FORMAT		@"Time Stamp"
 
 @interface AIWebKitMessageViewPlugin : AIPlugin <AIMessageViewPlugin> {
 
