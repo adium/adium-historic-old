75876fd25a2b7ece0d06a23fa6dbc512e66be7df
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-03-15 04:35:14 +0000

    Added gadu-gadu connection strings.

--- adium/Plugins/Gaim Service/ESGaimGaduGaduAccount.m
+++ adium/Plugins/Gaim Service/ESGaimGaduGaduAccount.m
@@ -15,4 +15,29 @@
     return "prpl-gg";
 }
 
+- (NSString *)connectionStringForStep:(int)step
+{
+	switch (step)
+	{
+		case 0:
+			return AILocalizedString(@"Connecting",nil);
+			break;
+		case 1:
+			return AILocalizedString(@"Looking up server",nil);
+			break;
+		case 2:
+			return AILocalizedString(@"Reading data",nil);
+			break;			
+		case 3:
+			return AILocalizedString(@"Balancer handshake",nil);
+			break;
+		case 4:
+			return AILocalizedString(@"Reading server key",nil);
+			break;
+		case 5:
+			return AILocalizedString(@"Exchanging key hash",nil);
+			break;
+	}
+	return nil;
+}
 @end
