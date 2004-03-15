8ab482789ae25efb4898618fb4283857c70aea35
Evan Schoenberg <Evan Schoenberg@localhost>
2004-03-15 04:45:50 +0000

    Added connecting strings for yahoo (such as it is - no real steps to the yahoo connect process), jabber, and trepia

--- adium/Plugins/Gaim Service/ESGaimTrepiaAccount.m
+++ adium/Plugins/Gaim Service/ESGaimTrepiaAccount.m
@@ -15,4 +15,20 @@
     return "prpl-trepia";
 }
 
+- (NSString *)connectionStringForStep:(int)step
+{
+	switch (step)
+	{
+		case 0:
+			return AILocalizedString(@"Connecting",nil);
+			break;
+		case 1:
+			return AILocalizedString(@"Logging in",nil);
+			break;
+		case 2:
+			return AILocalizedString(@"Retrieving buddy list",nil);
+			break;
+	}
+	return nil;
+}
 @end
