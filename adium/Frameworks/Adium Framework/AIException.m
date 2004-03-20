5c00c422fd46b968f11431e34abf22961c435512
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-03-20 20:42:45 +0000

    *quietly fixes the crash reporter to work for assertions again*

--- adium/Frameworks/Adium Framework/AIException.m
+++ adium/Frameworks/Adium Framework/AIException.m
@@ -11,10 +11,10 @@
 #define EXCEPTIONS_PATH		[@"~/Library/Logs/CrashReporter/Adium.exception.log" stringByExpandingTildeInPath]
 
 @implementation AIException
-/* initialize
+/* load
 *   install ourself to handle exceptions
  */
-+ (void)initialize
++ (void)load
 {
     //Anything you can do, I can do better...
     [self poseAsClass: [NSException class]];
@@ -26,7 +26,7 @@
     //Ignore various harmless or unavoidable exceptions the system uses
     if ((![self reason]) ||
 		([[self reason] isEqualToString:@"_sharedInstance is invalid."])) {
-    
+		NSLog(@"ignoring an exception with no reason");
 	    [super raise];
 		
     } else if ((![self name]) || 
@@ -34,14 +34,13 @@
 		  ([[self name] isEqualToString:@"NSPortTimeoutException"]) ||
 		  ([[self name] isEqualToString:@"NSAccessibilityException"]) /*||
 		  ([[self name] isEqualToString:@"NSInternalInconsistencyException"])*/) {
-	
 	    [super raise];
 	
     } else {
         NSDictionary    *dict = [self userInfo];
         NSString        *stackTrace = nil;
         NSMutableString *processedStackTrace = [[[NSMutableString alloc] init] autorelease];
-        
+		
         //Turn the nonsense of memory addresses into a human-readable backtrace complete with line numbers
         if (dict && (stackTrace = [dict objectForKey:NSStackTraceKey])) { 
             NSString *str = [NSString stringWithFormat:@"%s -p %d %@ | tail -n +3 | head -n +%d | %s | cat -n",
