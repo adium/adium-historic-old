77b9cd0d0a496352edc3db998e1bc49ded5d500c
dchoby98 <dchoby98@localhost>
2004-03-24 06:25:16 +0000

    Added the *very* beginnings of a plugin to display the last few lines of conversation you had with a user when you open a new message window. It's disabled and nothing functions yet.

---
+++ adium/Plugins/Message Context Display/DCMessageContextDisplayPlugin.h
@@ -0,0 +1,14 @@
+//
+//  DCMessageContextDisplayPlugin.h
+//  Adium
+//
+//  Created by David Clark on Tuesday, March 23, 2004.
+
+#define KEY_DISPLAY_CONTEXT			@"Display Message Context"
+#define PREF_GROUP_CONTEXT_DISPLAY  @"Message Context Display"
+
+@interface DCMessageContextDisplayPlugin : AIPlugin {
+    
+}
+
+@end
