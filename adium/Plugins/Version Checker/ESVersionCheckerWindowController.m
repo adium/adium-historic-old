749d483189bf68b9af94b5f38ab1e37709e60d37
Evan Schoenberg <Evan Schoenberg@localhost>
2004-03-31 02:45:44 +0000

    Added version checker window controller.  The nib isn't really properly done, but it's about right.

---
+++ adium/Plugins/Version Checker/ESVersionCheckerWindowController.m
@@ -0,0 +1,11 @@
+//
+//  ESVersionCheckerWindowController.m
+//  Adium
+//
+//  Created by Evan Schoenberg on Sun Mar 28 2004.
+
+#import "ESVersionCheckerWindowController.h"
+
+@implementation ESVersionCheckerWindowController
+
+@end
