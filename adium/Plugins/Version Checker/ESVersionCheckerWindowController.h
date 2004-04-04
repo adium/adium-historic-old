3ebba2d6040151c4e47d5f166459f1d511c18a9f
Evan Schoenberg <Evan Schoenberg@localhost>
2004-04-04 09:30:22 +0000

    Version checker window controller, which I somehow missed committing.

--- adium/Plugins/Version Checker/ESVersionCheckerWindowController.h
+++ adium/Plugins/Version Checker/ESVersionCheckerWindowController.h
@@ -2,10 +2,23 @@
 //  ESVersionCheckerWindowController.h
 //  Adium
 //
-//  Created by Evan Schoenberg on Sun Mar 28 2004.
+//  Created by Evan Schoenberg on Mon Mar 29 2004.
 
 @interface ESVersionCheckerWindowController : AIWindowController {
-
+	IBOutlet	NSTabView   *tabView_hidden;
+	
+	IBOutlet	NSTextField *textField_upToDate;
+	IBOutlet	NSButton	*button_upToDate_okay;
+	
+	IBOutlet	NSTextField *textField_updateAvailable;
+	IBOutlet	NSButton	*button_updateAvailable_close;
+	IBOutlet	NSButton	*button_updateAvailable_downloadPage;
 }
+
++ (void)showVersionCheckerWindowWithLatestBuildDate:(NSDate *)latestBuildDate checkingManually:(BOOL)checkingManually;
+- (void)showWindowWithLatestBuildDate:(NSDate *)latestBuildDate checkingManually:(BOOL)checkingManually;
+
+- (IBAction)pressedButton:(id)sender;
+- (IBAction)closeWindow:(id)sender;
 
 @end
