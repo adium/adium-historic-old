075dc0a2d10c1df41e87559ef9ffb5adeffdca08
Evan Schoenberg <Evan Schoenberg@localhost>
2004-04-05 01:55:18 +0000

    Resolve 2 warnings.

--- adium/Plugins/Gaim Service/ESGaimMSNAccount.h
+++ adium/Plugins/Gaim Service/ESGaimMSNAccount.h
@@ -5,12 +5,10 @@
 //  Created by Evan Schoenberg on Sun Dec 28 2003.
 
 #import "CBGaimAccount.h"
-#include "msn.h"
+#include <Libgaim/msn.h>
 
 @interface ESGaimMSNAccount : CBGaimAccount {
 
 }
-
-#warning extern void msn_set_friendly_name(GaimConnection *gc, const char *entry);
 
 @end
