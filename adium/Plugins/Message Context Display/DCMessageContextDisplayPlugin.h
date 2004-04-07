85c171b3816fb0b6d4fa55e62af979b33f0cdcbe
dchoby98 <dchoby98@localhost>
2004-04-07 05:11:41 +0000

    Message History Display!
    When a new conversation is opened, the last few lines of the previous conversation are displayed in the message window. Currently, you can only turn this on and off. More customization and prettiness to come later.
    
    The standard message view displays these history lines as greyed out. The webkit message view doesn't do anything special with them.
    
    Many more changes to come, later...

--- adium/Plugins/Message Context Display/DCMessageContextDisplayPlugin.h
+++ adium/Plugins/Message Context Display/DCMessageContextDisplayPlugin.h
@@ -4,11 +4,24 @@
 //
 //  Created by David Clark on Tuesday, March 23, 2004.
 
-#define KEY_DISPLAY_CONTEXT			@"Display Message Context"
+
 #define PREF_GROUP_CONTEXT_DISPLAY  @"Message Context Display"
+#define KEY_MESSAGE_CONTEXT			@"Message Context"
+#define KEY_DISPLAY_CONTEXT			@"Display Message Context"
+#define KEY_DISPLAY_LINES			@"Lines to Display"
+
+#define CONTEXT_DISPLAY_DEFAULTS	@"MessageContextDisplayDefaults"
+
+@class DCMessageContextDisplayPreferences;
 
 @interface DCMessageContextDisplayPlugin : AIPlugin {
-    
+	
+	BOOL								isObserving;
+	BOOL								shouldDisplay;
+	int									linesToDisplay;
+	DCMessageContextDisplayPreferences  *preferences;
 }
+
+- (NSDictionary *)savableContentObject:(AIContentObject *)content;
 
 @end
