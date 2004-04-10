3ab412455ec111fe4eb1d4636e6d209f26887487
adamiser <adamiser@localhost>
2004-04-10 14:13:09 +0000

    Changes for thumbnails

--- webcvs/htdocs/scripts/actionshot.php
+++ webcvs/htdocs/scripts/actionshot.php
@@ -8,7 +8,7 @@ function getDirList ($dirName) {
             if (is_dir($dirName."/".$entry)) { 
                 getDirList($dirName."/".$entry); 
             } else { 
-            $array[] = $dirName."/".$entry; 
+                $array[] = $entry; 
             } 
         } 
     } 
@@ -18,19 +18,23 @@ function getDirList ($dirName) {
 
 function RandomActionShoot()
 {
+    $thumbsDir = "images/actionthumbs";
     $picturesDir = "images/actionshots";
     $picturesList = getDirList($picturesDir);
     
     $index = rand(0, count($picturesList)-1);
     $picture = $picturesList[$index];
-    list($originalwidth, $originalheight, $type, $attr) = getimagesize($picture);
-    if ($originalwidth > 160) {
-        $proportion = $originalwidth / 160;
-        $width = 160;
-        $height = (int) ceil($originalheight / $proportion);
-    }
+    list($picWidth, $picHeight, $type, $attr) = getimagesize($picturesDir."/".$picture);
+    list($thumbWidth, $thumbHeight, $type, $attr) = getimagesize($thumbsDir."/".$picture);
     
-    return "<a href=\"$picture\" onclick=\"window.open('$picture','popup','width=$originalwidth,height=$originalheight,scrollbars=yes,toolbar=no,status=yes'); return false\"><img src=\"$picture\" width=\"$width\" height=\"$height\" alt=\"actionshot\" /></a>";
+    return "<a href=\"$picturesDir/$picture\" onclick=\"window.open('$picture','popup','width=$picWidth,height=$picHeight,scrollbars=yes,toolbar=no,status=yes'); return false\"><img src=\"$thumbsDir/$picture\" width=\"$thumbWidth\" height=\"$thumbHeight\" alt=\"actionshot\" /></a>";
 }
 
 ?>
+
+
+
+
+
+
+
