624b0d33d75a9e6353e11e388bffb989af585ee4
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-04-11 17:49:37 +0000

    Jabber account options galore! :) Server, port, resource, connect server, proxy options, encryption options (TSL/Old SSL/Plaintext auth).
    Also, efficiency and some small bugfixes (such as select-all delete performing strangely in some fields) in AIM, MSN, and Yahoo connection options.

--- adium/Plugins/Gaim Service/ESGaimJabberAccount.h
+++ adium/Plugins/Gaim Service/ESGaimJabberAccount.h
@@ -6,8 +6,17 @@
 
 #import "CBGaimAccount.h"
 
+#define KEY_JABBER_HOST				@"Jabber:Host"
+#define KEY_JABBER_PORT				@"Jabber:Port"
+
+#define KEY_JABBER_CONNECT_SERVER   @"Jabber:Connect Server"
+#define KEY_JABBER_RESOURCE			@"Jabber:Resource"
+#define KEY_JABBER_USE_TLS			@"Jabber:Use TLS"
+#define KEY_JABBER_FORCE_OLD_SSL	@"Jabber:Force Old SSL"
+#define KEY_JABBER_ALLOW_PLAINTEXT  @"Jabber:Allow Plaintext Authentication"
+
 @interface ESGaimJabberAccount : CBGaimAccount <AIAccount_Files> {
-    
+    NSString	*bareUID;
 }
 
 @end
--- adium/Plugins/Gaim Service/ESGaimJabberAccountViewController.m
+++ adium/Plugins/Gaim Service/ESGaimJabberAccountViewController.m
@@ -7,7 +7,82 @@
 
 #import "ESGaimJabberAccountViewController.h"
 
-
 @implementation ESGaimJabberAccountViewController
+- (NSString *)nibName{
+    return(@"ESGaimJabberAccountView");
+}
+
+//Configure our controls
+- (void)configureForAccount:(AIAccount *)inAccount
+{
+    [super configureForAccount:inAccount];
+	
+	[checkBox_useTLS setState:[[account preferenceForKey:KEY_JABBER_USE_TLS group:GROUP_ACCOUNT_STATUS] boolValue]];
+	[checkBox_forceOldSSL setState:[[account preferenceForKey:KEY_JABBER_FORCE_OLD_SSL group:GROUP_ACCOUNT_STATUS] boolValue]];
+	[checkBox_allowPlaintext setState:[[account preferenceForKey:KEY_JABBER_ALLOW_PLAINTEXT group:GROUP_ACCOUNT_STATUS] boolValue]];
+	
+	NSString *resource = [account preferenceForKey:KEY_JABBER_RESOURCE group:GROUP_ACCOUNT_STATUS];
+	[textField_resource setStringValue:[account preferenceForKey:KEY_JABBER_RESOURCE group:GROUP_ACCOUNT_STATUS]];
+	
+	NSString *connectServer = [account preferenceForKey:KEY_JABBER_CONNECT_SERVER group:GROUP_ACCOUNT_STATUS];
+	[textField_connectServer setStringValue:(connectServer ? connectServer : @"")];
+}
+/*
+//Save changes made to a preference control
+- (IBAction)changedPreference:(id)sender
+{
+    [super changedPreference:sender];
+}
+*/
+
+- (IBAction)changedConnectionPreference:(id)sender
+{	
+	if (sender == checkBox_useTLS){
+		[account setPreference:[NSNumber numberWithBool:[sender state]]
+						forKey:KEY_JABBER_USE_TLS
+						 group:GROUP_ACCOUNT_STATUS];
+			
+	}else if (sender == checkBox_forceOldSSL){
+		[account setPreference:[NSNumber numberWithBool:[sender state]]
+												 forKey:KEY_JABBER_FORCE_OLD_SSL
+												  group:GROUP_ACCOUNT_STATUS];
+			
+	}else if (sender == checkBox_allowPlaintext){
+		[account setPreference:[NSNumber numberWithBool:[sender state]]
+												 forKey:KEY_JABBER_ALLOW_PLAINTEXT
+												  group:GROUP_ACCOUNT_STATUS];
+			
+	}else if (sender == textField_resource){
+		//Access the host name so we can redisplay the default value if applicable
+		NSString *resource = [[adium preferenceController] preferenceForKey:KEY_JABBER_RESOURCE
+																	  group:GROUP_ACCOUNT_STATUS];
+		if (resource){
+			[textField_resource setStringValue:resource];
+		}
+		
+	}else{
+		[super changedConnectionPreference:sender];		
+	}
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification
+{
+	NSTextField *sender = [notification object];
+	if (sender == textField_connectServer){
+		NSString *connectServer = [sender stringValue];
+		[account setPreference:([connectServer length] ? connectServer : nil)
+						forKey:KEY_JABBER_CONNECT_SERVER
+						 group:GROUP_ACCOUNT_STATUS];
+		
+	}else if (sender == textField_resource){
+		NSString *resource = [textField_resource stringValue];
+		[account setPreference:([resource length] ? resource : nil)
+						forKey:KEY_JABBER_RESOURCE
+						 group:GROUP_ACCOUNT_STATUS];
+		
+	}else{
+		[super controlTextDidChange:notification];
+	}
+}
 
 @end
