2d4b2eeb2854bae900663442c8f82c32d0962500
ramoth4 <ramoth4@localhost>
2004-04-16 09:48:51 +0000

    Patch #936180 (Stven Holt): This patch will set system alert sounds to be on by default in Panther, but leave them off and the panel unregistered in Jaguar.

--- adium/Plugins/Output Device Control/SHOutputDeviceControlPlugin.m
+++ adium/Plugins/Output Device Control/SHOutputDeviceControlPlugin.m
@@ -7,6 +7,8 @@
 #import "SHOutputDeviceControlPlugin.h"
 #import "SHOutputDeviceControlPreferences.h"
 
+#define KEY_SYS_SOUND_PATHER_FIRST_RUN          @"System Sound Asserted on Panther"
+
 @interface SHOutputDeviceControlPlugin (PRIVATE)
 
 @end
@@ -15,9 +17,22 @@
 
 - (void)installPlugin
 {
+    BOOL assertSoundOnPather = ![[[adium preferenceController] preferenceForKey:KEY_SYS_SOUND_PATHER_FIRST_RUN
+                                                                          group:PREF_GROUP_GENERAL]
+                                                                          boolValue];
     //Install our preference view
     if([NSApp isOnPantherOrBetter]){
         preferences = [[SHOutputDeviceControlPreferences preferencePane] retain];
+        
+        if(assertSoundOnPather){
+            [[adium preferenceController] setPreference:[NSNumber numberWithBool:YES]
+                                                 forKey:KEY_USE_SYSTEM_SOUND_OUTPUT
+                                                  group:PREF_GROUP_GENERAL];
+                                                  
+            [[adium preferenceController] setPreference:[NSNumber numberWithBool:YES]
+                                                 forKey:KEY_SYS_SOUND_PATHER_FIRST_RUN
+                                                  group:PREF_GROUP_GENERAL];
+        }
     }
 }
 
