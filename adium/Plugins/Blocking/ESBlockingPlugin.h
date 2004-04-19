e593108c998113e54b6fda1d24bb1d96e6df8b7a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-04-19 00:10:45 +0000

    Added empty blocking plugin files

---
+++ adium/Plugins/Blocking/ESBlockingPlugin.h
@@ -0,0 +1,13 @@
+//
+//  ESBlockingPlugin.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Sun Apr 18 2004.
+
+@interface ESBlockingPlugin : AIPlugin {
+	NSMenuItem  *blockContactMenuItem;
+	NSMenuItem  *blockContactContextualMenuItem;
+	NSMenuItem  *editBlockList;
+}
+
+@end
