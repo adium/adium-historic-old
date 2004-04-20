46f13651c4bf74c939f4d6edc4a74f85e58fb57a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-04-20 02:53:25 +0000

    Created a bundle additions file; implements -(NSString *)name which returns the localized name of the bundle (code from Colloquy).

---
+++ adium/Frameworks/AIUtilities Framework/ESBundleAdditions.h
@@ -0,0 +1,16 @@
+//
+//  ESBundleAdditions.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Mon Apr 19 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ESBundleAdditions : NSObject {
+
+}
+
+@end
