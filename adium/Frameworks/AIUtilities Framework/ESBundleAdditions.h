1c0d670e54370ea0954550d4e51623558068ce6b
Evan Schoenberg <Evan Schoenberg@localhost>
2004-04-20 04:04:11 +0000

    Oops, small fix.

--- adium/Frameworks/AIUtilities Framework/ESBundleAdditions.h
+++ adium/Frameworks/AIUtilities Framework/ESBundleAdditions.h
@@ -3,14 +3,9 @@
 //  Adium
 //
 //  Created by Evan Schoenberg on Mon Apr 19 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
 
-#import <Foundation/Foundation.h>
+@interface NSBundle (ESBundleAdditions)
 
-
-@interface ESBundleAdditions : NSObject {
-
-}
+- (NSString *)name;
 
 @end
