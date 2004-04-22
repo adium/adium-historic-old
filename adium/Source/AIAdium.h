5648caa35a1f7b4dcfae94ffcca29d66efdbdb2b
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-04-22 14:15:55 +0000

    Added ReleaseString() macro

--- adium/Source/AIAdium.h
+++ adium/Source/AIAdium.h
@@ -109,6 +109,7 @@
 //Static strings
 #define DeclareString(var)			static NSString * (var) = nil;
 #define InitString(var,string)		if (! (var) ) (var) = [(string) retain];
+#define ReleaseString(var)			if ( (var) ) { [(var) release]; (var) = nil; } 
 
 //Adium events
 #define KEY_EVENT_DISPLAY_NAME		@"DisplayName"
