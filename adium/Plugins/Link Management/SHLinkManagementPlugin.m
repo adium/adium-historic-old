247eeb5e3dc6e2c744859620daf4c28e312db4f5
earthmkii <earthmkii@localhost>
2004-04-24 06:31:27 +0000

    Added an 'Add Link..' and 'Edit Link...' contextual menu

--- adium/Plugins/Link Management/SHLinkManagementPlugin.m
+++ adium/Plugins/Link Management/SHLinkManagementPlugin.m
@@ -17,17 +17,29 @@
 
 - (void)installPlugin
 {
+    //Add Link.. menu item (edit menu)
     menu_AddLink = [[[NSMenuItem alloc] initWithTitle:ADD_LINK_TITLE
                                                target:self
                                                action:@selector(addFormattedLink:)
                                         keyEquivalent:@"["] autorelease];
     [[adium menuController] addMenuItem:menu_AddLink toLocation:LOC_Edit_Bottom];
     
+    //contextual menu
+    contextMenu_AddLink = [[menu_AddLink copy] autorelease];
+    [contextMenu_AddLink setKeyEquivalent:@""];
+    [[adium menuController] addContextualMenuItem:contextMenu_AddLink toLocation:Context_TextView_LinkAction];
+    
+    //Edit Link... menu item (edit menu)
     menu_EditLink = [[[NSMenuItem alloc] initWithTitle:EDIT_LINK_TITLE
                                                 target:self
                                                 action:@selector(editFormattedLink:)
                                          keyEquivalent:@"]"] autorelease];
     [[adium menuController] addMenuItem:menu_EditLink toLocation:LOC_Edit_Bottom];
+    
+    //context menu
+    contextMenu_EditLink = [[menu_EditLink copy] autorelease];
+    [contextMenu_EditLink setKeyEquivalent:@""];
+    [[adium menuController] addContextualMenuItem:contextMenu_EditLink toLocation:Context_TextView_LinkAction];
     
     [[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:LINK_MANAGEMENT_DEFAULTS forClass:[self class]]
                                           forGroup:PREF_GROUP_LINK_FAVORITES];
