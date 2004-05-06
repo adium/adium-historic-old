7810ccd89707c2b701a84c2345599e91724a968c
Evan Schoenberg <Evan Schoenberg@localhost>
2004-05-06 23:20:57 +0000

    Just some internal bookkeeping

--- adium/Plugins/Gaim Service/ESGaimRequestActionWindowController.h
+++ adium/Plugins/Gaim Service/ESGaimRequestActionWindowController.h
@@ -5,7 +5,9 @@
 //  Created by Evan Schoenberg on Wed May 05 2004.
 
 @interface ESGaimRequestActionWindowController : AIWindowController {
-
+    IBOutlet	NSTextField	*textField_errorTitle;
+    IBOutlet	NSTextView	*textView_errorInfo;
+    IBOutlet	NSButton	*button_okay;
 }
 
 + (void)showActionWindowWithDict:(NSDictionary *)infoDict;
