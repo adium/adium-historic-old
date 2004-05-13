d4db77747888e893257e823306333452d4b89c87
Stephen Holt <sholt@steveholt.org>
2004-05-13 08:13:24 +0000

    A few small things: 	* add http and mailto specifiers to degenerate url's in linkify methods 	* added some commenting 	* some slight OCD

--- adium/Frameworks/AdiumHyperlinks Framework/SHMarkedHyperlink.h
+++ adium/Frameworks/AdiumHyperlinks Framework/SHMarkedHyperlink.h
@@ -3,26 +3,28 @@
 //  Adium
 //
 //  Created by Stephen Holt on Tue May 11 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
 
-#import <Foundation/Foundation.h>
+
+#import "SHLinkLexer.h"
 
 
 @interface SHMarkedHyperlink : NSObject {
-    NSRange      linkRange;
-    NSURL       *linkURL;
-    NSString    *pString;
+    NSRange                      linkRange;
+    NSURL                       *linkURL;
+    NSString                    *pString;
+    URI_VERIFICATION_STATUS      urlStatus;
 }
 
--(id)initWithString:(NSString *)inString parentString:(NSString *)pInString andRange:(NSRange)inRange;
+-(id)initWithString:(NSString *)inString withValidationStatus:(URI_VERIFICATION_STATUS)status parentString:(NSString *)pInString andRange:(NSRange)inRange;
 -(NSString *)parentString;
 -(NSRange)range;
 -(NSURL *)URL;
+-(URI_VERIFICATION_STATUS)validationStatus;
 
 -(void)setRange:(NSRange)inRange;
 -(void)setURL:(NSURL *)inURL;
 -(void)setURLFromString:(NSString *)inString;
+-(void)setValidationStatus:(URI_VERIFICATION_STATUS)status;
 -(void)setParentString:(NSString *)pInString;
 
 
