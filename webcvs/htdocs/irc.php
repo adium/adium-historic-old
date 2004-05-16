0d167d0b05d11b360e96c390789363f4e6791582
eevyl <eevyl@localhost>
2004-05-16 19:16:46 +0000

    I'm teh xhtml mastah!

--- webcvs/htdocs/irc.php
+++ webcvs/htdocs/irc.php
@@ -77,82 +77,50 @@
                 <h1>Why IRC?</h1>
                 <div class="boxWideTop"></div>
                 <div class="boxWideContent">
-                    <div class="whyirc">
                     <p>One way that Adium developers and users try to help support each other is through a group chat room that is on IRC (Internet Relay Chat). This allows for what is otherwise known as real time interaction. Everyone chats in a room, called a channel. You can ask any questions you want, and for the most part, they will be answered. We even talk about things not even remotely related to Adium!</p>
-                    </div>
-                    <div class="whyirc">
                     <p>All that is great, but you need to know how to get onto IRC to ask your question, right? Welp, here you go. Three simple steps that will tell you how to get onto irc:</p>
-                                      </div>
                     <div class="cleanHackLeft"> </div>
                 </div>
                 <div class="boxWideBottom"></div>
                 <h1>Step 1: Getting an IRC client</h1>
                 <div class="boxWideTop"></div>
                 <div class="boxWideContent">
-                    <div class="whyirc">
                     <p>There are many irc clients out there, some simple, some not so simple. Luckily, we are going to discuss one of the simple ones here. All you need for this are:</p>
-                    </div>
-                    <div class="whyirc">
-                    	<ul class="requirements">
+                    	<ul>
                     		<li>An apple computer</li>
                     		<li>An internet connection</li>
                     		<li>Some time.</li>
                     	</ul>
-                    </div>
-                    <div class="whyirc">
                     <p>Go to <a href="https://sourceforge.net/project/showfiles.php?group_id=74267">https://sourceforge.net/project/showfiles.php?group_id=74267</a> to download Colloquy. Here is a screen shot of that page as it is right now:</p>
                     <img class="irchowto" width="500" border="0" src="images/irchowto/irchowto1.jpg" alt="avatar" />
-                    </div>
-                    <div class="irchowto">
                     <p>You'll notice a couple of versions there, you probably want the latest version. Click on the file name which is underneath the bold title for the version you wish to download.</p>
-                    </div>
-                    <div class="irchowto">
                     <p>You will then come to a web page that looks a little like this:</p><br />
-                                        <img class="irchowto" width="500" border="0" src="images/irchowto/irchowto2.jpg" alt="avatar" />
-                    </div>
-                     <div class="irchowto">
-                    <p>Click on the picture under the download column on the right. The trick to this page is to figure out which of these places is closest to you, which makes it a much faster download.</p>                    </div>
-
-                    <div class="irchowto">
+                    <img class="irchowto" width="500" border="0" src="images/irchowto/irchowto2.jpg" alt="avatar" />
+                    <p>Click on the picture under the download column on the right. The trick to this page is to figure out which of these places is closest to you, which makes it a much faster download.</p>
                     <p>Once Colloquy is downloaded, if your browser has not automatically opened the file, go ahead and double click on it. It will expand the compressed file, and you will then get to copy it to where you want it.</p>
-                    </div>
-                    <div class="cleanHackLeft"> </div>
                 </div>
                 <div class="boxWideBottom"></div>
                 <h1>Step 2: Setting up Colloquy</h1>
                 <div class="boxWideTop"></div>
                 <div class="boxWideContent">
-                    <div class="teamMember">
 					<p>The second step to getting to talk to a Adium user or developer live, in real time, for support or to just chit chat, is to launch Colloquy, and then configure it to connect to the correct place. Go ahead and launch Colloquy from where you copied it to. Once you have done that, it should look something like this:</p>
                      <img class="irchowto" width="470" border="0" src="images/irchowto/irchowto3.jpg" alt="avatar" />
-                    </div>
-                    <div class="teamMember">
                 	<p>Click the details triangle, and then fill in the fields for this panel to look something like this:</p>
                      <img class="irchowto" width="470" border="0" src="images/irchowto/irchowto4.jpg" alt="avatar" />
-                    </div>
-                    <div class="teamMember">
                 	<p>Ensure that the Chat Server is setup to irc.freenode.net, and that you have added #Adium to the Join Rooms</p>
-                    </div>
-                    <div class="teamMember">
                 	<p>Once you are done filling everything out, click the connect button.</p>
-                    </div>
                     <div class="cleanHackLeft"> </div>
                 </div>
                 <div class="boxWideBottom"></div>
-          </div>
-            <div id="content">
                 <h1>Step 3: There is no step 3</h1>
                 <div class="boxWideTop"></div>
                 <div class="boxWideContent">
-                    <div class="whyirc">
                     <p>You should be connected within moments, and it should look like this:</p>
-                    </div>
-                    <div class="whyirc">
                      <img class="irchowto" width="470" border="0" src="images/irchowto/irchowto5.jpg" alt="avatar" />
-                                      </div>
                     <div class="cleanHackLeft"> </div>
                 </div>
                 <div class="boxWideBottom"></div>
+            </div>
             <div id="bottom">
                 <div id="powered">
                 	<a href="http://gaim.sourceforge.net"><img width="88" height="31" src="http://gaim.sourceforge.net/images/powered_by_libgaim.png" border="0" alt="Adium is powered by libgaim "/></a> <a href="http://www.blogger.com"><img width="88" height="31" src="http://buttons.blogger.com/bloggerbutton1.gif" border="0" alt="This page is powered by Blogger. Isn't yours?" /></a> <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=67507&amp;type=2" width="125" height="37" border="0" alt="SourceForge.net Logo"/></a>
