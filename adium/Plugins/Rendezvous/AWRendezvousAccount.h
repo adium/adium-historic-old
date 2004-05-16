4bdf021931d46e5e4a74e7157355fb4044d6f294
Evan Schoenberg <Evan Schoenberg@localhost>
2004-05-16 07:29:46 +0000

    That last one was basically just for our new buddy Rendezvous, so adding - (BOOL)disconnectOnFastUserSwitch { return YES; }

--- adium/Plugins/Rendezvous/AWRendezvousAccount.h
+++ adium/Plugins/Rendezvous/AWRendezvousAccount.h
@@ -26,7 +26,7 @@
 @class AWEzv;
 
 @interface AWRendezvousAccount : AIAccount <AIAccount_Content, AWEzvClientProtocol> {
-    AWEzv *libezv;
+    AWEzv				*libezv;
     NSMutableDictionary	*libezvContacts;
 }
 
