d870206addc73dae5b99cff83b10a6bdf1d569d5
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-05-19 03:16:42 +0000

    Fixed the URL additions method - (NSString *)queryArgumentForKey:(NSString *)key to return nil if not argument is found for the specified key.

--- adium/Frameworks/AIUtilities Framework/ESURLAdditions.m
+++ adium/Frameworks/AIUtilities Framework/ESURLAdditions.m
@@ -17,15 +17,18 @@
 - (NSString *)queryArgumentForKey:(NSString *)key
 {
     NSString *obj = nil;
-    NSArray *arguments = [[self query] componentsSeparatedByString:@"&"];
-    NSEnumerator *numer = [arguments objectEnumerator];
+    NSEnumerator *enumerator = [[[self query] componentsSeparatedByString:@"&"] objectEnumerator];
     
-    while(obj = [numer nextObject]){
+    while(obj = [enumerator nextObject]){
         NSArray *keyAndValue = [obj componentsSeparatedByString:@"="];
-        if([[keyAndValue objectAtIndex:0] isEqualToString:key]){
-            return [keyAndValue objectAtIndex:1];
-        }
+
+        if(([keyAndValue count] >= 2) &&
+		   ([[keyAndValue objectAtIndex:0] isEqualToString:key]){
+			   return [keyAndValue objectAtIndex:1];
+		   }
     }
+	
+	return nil;
 }
 
 @end
