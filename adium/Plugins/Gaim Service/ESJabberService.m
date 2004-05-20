cbada20a4acdd564889ce4de7de76638c8171231
adamiser <adamiser@localhost>
2004-05-20 13:25:25 +0000

    Adding support for parens in jabber UIDs

--- adium/Plugins/Gaim Service/ESJabberService.m
+++ adium/Plugins/Gaim Service/ESJabberService.m
@@ -20,7 +20,7 @@
                                                       description:@"Jabber"
                                                             image:nil
                                                     caseSensitive:NO
-                                                allowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"+abcdefghijklmnopqrstuvwxyz0123456789._@-"]
+                                                allowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"+abcdefghijklmnopqrstuvwxyz0123456789._@-()"]
 												ignoredCharacters:[NSCharacterSet characterSetWithCharactersInString:@""]
 													allowedLength:129] retain];
     
