f1acd9f3619e201145f6a5fd41af244a03f52512
earthmkii <earthmkii@localhost>
2004-05-23 21:48:44 +0000

    * An updated plugin file that, in theory, will place a bookmarks menu item in the 'Edit' menu and textView contextual menu, and allow for an extensible plugin articture for bookmark importing componets to be added/removed easily. * also, I'm back from boston, but not quite back at school yet so I don't have time to pop on IRC right now * lastly, I'd update the bookmark importers themselves right now, but I'm still having some memory mgt issues for some reason.  lots of double frees, which just shouldn't happen. probably something obvious I'm missinog though.  More at 11.

--- adium/Plugins/Bookmark Importing/SHBookmarksImporterPlugin.m
+++ adium/Plugins/Bookmark Importing/SHBookmarksImporterPlugin.m
@@ -9,16 +9,21 @@
 #define ROOT_MENU_TITLE     AILocalizedString(@"Bookmarks",nil)
 
 @interface SHBookmarksImporterPlugin(PRIVATE)
+- (void)installImporterClass:(Class)inClass;
 - (void)configureMenus;
 - (void)buildBookmarkMenuFor:(id <NSMenuItem>)menuItem;
 @end
+
+@class SHSafariBookmarksImporter, SHCaminoBookmarksImporter;
 
 @implementation SHBookmarksImporterPlugin
 
 - (void)installPlugin
 {
-    importerArray = [[NSArray arrayWithObjects:[[SHSafariBookmarkImporter alloc] init],
-                                                nil] autorelease];
+    importerArray = [[[NSMutableArray alloc] init] autorelease];
+    
+    [self installImporterClass:[SHSafariBookmarksImporter class]];
+    //[self installImporterClass:[SHCaminoBookmarksImporter class]];
                                                          
     bookmarkRootMenuItem = [[[NSMenuItem alloc] initWithTitle:ROOT_MENU_TITLE
                                                        target:self
@@ -35,13 +40,24 @@
     [self configureMenus];
     
     [[adium menuController] addMenuItem:bookmarkRootMenuItem toLocation:LOC_Edit_Additions];
-    [[adium menuController] addMenuItem:bookmarkRootContextualMenuItem toLocation:Context_TextView_LinkAction];
+    [[adium menuController] addContextualMenuItem:bookmarkRootContextualMenuItem toLocation:Context_TextView_LinkAction];
 }
 
 - (void)uninstallPlugin
 {
 }
 
+- (void)installImporterClass:(Class)inClass
+{
+    id object = [[[inClass alloc] init] autorelease];
+    
+    if(object){
+        [importerArray addObject:object];
+    }else{
+        NSString *failureNotice = [NSString stringWithFormat:@"Failed to load bookmark importer %@",NSStringFromClass(inClass)];
+        NSAssert(object,failureNotice);
+    }
+}
 - (void)configureMenus
 {
     NSEnumerator *enumerator = [importerArray objectEnumerator];
@@ -57,14 +73,18 @@
                                                        keyEquivalent:@""] autorelease];
             [menuItem setRepresentedObject:importer];
             [bookmarkSets addItem:menuItem];
-            [menuItem release];
-            NSMenu *subMenu = [[[NSMenu alloc] initWithTitle:@""] autorelease];
-            NSMenuItem *importerMenuItem = [[[NSMenuItem alloc] initWithTitle:@"placeholder"
-                                                   target:self
-                                                   action:nil
-                                            keyEquivalent:@""] autorelease];
-            [subMenu addItem:importerMenuItem];
+            //[menuItem release];
+            
+//            NSMenu *subMenu = [[[NSMenu alloc] initWithTitle:@""] autorelease];
+//            NSMenuItem *importerMenuItem = [[[NSMenuItem alloc] initWithTitle:@"placeholder"
+//                                                   target:self
+//                                                   action:nil
+//                                            keyEquivalent:@""] autorelease];
+//            [importerMenuItem setRepresentedObject:importer];
+//            [subMenu addItem:importerMenuItem];
+            NSMenu *subMenu = [[[self buildBookmarkMenuFor:menuItem] copy] autorelease];
             [menuItem setSubmenu:subMenu];
+            [bookmarkSets addItem:menuItem];
         }
     }
     if([bookmarkSets numberOfItems]){
@@ -79,17 +99,48 @@
     [menuItem setSubmenu:[importer parseBookmarksForOwner:self]];
 }
 
-- (BOOL)validateMenuItem:(id <NSMenuItem>)menuItem
+/*- (BOOL)validateMenuItem:(id <NSMenuItem>)menuItem
 {
 	NSResponder	*responder = [[[NSApplication sharedApplication] keyWindow] firstResponder];
-	if(responder && [responder isKindOfClass:[NSTextView class]] && nil != [bookmarkRootMenuItem submenu]){
-                if([[menuItem representedObject] respondsToSelector:@selector(parseBookmarksForOwner:)]){
+	if(responder && [responder isKindOfClass:[NSTextView class]]){
+                if([[menuItem representedObject] conformsToProtocol:@protocol(SHBookmarkImporter)]){
                     [self buildBookmarkMenuFor:menuItem];
                 }
 		return(YES);
 	}else{
 		return(NO); //Disable the menu item if a text field is not key
 	}
+}*/
+
+- (void)injectBookmarkFrom:(id)sender
+{
+    if([[(NSMenuItem *)sender representedObject] isKindOfClass:[SHMarkedHyperlink class]]){
+        SHMarkedHyperlink   *markedLink = [(NSMenuItem *)sender representedObject];
+        NSResponder         *responder = [[[NSApplication sharedApplication] keyWindow] firstResponder];
+	
+        if(responder && [responder isKindOfClass:[NSTextView class]]){
+            NSTextView      *topView = (NSTextView *)responder;
+            NSDictionary    *typingAttributes = [topView typingAttributes];
+            
+            NSMutableAttributedString	*linkString = [[[NSMutableAttributedString alloc] initWithString:[markedLink parentString]
+                                                                                              attributes:typingAttributes] autorelease];
+            [linkString addAttribute:NSLinkAttributeName value:[markedLink URL] range:[markedLink range]];
+            
+            NSRange selRange = [topView selectedRange];
+            [[topView textStorage] replaceCharactersInRange:selRange withAttributedString:linkString];
+            
+            if([[topView string] length] == selRange.location + selRange.length){
+                NSRange newSelRange = NSMakeRange(selRange.location + selRange.length, 0);
+                [topView setSelectedRange:newSelRange];
+                [topView setSelectedTextAttributes:typingAttributes];
+            }else if([[topView string] characterAtIndex:(selRange.location + [markedLink range].length + 1)] != ' '){
+                NSAttributedString  *tmpString = [[[NSAttributedString alloc] initWithString:@" "
+                                                                                  attributes:typingAttributes] autorelease];
+                [[topView textStorage] insertAttributedString:tmpString
+                                                      atIndex:(selRange.location + [markedLink range].length)];
+            }
+        }
+    }
 }
 
 @end
