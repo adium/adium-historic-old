f4892076730fefd85c482eb3a0b40fa6fbe2587e
Peter Hosey <Peter Hosey@localhost>
2004-05-24 09:41:42 +0000

    Protocol for objects to report their progress. Used by Activity Window.

---
+++ adium/Frameworks/AIUtilities Framework/BZProgressTracker.h
@@ -0,0 +1,41 @@
+#import <Foundation/Foundation.h>
+
+enum ProgressState {
+	ProgressState_Stopped,
+	ProgressState_Starting,
+	ProgressState_Stalled,
+	ProgressState_Paused,
+	ProgressState_Working,
+	ProgressState_Stopping,
+	ProgressState_Finished
+};
+
+@protocol BZProgressTracker <NSObject>
+
+- (NSString *)name;
+- (NSString *)type;
+//future expansion:
+//- (NSImage *)icon;
+
+- (float)maximum;
+- (float)current;
+- (float)speed;
+- (NSString *)unit; //e.g. @"MB" for a download
+//maybe we should have different units for the three numbers.
+
+- (void)setActivityWindowController:controller;
+
+- (BOOL)cancel;
+- (BOOL)canCancel;
+- (BOOL)pause;
+- (BOOL)canPause;
+- (BOOL)resume;
+- (BOOL)canResume;
+- (BOOL)canStart;
+- (BOOL)start;
+- (BOOL)reveal;
+- (BOOL)canReveal;
+
+- (enum ProgressState)progressState;
+
+@end
