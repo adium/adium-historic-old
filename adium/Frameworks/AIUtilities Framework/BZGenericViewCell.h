bdb4e600a03c705f39c344653aa7da1ca627cb07
Peter Hosey <Peter Hosey@localhost>
2004-05-24 09:41:01 +0000

    New class for embedding any view into a TableView. Set an instance of this as the cell for the column into which you want to set views as values.

---
+++ adium/Frameworks/AIUtilities Framework/BZGenericViewCell.h
@@ -0,0 +1,16 @@
+//
+//  BZGenericViewCell.h
+//  Adium
+//
+//  Created by Mac-arena the Bored Zo on Sun May 09 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//based on sample code at http://www.cocoadev.com/index.pl?NSViewInNSTableView
+
+@interface BZGenericViewCell : NSCell
+{}
+
+@end
