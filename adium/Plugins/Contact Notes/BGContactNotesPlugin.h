3d093c8ea9b051899026256c77107bda8a6209bd
adamiser <adamiser@localhost>
2004-05-24 12:01:25 +0000

    No local variable named timer

--- adium/Plugins/Contact Notes/BGContactNotesPlugin.h
+++ adium/Plugins/Contact Notes/BGContactNotesPlugin.h
@@ -22,6 +22,8 @@
     AIPreferenceViewController			*contactView;
     AIListObject                        *activeListObject;
     BOOL noteSync;
+	
+	NSTimer		*delayedChangesTimer;
 }
 
 - (void)installPlugin;
