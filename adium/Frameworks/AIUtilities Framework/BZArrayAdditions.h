5b7a5b167b4c0be46df085213b6ea31c492fe252
Peter Hosey <Peter Hosey@localhost>
2004-05-24 09:40:01 +0000

    New method, -[NSArray containsObjectIdenticalTo:].

---
+++ adium/Frameworks/AIUtilities Framework/BZArrayAdditions.h
@@ -0,0 +1,15 @@
+//
+//  BZArrayAdditions.h
+//  Adium
+//
+//  Created by Mac-arena the Bored Zo on Tue May 11 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (BZArrayAdditions)
+
+- (BOOL)containsObjectIdenticalTo:(id)obj;
+
+@end
