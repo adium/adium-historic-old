11443a739c993fde12a814fb141f60aded39ab76
Stephen Holt <sholt@steveholt.org>
2004-05-25 03:21:27 +0000

    now supporting camino bookmarks! (this would have been done sooner, but I'm an idiot and was looking up the wrong NSDictionary key)

--- adium/Plugins/Bookmark Importing/SHCaminoBookmarksImporter.h
+++ adium/Plugins/Bookmark Importing/SHCaminoBookmarksImporter.h
@@ -10,13 +10,11 @@
 
 @interface SHCaminoBookmarksImporter : NSObject <SHBookmarkImporter> {
     id       owner;
-    NSMenu  *bookmarksMenu;
-    NSMenu  *bookmarksSupermenu;
     NSDate  *lastModDate;
 }
 
 -(NSMenu *)parseBookmarksForOwner:(id)inObject;
 -(NSString *)menuTitle;
 -(BOOL)bookmarksExist;
-
+-(BOOL)bookmarksUpdated;
 @end
