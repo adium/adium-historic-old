599e68c1d466c3229ccb510b076d0048e7c1c439
Peter Hosey <git@boredzo.org>
2004-05-26 19:10:10 +0000

    Added a new protocol, BZProgressTrackerDelegate, and a new class, BZProgressTracker. These allow you to use a delegate system to track progress. See BZProgressTracker.h.

--- adium/Frameworks/AIUtilities Framework/BZProgressTracker.h
+++ adium/Frameworks/AIUtilities Framework/BZProgressTracker.h
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import <time.h>
 
 enum ProgressState {
 	ProgressState_Stopped,
@@ -29,11 +30,92 @@ enum ProgressState {
 - (BOOL)canPause;
 - (BOOL)resume;
 - (BOOL)canResume;
-- (BOOL)canStart;
 - (BOOL)start;
+- (BOOL)canStart;
 - (BOOL)reveal;
 - (BOOL)canReveal;
+- (BOOL)prepareForDelete;
+- (BOOL)canDelete;
 
 - (enum ProgressState)progressState;
+
+@end
+
+/*  BZProgressTracker class; BZProgressTrackerDelegate protocol
+ *  
+ *  these allow you to use a delegate system to track the progress of an
+ *  activity (e.g. an ESFileTransfer instance).
+ *  
+ *  create an instance of BZProgressTracker with [[BZProgressTracker alloc]
+ *  initWithActivity:self name: type:. install the instance as your delegate.
+ *  call its stateChanged::::: method (see the BZProgressTrackerDelegate
+ *  protocol) after installation, and when anything happens.
+ */
+
+@protocol BZProgressTrackerDelegate
+
+- (void)stateChanged:(enum ProgressState)newState current:(float)newCurrent maximum:(float)newMaximum unit:(NSString *)newUnit icon:(NSImage *)newIcon;
+
+@end
+
+@class BZActivityWindowController;
+
+@interface BZProgressTracker: NSObject <BZProgressTrackerDelegate, BZProgressTracker>
+{
+	id activity;
+	NSString *name, *type;
+	BZActivityWindowController *activityWindowController;
+
+	float current, maximum;
+	NSString *unit;
+	NSImage *icon;
+	enum ProgressState progressState;
+
+	float lastCurrent;
+	clock_t lastTime; //last time the progress changed.
+}
+
+- (id)initWithActivity:(id)activity name:(NSString *)name type:(NSString *)type activityWindowController:(BZActivityWindowController *)awc;
+
+//delegate conformance.
+- (void)stateChanged:(enum ProgressState)newState current:(float)newCurrent maximum:(float)newMaximum unit:(NSString *)newUnit icon:(NSImage *)newIcon;
+
+//tracker conformance.
+- (NSString *)name;
+- (NSString *)type;
+- (NSImage *)icon;
+
+- (float)maximum;
+- (float)current;
+- (float)speed;
+- (NSString *)unit;
+- (enum ProgressState)progressState;
+
+//all activities (objects using a BZProgressTracker as a delegate) should
+//  implement some subset of these methods.
+//also, if you implement the foo method but not the canFoo method,
+//  BZProgressTracker acts as though you have a canFoo method which returned YES.
+- (BOOL)cancel;
+- (BOOL)canCancel;
+- (BOOL)pause;
+- (BOOL)canPause;
+- (BOOL)resume;
+- (BOOL)canResume;
+- (BOOL)start;
+- (BOOL)canStart;
+- (BOOL)reveal;
+- (BOOL)canReveal;
+/* some notes on the delete methods:
+ *  (1) unlike cancel, pause, resume, start, and reveal, the delete method is
+ *      named 'prepareForDelete' (because it does not actually delete the
+ *      tracker or its activity; the progress view does that).
+ *  (2) make sure your activity releases the delegate when its -prepareForDelete
+ *      is called. BZProgressTracker expects this.
+ *  (3) even though BZProgressTracker's delete methods are implemented in an
+ *      absence-tolerant fashion, every activity should implement
+ *      -prepareForDelete (as described above).
+ */
+- (BOOL)prepareForDelete;
+- (BOOL)canDelete;
 
 @end
