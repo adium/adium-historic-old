a865295cfc35c4f13804479a7115aa9248e92a87
Stephen Holt <sholt@steveholt.org>
2004-05-31 05:59:39 +0000

    dding MSIE bookmarks to the mix. ...this is really getting anticlimactic

---
+++ adium/Plugins/Bookmark Importing/SHMSIEBookmarksImporter.h
@@ -0,0 +1,21 @@
+//
+//  SHMSIEBookmarksImporter.h
+//  Adium
+//
+//  Created by Stephen Holt on Mon May 31 2004.
+
+
+#import "SHBookmarksImporterPlugin.h"
+
+@protocol SHBookmarksImporter;
+
+@interface SHMSIEBookmarksImporter : NSObject <SHBookmarksImporter> {
+    id       owner;
+    NSDate  *lastModDate;
+}
+
+-(NSMenu *)parseBookmarksForOwner:(id)inObject;
+-(NSString *)menuTitle;
+-(BOOL)bookmarksExist;
+-(BOOL)bookmarksUpdated;
+@end
---
+++ adium/Plugins/Bookmark Importing/SHMSIEBookmarksImporter.m
@@ -0,0 +1,132 @@
+//
+//  SHMSIEBookmarksImporter.m
+//  Adium
+//
+//  Created by Stephen Holt on Mon May 31 2004.
+
+
+#import "SHMSIEBookmarksImporter.h"
+
+#define MSIE_BOOKMARKS_PATH  @"~/Library/Preferences/Explorer/Favorites.html"
+
+#define MSIE_ROOT_MENU_TITLE AILocalizedString(@"Internet Explorer",nil)
+
+@interface SHMSIEBookmarksImporter(PRIVATE)
+- (void)parseBookmarksFile:(NSString *)inString;
+@end
+
+@implementation SHMSIEBookmarksImporter
+
+static NSMenu   *msieBookmarksMenu;
+static NSMenu   *msieBookmarksSupermenu;
+static NSMenu   *msieTopMenu;
+
+#pragma mark protocol methods
++ (id)newInstanceOfImporter
+{
+    return [[[self alloc] init] autorelease];
+}
+
+-(NSMenu *)parseBookmarksForOwner:(id)inObject
+{
+    owner = inObject;
+    NSString        *bookmarkString = [NSString stringWithContentsOfFile:[MSIE_BOOKMARKS_PATH stringByExpandingTildeInPath]];
+    
+    // remove our root menu, if it exists
+    if(msieBookmarksMenu){
+        [msieBookmarksMenu removeAllItems];
+        [msieBookmarksMenu release];
+    }
+    
+    // store the modification date for future reference
+    if (lastModDate) [lastModDate release];
+    NSDictionary *fileProps = [[NSFileManager defaultManager] fileAttributesAtPath:[MSIE_BOOKMARKS_PATH stringByExpandingTildeInPath] traverseLink:YES];
+    lastModDate = [[fileProps objectForKey:NSFileModificationDate] retain];
+    
+    msieBookmarksMenu = [[[NSMenu alloc] initWithTitle:MSIE_ROOT_MENU_TITLE] autorelease];
+    msieBookmarksSupermenu = msieBookmarksMenu;
+    msieTopMenu = msieBookmarksMenu;
+    [self parseBookmarksFile:bookmarkString];
+    
+    return msieBookmarksMenu;
+}
+
+-(BOOL)bookmarksExist
+{
+    return [[NSFileManager defaultManager] fileExistsAtPath:[MSIE_BOOKMARKS_PATH stringByExpandingTildeInPath]];
+}
+
+-(NSString *)menuTitle
+{
+    return MSIE_ROOT_MENU_TITLE;
+}
+
+-(BOOL)bookmarksUpdated
+{
+    NSDictionary *fileProps = [[NSFileManager defaultManager] fileAttributesAtPath:[MSIE_BOOKMARKS_PATH stringByExpandingTildeInPath] traverseLink:YES];
+    NSDate *modDate = [fileProps objectForKey:NSFileModificationDate];
+    
+    return ![modDate isEqualToDate:lastModDate];
+}
+
+#pragma mark private methods
+
+- (void)parseBookmarksFile:(NSString *)inString
+{
+    NSScanner   *linkScanner = [NSScanner scannerWithString:inString];
+    NSString    *titleString, *urlString;
+    
+    while(![linkScanner isAtEnd]){
+        if([[inString substringFromIndex:[linkScanner scanLocation]] length] < 4){
+            [linkScanner setScanLocation:[inString length]];
+        }else if(NSOrderedSame == [[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],3)] compare:@"H3 "]){
+            [linkScanner setScanLocation:[linkScanner scanLocation] + 3];
+            [linkScanner scanUpToString:@">" intoString:nil];
+            [linkScanner setScanLocation:[linkScanner scanLocation] + 1];
+            [linkScanner scanUpToString:@"</H" intoString:&titleString];
+                
+            msieBookmarksSupermenu = msieBookmarksMenu;
+            msieBookmarksMenu = [[[NSMenu alloc] initWithTitle:titleString] autorelease];
+        
+            NSMenuItem *mozillaSubmenuItem = [[[NSMenuItem alloc] initWithTitle:titleString
+                                                                         target:owner
+                                                                         action:nil
+                                                                  keyEquivalent:@""] autorelease];
+            [msieBookmarksSupermenu addItem:mozillaSubmenuItem];
+            [msieBookmarksSupermenu setSubmenu:msieBookmarksMenu forItem:mozillaSubmenuItem];
+        }else if(NSOrderedSame == [[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],2)] compare:@"A "]){
+            //[linkScanner setScanLocation:[linkScanner scanLocation] + 3];
+            [linkScanner scanUpToString:@"HREF=\"" intoString:nil];
+            [linkScanner setScanLocation:[linkScanner scanLocation] + 6];
+            [linkScanner scanUpToString:@"\"" intoString:&urlString];
+                
+            [linkScanner scanUpToString:@"\">" intoString:nil];
+            [linkScanner setScanLocation:[linkScanner scanLocation] + 2];
+            [linkScanner scanUpToString:@"</A" intoString:&titleString];
+                
+            SHMarkedHyperlink *markedLink = [[[SHMarkedHyperlink alloc] initWithString:[urlString retain]
+                                                                  withValidationStatus:SH_URL_VALID
+                                                                          parentString:titleString
+                                                                              andRange:NSMakeRange(0,[titleString length])] autorelease];
+                                                                          
+            [msieBookmarksMenu addItemWithTitle:titleString
+                                            target:owner
+                                            action:@selector(injectBookmarkFrom:)
+                                     keyEquivalent:@""
+                                 representedObject:markedLink];
+        
+        }else if(NSOrderedSame == [[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],4)] compare:@"/DL>"]){
+            [linkScanner setScanLocation:[linkScanner scanLocation] + 4];
+            if([msieBookmarksMenu isNotEqualTo:msieTopMenu]){
+                msieBookmarksMenu = msieBookmarksSupermenu;
+                msieBookmarksSupermenu = [msieBookmarksSupermenu supermenu];
+            }
+        }else{
+            [linkScanner scanUpToString:@"<" intoString:nil];
+            if(![linkScanner isAtEnd])
+                [linkScanner setScanLocation:[linkScanner scanLocation] + 1];
+        }
+    }
+}
+
+@end
