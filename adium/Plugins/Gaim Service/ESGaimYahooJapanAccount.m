f39701197efb25a843c7fc3842d803fd02c121e6
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-06-02 14:39:32 +0000

    Libgaim 0.79 will add proper Yahoo! Japan support (including fixing the encoding bug we have listed on the Bugs list).  These changes will be needed then and don't hurt now.

--- adium/Plugins/Gaim Service/ESGaimYahooJapanAccount.m
+++ adium/Plugins/Gaim Service/ESGaimYahooJapanAccount.m
@@ -12,10 +12,29 @@
 
 @implementation ESGaimYahooJapanAccount
 
-
 - (NSString *)hostKey
 {
 	return KEY_YAHOO_JAPAN_HOST;
+}
+
+- (void)createNewGaimAccount
+{
+	[super createNewGaimAccount];
+
+	gaim_account_set_bool(account, "yahoojp", TRUE);
+}
+
+- (void)configureGaimAccount
+{
+	[super configureGaimAccount];
+	
+	NSString	*hostName;
+	
+	//Host (server) - Yahoo! Japan uses a different server preference key than other accounts, so set that here
+	hostName = [self host];
+	if (hostName && [hostName length]){
+		gaim_account_set_string(account, "serverjp", [hostName UTF8String]);
+	}
 }
 
 @end
