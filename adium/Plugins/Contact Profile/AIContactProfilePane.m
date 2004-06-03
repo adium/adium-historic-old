2d8b1c8565d5d47c7a25de9b3174062993a03d93
adamiser <adamiser@localhost>
2004-06-03 14:57:17 +0000

    Removing a few logs, adding profile background color support back

--- adium/Plugins/Contact Profile/AIContactProfilePane.m
+++ adium/Plugins/Contact Profile/AIContactProfilePane.m
@@ -38,7 +38,6 @@
 //Configure the pane for a list object
 - (void)configureForListObject:(AIListObject *)inObject
 {
-	NSLog(@"listObject = %@",[inObject displayName]);
 	//New list object
 	[listObject release];
 	listObject = [inObject retain];
@@ -48,7 +47,6 @@
 	
 	//Refresh the window's content (Contacts only)
 	if([listObject isKindOfClass:[AIListContact class]]){
-		NSLog(@"requesting info for %@",[listObject displayName]);
 		[[adium contactController] updateListContactStatus:(AIListContact *)listObject];
 	}
 }
@@ -56,9 +54,7 @@
 //Refresh if changes are made to the object we're displaying
 - (NSArray *)updateListObject:(AIListObject *)inObject keys:(NSArray *)inModifiedKeys silent:(BOOL)silent
 {
-	NSLog(@"%@ =? %@",[inObject displayName],[listObject displayName]);
     if(inObject == listObject){
-		NSLog(@"  updateListObject:%@",[inObject displayName]);
         [self updatePane];
     }
     return(nil);
@@ -67,9 +63,10 @@
 //Update our pane to reflect our contact
 - (void)updatePane
 {
-	NSImage	*userImage;
-	NSLog(@"updatePane");
-	
+	NSColor				*backgroundColor = nil;
+	NSImage				*userImage;
+	NSAttributedString	*infoString;
+		
 	//User Icon
 	if(userImage = [[listObject displayArrayForKey:@"UserIcon"] objectValue]){
 		//MUST make a copy, since resizing and flipping the original image here breaks it everywhere else
@@ -86,21 +83,22 @@
 	[textField_accountName setStringValue:[listObject formattedUID]];	
 	
 	//Text Profile
-	[[textView_profile textStorage] setAttributedString:[listObject statusObjectForKey:@"TextProfile"]];	
-	
-	//Set the background color
-	//	backgroundColor = [infoString attribute:AIBodyColorAttributeName
-	//									atIndex:0 
-	//					  longestEffectiveRange:nil 
-	//									inRange:NSMakeRange(0,[infoString length])];
-	//	[textView_contactProfile setBackgroundColor:(backgroundColor ? backgroundColor : [NSColor whiteColor])];
+	infoString = [listObject statusObjectForKey:@"TextProfile"];
+	[[textView_profile textStorage] setAttributedString:infoString];	
+	if(infoString && [infoString length]){
+		backgroundColor = [infoString attribute:AIBodyColorAttributeName
+										atIndex:0 
+						  longestEffectiveRange:nil 
+										inRange:NSMakeRange(0,[infoString length])];
+	}
+	[textView_profile setBackgroundColor:(backgroundColor ? backgroundColor : [NSColor whiteColor])];
 	
 	//Away & Status
 	NSAttributedString 	*statusMessage;
 	NSMutableString		*status = [NSMutableString string];
 	
 	if([[listObject numberStatusObjectForKey:@"Away"] boolValue]){
-		[status appendString:@"(Away)"];
+		[status appendString:@"(Away) "];
 	}
 	
 	if(statusMessage = [listObject statusObjectForKey:@"StatusMessage"]){
@@ -108,8 +106,5 @@
 	}
 	[textField_status setStringValue:status];	
 }
-
-
-
 
 @end
