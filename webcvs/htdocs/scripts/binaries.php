87f90bc606e68f411c2229d2719a301bc84f5883
adamiser <adamiser@localhost>
2004-06-05 16:03:23 +0000

    Versioning up to 0.58.1.  Do not sync until we're ready

--- webcvs/htdocs/scripts/binaries.php
+++ webcvs/htdocs/scripts/binaries.php
@@ -2,22 +2,22 @@
 
 function DownloadAddress()
 {
-    return("http://prdownloads.sourceforge.net/adium/AdiumX_0.57.1.dmg?download");
+    return("http://prdownloads.sourceforge.net/adium/AdiumX_0.58.1.dmg?download");
 }
 
 function DownloadSourceAddress()
 {
-    return("http://prdownloads.sourceforge.net/adium/AdiumX_Source_0.57.1.tar.gz?download");
+    return("http://prdownloads.sourceforge.net/adium/AdiumX_Source_0.58.1.tar.gz?download");
 }
 
 function DownloadTitle()
 {
-    return("Download v0.57");
+    return("Download v0.58.1");
 }
 
 function DownloadVersion()
 {
-    return("0.57");
+    return("0.58.1");
 }
 
 ?>
