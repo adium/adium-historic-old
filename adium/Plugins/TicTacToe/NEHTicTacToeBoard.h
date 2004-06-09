0f60daa3f4b58ea8b4306d5703eb62d8692a195e
Evan Schoenberg <Evan Schoenberg@localhost>
2004-06-09 16:56:45 +0000

    Ran into some compile-time errors when playing with compiler settings.. it wasn't happy about creating and initing the for() loop variable inside the declaration.

--- adium/Plugins/TicTacToe/NEHTicTacToeBoard.h
+++ adium/Plugins/TicTacToe/NEHTicTacToeBoard.h
@@ -17,8 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#import <Foundation/Foundation.h>
-
 typedef enum { PLAYER_NONE, PLAYER_X, PLAYER_O, PLAYER_DRAW } Player;
 
 enum {
