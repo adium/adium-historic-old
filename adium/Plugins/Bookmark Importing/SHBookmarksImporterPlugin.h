17ef2b1b3d42dafe8a8192088d0cf811db2de6da
Stephen Holt <sholt@steveholt.org>
2004-06-15 16:50:02 +0000

    Parameterizing some dict keys, and turning some @"foo" into static string declarations

--- adium/Plugins/Bookmark Importing/SHBookmarksImporterPlugin.h
+++ adium/Plugins/Bookmark Importing/SHBookmarksImporterPlugin.h
@@ -4,6 +4,9 @@
 //
 //  Created by Stephen Holt on Wed May 19 2004.
 
+#define SH_BOOKMARK_DICT_TITLE      @"Title"
+#define SH_BOOKMARK_DICT_CONTENT    @"Content"
+
 @protocol SHBookmarkImporter <NSObject>
 + (id)newInstanceOfImporter;
 - (NSArray *)availableBookmarks;	//All available bookmarks
