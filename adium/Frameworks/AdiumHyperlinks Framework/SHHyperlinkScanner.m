d5962741ab630cb4c57561574cd9ec7063dd04dd
Evan Schoenberg <Evan Schoenberg@localhost>
2004-06-22 18:51:51 +0000

    a copy] of a mutable object returns another mutable object.  The optimization tip I read somewhere about doing a copy to reduce the memory footprint must have been mistaken.

--- adium/Frameworks/AdiumHyperlinks Framework/SHHyperlinkScanner.m
+++ adium/Frameworks/AdiumHyperlinks Framework/SHHyperlinkScanner.m
@@ -7,7 +7,7 @@
 #import "SHMarkedHyperlink.h"
 #import "SHHyperlinkScanner.h"
 
-static NSCharacterSet *skipSet = nil;
+static NSMutableCharacterSet *skipSet = nil;
 
 @implementation SHHyperlinkScanner
 
@@ -93,14 +93,11 @@ static NSCharacterSet *skipSet = nil;
     NSString    *scanString = nil;
     int location = SHStringOffset; //get our location from SHStringOffset, so we can pick up where we left off.
 	if (!skipSet){
-		NSMutableCharacterSet   *mutableSkipSet = [[NSMutableCharacterSet alloc] init];
-		[mutableSkipSet formUnionWithCharacterSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-		[mutableSkipSet formUnionWithCharacterSet:[NSCharacterSet illegalCharacterSet]];
-		[mutableSkipSet formUnionWithCharacterSet:[NSCharacterSet controlCharacterSet]];
-		[mutableSkipSet formUnionWithCharacterSet:[NSCharacterSet characterSetWithCharactersInString:@"\"'"]];
-		
-		skipSet = [mutableSkipSet copy];
-		[mutableSkipSet release];
+		skipSet = [[NSMutableCharacterSet alloc] init];
+		[skipSet formUnionWithCharacterSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+		[skipSet formUnionWithCharacterSet:[NSCharacterSet illegalCharacterSet]];
+		[skipSet formUnionWithCharacterSet:[NSCharacterSet controlCharacterSet]];
+		[skipSet formUnionWithCharacterSet:[NSCharacterSet characterSetWithCharactersInString:@"\"'"]];
     }
 	
     // scan upto the next whitespace char so that we don't unnecessarity confuse flex
