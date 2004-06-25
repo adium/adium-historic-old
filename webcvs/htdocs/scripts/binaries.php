afb27e9a0b6c3712c35c04de9f9c3780384b2217
adamiser <adamiser@localhost>
2004-06-25 16:20:00 +0000

    Versioiioonning up site

--- webcvs/htdocs/scripts/binaries.php
+++ webcvs/htdocs/scripts/binaries.php
@@ -2,22 +2,22 @@
 
 function DownloadAddress()
 {
-    return("http://prdownloads.sourceforge.net/adium/AdiumX_0.60.1.dmg?download");
+    return("http://prdownloads.sourceforge.net/adium/AdiumX_0.61.dmg?download");
 }
 
 function DownloadSourceAddress()
 {
-    return("http://prdownloads.sourceforge.net/adium/AdiumX_Source_0.60.1.tar.gz?download");
+    return("http://prdownloads.sourceforge.net/adium/AdiumX_Source_0.61.tar.gz?download");
 }
 
 function DownloadTitle()
 {
-    return("Download v0.60");
+    return("Download v0.61");
 }
 
 function DownloadVersion()
 {
-    return("0.60");
+    return("0.61");
 }
 
 ?>
