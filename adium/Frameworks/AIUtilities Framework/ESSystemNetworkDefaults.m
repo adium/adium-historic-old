00dc3c556fd2af94d76319bbc78c3d4922966ce8
Evan Schoenberg <Evan Schoenberg@localhost>
2004-06-25 15:36:18 +0000

    Added ESSystemNetworkDefaults, which provides an easy accessor for getting a specified type of systemwide proxy setting information packaged in a cute lil' NSDictionary.

---
+++ adium/Frameworks/AIUtilities Framework/ESSystemNetworkDefaults.m
@@ -0,0 +1,146 @@
+//
+//  ESSystemNetworkDefaults.m
+//  Adium
+//
+//  Created by Evan Schoenberg on Fri Jun 25 2004.
+
+#import "ESSystemNetworkDefaults.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+#include <CoreServices/CoreServices.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+@implementation ESSystemNetworkDefaults
+
++ (NSDictionary *)systemProxySettingsDictionaryForType:(ProxyType)proxyType
+{
+	NSMutableDictionary *systemProxySettingsDictionary = nil;
+	CFDictionaryRef     proxyDict = nil;
+	
+    Boolean             result;
+    	
+	CFNumberRef			enableKey;
+	CFNumberRef         enableNum = nil;
+    int                 enable;
+    
+	CFNumberRef			portKey;
+	CFNumberRef         portNum = nil;
+    int                 portInt;
+
+	CFStringRef			proxyKey;
+	CFStringRef         hostStr = nil;
+    char				host[300];
+    size_t				hostSize;
+	
+	switch(proxyType){
+		case Proxy_HTTP: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesHTTPEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesHTTPPort;
+			proxyKey = kSCPropNetProxiesHTTPProxy;
+			break;
+		}
+		case Proxy_SOCKS4:
+		case Proxy_SOCKS5: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesSOCKSEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesSOCKSPort;
+			proxyKey = kSCPropNetProxiesSOCKSProxy;
+			break;
+		}
+		case Proxy_HTTPS: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesHTTPSEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesHTTPSPort;
+			proxyKey = kSCPropNetProxiesHTTPSProxy;
+			break;
+		}
+		case Proxy_FTP: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesFTPEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesFTPPort;
+			proxyKey = kSCPropNetProxiesFTPProxy;
+			break;
+		}
+		case Proxy_RTSP: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesRTSPEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesRTSPPort;
+			proxyKey = kSCPropNetProxiesRTSPProxy;
+			break;
+		}
+		case Proxy_Gopher: {
+			enableKey = (CFNumberRef)kSCPropNetProxiesGopherEnable;
+			portKey = (CFNumberRef)kSCPropNetProxiesGopherPort;
+			proxyKey = kSCPropNetProxiesGopherProxy;
+			break;
+		}
+		default: {
+			return nil;
+			break;
+		}
+	}
+	
+	proxyDict = SCDynamicStoreCopyProxies(NULL);
+    result = (proxyDict != NULL);
+	
+    // Get the enable flag.  This isn't a CFBoolean, but a CFNumber.
+    // Check if SOCKS is enabled
+    if (result) {
+        enableNum = (CFNumberRef) CFDictionaryGetValue(proxyDict, enableKey);
+        
+        result = (enableNum != NULL)
+            && (CFGetTypeID(enableNum) == CFNumberGetTypeID());
+    }
+    if (result) {
+        result = CFNumberGetValue(enableNum, kCFNumberIntType,
+                                  &enable) && (enable != 0);
+    }
+    
+    // Get the proxy host.  DNS names must be in ASCII.  If you 
+    // put a non-ASCII character  in the "Secure Web Proxy"
+    // field in the Network preferences panel, the CFStringGetCString
+    // function will fail and this function will return false.
+    if (result) {
+        hostStr = (CFStringRef) CFDictionaryGetValue(proxyDict, proxyKey);
+        
+        result = (hostStr != NULL)
+            && (CFGetTypeID(hostStr) == CFStringGetTypeID());
+    }
+    if (result) {
+        result = CFStringGetCString(hostStr, host,
+                                    (CFIndex) hostSize, [NSString defaultCStringEncoding]);
+    }
+    
+    //Get the proxy port
+    if (result) {
+        portNum = (CFNumberRef) CFDictionaryGetValue(proxyDict, portKey);
+        
+        result = (portNum != NULL)
+            && (CFGetTypeID(portNum) == CFNumberGetTypeID());
+    }
+    if (result) {
+        result = CFNumberGetValue(portNum, kCFNumberIntType, &portInt);
+    }
+    if (result) {
+		NSString		*hostString = [NSString stringWithCString:host];
+		NSDictionary	*authDict = [AIKeychain getDictionaryFromKeychainForKey:hostString];
+		
+		systemProxySettingsDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+			hostString,@"Host",
+			[NSNumber numberWithInt:portInt],@"Port",nil];
+		
+        if(authDict) {            
+			[systemProxySettingsDictionary setObject:[authDict objectForKey:@"username"] forKey:@"Username"];
+			[systemProxySettingsDictionary setObject:[authDict objectForKey:@"password"] forKey:@"Password"];
+            
+        } else {
+            //No username/password.  I think this doesn't need to be an error or anything since it should have been set in the system prefs
+        }
+		
+		// Could check and process kSCPropNetProxiesExceptionsList here, which returns: CFArray[CFString]
+    }    
+    
+    //Clean up
+    if (proxyDict != NULL) {
+        CFRelease(proxyDict);
+    }
+	
+    return systemProxySettingsDictionary;
+}    
+
+@end
