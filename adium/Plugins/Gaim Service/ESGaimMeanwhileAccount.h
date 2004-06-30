9eb6ffeda64be47ebe088e4ef913eebbd98dd8bb
Evan Schoenberg <Evan Schoenberg@localhost>
2004-06-30 20:19:42 +0000

    Added the Lotus Sametime service (via Meanwhile).  It's currently disabled (commented out in CBGaimServicePlugin) because I don't think it's ready for primetime yet.

---
+++ adium/Plugins/Gaim Service/ESGaimMeanwhileAccount.h
@@ -0,0 +1,16 @@
+//
+//  ESGaimMeanwhileAccount.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Mon Jun 28 2004.
+
+#import "CBGaimAccount.h"
+
+#define KEY_MEANWHILE_HOST				@"Meanwhile:Host"
+#define KEY_MEANWHILE_PORT				@"Meanwhile:Port"
+
+@interface ESGaimMeanwhileAccount : CBGaimAccount {
+
+}
+
+@end
---
+++ adium/Plugins/Gaim Service/ESMeanwhileService.m
@@ -0,0 +1,52 @@
+//
+//  ESMeanwhileService.m
+//  Adium
+//
+//  Created by Evan Schoenberg on Mon Jun 28 2004.
+
+#import "ESMeanwhileService.h"
+#import "ESGaimMeanwhileAccount.h"
+#import "ESGaimMeanwhileAccountViewController.h"
+
+@implementation ESMeanwhileService
+
+- (id)initWithService:(id)inService
+{
+    [super initWithService:inService];
+    
+    //Create our handle service type
+	//A sametime UID may need to be in the form "uid=aforbes ou=employee ..."
+    handleServiceType = [[AIServiceType serviceTypeWithIdentifier:@"Sametime"
+                                                      description:@"Lotus Sametime"
+                                                            image:nil
+                                                    caseSensitive:NO
+                                                allowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"+abcdefghijklmnopqrstuvwxyz0123456789@.,_-()= "]
+												ignoredCharacters:[NSCharacterSet characterSetWithCharactersInString:@""]
+													allowedLength:255] retain];
+    
+    //Register this service
+    [[adium accountController] registerService:self];
+    
+    return self;
+}
+
+- (NSString *)identifier
+{
+    return(@"Sametime-LIBGAIM");
+}
+- (NSString *)description
+{
+    return @"Sametime";
+}
+
+- (id)accountWithUID:(NSString *)inUID objectID:(int)inObjectID
+{    
+    return([[[ESGaimMeanwhileAccount alloc] initWithUID:inUID service:self objectID:inObjectID] autorelease]);
+}
+
+- (AIAccountViewController *)accountView
+{
+    return([ESGaimMeanwhileAccountViewController accountView]);
+}
+
+@end
