b1b5184dc2970f06f5e2e0dff95ce4945ce8af00
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-07-07 18:35:59 +0000

    Added a note to GaimCommon.h about compiling the Gaim plugin on UFS.

--- adium/Plugins/Gaim Service/GaimCommon.h
+++ adium/Plugins/Gaim Service/GaimCommon.h
@@ -1,3 +1,5 @@
+//Compiling note: To compile on UFS, do a global find (Project and Frameworks) for "<libgaim" and replace with "<Libgaim".
+//We could change ours in CVS but it wouldn't matter since Gaim itself uses 'libgaim' instead of 'Libgaim'.
 
 //Extensive debug logging
 #define GAIM_DEBUG  FALSE
