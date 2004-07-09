a17004d58fc496596e015c56a3bf3ba824569aeb
Evan Schoenberg <Evan Schoenberg@localhost>
2004-07-09 20:25:39 +0000

    Added ESOutlineViewAdditions, which provides: --- - (id)firstSelectedItem - (NSArray *)arrayOfSelectedItems ---
    If you would like to use the 10.3 methods coded into arrayOfSelectedItems instead of the deprecated ones which are clearly inferior and therefore needed to be removed, simply add
    #define I_WOULD_LIKE_THIS_STUPID_THING_TO_CRASH_CONSTANTLY
    to the top of the file or your target settings.

---
+++ adium/Frameworks/AIUtilities Framework/ESOutlineViewAdditions.m
@@ -0,0 +1,64 @@
+//
+//  ESOutlineViewAdditions.m
+//  Adium
+//
+//  Created by Evan Schoenberg on Fri Jul 09 2004.
+//
+
+#import "ESOutlineViewAdditions.h"
+
+
+@implementation NSOutlineView (ESOutlineViewAdditions)
+
+- (id)firstSelectedItem
+{
+    int selectedRow = [self selectedRow];
+	
+    if(selectedRow >= 0 && selectedRow < [self numberOfRows]){
+        return([self itemAtRow:selectedRow]);
+    }else{
+        return(nil);
+    }
+}
+- (NSArray *)arrayOfSelectedItems
+{
+	NSMutableArray *itemArray = [NSMutableArray array];
+	
+	//The nastiness that selectedRowIndexes returns will crash if called on an item which needs to be reloaded but hasn't been yet.
+	//Interestingly, selectedRowEnumerator does just fine.
+#ifdef I_WOULD_LIKE_THIS_STUPID_THING_TO_CRASH_CONSTANTLY
+	//selectedRowIndexes is recommended in 10.3 or better
+	if ([NSApp isOnPantherOrBetter]){
+		NSIndexSet *indices = [self selectedRowIndexes];
+		unsigned int bufSize = [indices count];
+		unsigned int *buf = malloc(bufSize + 1);
+		unsigned int i;
+		NSRange range = NSMakeRange([indices firstIndex], ([indices lastIndex]-[indices firstIndex]) + 1);
+		[indices getIndexes:buf maxCount:bufSize inIndexRange:&range];
+		
+		for(i = 0; i != bufSize; i++) {
+			unsigned int index = buf[i];
+			id  item = [self itemAtRow:index];
+			if (item && [item retainCount]){
+				[itemArray addObject:item];
+			}
+		}
+		
+		free(buf);
+		
+	}else{
+#endif
+		//selectedRowEnumerator is deprecated as of 10.3... but it works properly
+		NSNumber *row;
+		NSEnumerator *enumerator = [self selectedRowEnumerator]; 
+		while (row = [enumerator nextObject]){
+			[itemArray addObject:[self itemAtRow:[row intValue]]]; 
+		} 
+#ifdef I_WOULD_LIKE_THIS_STUPID_THING_TO_CRASH_CONSTANTLY
+	}
+#endif
+	
+	return (itemArray);
+}
+
+@end
