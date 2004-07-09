4ace46b3d27132e7a0bbe37dae94e7ed8f1db8ea
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-07-09 20:25:39 +0000

    Added ESOutlineViewAdditions, which provides: --- - (id)firstSelectedItem - (NSArray *)arrayOfSelectedItems ---
    If you would like to use the 10.3 methods coded into arrayOfSelectedItems instead of the deprecated ones which are clearly inferior and therefore needed to be removed, simply add
    #define I_WOULD_LIKE_THIS_STUPID_THING_TO_CRASH_CONSTANTLY
    to the top of the file or your target settings.

---
+++ adium/Frameworks/AIUtilities Framework/ESOutlineViewAdditions.h
@@ -0,0 +1,12 @@
+//
+//  ESOutlineViewAdditions.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Fri Jul 09 2004.
+
+@interface NSOutlineView (ESOutlineViewAdditions)
+
+- (id)firstSelectedItem;
+- (NSArray *)arrayOfSelectedItems;
+
+@end
