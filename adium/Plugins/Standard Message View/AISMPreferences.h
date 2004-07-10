79acbf8736a18e7eb4ac2a06b2e14cda1693c4d1
Evan Schoenberg <Evan Schoenberg@localhost>
2004-07-10 07:05:21 +0000

    I don't remember if I had a good reason for adding this, but it doesn't hurt

--- adium/Plugins/Standard Message View/AISMPreferences.h
+++ adium/Plugins/Standard Message View/AISMPreferences.h
@@ -13,6 +13,8 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
+@class JVFontPreviewField;
+
 @interface AISMPreferences : AIPreferencePane {
 
     IBOutlet    NSButton        *checkBox_showUserIcons;
