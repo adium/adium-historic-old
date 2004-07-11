742bb3efe84bead1a03f8188a7bbe21f5ef44146
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-07-11 20:00:21 +0000

    Adding these empty files since CERTAIN people seem to be under the impression that adium should actually compile. Bah.

---
+++ adium/Frameworks/AIUtilities Framework/ESFileWrapperExtension.h
@@ -0,0 +1,14 @@
+//
+//  ESFileWrapperExtension.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Sat Jul 10 2004.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESFileWrapperExtension : NSFileWrapper {
+
+}
+
+@end
---
+++ adium/Frameworks/AIUtilities Framework/ESFileWrapperExtension.m
@@ -0,0 +1,13 @@
+//
+//  ESFileWrapperExtension.m
+//  Adium
+//
+//  Created by Evan Schoenberg on Sat Jul 10 2004.
+//
+
+#import "ESFileWrapperExtension.h"
+
+
+@implementation ESFileWrapperExtension
+
+@end
