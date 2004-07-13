66db5bd5e1c6990d8b03698b3c08a6082bb17e44
Evan Schoenberg <Evan Schoenberg@localhost>
2004-07-13 07:56:40 +0000

    - Account side changes for drag-and-drop file transfer in message windows - A potential fix for an issue using systemwide proxy settings - Cleanup

--- adium/Plugins/Gaim Service/SLGaimCocoaAdapter.h
+++ adium/Plugins/Gaim Service/SLGaimCocoaAdapter.h
@@ -80,6 +80,7 @@
 - (oneway void)MSNRequestBuddyIconFor:(NSString *)inUID onAccount:(id)adiumAccount;
 - (oneway void)OSCAREditComment:(NSString *)comment forUID:(NSString *)inUID onAccount:(id)adiumAccount;
 
+- (void)displayFileSendError;
 - (void *)handleNotifyMessageOfType:(GaimNotifyType)type withTitle:(const char *)title primary:(const char *)primary secondary:(const char *)secondary;
 - (void *)handleNotifyEmails:(size_t)count detailed:(BOOL)detailed subjects:(const char **)subjects froms:(const char **)froms tos:(const char **)tos urls:(const char **)urls;
 - (NSString *)_processGaimImagesInString:(NSString *)inString forAdiumAccount:(id)adiumAccount;
