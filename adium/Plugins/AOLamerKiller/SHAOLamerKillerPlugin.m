39650832af03c12d96684e357dde13af14c0caea
earthmkii <earthmkii@localhost>
2004-07-14 04:30:24 +0000

    fun++; A quick hack of a plugin that counts frequently used annoying contractions sent from the user.  After a threshold (currently 25), all outgoing messages are replaced with one desgined to save the sanity and intelligence of whomever the user talks to thereafter.

---
+++ adium/Plugins/AOLamerKiller/SHAOLamerKillerPlugin.m
@@ -0,0 +1,54 @@
+//
+//  SHAOLamerKillerPlugin.m
+//  Adium
+//
+//  Created by Stephen Holt on Tue Jul 13 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import "SHAOLamerKillerPlugin.h"
+
+
+@implementation SHAOLamerKillerPlugin
+
+- (void)installPlugin
+{
+    [[adium contentController] registerContentFilter:self ofType:AIFilterContent direction:AIFilterOutgoing];
+    
+    stringShitlist = [[NSArray alloc] initWithObjects:@" a/s/l",@" u",@" r",@" omg",@" pic",@" mby",@" 2nite",@" cos",@" coz",@" 2day",
+                                                      @" waz",@" 4ever",@" B4",@" GR8",@" L8R",@" THX",@" TX",@" THKS",@" lyk",
+                                                      @" wut",@" asl",@" ic",@" pen0r",@" vagin0r",@" pr0n",@" fer",@" ffs",@" sif",
+                                                      @" 2morrow",@" 2marro",nil];
+    killCount = 0;
+}
+
+- (void)uninstallPlugin
+{
+}
+
+- (NSAttributedString *)filterAttributedString:(NSAttributedString *)inAttributedString context:(id)context
+{
+    if(inAttributedString && [inAttributedString length]){
+        NSString *localString = [inAttributedString string];
+    
+        NSEnumerator *enumerator = [stringShitlist objectEnumerator];
+        NSString *killString;
+        NSRange killRange;
+
+        while(killString = [enumerator nextObject]){
+            killRange = [[localString lowercaseString] rangeOfString:[killString lowercaseString]];
+            if(NSNotFound != killRange.location){
+                killCount++;
+                NSLog(@"\"%@\" found, killCount now: %i",killString,killCount);
+            }
+        }
+    }
+    
+    if(killCount <= 25){
+        return inAttributedString;
+    }else{
+        return [NSAttributedString stringWithString:@"This user is too lame to type out whole words.  Please stop talking to this person, if you care about your intelligence."];
+    }
+}
+
+@end
