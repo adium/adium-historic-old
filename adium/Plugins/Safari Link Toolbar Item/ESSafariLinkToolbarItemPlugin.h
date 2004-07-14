de3599c49d229e70c2019692c73ebf586a31d8bd
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-07-14 18:44:31 +0000

    Added the Safari Link Toolbar Item plugin, which adds a toolbar item to insert a link to the current page in Safari.

---
+++ adium/Plugins/Safari Link Toolbar Item/ESSafariLinkToolbarItemPlugin.h
@@ -0,0 +1,11 @@
+//
+//  ESSafariLinkToolbarItemPlugin.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Wed Jul 14 2004.
+
+@interface ESSafariLinkToolbarItemPlugin : AIPlugin {
+	NSAppleScript   *safariLinkScript;
+}
+
+@end
