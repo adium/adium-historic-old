245fc4ca810fb6977398ec6559a91d5cb5567a94
dchoby98 <dchoby98@localhost>
2004-07-28 02:36:24 +0000

    Did I mention Stress Test group chattiness? :P

---
+++ adium/Plugins/Stress Test/DCStressTestJoinChatViewController.h
@@ -0,0 +1,15 @@
+//
+//  DCJoinChatViewController.h
+//  Adium
+//
+//  Created by David Clark on Tue Jul 13 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+@interface DCStressTestJoinChatViewController : DCJoinChatViewController {
+
+}
+
+- (IBAction)buttonClicked:(id)sender;
+
+@end
