a2329e17e0ee2bb42cb01ef39e222592c5a80da7
Evan Schoenberg <Evan Schoenberg@localhost>
2004-08-02 07:22:55 +0000

    Hm, no, I got confused.  This is definitely where the unviewed content for chats is handled. I can't remember what the Statud Dock Overlays code had to do with this, but *shrug* updating to the new chat observer code.

--- adium/Plugins/Dock Unviewed Content/AIDockUnviewedContentPlugin.h
+++ adium/Plugins/Dock Unviewed Content/AIDockUnviewedContentPlugin.h
@@ -13,7 +13,7 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
-@interface AIDockUnviewedContentPlugin : AIPlugin <AIListObjectObserver> {
+@interface AIDockUnviewedContentPlugin : AIPlugin <AIChatObserver> {
     NSMutableArray		*unviewedObjectsArray;
     BOOL			unviewedState;
 
