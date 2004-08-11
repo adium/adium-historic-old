9ff7c6225870ac3a325e0d52151d9b44c09f40a4
xpander_ <xpander_@localhost>
2004-08-11 15:05:28 +0000

    Changes for the Migration Assistant.

--- adium/Other/Adium 1.6 Pref Importer/CBOldPrefsImporterAppController.h
+++ adium/Other/Adium 1.6 Pref Importer/CBOldPrefsImporterAppController.h
@@ -11,21 +11,36 @@
     IBOutlet	NSPopUpButton		*popUpButton_account;
     IBOutlet	NSProgressIndicator	*progressIndicator;
     IBOutlet	NSTextField		*currentTask;
-    
+    IBOutlet    NSImageView             *image_AdiumImage;
     IBOutlet	NSWindow		*theSheet;
     IBOutlet	NSPanel			*importListSheet;
     IBOutlet	NSPopUpButton		*popUpButton_user;
     IBOutlet	NSButton		*button_OK;
     IBOutlet	NSButton		*button_Cancel;
+    IBOutlet    NSButton                *button_ImportAwayMessages;
+    IBOutlet    NSPopUpButton           *popUpButton_Clients;
+    IBOutlet    NSButton                *button_Import;
+    IBOutlet    NSProgressIndicator     *spinner_importProgress;
+    IBOutlet    NSTabView               *tabView_ClientTabs;
+    IBOutlet    NSTabView               *tabView_optionsTab;
+    IBOutlet	AIContentController     *contentController;
+    
+    
 }
-
+- (AIContentController *)contentController;
 - (IBAction)importLogs:(id)sender;
 - (IBAction)importAliases:(id)sender;
 - (IBAction)importContacts:(id)sender;
 - (IBAction)sheetButton:(id)sender;
+- (IBAction)importAwayMessages:(id)sender;
+
 - (BOOL)ensureAdiumIsClosed;
+- (void)importiChatAways;
+- (void)importProteusAways;
+- (void)importFireAways;
 
 //NSApplication delegate methods
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication;
+
 
 @end
