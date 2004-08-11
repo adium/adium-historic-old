6d3ea26526f9926f561f21dc7e2b56b937c9bc52
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-08-11 20:46:42 +0000

    Added Sametime away message support

--- adium/Plugins/Gaim Service/ESGaimMeanwhileAccount.m
+++ adium/Plugins/Gaim Service/ESGaimMeanwhileAccount.m
@@ -7,12 +7,17 @@
 
 #import "ESGaimMeanwhileAccount.h"
 
+@interface ESGaimMeanwhileAccount (PRIVATE)
+- (NSAttributedString *)statusMessageForContact:(AIListContact *)theContact;
+@end
+
 @implementation ESGaimMeanwhileAccount
 
 static BOOL didInitMeanwhile = NO;
 
 - (const char*)protocolPlugin
 {
+	[super initSSL];
 	if (!didInitMeanwhile) didInitMeanwhile = gaim_init_meanwhile_plugin();
     return "prpl-meanwhile";
 }
@@ -25,6 +30,43 @@ static BOOL didInitMeanwhile = NO;
 - (NSString *)portKey
 {
 	return KEY_MEANWHILE_PORT;
+}
+
+//Away and away return
+- (oneway void)updateWentAway:(AIListContact *)theContact withData:(void *)data
+{
+	[super updateWentAway:theContact withData:data];
+	[theContact setStatusObject:[self statusMessageForContact:theContact]
+						 forKey:@"StatusMessage"
+						 notify:YES];
+}
+
+- (oneway void)updateAwayReturn:(AIListContact *)theContact withData:(void *)data
+{
+	[super updateWentAway:theContact withData:data];
+	
+	[theContact setStatusObject:[self statusMessageForContact:theContact]
+						 forKey:@"StatusMessage"
+						 notify:YES];
+}
+
+- (NSAttributedString *)statusMessageForContact:(AIListContact *)theContact
+{
+	NSAttributedString		*statusMessage = nil;
+	GaimConnection			*gc = [self gaimAccount]->gc;
+		
+	struct mw_plugin_data	*pd = ((struct mw_plugin_data *)(gc->proto_data));
+	struct mwAwareIdBlock	t = { mwAware_USER, (char *)[[theContact UID] UTF8String], NULL };
+	
+	const char				*statusMessageText = (const char *)mwServiceAware_getText(pd->srvc_aware, &t);
+	NSString				*statusMessageString = [NSString stringWithUTF8String:statusMessageText];
+	
+	if (statusMessageString && [statusMessageString length]){
+		statusMessage = [[[NSAttributedString alloc] initWithString:statusMessageString
+														 attributes:nil] autorelease];
+	}
+	
+	return statusMessage;
 }
 
 @end
