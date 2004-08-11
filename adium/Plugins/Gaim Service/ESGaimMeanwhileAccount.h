27f5dfce2671f13509f5d4a374d8bab88b5832c9
Evan Schoenberg <Evan Schoenberg@localhost>
2004-08-11 20:46:42 +0000

    Added Sametime away message support

--- adium/Plugins/Gaim Service/ESGaimMeanwhileAccount.h
+++ adium/Plugins/Gaim Service/ESGaimMeanwhileAccount.h
@@ -9,6 +9,36 @@
 #define KEY_MEANWHILE_HOST				@"Meanwhile:Host"
 #define KEY_MEANWHILE_PORT				@"Meanwhile:Port"
 
+//From mwgaim.c
+struct mw_plugin_data {
+	struct mwSession *session;
+	
+	struct mwServiceAware *srvc_aware;
+	
+	struct mwServiceConf *srvc_conf;
+	
+	struct mwServiceIM *srvc_im;
+	
+	struct mwServiceStorage *srvc_store;
+	
+	GHashTable *list_map;
+	GHashTable *convo_map;
+	
+	guint save_event;
+};
+
+
+//From libmeanwhile's common.h
+enum mwAwareType {
+	mwAware_USER  = 0x0002
+};
+
+struct mwAwareIdBlock {
+	enum mwAwareType type;
+	char *user;
+	char *community;
+};
+
 @interface ESGaimMeanwhileAccount : CBGaimAccount {
 
 }
