ece42d3e644a8a970de7293ad3f41be93b45dd62
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-08-12 00:55:32 +0000

    Added a tooltip item for meta contacts containing multiple contacts to show each contained contact, with its status icon before it and its service icon after it :)

---
+++ adium/Plugins/MetaContact Contents/ESMetaContactContentsPlugin.h
@@ -0,0 +1,11 @@
+//
+//  ESMetaContactContentsPlugin.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 8/11/04.
+
+@interface ESMetaContactContentsPlugin : AIPlugin <AIContactListTooltipEntry> {
+
+}
+
+@end
