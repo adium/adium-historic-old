a59eb34fb4d6ffaa2036eab25d09d5dcec818fdc
Evan Schoenberg <Evan Schoenberg@localhost>
2004-08-12 18:07:05 +0000

    Hooking up the Zephyr export to .anyone, export to .zephyr.sub, exposure, and encoding preferences

--- adium/Plugins/Gaim Service/ESGaimZephyrAccount.m
+++ adium/Plugins/Gaim Service/ESGaimZephyrAccount.m
@@ -18,4 +18,24 @@ static BOOL didInitZephyr = NO;
     return "prpl-zephyr";
 }
 
+- (void)configureGaimAccount
+{
+	[super configureGaimAccount];
+	
+	NSString	*exposure_level, encoding;
+	BOOL		write_anyone, write_zsubs, allowPlaintext;
+	
+	write_anyone = [[self preferenceForKey:KEY_ZEPHYR_EXPORT_ANYONE group:GROUP_ACCOUNT_STATUS] boolValue];
+	gaim_account_set_bool(account, "write_anyone", write_anyone);
+
+	write_zsubs = [[self preferenceForKey:KEY_ZEPHYR_EXPORT_SUBS group:GROUP_ACCOUNT_STATUS] boolValue];
+	gaim_account_set_bool(account, "write_zsubs", write_zsubs);
+	
+	exposure_level = [self preferenceForKey:KEY_ZEPHYR_EXPOSURE group:GROUP_ACCOUNT_STATUS];
+	gaim_account_set_string(account, "exposure_level", [exposure_level UTF8String]);
+
+	encoding = [self preferenceForKey:KEY_ZEPHYR_ENCODING group:GROUP_ACCOUNT_STATUS];
+	gaim_account_set_string(account, "encoding", [encoding UTF8String]);
+}
+
 @end
