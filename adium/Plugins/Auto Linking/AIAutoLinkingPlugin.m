d570742a8edc28c72fda7a65619165cf85b16693
earthmkii <earthmkii@localhost>
2004-08-23 02:45:18 +0000

    When autolinking, lets skip over custom titled links. Fixes this bug: http://www.funmac.com/showthread.php?t=8444

--- adium/Plugins/Auto Linking/AIAutoLinkingPlugin.m
+++ adium/Plugins/Auto Linking/AIAutoLinkingPlugin.m
@@ -26,7 +26,26 @@
 - (NSAttributedString *)filterAttributedString:(NSAttributedString *)inAttributedString context:(id)context
 {
     SHHyperlinkScanner          *scanner = [[[SHHyperlinkScanner alloc] initWithStrictChecking:NO] autorelease];
-    NSMutableAttributedString   *replacementMessage = [[[NSMutableAttributedString alloc] initWithAttributedString:[scanner linkifyString:inAttributedString]] autorelease];
+    NSMutableAttributedString   *replacementMessage = [[[NSMutableAttributedString alloc] initWithString:@""] autorelease];
+    NSRange                      linkRange = NSMakeRange(0,0);
+    unsigned                     index = 0;
+    unsigned                     stringLength = [inAttributedString length];
+    
+    // do some quick scanning to avoid overwriting custom titled links that happen to have domain names in them.
+    // e.g. if the entire string "check out the new story on adiumx.com" is linked to a specific story URI, then
+    // adiumx.com should not link to only http://adiumx.com
+    while(index < stringLength){
+        if([inAttributedString attribute:NSLinkAttributeName atIndex:index effectiveRange:&linkRange]){
+            // if the link is found at that index, append the link's whole range to the replacement string
+            [replacementMessage appendAttributedString:[inAttributedString attributedSubstringFromRange:linkRange]];
+        }else{
+            // if not, the range to the next link attribute is returned, and we linkify that range's substring
+            [replacementMessage appendAttributedString:[scanner linkifyString:[inAttributedString attributedSubstringFromRange:linkRange]]];
+        }
+        // increase the index
+        index += linkRange.length;
+    }
+    
     return (replacementMessage);
 }
 
