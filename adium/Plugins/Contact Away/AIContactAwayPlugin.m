e60ef6004100faa807111b38ba14eb8ddafc81a7
Evan Schoenberg <Evan Schoenberg@localhost>
2004-08-23 18:05:12 +0000

    If the display name and the away message are the same, show "Away: Yes" rather than "Away Message: Yes"

--- adium/Plugins/Contact Away/AIContactAwayPlugin.m
+++ adium/Plugins/Contact Away/AIContactAwayPlugin.m
@@ -15,6 +15,12 @@
 
 #import "AIContactAwayPlugin.h"
 
+#define	AWAY_LABEL			AILocalizedString(@"Away",nil);
+#define	AWAY_MESSAGE_LABEL	AILocalizedString(@"Away Message",nil);
+#define	STATUS_LABEL		AILocalizedString(@"Status",nil);
+
+#define AWAY_YES			AILocalizedString(@"Yes",nil);
+
 @implementation AIContactAwayPlugin
 
 - (void)installPlugin
@@ -39,12 +45,22 @@
     //Return the correct string
     if(statusMessage != nil && [statusMessage length] != 0){
 		if (away){
-			entry = @"Away Message";
+			
+			//Check to make sure we're not duplicating server display name information
+			NSString	*serverDisplayName = [inObject statusObjectForKey:@"Server Display Name"];
+			
+			//Return the correct string
+			if ([serverDisplayName isEqualToString:[statusMessage string]]){
+				entry = AWAY_LABEL;
+			}else{
+				entry = AWAY_MESSAGE_LABEL;
+			}
+			
 		}else{
-			entry = @"Status";
+			entry = STATUS_LABEL;
 		}
     }else if(away){
-		entry = @"Away";
+		entry = AWAY_LABEL;
     }
     
     return(entry);
@@ -56,7 +72,7 @@
     NSAttributedString 	*statusMessage = nil;
 	NSString			*serverDisplayName = nil;
 	
-    BOOL		away;
+    BOOL				away;
 	
     //Get the away state
     away = [inObject integerStatusObjectForKey:@"Away"];
@@ -72,13 +88,13 @@
 		//If the status and server display name are the same, just display YES for away since we'll display the
 		//server display name itself in the proper place.
 		if (away){
-			entry = [[[NSAttributedString alloc] initWithString:@"Yes"] autorelease];
+			entry = [[[NSAttributedString alloc] initWithString:AWAY_YES] autorelease];
 		}
 	}else{
 		if(statusMessage != nil && [statusMessage length] != 0){
 			entry = statusMessage;
 		}else if(away){
-			entry = [[[NSAttributedString alloc] initWithString:@"Yes"] autorelease];
+			entry = [[[NSAttributedString alloc] initWithString:AWAY_YES] autorelease];
 		}
 	}
 	
