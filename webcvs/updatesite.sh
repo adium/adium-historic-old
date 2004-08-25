32177948967891957b5cfca79503e40397556234
Evan Schoenberg <Evan Schoenberg@localhost>
2004-08-25 23:29:23 +0000

    Moving

--- webcvs/updatesite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if test "x$1" != x; then
-	SFUSER=$1
-else
-	SFUSER=$USER
-fi
-
-/usr/bin/ssh $SFUSER@shell1.sourceforge.net 'cd /home/groups/a/ad/adium/htdocs && cvs -q -z3 update -Pd'
