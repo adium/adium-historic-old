244cda60baad859b803aa8576f79e30d7e5bf897
adamiser <adamiser@localhost>
2004-08-27 23:40:02 +0000

    updating includes

--- Plugins/Gaim Service/GaimServices.h
+++ Plugins/Gaim Service/GaimServices.h
@@ -4,7 +4,9 @@
 //
 //  Created by Evan Schoenberg on Sun Dec 28 2003.
 
-#import "CBOscarService.h"
+#import "ESAIMService.h"
+#import "ESICQService.h"
+#import "ESDotMacService.h"
 #import "ESGaduGaduService.h"
 #import "ESJabberService.h"
 #import "ESMeanwhileService.h"
