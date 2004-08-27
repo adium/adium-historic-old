ff3b9a64eae4ac3aa005df6a109e553a1896ed5e
adamiser <adamiser@localhost>
2004-08-27 23:58:42 +0000

    Service class for rvous, wee

---
+++ Plugins/Rendezvous/AWRendezvousService.h
@@ -0,0 +1,16 @@
+//
+//  AWRendezvousService.h
+//  Adium
+//
+//  Created by Adam Iser on 8/26/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AWRendezvousService : AIService {
+
+}
+
+@end
