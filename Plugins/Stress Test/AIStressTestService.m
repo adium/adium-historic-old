4e1000a7bde60eb8edc8c4dcbea675fda85ecee8
adamiser <adamiser@localhost>
2004-08-27 23:26:46 +0000

    no cheering :P .. service class for stress test to bring it in line with the others.

---
+++ Plugins/Stress Test/AIStressTestService.m
@@ -0,0 +1,57 @@
+//
+//  AIStressTestService.m
+//  Adium
+//
+//  Created by Adam Iser on 8/26/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIStressTestService.h"
+#import "AIStressTestAccount.h"
+#import "DCStressTestJoinChatViewController.h"
+
+@implementation AIStressTestService
+
+//Account Creation
+- (Class)accountClass{
+	return([AIStressTestAccount class]);
+}
+
+- (AIAccountViewController *)accountView{
+    return(nil);
+}
+
+- (DCJoinChatViewController *)joinChatView{
+	return([DCStressTestJoinChatViewController joinChatView]);
+}
+
+//Service Description
+- (NSString *)serviceCodeUniqueID{
+	return(@"Stress-Test");
+}
+- (NSString *)serviceID{
+	return(@"Stress Test");
+}
+- (NSString *)serviceClass{
+	return(@"Stress Test");
+}
+- (NSString *)shortDescription{
+	return(@"Stress Test");
+}
+- (NSString *)longDescription{
+	return(@"Stress Test (Das ist verboten)");
+}
+- (NSCharacterSet *)allowedCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyz0123456789@."]);
+}
+- (NSCharacterSet *)ignoredCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@""]);
+}
+- (int)allowedLength{
+	return(20);
+}
+- (BOOL)caseSensitive{
+	return(NO);
+}
+
+@end
