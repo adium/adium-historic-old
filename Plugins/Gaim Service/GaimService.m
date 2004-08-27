d42dc506f81cd6702bb4127611bbef0748f3a5ec
adamiser <adamiser@localhost>
2004-08-27 23:39:50 +0000

    emptying, keeping shell incase we need this later

--- Plugins/Gaim Service/GaimService.m
+++ Plugins/Gaim Service/GaimService.m
@@ -10,48 +10,4 @@
 
 @implementation GaimService
 
-//Methods for subclasses to override
-- (NSString *)identifier
-{
-    return nil;
-}
-- (NSString *)description
-{
-    return nil;
-}
-- (id)accountWithUID:(NSString *)inUID objectID:(int)inObjectID
-{
-    return nil;
-}
-
-//GaimService methods
-- (id)initWithService:(id)inService
-{
-    [super init];
-    
-    service = [inService retain];
-    
-    return self;
-}
-
-- (AIServiceType *)handleServiceType
-{
-    return(handleServiceType);
-}
-
-- (NSString *)gaimDescriptionSuffix
-{
-    return(@"");
-}
-
-- (AIAccountViewController *)accountView
-{
-	return([ESGaimAccountViewController accountView]);
-}
-
-- (DCJoinChatViewController *)joinChatView
-{
-	return nil;
-}
-
 @end
