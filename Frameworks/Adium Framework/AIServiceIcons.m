490f9d0eece9417bf37a9cbf40ffc9f2cc8fde8f
adamiser <adamiser@localhost>
2004-08-27 22:38:43 +0000

    Service icon packs can now specify large icons, small icons, and list icons

--- Frameworks/Adium Framework/AIServiceIcons.m
+++ Frameworks/Adium Framework/AIServiceIcons.m
@@ -8,55 +8,61 @@
 
 #import "AIServiceIcons.h"
 
-static NSMutableDictionary	*serviceIcons = nil;
-static NSMutableDictionary	*serviceIconsFlipped = nil;
+static NSMutableDictionary	*serviceIcons[NUMBER_OF_SERVICE_ICON_TYPES][NUMBER_OF_ICON_DIRECTIONS];
 
 static NSString				*serviceIconBasePath = nil;
-static NSDictionary			*serviceIconNames = nil;
+static NSDictionary			*serviceIconNames[NUMBER_OF_SERVICE_ICON_TYPES];
 
 @implementation AIServiceIcons
 
-//Retrive the correct service icon for a contact
-+ (NSImage *)serviceIconForContact:(AIListContact *)inContact flipped:(BOOL)isFlipped
++ (void)initialize
 {
-#warning (Adam) [inContact serviceID] would be nice here, except that it returns vague values
-	/*
-	 serviceID is less of a service ID and more of a compatability ID type thing.  So it will be AIM for all contacts
-	 on that 'service' and not give us the correct values of aim/icq/.mac ... :\
-	 */
+	int i, j;
+
+	[super initialize];
 	
-	NSString			*serviceID = [[[[inContact account] service] handleServiceType] identifier];
-	NSMutableDictionary	*cacheDict;
+	//Allocate our service icon cache
+	for(i = 0; i < NUMBER_OF_SERVICE_ICON_TYPES; i++){
+		for(j = 0; j < NUMBER_OF_ICON_DIRECTIONS; j++){
+			serviceIcons[i][j] = [[NSMutableDictionary alloc] init];
+		}
+	}
+
+	//Hard coded icon pack for now
+	NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Service Icons"] stringByExpandingTildeInPath];
+	[self setActiveServiceIconsFromPath:[path stringByAppendingPathComponent:@"Gaim.AdiumServiceIcons"]];
+}
+
+//Retrive the correct service icon for a contact
++ (NSImage *)serviceIconForObject:(AIListObject *)inObject type:(AIServiceIconType)iconType direction:(AIIconDirection)iconDirection
+{
+	return([self serviceIconForService:[inObject service] type:iconType direction:iconDirection]);
+}
+
+//Retrieve the correct service icon for a service
++ (NSImage *)serviceIconForService:(AIService *)service type:(AIServiceIconType)iconType direction:(AIIconDirection)iconDirection
+{
+	NSString			*serviceID = [service serviceID];
 	NSImage				*serviceIcon;
 	
-#warning xxx temp
-	if(!serviceIconNames){
-		NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Service Icons"] stringByExpandingTildeInPath];
-		NSLog(@"%@",path);
-		[self setActiveServiceIconsFromPath:[path stringByAppendingPathComponent:@"Gaim.AdiumServiceIcons"]];
-	}
-#warning xxx temp
-
-	if(!serviceIcons) serviceIconsFlipped = [[NSMutableDictionary alloc] init];
-	if(!serviceIconsFlipped) serviceIconsFlipped = [[NSMutableDictionary alloc] init];
-
 	//Retrieve the service icon from our cache
-	cacheDict = (isFlipped ? serviceIconsFlipped : serviceIcons);
-	serviceIcon = [cacheDict objectForKey:serviceID];
+	serviceIcon = [serviceIcons[iconType][iconDirection] objectForKey:serviceID];
 	
 	//Load the service icon if necessary
 	if(!serviceIcon){
-		NSString	*path = [serviceIconBasePath stringByAppendingPathComponent:[serviceIconNames objectForKey:serviceID]];
-		NSLog(@"%@:%@",serviceID,path);
+		NSString	*path = [serviceIconBasePath stringByAppendingPathComponent:[serviceIconNames[iconType] objectForKey:serviceID]];
 
 		if(path){
 			serviceIcon = [[[NSImage alloc] initWithContentsOfFile:path] autorelease];
+
 			if(serviceIcon){
-				if(isFlipped) [serviceIcon setFlipped:YES];
-				[cacheDict setObject:serviceIcon forKey:serviceID];
+				if(iconDirection == AIIconFlipped) [serviceIcon setFlipped:YES];
+				[serviceIcons[iconType][iconDirection] setObject:serviceIcon forKey:serviceID];
 			}
 		}
 	}
+	
+	return(serviceIcon);
 }
 
 //Set the active service icon pack
@@ -68,8 +74,11 @@ static NSDictionary			*serviceIconNames = nil;
 		
 		if(serviceIconPack && [[serviceIconPack objectForKey:@"AdiumSetVersion"] intValue] == 1){
 			serviceIconBasePath = [inPath retain];
-			serviceIconNames = [[serviceIconPack objectForKey:@"Icons"] retain];
-			NSLog(@"%@",serviceIconNames);
+			
+			serviceIconNames[AIServiceIconSmall] = [[serviceIconPack objectForKey:@"Interface-Small"] retain];
+			serviceIconNames[AIServiceIconLarge] = [[serviceIconPack objectForKey:@"Interface-Large"] retain];
+			serviceIconNames[AIServiceIconList] = [[serviceIconPack objectForKey:@"List"] retain];
+
 			return(YES);
 		}else{
 			return(NO);
