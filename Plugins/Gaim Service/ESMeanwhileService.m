c8e725e2962cf27fe8abaeee37747c2f8653687b
adamiser <adamiser@localhost>
2004-08-27 23:37:52 +0000

    service updates

--- Plugins/Gaim Service/ESMeanwhileService.m
+++ Plugins/Gaim Service/ESMeanwhileService.m
@@ -11,53 +11,46 @@
 
 @implementation ESMeanwhileService
 
-- (id)initWithService:(id)inService
-{
-    [super initWithService:inService];
-    
-	NSImage *image = [NSImage imageNamed:@"meanwhile" forClass:[self class]];
-		
-    //Create our handle service type
-	//A sametime UID may need to be in the form "uid=C'\awef@@+ +3 ou=fuEWJGhw67_ efWEf ..." I'm serious.
-    handleServiceType = [[AIServiceType serviceTypeWithIdentifier:@"Sametime"
-                                                      description:@"Lotus Sametime"
-                                                            image:image
-														menuImage:nil
-                                                    caseSensitive:YES
-                                                allowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@.,_-()='/ "]
-												ignoredCharacters:[NSCharacterSet characterSetWithCharactersInString:@""]
-													allowedLength:1000] retain];
-    
-    //Register this service
-    [[adium accountController] registerService:self];
-    
-    return self;
+//Account Creation
+- (Class)accountClass{
+	return([ESGaimMeanwhileAccount class]);
 }
 
-- (NSString *)identifier
-{
-    return(@"Sametime-LIBGAIM");
-}
-- (NSString *)description
-{
-    return @"Sametime";
-}
-
-- (id)accountWithUID:(NSString *)inUID objectID:(int)inObjectID
-{    
-    return([[[ESGaimMeanwhileAccount alloc] initWithUID:inUID service:self objectID:inObjectID] autorelease]);
-}
-
-- (AIAccountViewController *)accountView
-{
+- (AIAccountViewController *)accountView{
     return([ESGaimMeanwhileAccountViewController accountView]);
 }
 
-
-- (DCJoinChatViewController *)joinChatView
-{
+- (DCJoinChatViewController *)joinChatView{
 	return([DCGaimMeanwhileJoinChatViewController joinChatView]);
 }
 
+//Service Description
+- (NSString *)serviceCodeUniqueID{
+	return(@"libgaim-Sametime");
+}
+- (NSString *)serviceID{
+	return(@"Sametime");
+}
+- (NSString *)serviceClass{
+	return(@"Sametime");
+}
+- (NSString *)shortDescription{
+	return(@"Sametime");
+}
+- (NSString *)longDescription{
+	return(@"Lotus Sametime");
+}
+- (NSCharacterSet *)allowedCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@"+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@.,_-()='/ "]);
+}
+- (NSCharacterSet *)ignoredCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@""]);
+}
+- (int)allowedLength{
+	return(1000);
+}
+- (BOOL)caseSensitive{
+	return(YES);
+}
 
 @end
