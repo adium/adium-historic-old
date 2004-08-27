c801f9a941189c3529e6045e0f16e31f7ed365ef
adamiser <adamiser@localhost>
2004-08-27 23:32:03 +0000

    Updating for new service methods

--- Plugins/Gaim Service/ESMSNService.m
+++ Plugins/Gaim Service/ESMSNService.m
@@ -13,53 +13,46 @@
 
 @implementation ESMSNService
 
-- (id)initWithService:(id)inService
-{
-    [super initWithService:inService];
-    
-	NSImage *image = [NSImage imageNamed:@"msn" forClass:[self class]];
-	
-    //Create our handle service type
-    handleServiceType = [[AIServiceType serviceTypeWithIdentifier:@"MSN"
-                                                      description:@"MSN"
-                                                            image:image
-														menuImage:nil
-                                                    caseSensitive:NO
-                                                allowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"+abcdefghijklmnopqrstuvwxyz0123456789@._-"]
-												ignoredCharacters:[NSCharacterSet characterSetWithCharactersInString:@""]
-													allowedLength:113] retain];
-    
-    //Register this service
-    [[adium accountController] registerService:self];
-    
-    return self;
+//Account Creation
+- (Class)accountClass{
+	return([ESGaimMSNAccount class]);
 }
 
-- (NSString *)identifier
-{
-    return(@"MSN-LIBGAIM");
-}
-- (NSString *)description
-{
-    return @"MSN";
-}
-
-- (id)accountWithUID:(NSString *)inUID objectID:(int)inObjectID
-{    
-    return([[[ESGaimMSNAccount alloc] initWithUID:inUID service:self objectID:inObjectID] autorelease]);
-}
-
-- (AIAccountViewController *)accountView
-{
+- (AIAccountViewController *)accountView{
     return([ESGaimMSNAccountViewController accountView]);
 }
 
-
-
-- (DCJoinChatViewController *)joinChatView
-{
+- (DCJoinChatViewController *)joinChatView{
 	return([DCGaimMSNJoinChatViewController joinChatView]);
 }
 
+//Service Description
+- (NSString *)serviceCodeUniqueID{
+	return(@"libgaim-MSN");
+}
+- (NSString *)serviceID{
+	return(@"MSN");
+}
+- (NSString *)serviceClass{
+	return(@"MSN");
+}
+- (NSString *)shortDescription{
+	return(@"MSN");
+}
+- (NSString *)longDescription{
+	return(@"MSN");
+}
+- (NSCharacterSet *)allowedCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@"+abcdefghijklmnopqrstuvwxyz0123456789@._-"]);
+}
+- (NSCharacterSet *)ignoredCharacters{
+	return([NSCharacterSet characterSetWithCharactersInString:@""]);
+}
+- (int)allowedLength{
+	return(113);
+}
+- (BOOL)caseSensitive{
+	return(NO);
+}
 
 @end
