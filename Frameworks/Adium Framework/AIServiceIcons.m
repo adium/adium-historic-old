e34fcc2325056656c4f8b3f8af7352a9d1ed20f2
adamiser <adamiser@localhost>
2004-08-29 14:17:09 +0000

    Adding a method for fetching a service icon by service ID (for the log viewer)

--- Frameworks/Adium Framework/AIServiceIcons.m
+++ Frameworks/Adium Framework/AIServiceIcons.m
@@ -36,13 +36,18 @@ static NSDictionary			*serviceIconNames[NUMBER_OF_SERVICE_ICON_TYPES];
 //Retrive the correct service icon for a contact
 + (NSImage *)serviceIconForObject:(AIListObject *)inObject type:(AIServiceIconType)iconType direction:(AIIconDirection)iconDirection
 {
-	return([self serviceIconForService:[inObject service] type:iconType direction:iconDirection]);
+	return([self serviceIconForServiceID:[[inObject service] serviceID] type:iconType direction:iconDirection]);
 }
-
+	
 //Retrieve the correct service icon for a service
 + (NSImage *)serviceIconForService:(AIService *)service type:(AIServiceIconType)iconType direction:(AIIconDirection)iconDirection
 {
-	NSString			*serviceID = [service serviceID];
+	return([self serviceIconForServiceID:[service serviceID] type:iconType direction:iconDirection]);
+}
+
+//Retrieve the correct service icon for a service by ID
++ (NSImage *)serviceIconForServiceID:(NSString *)serviceID type:(AIServiceIconType)iconType direction:(AIIconDirection)iconDirection
+{
 	NSImage				*serviceIcon;
 	
 	//Retrieve the service icon from our cache
