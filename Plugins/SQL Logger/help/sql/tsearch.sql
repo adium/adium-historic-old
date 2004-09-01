273118e8a1505c0d010b489a6990405f2fa6403a
jmelloy <jmelloy@localhost>
2004-09-01 14:48:26 +0000

    Added calendar to search, plus OCD

--- Plugins/SQL Logger/help/sql/tsearch.sql
+++ Plugins/SQL Logger/help/sql/tsearch.sql
@@ -1,6 +1,6 @@
 -- Jeffrey Melloy <jmelloy@visualdistortion.org>
 
--- $URL: http://svn.visualdistortion.org/repos/projects/adium/sql/tsearch.sql $
+-- $URL: http://svn.visualdistortion.org/repos/projects/sqllogger/sql/tsearch.sql $
 -- $Rev: 809 $ $Date$
 
 -- This script adds full-text index searching (fast).
