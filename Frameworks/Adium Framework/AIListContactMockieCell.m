fb678b40f7ba1612ba47cca344c41adf189f68c2
adamiser <adamiser@localhost>
2004-09-03 21:51:22 +0000

    Adding cell level highlighting for mockie cells (so we can get the bottom corners rounded)

--- Frameworks/Adium Framework/AIListContactMockieCell.m
+++ Frameworks/Adium Framework/AIListContactMockieCell.m
@@ -22,25 +22,42 @@
 //Draw the background of our cell
 - (void)drawBackgroundWithFrame:(NSRect)rect
 {
-	int		row = [controlView rowForItem:listObject];
-	NSColor	*labelColor;
-	
-	//Label color.  If there is no label color we draw the background color (taking care of gridding if needed)
-	//We cannot use the regular table background drawing for mockie cells because of the rounded corners
-	//at the top and bottom of the groups.
-	labelColor = [self labelColor];
-	if(!labelColor) labelColor = (drawGrid ? [controlView backgroundColorForRow:row] : [controlView backgroundColor]);
-	[labelColor set];
-
-	//Draw the bottom corners rounded if this is the last cell in a group
-	if(row >= [controlView numberOfRows]-1 || [controlView isExpandable:[controlView itemAtRow:row+1]]){
-		[[NSBezierPath bezierPathWithRoundedBottomCorners:rect radius:MOCKIE_RADIUS] fill];
-	}else{
-		[NSBezierPath fillRect:rect];
+	if(![self isSelectionInverted]){
+		int		row = [controlView rowForItem:listObject];
+		NSColor	*labelColor;
+		
+		//Label color.  If there is no label color we draw the background color (taking care of gridding if needed)
+		//We cannot use the regular table background drawing for mockie cells because of the rounded corners
+		//at the top and bottom of the groups.
+		labelColor = [self labelColor];
+		if(!labelColor) labelColor = (drawGrid ? [controlView backgroundColorForRow:row] : [controlView backgroundColor]);
+		[labelColor set];
+		
+		//Draw the bottom corners rounded if this is the last cell in a group
+		if(row >= [controlView numberOfRows]-1 || [controlView isExpandable:[controlView itemAtRow:row+1]]){
+			[[NSBezierPath bezierPathWithRoundedBottomCorners:rect radius:MOCKIE_RADIUS] fill];
+		}else{
+			[NSBezierPath fillRect:rect];
+		}
 	}
 }
 
-//No need to the grid if we have a status color to draw
+//Draw a custom selection
+- (void)drawSelectionWithFrame:(NSRect)cellFrame
+{
+	if([self isSelectionInverted]){
+		AIGradient	*gradient = [AIGradient selectedControlGradientWithDirection:AIVertical];
+		int			row = [controlView rowForItem:listObject];
+		
+		if(row >= [controlView numberOfRows]-1 || [controlView isExpandable:[controlView itemAtRow:row+1]]){
+			[gradient drawInBezierPath:[NSBezierPath bezierPathWithRoundedBottomCorners:cellFrame radius:MOCKIE_RADIUS]];
+		}else{
+			[gradient drawInRect:cellFrame];
+		}
+	}
+}
+
+//We handle gridding on our own
 - (BOOL)drawGridBehindCell
 {
 	return(NO);
