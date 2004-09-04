3b5ce9c36dd1d5e8c657a6bf5a9f306ece9ae7a1
dchoby98 <dchoby98@localhost>
2004-09-04 04:55:41 +0000

    Fixing the Version Checker: Added a automatic check ever 24 hours if automatic checking is enabled. Made sure that automatic checking (whether every 24 hours or not) really does happen if we have a net connection, not just if there's a net connection when we start up. Added clever comments.

--- Plugins/Version Checker/CPFVersionChecker.h
+++ Plugins/Version Checker/CPFVersionChecker.h
@@ -1,4 +1,9 @@
-/* CPFVersionChecker */
+//
+//  CPFVersionChecker.h
+//  Adium
+//
+//  Created by Christopher Forsythe on Sat Mar 20 2004.
+//
 
 #define KEY_LAST_UPDATE_ASKED		@"LastUpdateAsked"
 #define PREF_GROUP_UPDATING			@"Updating"
@@ -8,8 +13,13 @@
     NSMenuItem 	*versionCheckerMenuItem;
 	BOOL		observingListUpdates;
 	BOOL		checkingManually;
+	BOOL		timerActive;
+	NSTimer		*timer;
 }
 
 - (void)checkForNewVersion:(id)sender;
+- (void)timerCheckForNewVersion:(NSTimer *)timer;
+- (void)startTimerChecking;
+- (void)endTimerChecking;
 
 @end
