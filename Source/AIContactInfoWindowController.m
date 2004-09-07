037f1630f15dc5fe9cb2002e136535393702b597
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-07 06:52:13 +0000

    We can now drag to reorder contacts in the get info window on meta contacts.  The ordering information is saved and used to determine the preferred contact for various operations including opening a message window... higher up in the list is higher priority. Also, you can now add contacts to metacontacts by dragging them from the contact list into the metacontact info window drawer.
    Or, as it might be more succinctly put:
    Andthenyoudragandropanditmakesyougowheeee!

--- Source/AIContactInfoWindowController.m
+++ Source/AIContactInfoWindowController.m
@@ -442,6 +442,129 @@ static AIContactInfoWindowController *sharedContactInfoInstance = nil;
 	}
 }
 
+//
+- (NSDragOperation)outlineView:(NSOutlineView*)outlineView validateDrop:(id <NSDraggingInfo>)info proposedItem:(id)item proposedChildIndex:(int)index
+{
+	NSPasteboard	*draggingPasteboard = [info draggingPasteboard];
+    NSString		*avaliableType = [draggingPasteboard availableTypeFromArray:[NSArray arrayWithObject:@"AIListObject"]];
+	
+	//No dropping into contacts
+    if([avaliableType isEqualToString:@"AIListObject"]){
+		
+		id	primaryDragItem = nil;
+		
+		//If we don't have drag items, we are dragging from another instance; build our own dragItems array
+		//using the supplied internalObjectIDs
+		if (!dragItems){
+			if ([[draggingPasteboard availableTypeFromArray:[NSArray arrayWithObject:@"AIListObjectUniqueIDs"]] isEqualToString:@"AIListObjectUniqueIDs"]){
+				NSArray			*dragItemsUniqueIDs;
+				NSMutableArray	*arrayOfDragItems;
+				NSString		*uniqueID;
+				NSEnumerator	*enumerator;
+					
+				dragItemsUniqueIDs = [draggingPasteboard propertyListForType:@"AIListObjectUniqueIDs"];
+				arrayOfDragItems = [NSMutableArray array];
+				
+				enumerator = [dragItemsUniqueIDs objectEnumerator];
+				while (uniqueID = [enumerator nextObject]){
+					[arrayOfDragItems addObject:[[adium contactController] existingListObjectWithUniqueID:uniqueID]];
+				}
+
+				//We will release this when the drag is completed
+				dragItems = [arrayOfDragItems retain];
+			}
+		}
+
+		primaryDragItem = [dragItems objectAtIndex:0];			
+
+		if([primaryDragItem isKindOfClass:[AIListGroup class]]){
+			//Disallow dragging groups into the contact info window
+			return(NSDragOperationNone);
+			
+		}else{
+			//Disallow dragging contacts into anything besides the contact list
+			if(index == -1){
+
+				//The contactList is 'nil' to the outlineView
+				[outlineView setDropItem:nil dropChildIndex:[outlineView rowForItem:item]];
+			}
+		}
+	}
+	
+	return(NSDragOperationPrivate);
+}
+
+//
+- (BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id <NSDraggingInfo>)info item:(id)item childIndex:(int)index
+{
+	NSPasteboard	*draggingPasteboard = [info draggingPasteboard];
+    NSString	*availableType = [draggingPasteboard availableTypeFromArray:[NSArray arrayWithObject:@"AIListObject"]];
+    
+	//No longer in a drag, so allow tooltips again
+    if([availableType isEqualToString:@"AIListObject"]){
+		
+		//If we don't have drag items, we are dragging from another instance; build our own dragItems array
+		//using the supplied internalObjectIDs
+		if (!dragItems){
+			if ([[draggingPasteboard availableTypeFromArray:[NSArray arrayWithObject:@"AIListObjectUniqueIDs"]] isEqualToString:@"AIListObjectUniqueIDs"]){
+				NSArray			*dragItemsUniqueIDs;
+				NSMutableArray	*arrayOfDragItems;
+				NSString		*uniqueID;
+				NSEnumerator	*enumerator;
+				
+				dragItemsUniqueIDs = [draggingPasteboard propertyListForType:@"AIListObjectUniqueIDs"];
+				arrayOfDragItems = [NSMutableArray array];
+				
+				enumerator = [dragItemsUniqueIDs objectEnumerator];
+				while (uniqueID = [enumerator nextObject]){
+					[arrayOfDragItems addObject:[[adium contactController] existingListObjectWithUniqueID:uniqueID]];
+				}
+				
+				//We will release this when the drag is completed
+				dragItems = [arrayOfDragItems retain];
+			}
+		}
+		
+		//The tree root is not associated with our root contact list group, so we need to make that association here
+		if(item == nil) item = contactList;
+		
+		//Move the list object to its new location
+		if([item isKindOfClass:[AIMetaContact class]]){
+			
+			NSMutableArray	*realDragItems = [NSMutableArray array];
+			NSEnumerator	*enumerator;
+			AIListObject	*aDragItem;
+			
+			if (index == [outlineView numberOfRows]){
+				index = [item containedObjectsCount];
+			}else{
+				//The outline view has one unique contact for each service/UID combination, while the metacontact
+				//has a containedObjectsArray with multiple contacts for each.  We want to find which item is at
+				//our drop row, then determine the index in the metacontact of that item.  That's the index we move to.
+				index = [(AIMetaContact *)item indexOfObject:[outlineView itemAtRow:index]];
+			}
+			
+			enumerator = [dragItems objectEnumerator];
+			while (aDragItem = [enumerator nextObject]){
+				if ([aDragItem isMemberOfClass:[AIListContact class]]){
+					//For listContacts, add all contacts with the same service and UID (on all accounts)
+					[realDragItems addObjectsFromArray:[[adium contactController] allContactsWithService:[aDragItem service] 
+																									 UID:[aDragItem UID]]];
+				}else{
+					[realDragItems addObject:aDragItem];
+				}
+			}
+			
+			[[adium contactController] moveListObjects:realDragItems toGroup:item index:index];
+			[outlineView reloadData];
+		}
+	}
+	
+    return(YES);
+}
+
+
+
 //Due to a bug in NSDrawer, convertPoint:fromView reports a point too low by the trailingOffset 
 //when our contact list is in a drawer.
 - (AIListObject *)contactListItemAtScreenPoint:(NSPoint)screenPoint
