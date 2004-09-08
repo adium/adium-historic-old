e6a07be3f83e9017b77ff4270a096b8044e522e1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-08 20:36:43 +0000

    Added another argument to AIGhettoHTMLEncoder, encodeSpaces:, which only needs to be YES when the encoded message is destined for display in an actual web browser or a webkit view.

--- Frameworks/AIUtilities Framework/AIHTMLDecoder.h
+++ Frameworks/AIUtilities Framework/AIHTMLDecoder.h
@@ -21,7 +21,20 @@
 
 + (NSAttributedString *)decodeHTML:(NSString *)inMessage;
 + (NSString *)encodeHTML:(NSAttributedString *)inMessage encodeFullString:(BOOL)encodeFullString;
-+ (NSString *)encodeHTML:(NSAttributedString *)inMessage headers:(BOOL)includeHeaders fontTags:(BOOL)includeFontTags includingColorTags:(BOOL)includeColorTags closeFontTags:(BOOL)closeFontTags styleTags:(BOOL)includeStyleTags closeStyleTagsOnFontChange:(BOOL)closeStyleTagsOnFontChange encodeNonASCII:(BOOL)encodeNonASCII imagesPath:(NSString *)imagesPath attachmentsAsText:(BOOL)attachmentsAsText attachmentImagesOnlyForSending:(BOOL)attachmentImagesOnlyForSending simpleTagsOnly:(BOOL)simpleOnly;
+
++ (NSString *)encodeHTML:(NSAttributedString *)inMessage
+				 headers:(BOOL)includeHeaders 
+				fontTags:(BOOL)includeFontTags
+	  includingColorTags:(BOOL)includeColorTags 
+		   closeFontTags:(BOOL)closeFontTags
+			   styleTags:(BOOL)includeStyleTags
+ closeStyleTagsOnFontChange:(BOOL)closeStyleTagsOnFontChange 
+		  encodeNonASCII:(BOOL)encodeNonASCII
+			encodeSpaces:(BOOL)encodeSpaces
+			  imagesPath:(NSString *)imagesPath
+	   attachmentsAsText:(BOOL)attachmentsAsText
+attachmentImagesOnlyForSending:(BOOL)attachmentImagesOnlyForSending
+		  simpleTagsOnly:(BOOL)simpleOnly;
 
 + (NSDictionary *)parseArguments:(NSString *)arguments;
 
