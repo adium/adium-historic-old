34cbbb0469d4c4480e243a371570a4726f4d1a43
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-08 06:20:24 +0000

    Added a delegate method for ESImageViewWithImagePicker, - (NSImage *)imageForImageViewWithImagePicker:(ESImageViewWithImagePicker *)picker which allows the delegate to supply an image other than the NSImageView's image (since the NSImageView may have resized or scaled the image it has stored).

--- Frameworks/AIUtilities Framework/ESImageViewWithImagePicker.h
+++ Frameworks/AIUtilities Framework/ESImageViewWithImagePicker.h
@@ -6,6 +6,23 @@
 
 #import "NSImagePicker.h"
 
+@class NSImagePickerController, ESImageViewWithImagePicker;
+@class NSImage, NSString, NSResponder;
+
+//Protocol merely for documentation purposes; delegate does not need to actually implement this protocol,
+//and all methods are optional.
+@protocol ESImageViewWithImagePickerDelegate
+//The user selected an image in the image picker
+- (void)imageViewWithImagePicker:(ESImageViewWithImagePicker *)picker didChangeToImage:(NSImage *)image;
+
+//The user deleted the image from the image picker (via delete or cut, for example)
+- (void)deleteInImageViewWithImagePicker:(ESImageViewWithImagePicker *)picker;
+
+//The delegate is given the opporutinty to supply an image other than the NSImageView's image; if this method
+//returns nil or is not implemented, the NSImageView's image is used.
+- (NSImage *)imageForImageViewWithImagePicker:(ESImageViewWithImagePicker *)picker;
+@end
+
 @interface ESImageViewWithImagePicker : NSImageView {
 	NSImagePickerController *pickerController;
 	NSString				*title;
@@ -13,8 +30,8 @@
 	BOOL					useNSImagePickerController;
 	IBOutlet	id			delegate;
 	
-	BOOL shouldDrawFocusRing;
-	NSResponder *lastResp;
+	BOOL					shouldDrawFocusRing;
+	NSResponder				*lastResp;
 }
 
 - (void)setDelegate:(id)inDelegate;
