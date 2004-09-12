0160928f75644a930ccc4d38ca2b9fc9d6c93f9a
adamiser <adamiser@localhost>
2004-09-12 23:46:01 +0000

    Adding a special BETA #define to the version checker.  When enabled adium checks a different key in the version.plist.  This allows us to have different 'newest' versions for regular and beta releases.
    When the flag is enabled, as an extra treat, the update notifications become a bit more "intrusive" and cannot
    be disabled >:D

--- Plugins/Version Checker/CPFVersionChecker.m
+++ Plugins/Version Checker/CPFVersionChecker.m
@@ -11,12 +11,16 @@
 #define VERSION_CHECKER_TITLE		AILocalizedString(@"Check for Updates...",nil)
 #define VERSION_PLIST_URL			@"http://www.adiumx.com/version.plist"
 #define VERSION_PLIST_KEY			@"adium-version"
+#define VERSION_BETA_PLIST_KEY		@"adium-beta-version"
+
+#define VERSION_CHECK_INTERVAL		24		//24 hours
+#define BETA_VERSION_CHECK_INTERVAL	4		//4 hours - Beta releases have a nice annoying refresh >:D
 
 #define VERSION_CHECKER_DEFAULTS	@"VersionChecker Defaults"
 
 @interface CPFVersionChecker (PRIVATE)
 - (void)_requestVersionThread;
-- (void)_versionReceived:(NSDate *)dateOfLatestBuild;
+- (void)_versionReceived:(NSDictionary *)versionDict;
 - (NSDate *)dateOfThisBuild;
 - (NSDate *)dateOfLatestBuild;
 @end
@@ -26,6 +30,10 @@
 //Install
 - (void)installPlugin
 {
+	//Friendly reminder that we are running with the beta flag on
+	if(BETA_RELEASE_EXPIRATION) NSLog(@"#### THIS IS A BETA RELEASE ####");
+
+	//Configure our default preferences
 	[[adium preferenceController] registerDefaults:[NSDictionary dictionaryNamed:VERSION_CHECKER_DEFAULTS 
 																		forClass:[self class]]
 																		forGroup:PREF_GROUP_UPDATING];
@@ -47,7 +55,7 @@
 	[self automaticCheckForNewVersion:nil];
 	
 	//Check for updates again every 24 hours (60 seconds * 60 minutes * 24 hours)
-	timer = [[NSTimer scheduledTimerWithTimeInterval:(60 * 60 * 24)
+	timer = [[NSTimer scheduledTimerWithTimeInterval:(60 * 60 * (BETA_RELEASE_EXPIRATION ? BETA_VERSION_CHECK_INTERVAL : VERSION_CHECK_INTERVAL))
 											  target:self
 											selector:@selector(automaticCheckForNewVersion:)
 											userInfo:nil
@@ -79,7 +87,7 @@
 	BOOL updateAutomatically = [[[adium preferenceController] preferenceForKey:KEY_CHECK_AUTOMATICALLY
 																		 group:PREF_GROUP_UPDATING] boolValue];
 
-	if(updateAutomatically){
+	if(BETA_RELEASE_EXPIRATION || updateAutomatically){
 		if([AINetworkConnectivity networkIsReachable]){
 			//If the network is available, check for updates now
 			checkingManually = NO;
@@ -101,7 +109,7 @@
 	}
 }
 
-//Thread: Request a version
+//Thread Request a version
 //The URL load (dateOfLatestBuild) can block, so we do it in a separate thread.  Once the URL load is finished we pass
 //control back to the main thread and display the appropriate panel
 - (void)_requestVersionThread
@@ -114,10 +122,19 @@
 	[pool release];
 }
 
-//Thread: Version received
-- (void)_versionReceived:(NSDate *)newestDate
+//Version received
+- (void)_versionReceived:(NSDictionary *)versionDict
 {
+	NSString	*number = [versionDict objectForKey:VERSION_PLIST_KEY];
+	NSDate		*newestDate = nil;
+
 	NSLog(@"## Version Received ##");
+
+	//Get the newest version date from the passed version dict
+	if(versionDict && number){
+		newestDate = [NSDate dateWithTimeIntervalSince1970:[number doubleValue]];
+	}
+		
 	//Load relevant dates which we weren't passed
 	NSDate	*thisDate = [self dateOfThisBuild];
 	NSDate	*lastDateDisplayedToUser = [[adium preferenceController] preferenceForKey:KEY_LAST_UPDATE_ASKED
@@ -138,6 +155,20 @@
 			//Remember that the user has been prompted for this version so we don't bug them about it again
 			[[adium preferenceController] setPreference:newestDate forKey:KEY_LAST_UPDATE_ASKED 
 												  group:PREF_GROUP_UPDATING];
+		}
+	}
+	
+	//Beta Expiration (Designed to be annoying)
+	//Beta expiration checking is performed in addition to regular version checking
+	if(BETA_RELEASE_EXPIRATION){
+		NSString	*betaNumber = [versionDict objectForKey:VERSION_BETA_PLIST_KEY];
+		NSDate		*betaDate = nil;
+		
+		if(versionDict && number) betaDate = [NSDate dateWithTimeIntervalSince1970:[betaNumber doubleValue]];
+		if(!betaDate){
+			[ESVersionCheckerWindowController showCannotConnectWindow];
+		}else if(![thisDate isEqualToDate:betaDate] && ![thisDate isEqualToDate:[thisDate laterDate:betaDate]]){
+			[ESVersionCheckerWindowController showUpdateWindowFromBuild:thisDate toBuild:betaDate];
 		}
 	}
 }
@@ -166,18 +197,7 @@
 //Returns the date of the most recent Adium build (contacts adiumx.com, may block)
 - (NSDate *)dateOfLatestBuild
 {
-	NSURL			*versionURL = [NSURL URLWithString:VERSION_PLIST_URL];
-	NSDictionary 	*productVersionDict = [NSDictionary dictionaryWithContentsOfURL:versionURL];
-	NSString		*number = [productVersionDict objectForKey:VERSION_PLIST_KEY];
-	
-	//if everything works out ok, then pass the date along as usual.
-	if(productVersionDict && number){
-		return([NSDate dateWithTimeIntervalSince1970:[number doubleValue]]);
-	}else{
-		//if the dictionary is, for some reason, invalid or we can't connect to server,
-		//so pass a nil for the date
-		return(nil);
-	}
+	return([NSDictionary dictionaryWithContentsOfURL:[NSURL URLWithString:VERSION_PLIST_URL]]);
 }
 
 @end
