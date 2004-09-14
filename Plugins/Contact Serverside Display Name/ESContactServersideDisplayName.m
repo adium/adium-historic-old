343864a53725061fd1a1da0705e30e78493dc661
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-14 06:47:16 +0000

    Should fix serverside display names in tooltips for away MSN contacts.  Thanks, Tedger. :)

--- Plugins/Contact Serverside Display Name/ESContactServersideDisplayName.m
+++ Plugins/Contact Serverside Display Name/ESContactServersideDisplayName.m
@@ -32,13 +32,7 @@
     
     //Return the correct string
     if(serverDisplayName && ![serverDisplayName isEqualToString:[inObject displayName]]){
-
-		//If we are away and the status message and server display name are the same, don't display the server display name
-		if(!([inObject integerStatusObjectForKey:@"Away" fromAnyContainedObject:NO]) ||
-		   !([serverDisplayName isEqualToString:[inObject stringFromAttributedStringStatusObjectForKey:@"StatusMessage"]])){
-			
-			entry = [[NSAttributedString alloc] initWithString:serverDisplayName];
-		}
+		entry = [[NSAttributedString alloc] initWithString:serverDisplayName];
     }
 	
     return([entry autorelease]);
