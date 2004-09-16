a1229538d7b781d589c9a1d3fecc427fd9da74b3
Evan Schoenberg <Evan Schoenberg@localhost>
2004-09-16 21:47:32 +0000

    Adding these files sooner than later...

---
+++ Plugins/Dual Window Interface/ESChatUserListController.m
@@ -0,0 +1,13 @@
+//
+//  ESChatUserListController.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/14/04.
+//
+
+#import "ESChatUserListController.h"
+
+
+@implementation ESChatUserListController
+
+@end
