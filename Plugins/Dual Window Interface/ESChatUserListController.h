b780c3b66441968cacaca6e4f15a3acfa1305a2e
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-16 21:47:32 +0000

    Adding these files sooner than later...

---
+++ Plugins/Dual Window Interface/ESChatUserListController.h
@@ -0,0 +1,15 @@
+//
+//  ESChatUserListController.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/14/04.
+
+#import "AIContactInfoWindowController.h"
+
+@class AIAbstractListController;
+
+@interface ESChatUserListController : AIAbstractListController {
+
+}
+
+@end
