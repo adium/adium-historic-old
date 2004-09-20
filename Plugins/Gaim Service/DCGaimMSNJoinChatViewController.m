e7008439423cd97ab3580d8ab21642bdcb52219e
dchoby98 <dchoby98@localhost>
2004-09-20 23:00:35 +0000

    Group chat refinement. - "Join" button in the Join Chat window is disabled until the bare minimum of data is entered. - Made the right fields hilite and tab correctly when opening the Join Chat window. - Deloggification.

--- Plugins/Gaim Service/DCGaimMSNJoinChatViewController.m
+++ Plugins/Gaim Service/DCGaimMSNJoinChatViewController.m
@@ -12,6 +12,8 @@
 - (void)configureForAccount:(AIAccount *)inAccount
 {
 	[super configureForAccount:inAccount];
+	if( delegate )
+		[delegate setJoinChatEnabled:NO];
 }
 
 - (void)joinChatWithAccount:(AIAccount *)inAccount
--- Plugins/Gaim Service/DCGaimYahooJoinChatViewController.m
+++ Plugins/Gaim Service/DCGaimYahooJoinChatViewController.m
@@ -7,10 +7,16 @@
 
 #import "DCGaimYahooJoinChatViewController.h"
 
+@interface DCGaimYahooJoinChatViewController (PRIVATE)
+- (void)validateEnteredText;
+@end
+
 @implementation DCGaimYahooJoinChatViewController
 
 - (void)configureForAccount:(AIAccount *)inAccount
 {
+	[delegate setJoinChatEnabled:([[textField_roomName stringValue] length] > 0)];
+	[[view window] makeFirstResponder:textField_roomName];
 	[super configureForAccount:inAccount];
 }
 
@@ -19,8 +25,6 @@
 	NSString		*room = [textField_roomName stringValue];
 	NSDictionary	*chatCreationInfo;
 	
-	//NSLog(@"#### Yahoo! joinChatWithAccount: %@ joining",inAccount);
-
 	chatCreationInfo = [NSDictionary dictionaryWithObjectsAndKeys:room,@"room",nil];
 	
 	[self doJoinChatWithName:room
@@ -34,6 +38,27 @@
 - (NSString *)nibName
 {
 	return @"DCGaimYahooJoinChatView";
+}
+
+//Entered text is changing
+- (void)controlTextDidChange:(NSNotification *)notification
+{
+	if([notification object] == textField_roomName){
+		[self validateEnteredText];
+	}
+}
+
+- (void)validateEnteredText
+{
+	NSString *roomName = [textField_roomName stringValue];
+	BOOL enabled = NO;
+	
+	if( roomName && [roomName length] ) {
+		enabled = YES;
+	}
+	
+	if( delegate )
+		[(DCJoinChatWindowController *)delegate setJoinChatEnabled:enabled];
 }
 
 @end
