f6960e8d2e2b40b6e5adfa9018bec6f96c400465
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-20 01:54:10 +0000

    Added: - (void)removeFilesInDirectory:(NSString *)dirPath withPrefix:(NSString *)prefix movingToTrash:(BOOL)moveToTrash to our NSFileManager additions.

--- Frameworks/AIUtilities Framework/AIFileManagerAdditions.h
+++ Frameworks/AIUtilities Framework/AIFileManagerAdditions.h
@@ -7,8 +7,8 @@
 
 #import <Foundation/Foundation.h>
 
-
 @interface NSFileManager (AIFileManagerAdditions)
 - (BOOL)trashFileAtPath:(NSString *)sourcePath;
 - (void)createDirectoriesForPath:(NSString *)fullPath;
+- (void)removeFilesInDirectory:(NSString *)dirPath withPrefix:(NSString *)prefix movingToTrash:(BOOL)moveToTrash;
 @end
