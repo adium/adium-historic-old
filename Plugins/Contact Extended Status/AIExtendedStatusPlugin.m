3fa29fbe6cea531c4be55611e2e1724ae2da6813
Evan Schoenberg <Evan Schoenberg@localhost>
2004-09-21 22:37:03 +0000

    The extended status on the contact list may now be specified to be just the status, just the idle time, or (idle) status.

--- Plugins/Contact Extended Status/AIExtendedStatusPlugin.m
+++ Plugins/Contact Extended Status/AIExtendedStatusPlugin.m
@@ -3,18 +3,44 @@
 //  Adium
 //
 //  Created by Adam Iser on 9/7/04.
-//  Copyright 2004 __MyCompanyName__. All rights reserved.
 //
 
 #import "AIExtendedStatusPlugin.h"
 
 #define STATUS_MAX_LENGTH	100
 
+@interface AIExtendedStatusPlugin (PRIVATE)
+- (void)preferencesChanged:(NSNotification *)notification;
+@end
+
 @implementation AIExtendedStatusPlugin
 
 - (void)installPlugin
 {
-    [[adium contactController] registerListObjectObserver:self];
+	//Set up our initial preferences
+	[self preferencesChanged:nil];
+	
+	//Observe preferences changes
+    [[adium notificationCenter] addObserver:self 
+								   selector:@selector(preferencesChanged:) 
+									   name:Preference_GroupChanged 
+									 object:nil];
+}
+
+- (void)preferencesChanged:(NSNotification *)notification
+{
+    if(notification == nil || [[[notification userInfo] objectForKey:@"Group"] isEqualToString:PREF_GROUP_LIST_LAYOUT]){
+		EXTENDED_STATUS_STYLE statusStyle = [[[adium preferenceController] preferenceForKey:KEY_LIST_LAYOUT_EXTENDED_STATUS_STYLE
+																					  group:PREF_GROUP_LIST_LAYOUT] intValue];
+		showStatus = ((statusStyle == STATUS_ONLY) || (statusStyle == IDLE_AND_STATUS));
+		showIdle = ((statusStyle == IDLE_ONLY) || (statusStyle == IDLE_AND_STATUS));
+		
+		if (notification == nil){
+			[[adium contactController] registerListObjectObserver:self];
+		}else{
+			[[adium contactController] updateAllListObjectsForObserver:self];
+		}
+	}
 }
 
 //Called when a handle's status changes
@@ -24,28 +50,34 @@
 
 	//Idle time
     if(inModifiedKeys == nil || [inModifiedKeys containsObject:@"Idle"] || [inModifiedKeys containsObject:@"StatusMessage"]){
-		NSMutableString	*statusMessage = [[[inObject statusObjectForKey:@"StatusMessage"] string] mutableCopy];
-		int				idle = [inObject integerStatusObjectForKey:@"Idle"];
+		NSMutableString	*statusMessage = nil;
+		NSString		*finalMessage = nil;
+		int				idle;
 		
-		//Incredibly long status messages are slow to size, so we crop them to a reasonable length
-		if([statusMessage length] > STATUS_MAX_LENGTH){
-			[statusMessage deleteCharactersInRange:NSMakeRange(STATUS_MAX_LENGTH,
-															   [statusMessage length] - STATUS_MAX_LENGTH)];
+		if (showStatus){
+			statusMessage = [[[[inObject statusObjectForKey:@"StatusMessage"] string] mutableCopy] autorelease];
+			
+			//Incredibly long status messages are slow to size, so we crop them to a reasonable length
+			if([statusMessage length] > STATUS_MAX_LENGTH){
+				[statusMessage deleteCharactersInRange:NSMakeRange(STATUS_MAX_LENGTH,
+																   [statusMessage length] - STATUS_MAX_LENGTH)];
+			}
+			
+			//Linebreaks in the status message cause vertical alignment issues.  We can either cut off at the first break
+			//or prune them all.
+			[statusMessage replaceOccurrencesOfString:@"\r"
+										   withString:@" / "
+											  options:0
+												range:NSMakeRange(0,[statusMessage length])];
+			[statusMessage replaceOccurrencesOfString:@"\n"
+										   withString:@" / "
+											  options:0
+												range:NSMakeRange(0,[statusMessage length])];
 		}
 		
-		//Linebreaks in the status message cause vertical alignment issues.  We can either cut off at the first break
-		//or prune them all.
-		[statusMessage replaceOccurrencesOfString:@"\r"
-									   withString:@" / "
-										  options:0
-											range:NSMakeRange(0,[statusMessage length])];
-		[statusMessage replaceOccurrencesOfString:@"\n"
-									   withString:@" / "
-										  options:0
-											range:NSMakeRange(0,[statusMessage length])];
-
+		idle = (showIdle ? [inObject integerStatusObjectForKey:@"Idle"] : 0);
+		
 		//
-		NSString	*finalMessage;
 		if(idle > 0 && statusMessage){
 			finalMessage = [NSString stringWithFormat:@"(%@) %@",[self idleStringForSeconds:idle], statusMessage];
 		}else if(idle > 0){
@@ -56,8 +88,6 @@
 
 		[[inObject displayArrayForKey:@"ExtendedStatus"] setObject:finalMessage withOwner:self];
 		modifiedAttributes = [NSArray arrayWithObject:@"ExtendedStatus"];
-
-		[statusMessage release];
 	}
 	
    return(modifiedAttributes);
