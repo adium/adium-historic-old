ba1b0cfe0fe646f349fc4f497f4e936f2d75eb80
Peter Hosey <Peter Hosey@localhost>
2004-09-23 21:22:58 +0000

    The new and improved AIHTMLDecoder.

--- Frameworks/AIUtilities Framework/AIHTMLDecoder.h
+++ Frameworks/AIUtilities Framework/AIHTMLDecoder.h
@@ -16,8 +16,112 @@
 #import "AIStringAdditions.h"
 
 @interface AIHTMLDecoder : NSObject {
+	struct AIHTMLDecoderOptionsBitField {
+		unsigned reserved: 22;
 
+		//these next ten members are derived from the arguments to
+		//  +encodeHTML:::::::::::: in the old AIHTMLDecoder.
+		unsigned headers: 1;
+		unsigned fontTags: 1;
+		unsigned closingFontTags: 1;
+		unsigned colorTags: 1;
+		unsigned styleTags: 1;
+
+		unsigned nonASCII: 1;
+		unsigned allSpaces: 1;
+		unsigned attachmentTextEquivalents: 1;
+		unsigned attachmentImagesOnlyForSending: 1;
+
+		unsigned simpleTagsOnly: 1;
+	} thingsToInclude;
 }
+
+#pragma mark Creation
+
+//+decoder, +new, and -init all return an instance with all flags set to 0.
+
++ (AIHTMLDecoder *)decoder;
+
+//convenience methods to get a decoder that's already been set up a certain way.
+
+- (id)initWithHeaders:(BOOL)includeHeaders
+			 fontTags:(BOOL)includeFontTags
+		closeFontTags:(BOOL)closeFontTags
+			colorTags:(BOOL)includeColorTags
+			styleTags:(BOOL)includeStyleTags
+	   encodeNonASCII:(BOOL)encodeNonASCII
+		 encodeSpaces:(BOOL)encodeSpaces
+	attachmentsAsText:(BOOL)attachmentsAsText
+attachmentImagesOnlyForSending:(BOOL)attachmentImagesOnlyForSending
+	   simpleTagsOnly:(BOOL)simpleOnly;
+
++ (AIHTMLDecoder *)decoderWithHeaders:(BOOL)includeHeaders
+							 fontTags:(BOOL)includeFontTags
+						closeFontTags:(BOOL)closeFontTags
+							colorTags:(BOOL)includeColorTags
+							styleTags:(BOOL)includeStyleTags
+					   encodeNonASCII:(BOOL)encodeNonASCII
+						 encodeSpaces:(BOOL)encodeSpaces
+					attachmentsAsText:(BOOL)attachmentsAsText
+	   attachmentImagesOnlyForSending:(BOOL)attachmentImagesOnlyForSending
+					   simpleTagsOnly:(BOOL)simpleOnly;
+
+#pragma mark Work methods
+
+//turn HTML source into an attributed string.
+//uses no options.
+- (NSAttributedString *)decodeHTML:(NSString *)inMessage;
+
+//turn an attributed string into HTML source.
+//uses all options.
+- (NSString *)encodeHTML:(NSAttributedString *)inMessage imagesPath:(NSString *)imagesPath;
+
+//pass a string containing all the attributes of a tag (for example,
+//  @"src=\"window.jp2\" alt=\"Window on the World\""). you will get back a
+//  dictionary containing those attributes (for example, @{ @"src" =
+//  @"window.jp2", @"alt" = @"Window on the World" }).
+//uses no options.
+- (NSDictionary *)parseArguments:(NSString *)arguments;
+
+#pragma mark Accessors
+
+//meaning <HTML> and </HTML>.
+- (BOOL)includesHeaders;
+- (void)setIncludesHeaders:(BOOL)newValue;
+
+- (BOOL)includesFontTags;
+- (void)setIncludesFontTags:(BOOL)newValue;
+
+- (BOOL)closesFontTags;
+- (void)setClosesFontTags:(BOOL)newValue;
+
+- (BOOL)includesColorTags;
+- (void)setIncludesColorTags:(BOOL)newValue;
+
+//turn non-printable characters into entities.
+- (BOOL)encodesNonASCII;
+- (void)setEncodesNonASCII:(BOOL)newValue;
+
+- (BOOL)preservesAllSpaces;
+- (void)setPreservesAllSpaces:(BOOL)newValue;
+
+- (BOOL)usesAttachmentTextEquivalents;
+- (void)setUsesAttachmentTextEquivalents:(BOOL)newValue;
+
+- (BOOL)onlyConvertImageAttachmentsToIMGTagsWhenSendingAMessage;
+- (void)setOnlyConvertImageAttachmentsToIMGTagsWhenSendingAMessage:(BOOL)newValue;
+
+- (BOOL)onlyUsesSimpleTags;
+- (void)setOnlyUsesSimpleTags:(BOOL)newValue;
+
+@end
+
+@interface AIHTMLDecoder (ClassMethodCompatibility)
+
+/*these bring back the class methods that I (boredzo) turned into instance
+ *  methods for the sake of clarity.
+ *when these methods are no longer used, this category should be deleted.
+ */
 
 + (NSAttributedString *)decodeHTML:(NSString *)inMessage;
 + (NSString *)encodeHTML:(NSAttributedString *)inMessage encodeFullString:(BOOL)encodeFullString;
