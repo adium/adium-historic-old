328040ba693492135d96a1956b28024511d40eea
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-09-24 08:48:30 +0000

    Added a plugin class to handle routing file transfer status messages to appropriate chats

---
+++ Plugins/File Transfer Messages/ESFileTransferMessagesPlugin.h
@@ -0,0 +1,13 @@
+//
+//  ESFileTransferMessagesPlugin.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/23/04.
+//  Copyright 2004 The Adium Team. All rights reserved.
+//
+
+@interface ESFileTransferMessagesPlugin : AIPlugin {
+
+}
+
+@end
