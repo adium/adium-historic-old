a023b5247d289bcac49f2294539ccecfc24ebf5c
Evan Schoenberg <Evan Schoenberg@localhost>
2004-09-29 20:45:22 +0000

    Gaim debug output now goes through AILog()

--- Plugins/Gaim Service/SLGaimCocoaAdapter.m
+++ Plugins/Gaim Service/SLGaimCocoaAdapter.m
@@ -409,7 +409,7 @@ static void adiumGaimDebugPrint(GaimDebugLevel level, const char *category, cons
 	
 	//Log error
 	if(!category) category = "general"; //Category can be nil
-	NSLog(@"%x: (Debug: %s) %s",[NSRunLoop currentRunLoop], category, arg_s);
+	AILog(@"%x: (Debug: %s) %s",[NSRunLoop currentRunLoop], category, arg_s);
 	
 	g_free(arg_s);
 }
