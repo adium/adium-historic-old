c0d5dd2badd44f8c99742d100a10dd7dbf27c47d
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-10-01 07:47:48 +0000

    removing commented out code we will never want to use again

--- Plugins/Contact List/AIListWindowController.m
+++ Plugins/Contact List/AIListWindowController.m
@@ -307,14 +307,7 @@
     }else if([selectedObject isKindOfClass:[AIListContact class]]){
         //Open a new message with the contact
 		AIListContact	*contact = (AIListContact *)selectedObject;
-		
-		/*
-		//If the contact is a meta contact, find the preferred contact for it
-		if([contact isKindOfClass:[AIMetaContact class]]){
-			contact = [[adium contactController] preferredContactForContentType:CONTENT_MESSAGE_TYPE
-																 forListContact:contact];
-		}
-		*/
+
 		[[adium interfaceController] setActiveChat:[[adium contentController] openChatWithContact:contact]];
 		
     }
