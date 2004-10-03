302c5fa505f49be85a08035edf281e172fbec2c3
adamiser <adamiser@localhost>
2004-10-03 18:30:57 +0000

    This code was leaking a lot of NSTimers, which were holding onto AIChats and causing them to leak.  I've reworked this code so that timers are correctly cleaned up when we are done with them.  I've also made efficiency improvements when typing by restarting the existing timers instead of removing and re-creating them.

--- Plugins/Typing Notification/AITypingNotificationPlugin.m
+++ Plugins/Typing Notification/AITypingNotificationPlugin.m
@@ -11,6 +11,9 @@
 @interface AITypingNotificationPlugin (PRIVATE)
 - (void)_sendTypingState:(AITypingState)typingState toChat:(AIChat *)chat;
 - (void)_processTypingInView:(NSText<AITextEntryView> *)inTextEntryView;
+- (void)_addTypingTimerForChat:(AIChat *)chat;
+- (void)_resetTypingTimerForChat:(AIChat *)chat;
+- (void)_removeTypingTimerForChat:(AIChat *)chat;
 @end
 
 #define CAN_RECEIVE_TYPING		@"CanReceiveTyping"
@@ -18,16 +21,13 @@
 
 #define ENTERED_TEXT_TIMER		@"EnteredTextTimer"
 #define ENTERED_TEXT_INTERVAL   3.0
+
 @implementation AITypingNotificationPlugin
 
 - (void)installPlugin
 {
-    //Register as an entry filter
-    [[adium contentController] registerTextEntryFilter:self];
-
-    //typingDict = [[NSMutableDictionary alloc] init];
-    //messagedDict = [[NSMutableDictionary alloc] init];
-
+    //Register as an entry filter and observe content
+	[[adium contentController] registerTextEntryFilter:self];
     [[adium notificationCenter] addObserver:self selector:@selector(didReceiveContent:) name:Content_DidReceiveContent object:nil];
     [[adium notificationCenter] addObserver:self selector:@selector(didReceiveContent:) name:Content_FirstContentRecieved object:nil];
 }
@@ -48,6 +48,30 @@
     }
 }
 
+
+//Text entry -----------------------------------------------------------------------------------------------------------
+- (void)didOpenTextEntryView:(NSText<AITextEntryView> *)inTextEntryView
+{
+    //Ignored
+}
+
+- (void)willCloseTextEntryView:(NSText<AITextEntryView> *)inTextEntryView
+{
+    AIChat		*chat = [inTextEntryView chat];
+	
+    //Send a 'not-typing' message to this contact
+    if(([chat integerStatusObjectForKey:WE_ARE_TYPING] != AINotTyping) && ([chat statusObjectForKey:CAN_RECEIVE_TYPING] != nil)){
+        [self _sendTypingState:AINotTyping toChat:chat];
+    }
+	
+	//Remove our typing timer
+	[self _removeTypingTimerForChat:chat];
+	
+    //We could choose to de-clear the contact for typing notifications here as well
+    //AIChat		*chat = [inTextEntryView chat];
+    //[[chat statusDictionary] removeObjectForKey:CAN_RECEIVE_TYPING];
+}
+
 - (void)stringAdded:(NSString *)inString toTextEntryView:(NSText<AITextEntryView> *)inTextEntryView
 {
     [self _processTypingInView:inTextEntryView];
@@ -60,48 +84,38 @@
 
 - (void)_processTypingInView:(NSText<AITextEntryView> *)inTextEntryView
 {
-    AIChat				*chat = [inTextEntryView chat];
+    AIChat		*chat = [inTextEntryView chat];
 	
     if(chat && [chat statusObjectForKey:CAN_RECEIVE_TYPING] != nil){
+		NSTimer			*enteredTextTimer = [chat statusObjectForKey:ENTERED_TEXT_TIMER];
 		NSNumber		*previousTypingNumber = [chat statusObjectForKey:WE_ARE_TYPING];
 		AITypingState   previousTypingState = (previousTypingNumber ? [previousTypingNumber intValue] : AINotTyping);
 		AITypingState   currentTypingState;
-		NSTimer			*enteredTextTimer;
 
-		//Invalidate any timer currently watching this chat for switching it to entered text
-		if (enteredTextTimer = [chat statusObjectForKey:ENTERED_TEXT_TIMER]){
-			[enteredTextTimer invalidate];
-		}
-		
-	     
 		//Determine if this change indicated the user was typing or indicated the user had no longer entered text
-		if ([[inTextEntryView attributedString] length] != 0){
-			//The text just changed, and the length is non-zero; the user is therefore typing
+		if([[inTextEntryView attributedString] length] != 0){ //User is typing
 			currentTypingState = AITyping;
-			
-			//Schedule the switchover to "Entered Text" after ENTERED_TEXT_INTERVAL
-			enteredTextTimer = [NSTimer scheduledTimerWithTimeInterval:ENTERED_TEXT_INTERVAL
-																target:self
-															  selector:@selector(_switchToEnteredText:)
-															  userInfo:chat
-															   repeats:NO];
-			//Keep track of the timer object for early invalidation if necessary
-			[chat setStatusObject:enteredTextTimer 
-						   forKey:ENTERED_TEXT_TIMER
-						   notify:NotifyNever];
-			
-		}else{
-			//The text just changed, and the length is zero; the user is therefore not typing and has not entered text
+
+			if(enteredTextTimer){
+				[self _resetTypingTimerForChat:chat];
+			}else{
+				[self _addTypingTimerForChat:chat];
+			}
+
+		}else{ //User is not typing
 			currentTypingState = AINotTyping;
+			[self _removeTypingTimerForChat:chat];
 		}
 		
 		//We don't want to send the same typing value more than once
-        if(previousTypingState != currentTypingState) {
+        if(previousTypingState != currentTypingState){
 			[self _sendTypingState:currentTypingState toChat:chat];
 		}
     }    
 }
 
+
+//Typing state ---------------------------------------------------------------------------------------------------------
 - (void)_sendTypingState:(AITypingState)typingState toChat:(AIChat *)chat
 {
     AIAccount		*account = [chat account];
@@ -124,25 +138,37 @@
 {
 	AIChat  *chat = [inTimer userInfo];
 	[self _sendTypingState:AIEnteredText toChat:chat];
+	[self _removeTypingTimerForChat:chat];
 }
 
-- (void)didOpenTextEntryView:(NSText<AITextEntryView> *)inTextEntryView
+
+//Typing timer ---------------------------------------------------------------------------------------------------------
+//Add, remove, or reset the timer responsible for detecting when the user stops typing
+- (void)_addTypingTimerForChat:(AIChat *)chat
 {
-    //Ignored
+	NSTimer	*enteredTextTimer = [NSTimer scheduledTimerWithTimeInterval:ENTERED_TEXT_INTERVAL
+																 target:self
+															   selector:@selector(_switchToEnteredText:)
+															   userInfo:chat
+																repeats:NO];
+	[chat setStatusObject:enteredTextTimer forKey:ENTERED_TEXT_TIMER notify:NotifyNever];
 }
 
-- (void)willCloseTextEntryView:(NSText<AITextEntryView> *)inTextEntryView
+- (void)_resetTypingTimerForChat:(AIChat *)chat
 {
-    AIChat	*chat = [inTextEntryView chat];
+	NSTimer	*enteredTextTimer = [chat statusObjectForKey:ENTERED_TEXT_TIMER];
+	if(enteredTextTimer){
+		[enteredTextTimer setFireDate:[NSDate dateWithTimeIntervalSinceNow:ENTERED_TEXT_INTERVAL]];
+	}
+}
 
-    //Send a 'not-typing' message to this contact
-    if(([chat integerStatusObjectForKey:WE_ARE_TYPING] != AINotTyping) && ([chat statusObjectForKey:CAN_RECEIVE_TYPING] != nil)){
-        [self _sendTypingState:AINotTyping toChat:chat];
-    }
-
-    //We could choose to de-clear the contact for typing notifications here as well
-    //AIChat		*chat = [inTextEntryView chat];
-    //[[chat statusDictionary] removeObjectForKey:CAN_RECEIVE_TYPING];
+- (void)_removeTypingTimerForChat:(AIChat *)chat
+{
+	NSTimer	*enteredTextTimer = [chat statusObjectForKey:ENTERED_TEXT_TIMER];
+	if(enteredTextTimer){
+		[enteredTextTimer invalidate];
+		[chat setStatusObject:nil forKey:ENTERED_TEXT_TIMER notify:NotifyNever];
+	}
 }
 
 @end
