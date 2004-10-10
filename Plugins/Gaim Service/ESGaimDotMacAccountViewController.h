fd5a46a12612259266b6f3d2f3ac4214c5616001
adamiser <adamiser@localhost>
2004-10-10 18:03:12 +0000

    Missed this earlier

---
+++ Plugins/Gaim Service/ESGaimDotMacAccountViewController.h
@@ -0,0 +1,16 @@
+//
+//  ESGaimDotMacAccountViewController.h
+//  Adium
+//
+//  Created by Adam Iser on 10/10/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "AIGaimOscarAccountViewController.h"
+
+@interface ESGaimDotMacAccountViewController : AIGaimOscarAccountViewController {
+
+}
+
+@end
