4dbe74fe96796a1d281a34dc4fd3b264e321f9da
Evan Schoenberg <Evan Schoenberg@localhost>
2004-10-14 02:51:07 +0000

    Disabling tooltips in the group user chat list, added a warning about why

--- Plugins/Dual Window Interface/ESChatUserListController.m
+++ Plugins/Dual Window Interface/ESChatUserListController.m
@@ -18,7 +18,17 @@
 
 //We don't want to change text colors based on the user's status or state
 - (BOOL)shouldUseContactTextColors{
-	return NO;
+	return(NO);
 }
 
+#warning Evan: Tooltips currently crash after the tab is closed. Disabling in the userlist for now.
+/*
+	Somehow the mouseEntered: and mouseExited: calls are being sent to the tracking rect of the
+	user list even after AISmoothTooltipTracker deallocs and removes its tracking rect.  This shows up in
+	a stack trace as NSWindow doing a sendEvent: and hitting a released object (which of course the
+	AISmoothTooltipTracker object is after it is released).
+ */
+- (BOOL)shouldShowTooltips{
+	return(NO);
+}
 @end
