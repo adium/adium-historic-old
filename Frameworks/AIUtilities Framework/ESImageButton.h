a71e2be65a23d5e3d00d386db763fb10d80ae9ba
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-10-17 16:58:57 +0000

    Added ESImageButton, which is a MVMenuButton subclass which displays its image full-size when clicked (attached directly below it), fading it in and out all pretty-like via ESFloater

---
+++ Frameworks/AIUtilities Framework/ESImageButton.h
@@ -0,0 +1,17 @@
+//
+//  ESImageButton.h
+//  AIUtilities.framework
+//
+//  Created by Evan Schoenberg on 10/16/04.
+//  Copyright 2004 The Adium Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class ESFloater;
+
+@interface ESImageButton : MVMenuButton {
+	ESFloater	*imageFloater;
+}
+
+@end
