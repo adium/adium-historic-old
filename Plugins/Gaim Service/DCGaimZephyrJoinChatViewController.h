3539ac13f84699d521646f95691a3859cd09b68b
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-10-22 17:28:59 +0000

    Added support for joining Zephyr group chats.

--- Plugins/Gaim Service/DCGaimZephyrJoinChatViewController.h
+++ Plugins/Gaim Service/DCGaimZephyrJoinChatViewController.h
@@ -5,7 +5,9 @@
 //  Created by Evan Schoenberg on 8/12/04.
 
 @interface DCGaimZephyrJoinChatViewController : DCJoinChatViewController {
-
+	IBOutlet	NSTextField		*textField_class;
+	IBOutlet	NSTextField		*textField_instance;
+	IBOutlet	NSTextField		*textField_recipient;
 }
 
 @end
