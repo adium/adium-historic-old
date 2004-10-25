416521a92651228600ba4ab2f1ad34a44e00fbc9
Colin Barrett <Colin Barrett@localhost>
2004-10-25 22:32:04 +0000

    The EMD now closes it's eyes when all of your accounts are offline. Also code improvements to the way we handle icons.

--- Plugins/Status Menu Item/CBStatusMenuItemController.m
+++ Plugins/Status Menu Item/CBStatusMenuItemController.m
@@ -10,6 +10,7 @@
 @interface CBStatusMenuItemController (PRIVATE)
 - (void)activateAdium:(id)sender;
 - (void)menuNeedsUpdate:(NSMenu *)menu;
+- (void)accountStateChanged:(NSNotification *)notification;
 @end
 
 @implementation CBStatusMenuItemController
@@ -33,12 +34,13 @@ static	NSImage						*unviewedContentImage = nil;
         //Create and set up the status item
         statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
         [statusItem setHighlightMode:YES];
-        [statusItem setImage:[NSImage imageNamed:@"adium.png" forClass:[self class]]];
-        [statusItem setAlternateImage:[NSImage imageNamed:@"adiumHighlight.png" forClass:[self class]]];
-
 		if (!unviewedContentImage){
 			unviewedContentImage = [[NSImage imageNamed:@"unviewedContent.png" forClass:[self class]] retain];
 		}
+		
+		//Initialize our state
+		iconState = -1;
+		[self setIconState:OFFLINE];
 		
         //Create and install the menu
         theMenu = [[NSMenu alloc] init];
@@ -52,11 +54,21 @@ static	NSImage						*unviewedContentImage = nil;
         //Setup for unviewed content catching
         accountMenuItemsArray = [[NSMutableArray alloc] init];
         unviewedObjectsArray = [[NSMutableArray alloc] init];
-        unviewedState = NO;
         needsUpdate = YES;
 
         //Register as a chat observer (So we can catch the unviewed content status flag)
         [[adium contentController] registerChatObserver:self];
+				
+		//Register to recieve connect/disconnect notifications
+		[[adium notificationCenter] addObserver:self
+									   selector:@selector(accountStateChanged:)
+										   name:ACCOUNT_CONNECTED
+										 object:nil];
+		[[adium notificationCenter] addObserver:self
+									   selector:@selector(accountStateChanged:)
+										   name:ACCOUNT_DISCONNECTED
+								         object:nil];
+
     }
     
     return self;
@@ -75,6 +87,29 @@ static	NSImage						*unviewedContentImage = nil;
         
     //To the superclass, Robin!
     [super dealloc];
+}
+
+//Icon State --------------------------------------------------------
+#pragma mark Icon State
+
+- (void)setIconState:(SMI_Icon_State)state
+{
+	//If we're not already in that state
+	if(state != iconState){
+		//Set our state to the new one
+		iconState = state;
+		//And set the appropriate icon
+		if(iconState == OFFLINE){
+			[statusItem setImage:[NSImage imageNamed:@"adiumOffline.png" forClass:[self class]]];
+			[statusItem setAlternateImage:[NSImage imageNamed:@"adiumOfflineHighlight.png" forClass:[self class]]];
+		}else if(iconState == ONLINE){
+			[statusItem setImage:[NSImage imageNamed:@"adium.png" forClass:[self class]]];
+			[statusItem setAlternateImage:[NSImage imageNamed:@"adiumHighlight.png" forClass:[self class]]];
+		}else{
+			[statusItem setImage:[NSImage imageNamed:@"adiumRed.png" forClass:[self class]]];
+			[statusItem setAlternateImage:[NSImage imageNamed:@"adiumRedHighlight.png" forClass:[self class]]];
+		}
+	}
 }
 
 //AccountMenuPlugin --------------------------------------------------------
@@ -106,6 +141,7 @@ static	NSImage						*unviewedContentImage = nil;
 
 //Twiddle visibility --------------------------------------------------------
 #pragma mark Twiddle visibility
+
 - (void)showStatusItem
 {
 	//Kinda cheap hack, but it works
@@ -120,6 +156,7 @@ static	NSImage						*unviewedContentImage = nil;
 
 //Chat Observer --------------------------------------------------------
 #pragma mark Chat Observer
+
 - (NSArray *)updateChat:(AIChat *)inChat keys:(NSArray *)inModifiedKeys silent:(BOOL)silent
 {
 	//If the contact's unviewed content state has changed
@@ -133,12 +170,9 @@ static	NSImage						*unviewedContentImage = nil;
                 //We need to update our menu
                 needsUpdate = YES;
                 //If this is the first contact with unviewed content, set our icon to unviewed content
-                if(!unviewedState){
-                    //Set the image, with the highlight for 10.3 peoples.
-                    [statusItem setImage:[NSImage imageNamed:@"adiumRed.png" forClass:[self class]]];
-                    [statusItem setAlternateImage:[NSImage imageNamed:@"adiumRedHighlight.png" forClass:[self class]]];
-                    //Set our state variable
-                    unviewedState = YES;
+                if(iconState != UNVIEWED){
+                    //Set our state
+					[self setIconState:UNVIEWED];
                 }
             }
         //If they've viewed the content
@@ -150,22 +184,20 @@ static	NSImage						*unviewedContentImage = nil;
                 //We need to update our menu
                 needsUpdate = YES;
                 //If there are no more contacts with unviewed content, set our icon to normal
-                if([unviewedObjectsArray count] == 0 && unviewedState){
-                    //Set the image, and the highlight
-                    [statusItem setImage:[NSImage imageNamed:@"adium.png" forClass:[self class]]];
-                    [statusItem setAlternateImage:[NSImage imageNamed:@"adiumHighlight.png" forClass:[self class]]];
-                    //Set our state variable
-                    unviewedState = NO;
-                }
+                if([unviewedObjectsArray count] == 0 && iconState == UNVIEWED){
+                    //Set our state
+					[self setIconState:ONLINE];
+				}
             }
         }
     }
 	//We didn't modify contacts, so return nil 
-    return(nil);
+    return nil;
 }
 
 //Menu Delegate --------------------------------------------------------
 #pragma mark Menu Delegate
+
 - (void)menuNeedsUpdate:(NSMenu *)menu
 {
     //If something has changed
@@ -222,7 +254,6 @@ static	NSImage						*unviewedContentImage = nil;
 
 //Menu Actions --------------------------------------------------------
 #pragma mark Menu Actions
-
 - (void)switchToChat:(id)sender
 {
     //If we're not the active app, activate 
@@ -237,6 +268,25 @@ static	NSImage						*unviewedContentImage = nil;
 {
     [NSApp activateIgnoringOtherApps:YES];
     [NSApp arrangeInFront:nil];
+}
+
+//Offline Icon Control --------------------------------------------------------
+#pragma mark Offline Icon Control
+
+- (void)accountStateChanged:(NSNotification *)notification
+{
+	static int onlineCount = 0;
+	
+	//Increase our counter when accounts come online
+	if([[notification name] isEqualToString:ACCOUNT_CONNECTED]){
+		onlineCount++;
+	//Decrease our counter when accounts go offline
+	}else{
+		onlineCount--;
+	}
+	
+	//Set our Icon State accordingly
+	[self setIconState:(onlineCount > 0 ? ONLINE : OFFLINE)];
 }
 
 @end
