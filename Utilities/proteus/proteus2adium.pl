34a9a1f270599a031671d3a39b4c5ad2521a3661
Jeffrey Melloy <jmelloy@gmail.com>
2004-10-28 17:13:43 +0000

    Updated the sqlite binary and the proteus2adium script so it works with Proteus 4 and loops over all services instead of forcing command line switches.

--- Utilities/proteus/proteus2adium.pl
+++ Utilities/proteus/proteus2adium.pl
@@ -5,49 +5,29 @@
 # It will create a folder with your username that you can drop into Adium's
 # log folder or copy the contents.
 #
-# Run it by doing "./proteus2adium.pl --aim AIM_USER_NAME"
-#
-# Updated 31jan2004 by William Knop <w_knop@hotmail.com>
-#   - added support for icq logs
-#   - added usage statement
-#   - changed the behavior if usage is incorrect (it now exits when required
-#     args are not given)
-# Updated 24jan2004 by Seth Dillingham <seth@macrobyte.net>
-#   - pipe the input from the sqlite script, rather than running 
-#     it in backticks (more memory efficient for large histories)
-#   - correctly handles multi-line history records
-#     (Proteus uses \n whereas Adium uses <BR>)
+# Run it by doing "./proteus2adium.pl"
 
 use warnings;
 use strict;
 
-my $proteus_log_file = "$ENV{HOME}/Library/Application Support/Instant Messaging/Profile/History.db";
+my $proteus_log_file = "$ENV{HOME}/Library/Application Support/Proteus/Profile/History.db";
 
 my $user_name;
-my $service_type;
+my @service_type;
 my $base_out;
-for(my $i = 0; $i < @ARGV; $i++) {
-    if ($ARGV[$i] eq "--aim") {
-        $user_name = $ARGV[$i + 1];
-        $service_type = "aim-oscar";
-        $base_out = "AIM.$user_name";
-    } elsif ($ARGV[$i] eq "--icq") {
-    	$user_name = $ARGV[$i + 1];
-    	$service_type = "icq";
-    	$base_out = "ICQ.$user_name";
-    } elsif ($ARGV[$i] eq "--msn") {
-        $user_name = $ARGV[$i + 1];
-        $service_type= "msn";
-        $base_out = "MSN.$user_name";
-    }
-}
-
-if (($service_type eq "") || ($user_name eq "")) {
-	print "Usage: proteus2adium [--aim]|[--icq]|[--msn] <your screen name>\n";
-	exit;
-}
 
 my @messages;
+
+# Proteus makes a table for each service, let's get them.
+
+my $query = ".tables";
+
+open( QUERYPIPE, './sqlite "' . $proteus_log_file . '" "' . $query . '" |' ) or die "Could not open the pipe: $!";
+
+while(<QUERYPIPE>) {
+    chomp;
+    @service_type = split(' ');
+}
 
 # The proteus tables are dumb.
 # the schema for each is:
@@ -61,47 +41,53 @@ my @messages;
 # I'm not actually sure what type and URL are for.
 # Type seems to always be 1, and URL seems to always be empty.
 
+foreach my $service (@service_type) {
+    print "Please enter the username for $service: ";
+    $user_name = <STDIN>;
+    chomp($user_name);
+    $base_out = $service . "." . $user_name;
 
-my $query = "select substr(date, 0, 10), identifier, \'<div class=\\\"\' || case when incoming=1 then \'receive\' else \'send\' end || \'\\\"><span class=\\\"timestamp\\\">\' || substr(date, 12, 8) || \'</span><span class=\\\"sender\\\">\' || case when incoming = 1 then identifier else \'$user_name\' end || \': </span><pre class=\\\"message\\\">\' || message || \'</pre></div>\' from \\\"$service_type\\\"";
+    $query = "select substr(date, 0, 10), identifier, \'<div class=\\\"\' || case when incoming=1 then \'receive\' else \'send\' end || \'\\\"><span class=\\\"timestamp\\\">\' || substr(date, 12, 8) || \'</span><span class=\\\"sender\\\">\' || case when incoming = 1 then identifier else \'$user_name\' end || \': </span><pre class=\\\"message\\\">\' || message || \'</pre></div>\' from \\\"$service\\\"";
 
-open( QUERYPIPE, './sqlite "' . $proteus_log_file . '" "' . $query . '" |' ) or die "Could not open the pipe: $!";
+    open( QUERYPIPE, './sqlite "' . $proteus_log_file . '" "' . $query . '" |' ) or die "Could not open the pipe: $!";
 
-umask(000);
+    umask(000);
 
-# make sure the basic output dir exists
-mkdir($base_out, 0777) unless (-d $base_out); 
+    # make sure the basic output dir exists
+    mkdir($base_out, 0777) unless (-d $base_out);
 
-my $history_line = '';
+    my $history_line = '';
 
-while ( <QUERYPIPE> )
-{
-    chomp;
-    
-    # some of the records coming from the proteus logs are multi-line
-    # so we build $history_line by appending the record to it
-    $history_line .= $_;
-    
-    # if the pattern matches, we have the whole record
-    if ( $history_line =~ /(\d*)-(\d*)-(\d*)\|(.*)\|(<div.*<\/div>)/s )
+    while ( <QUERYPIPE> )
     {
-        my ($year, $month, $day, $ident, $message) = ($1, $2, $3, $4, $5);
-        
-        #make sure the output dir exists for the current contact
-        mkdir( "$base_out/$ident/", 0777 ) unless ( -d "$base_out/$ident" );
-        
-        my $output_file = "$base_out/$ident/$ident ($year|$month|$day).html";
-        open( OUTFILE, ">>$output_file" ) or die( "Could not open output file $output_file: $!" );
-        
-        print OUTFILE "$message\n";
-        
-        close OUTFILE or warn( "Could not close the output file $output_file: $!" );
-        
-        # clear history_line in preparation for the next record
-        $history_line = '';
-    }
-    else
-    {
-        # adium uses <BR> to separate lines instead of \n
-        $history_line .= "<BR>";
+        chomp;
+
+        # some of the records coming from the proteus logs are multi-line
+        # so we build $history_line by appending the record to it
+        $history_line .= $_;
+
+        # if the pattern matches, we have the whole record
+        if ( $history_line =~ /(\d*)-(\d*)-(\d*)\|(.*)\|(<div.*<\/div>)/s )
+        {
+            my ($year, $month, $day, $ident, $message) = ($1, $2, $3, $4, $5);
+
+            #make sure the output dir exists for the current contact
+            mkdir( "$base_out/$ident/", 0777 ) unless ( -d "$base_out/$ident" );
+
+            my $output_file = "$base_out/$ident/$ident ($year|$month|$day).html";
+            open( OUTFILE, ">>$output_file" ) or die( "Could not open output file $output_file: $!" );
+
+            print OUTFILE "$message\n";
+
+            close OUTFILE or warn( "Could not close the output file $output_file: $!" );
+
+            # clear history_line in preparation for the next record
+            $history_line = '';
+        }
+        else
+        {
+            # adium uses <BR> to separate lines instead of \n
+            $history_line .= "<BR>";
+        }
     }
 }
