8f3e6a11f8c928bc42d2dfc64be8badca5586153
adamiser <adamiser@localhost>
2004-10-30 14:42:09 +0000

    Setting up the basics for a service icon selection preferences plugin

---
+++ Plugins/Service Icon Preferences/AIServiceIconPreferencesPlugin.h
@@ -0,0 +1,16 @@
+//
+//  AIServiceIconPreferencesPlugin.h
+//  Adium
+//
+//  Created by Adam Iser on 10/30/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AIServiceIconPreferencesPlugin : AIPlugin {
+
+}
+
+@end
