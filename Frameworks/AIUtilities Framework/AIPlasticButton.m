a3789f46f78aafd4902281e8cdb1cc83d3d2e1bb
Evan Schoenberg <Evan Schoenberg@localhost>
2004-11-10 01:22:53 +0000

    Added dropdown menu support to AIPlasticButton

--- Frameworks/AIUtilities Framework/AIPlasticButton.m
+++ Frameworks/AIUtilities Framework/AIPlasticButton.m
@@ -15,6 +15,16 @@
 
 @implementation AIPlasticButton
 
+//
+- (id)copyWithZone:(NSZone *)zone
+{
+	AIPlasticButton	*newButton = [[[self class] allocWithZone:zone] initWithFrame:[self frame]];
+	
+	[newButton setMenu:[[[self menu] copy] autorelease]];
+	
+	return(newButton);
+}
+
 - (id)initWithFrame:(NSRect)frameRect
 {
     [super initWithFrame:frameRect];
@@ -124,9 +134,56 @@
                                   size.height);
 
         [[self image] setFlipped:YES];
-        [[self image] drawInRect:centeredRect fromRect:NSMakeRect(0,0,size.width,size.height) operation:NSCompositeSourceOver fraction:([self isEnabled] ? 1.0 : 0.5)];
+        [[self image] drawInRect:centeredRect
+						fromRect:NSMakeRect(0,0,size.width,size.height) 
+					   operation:NSCompositeSourceOver 
+						fraction:([self isEnabled] ? 1.0 : 0.5)];
     }
     
+}
+
+//Mouse Tracking -------------------------------------------------------------------------------------------------------
+#pragma mark Mouse Tracking
+//Custom mouse down tracking to display our menu and highlight
+- (void)mouseDown:(NSEvent *)theEvent
+{
+	if(![self menu]){
+		[super mouseDown:theEvent];
+	}else{
+		if([self isEnabled]){
+			[self highlight:YES];
+			
+			NSPoint point = [self convertPoint:[self bounds].origin toView:nil];
+			point.y -= NSHeight([self frame]) + 2;
+			point.x -= 1;
+			
+			NSEvent *event = [NSEvent mouseEventWithType:[theEvent type]
+												location:point
+										   modifierFlags:[theEvent modifierFlags]
+											   timestamp:[theEvent timestamp]
+											windowNumber:[[theEvent window] windowNumber]
+												 context:[theEvent context]
+											 eventNumber:[theEvent eventNumber]
+											  clickCount:[theEvent clickCount]
+												pressure:[theEvent pressure]];
+			[NSMenu popUpContextMenu:[self menu] withEvent:event forView:self];
+			
+			[self mouseUp:[[NSApplication sharedApplication] currentEvent]];
+		}
+	}
+}
+
+//Remove highlight on mouse up
+- (void)mouseUp:(NSEvent *)theEvent
+{
+	[self highlight:NO];
+	[super mouseUp:theEvent];
+}
+
+//Ignore dragging
+- (void)mouseDragged:(NSEvent *)theEvent
+{
+	//Empty
 }
 
 - (BOOL)isOpaque
