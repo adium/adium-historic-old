7c6142950cc5cb4a3a7a8b9ea1e8aa1d12c72ea9
Evan Schoenberg <Evan Schoenberg@localhost>
2004-11-15 02:59:47 +0000

    Fixed silliness preventing the Zephyr join chat window from working

--- Plugins/Gaim Service/DCGaimZephyrJoinChatViewController.m
+++ Plugins/Gaim Service/DCGaimZephyrJoinChatViewController.m
@@ -56,7 +56,7 @@
 															  forKey:@"class"];
 
 		//The chatCreationInfo has keys corresponding to the GHashTable keys and values to match them.
-		chatCreationInfo = [NSDictionary dictionaryWithObjectsAndKeys:class,@"class",instance,@"instance",recipient,@"recipient"];
+		chatCreationInfo = [NSDictionary dictionaryWithObjectsAndKeys:class,@"class",instance,@"instance",recipient,@"recipient",nil];
 		
 		name = [NSString stringWithFormat:@"%@,%@,%@",class,instance,recipient];
 		
