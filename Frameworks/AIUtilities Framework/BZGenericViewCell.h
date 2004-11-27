f20efbde42ca51d94cdb822468e3eff3b8989c22
Evan Schoenberg <Evan Schoenberg@localhost>
2004-11-27 03:22:35 +0000

    BZGenericViewCell is now an AIGradientCell subclass, 'cause gradients is pretty.

--- Frameworks/AIUtilities Framework/BZGenericViewCell.h
+++ Frameworks/AIUtilities Framework/BZGenericViewCell.h
@@ -5,11 +5,11 @@
 //  Created by Mac-arena the Bored Zo on Sun May 09 2004.
 //
 
-#import <Foundation/Foundation.h>
+#import "AIGradientCell.h"
 
-//based on sample code at http://www.cocoadev.com/index.pl?NSViewInNSTableView
+//Based on sample code from SubViewTableView by Joar Wingfors, http://www.joar.com/code/
 
-@interface BZGenericViewCell : NSCell
+@interface BZGenericViewCell : AIGradientCell
 {
 	NSView	*embeddedView;
 }
