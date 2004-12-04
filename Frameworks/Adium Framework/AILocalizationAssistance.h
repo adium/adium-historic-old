0be238f126246854801fa7fad9ea14d4c0d27223
Evan Schoenberg <Evan Schoenberg@localhost>
2004-12-04 20:50:32 +0000

    Updates to the localization assistance classes. Added support for NSButton, centered text, improved handling of anchored views

---
+++ Frameworks/Adium Framework/AILocalizationAssistance.h
@@ -0,0 +1,7 @@
+typedef enum {
+	AILOCALIZATION_MOVE_SELF = 0,
+	AILOCALIZATION_MOVE_ANCHOR
+} AILocalizationAnchorMovementType;
+
+#import "AILocalizationTextField.h"
+#import "AILocalizationButton.h"
