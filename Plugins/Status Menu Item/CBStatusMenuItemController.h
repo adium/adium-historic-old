6cc6218f320780c444ad74d8d9b4d5ef6fee8de5
Colin Barrett <colin@springsandstruts.com>
2004-12-09 16:16:28 +0000

    The EMD now shows not just conversations with unviewed content, but all open conversations as well. Unfortunately, we don't get icons for those. Still looking in to that. Other than that, it works great :)

--- Plugins/Status Menu Item/CBStatusMenuItemController.h
+++ Plugins/Status Menu Item/CBStatusMenuItemController.h
@@ -18,6 +18,7 @@ typedef enum {
     
     NSMutableArray          *accountMenuItemsArray;
     NSMutableArray          *unviewedObjectsArray;
+	NSMutableArray			*openChatsArray;
     
     BOOL                    needsUpdate;
 	
