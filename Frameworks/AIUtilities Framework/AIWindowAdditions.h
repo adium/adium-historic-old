077c4968cd3ebc53aa624c62f61918e844e72daa
adamiser <adamiser@localhost>
2004-12-13 16:30:08 +0000

    Adding a method for animating a window to a new frame with setContentSize: .  Not being used anywhere in our code, but could be user many places I believe.

--- Frameworks/AIUtilities Framework/AIWindowAdditions.h
+++ Frameworks/AIUtilities Framework/AIWindowAdditions.h
@@ -14,6 +14,7 @@
  \------------------------------------------------------------------------------------------------------ */
 
 @interface NSWindow (AIWindowAdditions)
+- (void)setContentSize:(NSSize)aSize display:(BOOL)displayFlag animate:(BOOL)animateFlag;
 - (BOOL)isTextured;
 - (void)compatibleInvalidateShadow;
 -(void)setIgnoresExpose:(BOOL)flag;
