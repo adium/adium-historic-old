c44fc2e8da865cb7abe4328042e2d204f1cfe5b1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-12-21 06:49:19 +0000

    Moved the custom message handling to the contact alert; removed the old contentObjectReceived stuff.  Speak Time now applies to all spoken text, including Speak Specific Text.  Preferences are just messed up and mostly nonfunctional for now; will move to the contact alert's detail pane later.

--- Plugins/Announcer/ESAnnouncerPlugin.h
+++ Plugins/Announcer/ESAnnouncerPlugin.h
@@ -9,43 +9,31 @@
 
 #define ANNOUNCER_DEFAULT_PREFS 	@"AnnouncerDefaults"
 #define PREF_GROUP_ANNOUNCER		@"Announcer"
-#define KEY_ANNOUNCER_ENABLED		@"Speech Enabled"
-#define KEY_ANNOUNCER_OUTGOING		@"Speak Outgoing"
-#define KEY_ANNOUNCER_INCOMING		@"Speak Incoming"
-#define KEY_ANNOUNCER_STATUS		@"Speak Status"
 #define KEY_ANNOUNCER_TIME			@"Speak Time"
 #define KEY_ANNOUNCER_SENDER		@"Speak Sender"
-#define KEY_ANNOUNCER_MESSAGETEXT	@"Speak Message Text"
 
 #define KEY_ANNOUNCER_TEXT_TO_SPEAK @"TextToSpeak"
 
-#define VOICE_STRING				@"Voice"
-#define PITCH						@"Pitch"
-#define RATE						@"Rate"
+#define KEY_VOICE_STRING				@"Voice"
+#define KEY_PITCH						@"Pitch"
+#define KEY_RATE						@"Rate"
 
 #define CONTACT_ALERT_SPEAK_TEXT_IDENTIFIER		@"SpeakText"
 #define CONTACT_ALERT_SPEAK_EVENT_IDENTIFIER	@"SpeakEvent"
 
 @interface ESAnnouncerPlugin : AIPlugin <AIActionHandler> {
-    ESAnnouncerPreferences	*preferences;
+    ESAnnouncerPreferences		*preferences;
 
-    NSString 			*lastSenderString;
+    NSString					*lastSenderString;
     
-	BOOL						speechEnabled;
-    BOOL                        speakIncoming;
-    BOOL                        speakOutgoing;
-    BOOL                        speakMessages;
-    BOOL                        speakMessageText;
-    BOOL                        speakStatus;
     BOOL                        speakTime;
     BOOL                        speakSender;
-    BOOL                        observingContent;
 
     //Contact Editor view
-    IBOutlet	NSView		*view_contactAnnouncerInfoView;
+    IBOutlet	NSView			*view_contactAnnouncerInfoView;
     IBOutlet	NSPopUpButton	*popUp_voice;
-    IBOutlet	NSSlider	*slider_pitch;
-    IBOutlet	NSSlider	*slider_rate;
+    IBOutlet	NSSlider		*slider_pitch;
+    IBOutlet	NSSlider		*slider_rate;
     AIPreferenceViewController	*contactView;
     AIListObject                *activeListObject;
 }
