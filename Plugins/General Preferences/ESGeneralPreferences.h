8adbd07ad7a44748d99bfd62a205c84246805ae5
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-12-22 00:58:27 +0000

    Adding empty files

---
+++ Plugins/General Preferences/ESGeneralPreferences.h
@@ -0,0 +1,17 @@
+//
+//  ESGeneralPreferences.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/21/04.
+//  Copyright 2004 The Adium Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class AIPreferencePane;
+
+@interface ESGeneralPreferences : AIPreferencePane {
+
+}
+
+@end
