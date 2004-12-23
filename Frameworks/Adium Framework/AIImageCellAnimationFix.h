fc780ab86f006b48e8f1dd1382a86ff10638594a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2004-12-23 08:30:50 +0000

    Added AIImageCellAnimationFix, which poses as NSImageCell to fix the bug where NSImageView objects with animating NSImages weren't updating their images if the images animated.  This could be seen in the Get Info window.

---
+++ Frameworks/Adium Framework/AIImageCellAnimationFix.h
@@ -0,0 +1,15 @@
+//
+//  AIImageCellAnimationFix.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/23/04.
+//  Copyright 2004 The Adium Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AIImageCellAnimationFix : NSImageCell {
+
+}
+
+@end
---
+++ Frameworks/Adium Framework/AIImageCellAnimationFix.m
@@ -0,0 +1,31 @@
+//
+//  AIImageCellAnimationFix.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/23/04.
+//  Copyright 2004 The Adium Team. All rights reserved.
+//
+
+/* 
+ This NSImageCell poseAsClass implementation exists to fix a simple bug: an animating NSImageCell, as of
+ 10.3, does not mark its controlView as needing display, so the animation occurs but is not reflected in the view
+ until something else causes a display update.
+ */
+
+#import "AIImageCellAnimationFix.h"
+
+@implementation AIImageCellAnimationFix
+
++ (void)load
+{
+	[self poseAsClass:[NSImageCell class]];
+}
+
+- (void)_animationTimerCallback:(NSTimer *)inTimer
+{
+	[super _animationTimerCallback:inTimer];
+	
+	[[self controlView] setNeedsDisplay:YES];
+}
+
+@end
