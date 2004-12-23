497a4f16751708b10546dd4db41463c942715447
Evan Schoenberg <Evan Schoenberg@localhost>
2004-12-23 05:35:32 +0000

    Removed chat cycling preferences

--- Plugins/Chat Cycling/AIChatCyclingPlugin.h
+++ Plugins/Chat Cycling/AIChatCyclingPlugin.h
@@ -6,20 +6,7 @@
 //  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
 //
 
-#define PREF_GROUP_CHAT_CYCLING			@"Chat Cycling"
-#define KEY_TAB_SWITCH_KEYS				@"Tab Switching Keys"
-
-typedef enum {
-	AISwitchArrows = 0,
-	AISwitchShiftArrows,
-	AIBrackets
-} AITabKeys;
-
-@class AIChatCyclingPreferences;
-
 @interface AIChatCyclingPlugin : AIPlugin {
-	AIChatCyclingPreferences	*preferences;
-	
 	NSMenuItem					*previousChatMenuItem;
 	NSMenuItem					*nextChatMenuItem;
 }
