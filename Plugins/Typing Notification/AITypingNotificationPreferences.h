7943a302a90480f36d08502af3e295c7fc6f6edf
adamiser <adamiser@localhost>
2004-12-28 17:46:54 +0000

    Adding an advanced preference toggle to typing notifications

---
+++ Plugins/Typing Notification/AITypingNotificationPreferences.h
@@ -0,0 +1,16 @@
+//
+//  AITypingNotificationPreferences.h
+//  Adium
+//
+//  Created by Adam Iser on 12/28/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AITypingNotificationPreferences : AIPreferencePane {
+    IBOutlet	NSButton			*checkBox_disableTyping;
+}
+
+@end
---
+++ Plugins/Typing Notification/AITypingNotificationPreferences.m
@@ -0,0 +1,43 @@
+//
+//  AITypingNotificationPreferences.m
+//  Adium
+//
+//  Created by Adam Iser on 12/28/04.
+//  Copyright 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import "AITypingNotificationPreferences.h"
+#import "AITypingNotificationPlugin.h"
+
+@implementation AITypingNotificationPreferences
+
+//Preference pane properties
+- (PREFERENCE_CATEGORY)category{
+    return(AIPref_Advanced_Messages);
+}
+- (NSString *)label{
+    return(@"Typing Notification");
+}
+- (NSString *)nibName{
+    return(@"TypingPrefs");
+}
+
+//Configure the preference view
+- (void)viewDidLoad
+{
+    NSDictionary	*preferenceDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_TYPING_NOTIFICATIONS];
+
+    [checkBox_disableTyping setState:[[preferenceDict objectForKey:KEY_DISABLE_TYPING_NOTIFICATIONS] boolValue]];
+}
+
+//Called in response to all preference controls, applies new settings
+- (IBAction)changePreference:(id)sender
+{
+    if(sender == checkBox_disableTyping){
+        [[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+                                             forKey:KEY_DISABLE_TYPING_NOTIFICATIONS
+                                              group:PREF_GROUP_TYPING_NOTIFICATIONS];
+    }
+}
+
+@end
