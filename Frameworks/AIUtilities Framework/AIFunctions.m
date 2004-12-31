13724447cd374b05fc3fb14a9708c06debb34653
Peter Hosey <git@boredzo.org>
2004-12-31 17:21:28 +0000

    Added AIGetSurrogates function: splits a UTF-32 character into two UTF-16 surrogates. Added method to AIScanner to scan an unsigned int. -[NSString stringByUnescapingFromHTML:] now handles surrogates, and properly uses -scanUnsignedInt: rather than -scanInt:.

---
+++ Frameworks/AIUtilities Framework/AIFunctions.m
@@ -0,0 +1,48 @@
+/*-------------------------------------------------------------------------------------------------------*\
+| Adium, Copyright (C) 2001-2004, Adam Iser  (adamiser@mac.com | http://www.adiumx.com)                   |
+\---------------------------------------------------------------------------------------------------------/
+ | This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ | General Public License as published by the Free Software Foundation; either version 2 of the License,
+ | or (at your option) any later version.
+ |
+ | This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ | the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ | Public License for more details.
+ |
+ | You should have received a copy of the GNU General Public License along with this program; if not,
+ | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ \------------------------------------------------------------------------------------------------------ */
+
+#import "AIFunctions.h"
+
+BOOL AIGetSurrogates(UTF32Char in, UTF16Char *outHigh, UTF16Char *outLow) {
+	if(in < 0x10000) {
+		if(outHigh) *outHigh = 0;
+		if(outLow)  *outLow  = in;
+		return NO;
+	} else {
+		enum {
+			UTF32LowShiftToUTF16High = 10,
+			UTF32HighShiftToUTF16High,
+			UTF16HighMask = 31,  //0b0000 0111 1100 0000
+			UTF16LowMask  = 63,  //0b0000 0000 0011 1111
+			UTF32LowMask = 1023, //0b0000 0011 1111 1111
+			UTF16HighAdditiveMask = 55296, //0b1101 1000 0000 0000
+			UTF16LowAdditiveMask  = 56320, //0b1101 1100 0000 0000
+		};
+		UTF32Char temp;
+
+		if(outHigh) {
+			*outHigh = \
+				  ((in >> UTF32HighShiftToUTF16High) & UTF16HighMask) \
+				| ((in >> UTF32LowShiftToUTF16High) & UTF16LowMask) \
+				| UTF16HighAdditiveMask;
+		}
+
+		if(outLow) {
+			*outLow = (in & UTF32LowMask) | UTF16LowAdditiveMask;
+		}
+
+		return YES;
+	}
+}
