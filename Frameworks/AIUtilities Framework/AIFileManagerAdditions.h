5c5def9c964fe91355db74cd264dce50773985bc
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-01-18 06:52:15 +0000

    createDirectoriesForPath: now returns a BOOL indicating if any directories were created. Documentation updates.

--- Frameworks/AIUtilities Framework/AIFileManagerAdditions.h
+++ Frameworks/AIUtilities Framework/AIFileManagerAdditions.h
@@ -8,8 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+/*
+ * @category NSFileManager(AIFileManagerAdditions)
+ * @brief Additions to <tt>NSFileManager</tt> for trashing files and creating directories
+ */
 @interface NSFileManager (AIFileManagerAdditions)
+/*
+ * @brief Move a file or directory to the trash
+ *
+ * sourcePath does not need to be tildeExpanded; it will be expanded if necessary.
+ * @param sourcePath Path to the file or directory to trash
+ * @result YES if trashing was successful or the file already does not exist; NO if it failed
+ */
 - (BOOL)trashFileAtPath:(NSString *)sourcePath;
-- (void)createDirectoriesForPath:(NSString *)fullPath;
+
+/*
+ * @brief Create all directories for a path
+ *
+ * Like <b>mkdir -p</b>, create the specified directory if it does not exist and create all intermediate directories as needed.
+ * @param fullPath Path to be created
+ * @result YES if one or more directories were created
+ */
+- (BOOL)createDirectoriesForPath:(NSString *)fullPath;
+
+/*
+ * @brief Delete or trash all files in a directory starting with <b>prefix</b>
+ *
+ * The files must begin with characters matching <b>prefix</b> exactly; the comparison is case sensitive.
+ * @param dirPath The directory in which to search
+ * @param prefix The prefix for which to look, case sensitively
+ * @param moveToTrash If YES, move the files to the trash. If NO, delete them permanently.
+ */
 - (void)removeFilesInDirectory:(NSString *)dirPath withPrefix:(NSString *)prefix movingToTrash:(BOOL)moveToTrash;
 @end
