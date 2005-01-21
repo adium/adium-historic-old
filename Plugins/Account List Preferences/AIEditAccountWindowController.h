5f5e0ed786b5ff961c33228d43fa2307c9b4b34f
adamiser <adamiser@localhost>
2005-01-21 18:12:56 +0000

    Separating the actual editing away from the list preference and into its own sheet.  Updated for the new account view controller changes and cleaned up in general

---
+++ Plugins/Account List Preferences/AIEditAccountWindowController.h
@@ -0,0 +1,37 @@
+//
+//  AIEditAccountWindowController.h
+//  Adium
+//
+//  Created by Adam Iser on 1/16/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class AIAccountProxySettings, AIAccountViewController;
+
+@interface AIEditAccountWindowController : AIWindowController {
+	//Account preferences
+	IBOutlet	NSImageView					*image_serviceIcon;
+	IBOutlet	NSTextField					*textField_accountDescription;
+	IBOutlet	NSTextField					*textField_serviceName;
+	IBOutlet	ESImageViewWithImagePicker  *imageView_userIcon;
+    IBOutlet	NSTabView					*tabView_auxiliary;
+
+	//Replacable views
+	IBOutlet	NSView						*view_accountSetup;
+	IBOutlet	NSView						*view_accountProxy;
+	IBOutlet	NSView						*view_accountProfile;
+	IBOutlet	NSView						*view_accountOptions;
+
+	//Current configuration
+    AIAccountViewController		*accountViewController;
+	AIAccountProxySettings 		*accountProxyController;
+	AIAccount					*account;	
+}
+
++ (void)editAccount:(AIAccount *)account onWindow:(id)parentWindow;
+- (IBAction)cancel:(id)sender;
+- (IBAction)okay:(id)sender;
+
+@end
