93d9d622c2d5289c6bf1933c7d2f1d727c19b919
adamiser <adamiser@localhost>
2005-01-21 18:35:24 +0000

    Updating for account view controller changes, non-immediate change saving

--- Plugins/Gaim Service/AIGaimOscarAccountViewController.h
+++ Plugins/Gaim Service/AIGaimOscarAccountViewController.h
@@ -13,9 +13,9 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
-#include "ESGaimAccountViewController.h"
+#include "AIAccountViewController.h"
 
-@interface AIGaimOscarAccountViewController : ESGaimAccountViewController {
+@interface AIGaimOscarAccountViewController : AIAccountViewController {
     IBOutlet        NSTextView                      *textView_textProfile;
 }
 
