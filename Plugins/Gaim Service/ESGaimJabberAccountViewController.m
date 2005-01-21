c19f5e4cf0caeb17c4bf3a93cebdced0e7cceecf
adamiser <adamiser@localhost>
2005-01-21 18:33:26 +0000

    Updating for account view controller changes, pruning dimming code, modifying to support non-immediate change saving

--- Plugins/Gaim Service/ESGaimJabberAccountViewController.m
+++ Plugins/Gaim Service/ESGaimJabberAccountViewController.m
@@ -19,90 +19,39 @@
 {
     [super configureForAccount:inAccount];
 	
+	//Connection security
 	[checkBox_useTLS setState:[[account preferenceForKey:KEY_JABBER_USE_TLS group:GROUP_ACCOUNT_STATUS] boolValue]];
 	[checkBox_forceOldSSL setState:[[account preferenceForKey:KEY_JABBER_FORCE_OLD_SSL group:GROUP_ACCOUNT_STATUS] boolValue]];
 	[checkBox_allowPlaintext setState:[[account preferenceForKey:KEY_JABBER_ALLOW_PLAINTEXT group:GROUP_ACCOUNT_STATUS] boolValue]];
 	
+	//Resource
 	[textField_resource setStringValue:[account preferenceForKey:KEY_JABBER_RESOURCE group:GROUP_ACCOUNT_STATUS]];
 	
+	//Connect server
 	NSString *connectServer = [account preferenceForKey:KEY_JABBER_CONNECT_SERVER group:GROUP_ACCOUNT_STATUS];
 	[textField_connectServer setStringValue:(connectServer ? connectServer : @"")];
 }
-/*
-//Save changes made to a preference control
-- (IBAction)changedPreference:(id)sender
+
+//Save controls
+- (void)saveConfiguration
 {
-    [super changedPreference:sender];
-}
-*/
+    [super saveConfiguration];
+	
+	//Connection security
+	[account setPreference:[NSNumber numberWithBool:[checkBox_useTLS state]]
+					forKey:KEY_JABBER_USE_TLS group:GROUP_ACCOUNT_STATUS];
+	[account setPreference:[NSNumber numberWithBool:[checkBox_forceOldSSL state]]
+					forKey:KEY_JABBER_FORCE_OLD_SSL group:GROUP_ACCOUNT_STATUS];
+	[account setPreference:[NSNumber numberWithBool:[checkBox_allowPlaintext state]]
+					forKey:KEY_JABBER_ALLOW_PLAINTEXT group:GROUP_ACCOUNT_STATUS];
 
-- (IBAction)changedConnectionPreference:(id)sender
-{	
-	if (sender == checkBox_useTLS){
-		[account setPreference:[NSNumber numberWithBool:[sender state]]
-						forKey:KEY_JABBER_USE_TLS
-						 group:GROUP_ACCOUNT_STATUS];
-			
-	}else if (sender == checkBox_forceOldSSL){
-		[account setPreference:[NSNumber numberWithBool:[sender state]]
-												 forKey:KEY_JABBER_FORCE_OLD_SSL
-												  group:GROUP_ACCOUNT_STATUS];
-			
-	}else if (sender == checkBox_allowPlaintext){
-		[account setPreference:[NSNumber numberWithBool:[sender state]]
-												 forKey:KEY_JABBER_ALLOW_PLAINTEXT
-												  group:GROUP_ACCOUNT_STATUS];
-			
-	}else if (sender == textField_resource){
-		//Access the host name so we can redisplay the default value if applicable
-		if (![[textField_resource stringValue] length]){
-			NSString *resource = [[adium preferenceController] preferenceForKey:KEY_JABBER_RESOURCE
-																		  group:GROUP_ACCOUNT_STATUS];
-			if (resource){
-				[textField_resource setStringValue:resource];
-			}
-		}
-		
-	}else{
-		[super changedConnectionPreference:sender];		
-	}
+	//Resource
+	[account setPreference:([[textField_resource stringValue] length] ? [textField_resource stringValue] : nil)
+					forKey:KEY_JABBER_RESOURCE group:GROUP_ACCOUNT_STATUS];
+	
+	//Connect server
+	[account setPreference:([[textField_connectServer stringValue] length] ? [textField_connectServer stringValue] : nil)
+					forKey:KEY_JABBER_CONNECT_SERVER group:GROUP_ACCOUNT_STATUS];
 }
 
-- (void)controlTextDidChange:(NSNotification *)notification
-{
-	NSTextField *sender = [notification object];
-	if (sender == textField_connectServer){
-		NSString *connectServer = [sender stringValue];
-		[account setPreference:([connectServer length] ? connectServer : nil)
-						forKey:KEY_JABBER_CONNECT_SERVER
-						 group:GROUP_ACCOUNT_STATUS];
-		
-	}else if (sender == textField_resource){
-		NSString *resource = [textField_resource stringValue];
-		[account setPreference:([resource length] ? resource : nil)
-						forKey:KEY_JABBER_RESOURCE
-						 group:GROUP_ACCOUNT_STATUS];
-		
-	}else{
-		[super controlTextDidChange:notification];
-	}
-}
-
-//Update display for account status change
-//XXX - We're not doing any dynamic disabling/enabling at the moment in account views -ai
-//- (NSSet *)updateListObject:(AIListObject *)inObject keys:(NSSet *)inModifiedKeys silent:(BOOL)silent
-//{	
-//	if(inObject == nil || inObject == account){
-//		if(inModifiedKeys == nil || [inModifiedKeys containsObject:@"Online"]){
-//			BOOL shouldEnable = ![[account statusObjectForKey:@"Online"] boolValue];
-//			[checkBox_useTLS setEnabled:shouldEnable];
-//			[checkBox_forceOldSSL setEnabled:shouldEnable];
-//			[checkBox_allowPlaintext setEnabled:shouldEnable];
-//			[textField_connectServer setEnabled:shouldEnable];
-//			[textField_resource setEnabled:shouldEnable];
-//		}
-//	}
-//	
-//	return([super updateListObject:inObject keys:inModifiedKeys silent:silent]);
-//}
 @end
