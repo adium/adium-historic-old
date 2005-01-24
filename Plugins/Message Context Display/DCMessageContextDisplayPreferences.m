9ab97c0ddf1453f6d8c91300b9156ea1289e09b8
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-01-24 09:29:11 +0000

    Fix0r

--- Plugins/Message Context Display/DCMessageContextDisplayPreferences.m
+++ Plugins/Message Context Display/DCMessageContextDisplayPreferences.m
@@ -16,6 +16,7 @@
 #import "DCMessageContextDisplayPlugin.h"
 #import "DCMessageContextDisplayPreferences.h"
 
+@class AIPreferenceWindowController;
 
 @implementation DCMessageContextDisplayPreferences
 
