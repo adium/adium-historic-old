e4141529fa795a1b13f3b9bfbfdfed3e7abc33fd
Evan Schoenberg <Evan Schoenberg@localhost>
2005-01-24 08:56:47 +0000

    Advanced preferences updates

--- Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.h
+++ Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.h
@@ -6,9 +6,22 @@
 //  Copyright (c) 2003-2005 The Adium Team. All rights reserved.
 //
 
+#define PREF_GROUP_WEBKIT_MESSAGE_DISPLAY	@"WebKit Message Display"
+#define WEBKIT_DEFAULT_PREFS				@"WebKit Defaults"
+
+#define KEY_WEBKIT_NAME_FORMAT				@"Name Format"
+#define KEY_WEBKIT_COMBINE_CONSECUTIVE		@"Combine Consecutive Messages"
+#define KEY_WEBKIT_USE_BACKGROUND			@"Use Background Color"
+#define KEY_WEBKIT_USE_NAME_FORMAT			@"Use Custom Name Format"
+
 @interface ESDualWindowMessageAdvancedPreferences : AIPreferencePane {
     IBOutlet	NSButton		*autohide_tabBar;
     IBOutlet    NSButton		*checkBox_allowInactiveClosing;
+	
+	IBOutlet	NSButton		*checkBox_customNameFormatting;
+	IBOutlet	NSPopUpButton   *popUp_nameFormat;
+	IBOutlet	NSButton		*checkBox_combineConsecutive;
+	IBOutlet	NSButton		*checkBox_backgroundColorFormatting;
 }
 
 @end
--- Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
+++ Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
@@ -9,6 +9,7 @@
 #import "ESDualWindowMessageAdvancedPreferences.h"
 #import "AIDualWindowInterfacePlugin.h"
 
+@class AIPreferenceWindowController;
 
 @implementation ESDualWindowMessageAdvancedPreferences
 
@@ -17,14 +18,18 @@
     return(AIPref_Advanced_Messages);
 }
 - (NSString *)label{
-    return(AILocalizedString(@"Window Preferences",nil));
+    return(AILocalizedString(@"Messages",nil));
 }
 - (NSString *)nibName{
     return(@"DualWindowMessageAdvanced");
 }
+- (NSImage *)image{
+	return([NSImage imageNamed:@"pref-messages" forClass:[AIPreferenceWindowController class]]);
+}
 
 - (NSDictionary *)restorablePreferences
 {
+#warning Evan: Need to fix these defaults
 	NSDictionary *defaultPrefs = [NSDictionary dictionaryNamed:DUAL_INTERFACE_WINDOW_DEFAULT_PREFS forClass:[self class]];
 	NSDictionary *defaultsTemp = [NSDictionary dictionaryWithObjectsAndKeys:
 		[defaultPrefs objectForKey:KEY_ALWAYS_CREATE_NEW_WINDOWS],KEY_ALWAYS_CREATE_NEW_WINDOWS,
@@ -48,20 +53,51 @@
 											 forKey:KEY_ENABLE_INACTIVE_TAB_CLOSE
 											  group:PREF_GROUP_DUAL_WINDOW_INTERFACE];
 		
+	}else if (sender == checkBox_combineConsecutive){
+		[[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+											 forKey:KEY_WEBKIT_COMBINE_CONSECUTIVE
+											  group:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];	
+	}else if(sender == checkBox_customNameFormatting){
+		[[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+											 forKey:KEY_WEBKIT_USE_NAME_FORMAT
+											  group:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+	}else if(sender == popUp_nameFormat){
+		[[adium preferenceController] setPreference:[NSNumber numberWithInt:[[sender selectedItem] tag]]
+											 forKey:KEY_WEBKIT_NAME_FORMAT
+											  group:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+	}else if(sender == checkBox_backgroundColorFormatting){
+		[[adium preferenceController] setPreference:[NSNumber numberWithBool:[sender state]]
+											 forKey:KEY_WEBKIT_USE_BACKGROUND
+											  group:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
 	}
+	
+	[self configureControlDimming];
 }
 
 //Configure the preference view
 - (void)viewDidLoad
 {
-    NSDictionary	*preferenceDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+    NSDictionary	*prefDict;
+	
+	prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_DUAL_WINDOW_INTERFACE];
+    [autohide_tabBar setState:![[prefDict objectForKey:KEY_AUTOHIDE_TABBAR] boolValue]];
+    [checkBox_allowInactiveClosing setState:[[prefDict objectForKey:KEY_ENABLE_INACTIVE_TAB_CLOSE] boolValue]];
 
-    [autohide_tabBar setState:![[preferenceDict objectForKey:KEY_AUTOHIDE_TABBAR] boolValue]];
-    [checkBox_allowInactiveClosing setState:[[preferenceDict objectForKey:KEY_ENABLE_INACTIVE_TAB_CLOSE] boolValue]];
-
+	prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+	[popUp_nameFormat compatibleSelectItemWithTag:[[prefDict objectForKey:KEY_WEBKIT_NAME_FORMAT] intValue]];
+	[checkBox_customNameFormatting setState:[[prefDict objectForKey:KEY_WEBKIT_USE_NAME_FORMAT] boolValue]];
+	[checkBox_combineConsecutive setState:[[prefDict objectForKey:KEY_WEBKIT_COMBINE_CONSECUTIVE] boolValue]];
+	[checkBox_backgroundColorFormatting setState:[[prefDict objectForKey:KEY_WEBKIT_USE_BACKGROUND] boolValue]];
+	
     [self configureControlDimming];
 }
 
+- (void)configureControlDimming
+{
+	NSDictionary	*prefDict = [[adium preferenceController] preferencesForGroup:PREF_GROUP_WEBKIT_MESSAGE_DISPLAY];
+	
+	[popUp_nameFormat setEnabled:[[prefDict objectForKey:KEY_WEBKIT_USE_NAME_FORMAT] boolValue]];
+}
 @end
 
 
