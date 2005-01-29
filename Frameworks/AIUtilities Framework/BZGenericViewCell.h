049a5478140cae07927b099cd2f43c09e26c3611
Evan Schoenberg <Evan Schoenberg@localhost>
2005-01-29 23:47:00 +0000

    Documentation for BZGenericViewCell

--- Frameworks/AIUtilities Framework/BZGenericViewCell.h
+++ Frameworks/AIUtilities Framework/BZGenericViewCell.h
@@ -8,13 +8,35 @@
 
 #import "AIGradientCell.h"
 
-//Based on sample code from SubViewTableView by Joar Wingfors, http://www.joar.com/code/
-
+/*
+ * @class BZGenericViewCell
+ * @brief A cell which can display any view
+ *
+ * This cell allows any view to be used in a table or outlineview.
+ * Based on sample code from SubViewTableView by Joar Wingfors, http://www.joar.com/code/
+ */
 @interface BZGenericViewCell : AIGradientCell
 {
 	NSView	*embeddedView;
 }
 
+/*
+ * @brief Set the NSView this cell displays
+ *
+ * This should be called before the cell is used, such as in a tableView:willDisplayCell: type delegate method.
+ *
+ * @param inView The view to display
+ */
 - (void)setEmbeddedView:(NSView *)inView;
+
+/*
+ * @brief Used within AIUtilities to generate a drag image from this cell
+ *
+ * This is a hack, and it's not a particularly great one.  A drawing context must be locked before this is called.
+ *
+ * @param cellFrame The frame of the cell
+ * @param controlView The view into which the cell is drawing
+ */
+- (void)drawEmbeddedViewWithFrame:(NSRect)cellFrame inView:(NSView *)controlView;
 
 @end
