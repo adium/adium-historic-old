2fc4aabc38c22ec2baae65e455d74c7faea2c43b
Evan Schoenberg <Evan Schoenberg@localhost>
2005-01-31 08:23:57 +0000

    Fixed emoticon pack preview drag images

---
+++ Plugins/Emoticons/AIEmoticonPackPreviewTableView.h
@@ -0,0 +1,16 @@
+//
+//  AIEmoticonPackPreviewTableView.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 1/30/05.
+//  Copyright 2005 The Adium Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AIEmoticonPackPreviewTableView : AIAlternatingRowTableView {
+
+}
+
+@end
