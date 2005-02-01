2baf6e8ef66e09a68fbee3cf193323b6e3fc8575
adamiser <adamiser@localhost>
2005-02-01 15:20:10 +0000

    Advanced preferences consolidation

--- Plugins/Message Context Display/DCMessageContextDisplayPreferences.m
+++ Plugins/Message Context Display/DCMessageContextDisplayPreferences.m
@@ -22,7 +22,7 @@
 
 //Preference pane properties
 - (PREFERENCE_CATEGORY)category{
-    return(AIPref_Advanced_Messages);
+    return(AIPref_Advanced);
 }
 - (NSString *)label{
     return(AILocalizedString(@"Message History",nil));
