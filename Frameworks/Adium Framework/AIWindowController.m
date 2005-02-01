212bdfcd10a24918ac9ba90f8687c31d4f17c0f5
adamiser <adamiser@localhost>
2005-02-01 15:15:40 +0000

    Adding the "Close window" method to our common window controller subclass, since every subclass seems to want it.  Documenting

--- Frameworks/Adium Framework/AIWindowController.m
+++ Frameworks/Adium Framework/AIWindowController.m
@@ -1,22 +1,44 @@
-//
-//  AIWindowController.m
-//  Adium
-//
-//  Created by Adam Iser on Sun Dec 14 2003.
-//  Copyright (c) 2003-2005 The Adium Team. All rights reserved.
-//
+/* 
+Adium, Copyright 2001-2005, Adam Iser
+ 
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ General Public License as published by the Free Software Foundation; either version 2 of the License,
+ or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with this program; if not,
+ write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 #import "AIWindowController.h"
 
+/*!
+ * @class AIWindowController
+ * @brief Base class for window controllers
+ *
+ * This base class provides some essentials for window controllers to cut down on duplicate code.  It currently
+ * handles window frame saving and restoration, establishes a local 'adium' references, and provides methods
+ * which every good window controller cannot be without.
+ */
 @implementation AIWindowController
 
+/*!
+ * @brief Initialize
+ */
 - (id)initWithWindowNibName:(NSString *)windowNibName
 {
     adium = [AIObject sharedAdiumInstance];
     return([super initWithWindowNibName:windowNibName]);
 }
 
-//Custom window frame saving code for Adium's silly multiple-user system
+/*!
+ * @brief Configure the window after it loads
+ *
+ * Here we restore the window's saved position and size before it's displayed on screen.
+ */
 - (void)windowDidLoad
 {
 	NSString	*key = [self adiumFrameAutosaveName];
@@ -49,6 +71,23 @@
 	}
 }
 
+/*!
+ * @brief Close the window
+ */
+- (IBAction)closeWindow:(id)sender
+{
+    if([self windowShouldClose:nil]){
+        [[self window] close];
+    }
+}
+
+/*!
+ * @brief Called before the window closes
+ *
+ * This is called before the window closes.  By default we always allow closing of our window, so YES is always
+ * returned from this method.  Also we take the opportunity to save the current window position and size here.
+ * When subclassing be sure to call super in this method, or window frames will not save.
+ */
 - (BOOL)windowShouldClose:(id)sender
 {
 	NSString	*key = [self adiumFrameAutosaveName];
@@ -62,6 +101,11 @@
 	return(YES);
 }
 
+/*!
+ * @brief Auto-saving window frame key
+ *
+ * This is the string used for saving this window's frame.  It should be unique to this window.
+ */
 - (NSString *)adiumFrameAutosaveName
 {
 	return(nil);
