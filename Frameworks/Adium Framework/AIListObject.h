7e74a5faf79fab3b9dba8ddd42707dcd7ef88bf8
Peter Hosey <Peter Hosey@localhost>
2005-02-06 08:17:57 +0000

    You can't use a class name before it is declared. This includes in protocols. We now declare AIListObject in the @class statement near the top of the file.

--- Frameworks/Adium Framework/AIListObject.h
+++ Frameworks/Adium Framework/AIListObject.h
@@ -13,7 +13,7 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
-@class AIService, AIMutableOwnerArray, AIListGroup, AISortController, ESObjectWithStatus;
+@class AIListObject, AIService, AIMutableOwnerArray, AIListGroup, AISortController, ESObjectWithStatus;
 
 #import "ESObjectWithStatus.h"
 
