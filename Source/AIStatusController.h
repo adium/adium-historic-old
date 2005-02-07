92b06f1d882f50f44843388e320f8a1eeed4b6f1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-02-07 22:30:40 +0000

    Taking off these Adium: prefixes for these names.. thinking we may use them in Info.plist files at some point, and that's just annoying.  Third party plugins can put their -own- prefix on if necessary ;)

--- Source/AIStatusController.h
+++ Source/AIStatusController.h
@@ -33,43 +33,46 @@ Adium, Copyright 2001-2005, Adam Iser
 #define	KEY_STATUS_TYPE				@"Status Type"
 
 //Built-in names and descriptions, which services should use when they support identical or approximately identical states
-#define	STATUS_NAME_AVAILABLE				@"Adium: Generic Available"
+#define	STATUS_NAME_AVAILABLE				@"Generic Available"
 #define	STATUS_DESCRIPTION_AVAILABLE		AILocalizedString(@"Available", nil)
-#define STATUS_NAME_FREE_FOR_CHAT			@"Adium: Free for Chat"
+#define STATUS_NAME_FREE_FOR_CHAT			@"Free for Chat"
 #define STATUS_DESCRIPTION_FREE_FOR_CHAT	AILocalizedString(@"Free for chat", nil)
-#define STATUS_NAME_AVAILABLE_FRIENDS_ONLY	@"Adium: Available for Friends Only"
+#define STATUS_NAME_AVAILABLE_FRIENDS_ONLY	@"Available for Friends Only"
 #define STATUS_DESCRIPTION_AVAILABLE_FRIENDS_ONLY AILocalizedString(@"Available for friends only",nil)
 
-#define	STATUS_NAME_AWAY					@"Adium: Generic Away"
+#define	STATUS_NAME_AWAY					@"Generic Away"
 #define STATUS_DESCRIPTION_AWAY				AILocalizedString(@"Away", nil)
-#define STATUS_NAME_EXTENDED_AWAY			@"Adium: Extended Away"
+#define STATUS_NAME_EXTENDED_AWAY			@"Extended Away"
 #define STATUS_DESCRIPTION_EXTENDED_AWAY	AILocalizedString(@"Extended away",nil)
-#define STATUS_NAME_AWAY_FRIENDS_ONLY		@"Adium: Away for Friends Only"
+#define STATUS_NAME_AWAY_FRIENDS_ONLY		@"Away for Friends Only"
 #define STATUS_DESCRIPTION_AWAY_FRIENDS_ONLY AILocalizedString(@"Away for friends only",nil)
-#define STATUS_NAME_DND						@"Adium: DND"
+#define STATUS_NAME_DND						@"DND"
 #define STATUS_DESCRIPTION_DND				AILocalizedString(@"Do not disturb", nil)
-#define STATUS_NAME_NOT_AVAILABLE			@"Adium: Not Available"
+#define STATUS_NAME_NOT_AVAILABLE			@"Not Available"
 #define STATUS_DESCRIPTION_NOT_AVAILABLE	AILocalizedString(@"Not available", nil)
-#define STATUS_NAME_OCCUPIED				@"Adium: Occupied"
+#define STATUS_NAME_OCCUPIED				@"Occupied"
 #define STATUS_DESCRIPTION_OCCUPIED			AILocalizedString(@"Occupied", nil)
-#define STATUS_NAME_BRB						@"Adium: BRB"
+#define STATUS_NAME_BRB						@"BRB"
 #define STATUS_DESCRIPTION_BRB				AILocalizedString(@"Be right back",nil)
-#define STATUS_NAME_BUSY					@"Adium: Busy"
+#define STATUS_NAME_BUSY					@"Busy"
 #define	STATUS_DESCRIPTION_BUSY				AILocalizedString(@"Busy",nil)
-#define STATUS_NAME_PHONE					@"Adium: Phone"
+#define STATUS_NAME_PHONE					@"Phone"
 #define STATUS_DESCRIPTION_PHONE			AILocalizedString(@"On the phone",nil)
-#define STATUS_NAME_LUNCH					@"Adium: Lunch"
+#define STATUS_NAME_LUNCH					@"Lunch"
 #define STATUS_DESCRIPTION_LUNCH			AILocalizedString(@"Out to lunch",nil)
-#define STATUS_NAME_NOT_AT_HOME				@"Adium: Not At Home"
+#define STATUS_NAME_NOT_AT_HOME				@"Not At Home"
 #define STATUS_DESCRIPTION_NOT_AT_HOME		AILocalizedString(@"Not at home",nil)
-#define STATUS_NAME_NOT_AT_DESK				@"Adium: Not At Desk"
+#define STATUS_NAME_NOT_AT_DESK				@"Not At Desk"
 #define STATUS_DESCRIPTION_NOT_AT_DESK		AILocalizedString(@"Not at my desk",nil)
-#define STATUS_NAME_NOT_IN_OFFICE			@"Adium: Not In Office"
+#define STATUS_NAME_NOT_IN_OFFICE			@"Not In Office"
 #define STATUS_DESCRIPTION_NOT_IN_OFFICE	AILocalizedString(@"Not in the office",nil)
-#define STATUS_NAME_VACATION				@"Adium: Vacation"
+#define STATUS_NAME_VACATION				@"Vacation"
 #define STATUS_DESCRIPTION_VACATION			AILocalizedString(@"On vacation",nil)
-#define STATUS_NAME_STEPPED_OUT				@"Adium: Stepped Out"
+#define STATUS_NAME_STEPPED_OUT				@"Stepped Out"
 #define STATUS_DESCRIPTION_STEPPED_OUT		AILocalizedString(@"Stepped out",nil)
+
+#define STATUS_NAME_INVISIBLE				@"Invisible"
+#define STATUS_DESCRIPTION_INVISIBLE		AILocalizedString(@"Invisible",nil)
 
 //Current version state ID string
 #define STATE_SAVED_STATE			@"State"
