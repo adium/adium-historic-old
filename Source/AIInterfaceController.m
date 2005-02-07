7ff13f067d83f85a44a2cc806b05018886adcf42
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-02-07 22:31:07 +0000

    Deloggification

--- Source/AIInterfaceController.m
+++ Source/AIInterfaceController.m
@@ -979,12 +979,6 @@
 		NSString	*displayServiceID;
 		if ([object isKindOfClass:[AIMetaContact class]]){
 			if ([(AIMetaContact *)object containsOnlyOneUniqueContact]){
-				NSLog(@"preferred %@ ; listContacts %@ ; contained %@ ; service %@ ; shortDescription %@",
-					  [(AIMetaContact *)object preferredContact],
-					  [(AIMetaContact *)object listContacts],
-					  [(AIMetaContact *)object containedObjects],
-					  [[(AIMetaContact *)object preferredContact] service],
-					  [[[(AIMetaContact *)object preferredContact] service] shortDescription]);
 				displayServiceID = [[[(AIMetaContact *)object preferredContact] service] shortDescription];
 			}else{
 				displayServiceID = META_SERVICE_STRING;
