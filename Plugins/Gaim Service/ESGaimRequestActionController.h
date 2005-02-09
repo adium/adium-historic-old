fab56e6b2ad4aedc6b7825662ae416a325e33238
Evan Schoenberg <Evan Schoenberg@localhost>
2005-02-09 04:39:09 +0000

    Simplified and cleaned up a lot of code by making use of the new, generic ESTextWithButtonsWindowController class.

---
+++ Plugins/Gaim Service/ESGaimRequestActionController.h
@@ -0,0 +1,17 @@
+//
+//  ESGaimRequestActionController.h
+//  Adium
+//
+//  Created by Evan Schoenberg on Wed May 05 2004.
+//  Copyright (c) 2004-2005 The Adium Team. All rights reserved.
+//
+
+#import "CBGaimServicePlugin.h"
+
+@interface ESGaimRequestActionController : AIObject {
+
+}
+
++ (void)showActionWindowWithDict:(NSDictionary *)infoDict;
+
+@end
