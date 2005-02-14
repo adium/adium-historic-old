f4b39a2398ca28f8e5d96aa9c06ae632a0a689f4
Evan Schoenberg <Evan Schoenberg@localhost>
2005-02-14 21:57:38 +0000

    Updated to latest Growl-WithInstaller from growl 0.6, which will properly upgrade Growl 0.5 installations.

--- Plugins/Growl/Growl-WithInstaller.framework/Versions/A/Headers/Growl.h
+++ Plugins/Growl/Growl-WithInstaller.framework/Versions/A/Headers/Growl.h
@@ -1,7 +1,6 @@
-#import "GrowlDefinesCarbon.h"
-#import "GrowlApplicationBridge-Carbon.h"
+#include "GrowlDefines.h"
+#include "GrowlApplicationBridge-Carbon.h"
 
 #ifdef __OBJC__
-#import "GrowlDefines.h"
-#import "GrowlApplicationBridge.h"
+#include "GrowlApplicationBridge.h"
 #endif
