4c398cdd3038d26f39cf8310263f3203817f6917
adamiser <adamiser@localhost>
2005-02-14 23:11:22 +0000

    Major header updates so everbody includes only what they need.  Updating copyright headers.

--- Frameworks/Adium Framework/AIAbstractListController.m
+++ Frameworks/Adium Framework/AIAbstractListController.m
@@ -1,22 +1,40 @@
-//
-//  AIAbstractListController.m
-//  Adium
-//
-//  Created by Evan Schoenberg on 8/21/04.
-//
+/* 
+ * Adium is the legal property of its developers, whose names are listed in the copyright file included
+ * with this source distribution.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 #import "AIAbstractListController.h"
-
+#import "AIInterfaceController.h"
 #import "AIListCell.h"
-#import "AIListOutlineView.h"
-#import "AIListGroupCell.h"
-#import "AIListContactCell.h"
 #import "AIListContactBubbleCell.h"
-#import "AIListGroupMockieCell.h"
-#import "AIListContactMockieCell.h"
 #import "AIListContactBubbleToFitCell.h"
+#import "AIListContactCell.h"
+#import "AIListContactMockieCell.h"
+#import "AIListGroup.h"
 #import "AIListGroupBubbleCell.h"
 #import "AIListGroupBubbleToFitCell.h"
+#import "AIListGroupCell.h"
+#import "AIListGroupMockieCell.h"
+#import "AIListObject.h"
+#import "AIListOutlineView.h"
+#import "AIMenuController.h"
+#import "AIMetaContact.h"
+#import <AIUtilities/AIAutoScrollView.h>
+#import <AIUtilities/AIColorAdditions.h>
+#import <AIUtilities/AIFontAdditions.h>
+#import <AIUtilities/CBApplicationAdditions.h>
+#import <AIUtilities/ESOutlineViewAdditions.h>
 
 #define CONTENT_FONT_IF_FONT_NOT_FOUND	[NSFont systemFontOfSize:10]
 #define STATUS_FONT_IF_FONT_NOT_FOUND	[NSFont systemFontOfSize:10]
