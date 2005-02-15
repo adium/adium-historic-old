99c8e2693605de240a89ecc5eaf3fd81bf6d70e4
Peter Hosey <Peter Hosey@localhost>
2005-02-15 20:09:17 +0000

    In AIAutoScrollView: - Changed height variables from int to float. - Changed NSMakePoint(0, 0) to NSZeroPoint. - Columnarisation. In ESQuicklyResizingPanel: - We now assign to self the result of [super init...], and we only set resizeInterval when that result is non-nil. - Added missing space before return type on several methods (e.g. - (id)init instead of -(id)init).

--- Frameworks/AIUtilities Framework/ESQuicklyResizingPanel.m
+++ Frameworks/AIUtilities Framework/ESQuicklyResizingPanel.m
@@ -13,37 +13,37 @@
 @implementation ESQuicklyResizingPanel
 
 //Override all three panel initialization methods to set the default resizeInterval at init
--(id)init
+- (id)init
 {
-    resizeInterval = DEFAULT_RESIZE_INTERVAL;
-    [super init];
-    return self;
+	if((self = [super init]))
+		resizeInterval = DEFAULT_RESIZE_INTERVAL;
+	return self;
 }
 - (id)initWithContentRect:(NSRect)contentRect styleMask:(unsigned int)styleMask backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag screen:(NSScreen *)aScreen
 {
-    resizeInterval = DEFAULT_RESIZE_INTERVAL;
-    [super initWithContentRect:contentRect styleMask:styleMask backing:bufferingType defer:flag screen:aScreen];
-    return self;
+	if((self = [super initWithContentRect:contentRect styleMask:styleMask backing:bufferingType defer:flag screen:aScreen]))
+		resizeInterval = DEFAULT_RESIZE_INTERVAL;
+	return self;
 }
 - (id)initWithContentRect:(NSRect)contentRect styleMask:(unsigned int)styleMask backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag
 {
-    resizeInterval = DEFAULT_RESIZE_INTERVAL;
-    [super initWithContentRect:contentRect styleMask:styleMask backing:bufferingType defer:flag];
-    return self;
+	if((self = [super initWithContentRect:contentRect styleMask:styleMask backing:bufferingType defer:flag]))
+		resizeInterval = DEFAULT_RESIZE_INTERVAL;
+	return self;
 }
 
 //accessing and setting the resizeInterval
--(NSTimeInterval)resizeInterval
+- (NSTimeInterval)resizeInterval
 {
     return resizeInterval;    
 }
--(void)setResizeInterval:(NSTimeInterval)inInterval
+- (void)setResizeInterval:(NSTimeInterval)inInterval
 {
     resizeInterval = inInterval;   
 }
 
 //newFrame is the desired frame size as the panel resizes
--(NSTimeInterval)animationResizeTime:(NSRect)newFrame
+- (NSTimeInterval)animationResizeTime:(NSRect)newFrame
 {
     return resizeInterval;    
 }
