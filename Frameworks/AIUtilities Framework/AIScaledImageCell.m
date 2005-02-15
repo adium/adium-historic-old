03fa674606d5191709a819ce55ba04be7f8d30f7
Peter Hosey <git@boredzo.org>
2005-02-15 19:40:14 +0000

    In -init, we now use the return value of [super init]. Expanded the comment for -[AIImageTextCell _drawHighlightWithFrame:]. In -cellSizeForBounds:, we now use NSZeroSize instead of NSMakeSize(0,0). In -[AIImageTextCell drawInteriorWithFrame:inView:], made the call to -dictionaryWithObjectsAndKeys: multi-line, and made stringHeight a float. In -[AIVerticallyCenteredTextCell drawInteriorWithFrame:inView:], made stringHeight a float. In -[AIScaledImageCell drawInteriorWithFrame:inView:], s/round/&f/. Also some columnarisation to those calls. OCD: s/ {4}/\t/g OCD: Moved a couple of methods' opening braces onto their own line.

--- Frameworks/AIUtilities Framework/AIScaledImageCell.m
+++ Frameworks/AIUtilities Framework/AIScaledImageCell.m
@@ -19,68 +19,69 @@
 
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
 {
-    //Highlight
-    if([self isHighlighted]){
-        [[NSColor alternateSelectedControlColor] set];
-        [[NSBezierPath bezierPathWithRoundedRect:cellFrame radius:4] fill];
-    }
+	//Highlight
+	if([self isHighlighted]){
+		[[NSColor alternateSelectedControlColor] set];
+		[[NSBezierPath bezierPathWithRoundedRect:cellFrame radius:4] fill];
+	}
 	
-    //Draw our interior
-    [super drawWithFrame:cellFrame inView:controlView];
+	//Draw our interior
+	[super drawWithFrame:cellFrame inView:controlView];
 }
 
 - (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
 {
-    NSImage	*img = [self image];
-    
-    if(img){
-        //Handle flipped axis
+	NSImage	*img = [self image];
+
+	if(img){
+		//Handle flipped axis
 		[img setFlipped:![img isFlipped]];
 		
-        //Size and location
-        //Get image metrics
-        NSSize	imgSize = [img size];
-        NSRect	imgRect = NSMakeRect (0, 0, imgSize.width, imgSize.height);
+		//Size and location
+		//Get image metrics
+		NSSize	imgSize = [img size];
+		NSRect	imgRect = NSMakeRect(0, 0, imgSize.width, imgSize.height);
 		
-        //Scaling
-        NSRect	targetRect = cellFrame;
-        if ((imgSize.height > cellFrame.size.height) ||
-            (imgSize.width  >  cellFrame.size.width)) {
+		//Scaling
+		NSRect	targetRect = cellFrame;
+		if ((imgSize.height > cellFrame.size.height) ||
+			(imgSize.width  >  cellFrame.size.width)) {
 			
-            if ((imgSize.height / cellFrame.size.height) >
-                (imgSize.width / cellFrame.size.width)) {
-                targetRect.size.width = round(imgSize.width / (imgSize.height / cellFrame.size.height));
-            } else {
-                targetRect.size.height = round(imgSize.height / (imgSize.width / cellFrame.size.width));
-            }
+			if ((imgSize.height / cellFrame.size.height) >
+				(imgSize.width / cellFrame.size.width)) {
+				targetRect.size.width  = roundf(imgSize.width  / (imgSize.height / cellFrame.size.height));
+			} else {
+				targetRect.size.height = roundf(imgSize.height / (imgSize.width  / cellFrame.size.width));
+			}
 			
-        }else{
-            targetRect.size.width = imgSize.width;
-            targetRect.size.height = imgSize.height;
-        }
+		}else{
+			targetRect.size.width = imgSize.width;
+			targetRect.size.height = imgSize.height;
+		}
 		
-        //Centering
-        targetRect = NSOffsetRect(targetRect, round((cellFrame.size.width - targetRect.size.width) / 2), round((cellFrame.size.height - targetRect.size.height) / 2));
-        
-        //Draw Image
-        [img drawInRect:targetRect
+		//Centering
+		targetRect = NSOffsetRect(targetRect, round((cellFrame.size.width - targetRect.size.width) / 2), round((cellFrame.size.height - targetRect.size.height) / 2));
+
+		//Draw Image
+		[img drawInRect:targetRect
 			   fromRect:imgRect
 			  operation:NSCompositeSourceOver 
 			   fraction:([self isEnabled] ? 1.0 : 0.5)];
 		
-        //Clean-up
+		//Clean-up
 		[img setFlipped:![img isFlipped]];
-    }
+	}
 }
 
 //Super doesn't appear to handle the isHighlighted flag correctly, so we handle it to be safe.
 - (void)setHighlighted:(BOOL)flag
 {
-    [self setState:(flag ? NSOnState : NSOffState)];
-    isHighlighted = flag;
+	[self setState:(flag ? NSOnState : NSOffState)];
+	isHighlighted = flag;
 }
-- (BOOL)isHighlighted{
-    return(isHighlighted);
+- (BOOL)isHighlighted
+{
+	return isHighlighted;
 }
 
 @end
