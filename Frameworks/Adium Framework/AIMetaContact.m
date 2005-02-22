144da24c257d7e20fcfc43ed4d1917124927b72e
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-02-22 07:22:49 +0000

    warnings--;

--- Frameworks/Adium Framework/AIMetaContact.m
+++ Frameworks/Adium Framework/AIMetaContact.m
@@ -14,8 +14,9 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#import "AIContactController.h"
 #import "AIMetaContact.h"
+#import "AIContactController.h"
+#import "AIListContact.h"
 #import "AIService.h"
 #import <AIUtilities/AIMutableOwnerArray.h>
 #import <AIUtilities/AIArrayAdditions.h>
