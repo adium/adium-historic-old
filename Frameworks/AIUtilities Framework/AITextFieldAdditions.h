8c7d492ef414c28f31b6939e40101cc8dded0f92
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-02-22 07:40:44 +0000

    Import AIWiredString.h in AITextFieldAdditions.h so that classes using AITextFieldAdditions don't have to do it manually.

--- Frameworks/AIUtilities Framework/AITextFieldAdditions.h
+++ Frameworks/AIUtilities Framework/AITextFieldAdditions.h
@@ -13,7 +13,7 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
-@class AIWiredString;
+#import "AIWiredString.h"
 
 @interface NSTextField (AITextFieldAdditions)
 
