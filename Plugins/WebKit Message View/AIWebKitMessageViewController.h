9f71900797ed447ab3420b53853c47a201e13db1
adamiser <adamiser@localhost>
2005-02-24 22:10:34 +0000

    Major cleanup, documentation.  Removed a lot of old code that was hanging around in here, other code moved to the new messageStyle class.  Updated this to use delayed performs instead of single fire timers, also updated for the new messageStyle class which makes some of this code very clean.  Major refactoring of how the webview is set up, refreshed in response to preference changes.  Changes to variant no longer invoke a refresh of the style.  Adding support for the new font/color options, and a ton of other stuff I can't remember... :D

--- Plugins/WebKit Message View/AIWebKitMessageViewController.h
+++ Plugins/WebKit Message View/AIWebKitMessageViewController.h
@@ -18,55 +18,37 @@
 #import "ESWebView.h"
 #import <WebKit/WebKit.h>
 
+@class AIWebkitMessageViewStyle;
+
 @interface AIWebKitMessageViewController : AIObject <AIMessageViewController> {
-	ESWebView					*webView;
-	NSString					*stylePath;
-	NSString					*loadedStyleID;
-	NSString					*loadedVariantID;
-	AIChat						*chat;
-	
-	BOOL						webViewIsReady;
-	BOOL						shouldRefreshContent;
-	
-	AIContentObject				*previousContent;
-	NSMutableArray				*newContent;
-	NSTimer						*newContentTimer;
-	NSTimer						*setStylesheetTimer;
-	
 	id							plugin;
+	ESWebView					*webView;
+	AIChat						*chat;
+
+	//Content processing
+	AIContentObject				*previousContent;
+	NSMutableArray				*contentQueue;
+	BOOL						webViewIsReady;
 	
-	NSString					*contentInHTML;
-	NSString					*nextContentInHTML;
-	NSString					*contextInHTML;
-	NSString					*nextContextInHTML;
-	NSString					*contentOutHTML;
-	NSString					*nextContentOutHTML;
-	NSString					*contextOutHTML;
-	NSString					*nextContextOutHTML;
-	NSString					*statusHTML;
-	
-	NSString					*background;
-	NSString					*backgroundOriginalPath;
-	NSColor						*backgroundColor;
-	
-	NSDateFormatter				*timeStampFormatter;
-	NameFormat					nameFormat;
-	BOOL						allowColors;
-	BOOL						showUserIcons;
-	BOOL						allowBackgrounds;
-	BOOL						useCustomNameFormat;
-	BOOL						combineConsecutive;
-	BOOL						allowTextBackgrounds;
-	int							styleVersion;	
+	//Style & Variant
+	AIWebkitMessageViewStyle	*messageStyle;
+	NSString					*activeStyle;
+	NSString					*activeVariant;
+
+	//User icon masking
 	NSImage						*imageMask;
 	NSMutableArray				*objectsWithUserIconsArray;
-	AIImageBackgroundStyle		imageBackgroundStyle;
 }
 
 + (AIWebKitMessageViewController *)messageViewControllerForChat:(AIChat *)inChat withPlugin:(AIWebKitMessageViewPlugin *)inPlugin;
-- (void)forceReload;
+- (void)adiumPrint:(id)sender;
 
-- (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender;
-- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender;
-- (void)concludeDragOperation:(id <NSDraggingInfo>)sender;
+//Webview
+- (NSView *)messageView;
+- (NSView *)messageScrollView;
+- (AIWebkitMessageViewStyle *)messageStyle;
+
+//Content
+- (void)processQueuedContent;
+
 @end
