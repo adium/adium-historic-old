1084fe6123db653894e7e439d8912784f2768836
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-02-24 07:39:19 +0000

    Documentation

--- Plugins/Dock Behavior/AIDockBehaviorPlugin.m
+++ Plugins/Dock Behavior/AIDockBehaviorPlugin.m
@@ -29,47 +29,80 @@
 - (BOOL)_upgradeCustomDockBehavior;
 @end
 
+/*!
+ * @class AIDockBehaviorPlugin
+ * @brief Bounce Dock action component
+ */
 @implementation AIDockBehaviorPlugin
 
+/*!
+ * @brief Install
+ */
 - (void)installPlugin
 {
 	//Install our contact alert
 	[[adium contactAlertsController] registerActionID:DOCK_BEHAVIOR_ALERT_IDENTIFIER withHandler:self];
 }
 
-- (void)uninstallPlugin
-{
-
-}
-
-//Bounce Dock Alert ----------------------------------------------------------------------------------------------------
-#pragma mark Bounce Dock Alert
+/*!
+ * @brief Short description
+ * @result A short localized description of the action
+ */
 - (NSString *)shortDescriptionForActionID:(NSString *)actionID
 {
 	return(DOCK_BEHAVIOR_ALERT_SHORT);
 }
 
+/*!
+ * @brief Long description
+ * @result A longer localized description of the action which should take into account the details dictionary as appropraite.
+ */
 - (NSString *)longDescriptionForActionID:(NSString *)actionID withDetails:(NSDictionary *)details
 {
 	int behavior = [[details objectForKey:KEY_DOCK_BEHAVIOR_TYPE] intValue];
 	return([NSString stringWithFormat:DOCK_BEHAVIOR_ALERT_LONG, [[[adium dockController] descriptionForBehavior:behavior] lowercaseString]]);
 }
 
+/*!
+ * @brief Image
+ */
 - (NSImage *)imageForActionID:(NSString *)actionID
 {
 	return([NSImage imageNamed:@"DockAlert" forClass:[self class]]);
 }
 
+/*!
+ * @brief Details pane
+ * @result An <tt>AIModularPane</tt> to use for configuring this action, or nil if no configuration is possible.
+ */
 - (AIModularPane *)detailsPaneForActionID:(NSString *)actionID
 {
 	return([ESDockAlertDetailPane actionDetailsPane]);
 }
 
+/*!
+ * @brief Perform an action
+ *
+ * @param actionID The ID of the action to perform
+ * @param listObject The listObject associated with the event triggering the action. It may be nil
+ * @param details If set by the details pane when the action was created, the details dictionary for this particular action
+ * @param eventID The eventID which triggered this action
+ * @param userInfo Additional information associated with the event; userInfo's type will vary with the actionID.
+ */
 - (void)performActionID:(NSString *)actionID forListObject:(AIListObject *)listObject withDetails:(NSDictionary *)details triggeringEventID:(NSString *)eventID userInfo:(id)userInfo
 {
 	[[adium dockController] performBehavior:[[details objectForKey:KEY_DOCK_BEHAVIOR_TYPE] intValue]];
 }
 
+/*!
+* @brief Allow multiple actions?
+ *
+ * If this method returns YES, every one of this action associated with the triggering event will be executed.
+ * If this method returns NO, only the first will be.
+ *
+ * Don't allow multiple dock actions to occur.  While a series of "Bounce every 5 seconds," "Bounce every 10 seconds,"
+ * and so on actions could be combined sanely, a series of "Bounce once" would make the dock go crazy.
+ */
 - (BOOL)allowMultipleActionsWithID:(NSString *)actionID
 {
 	return(NO);
