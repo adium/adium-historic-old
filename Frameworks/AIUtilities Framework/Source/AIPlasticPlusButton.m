5438f018affaed2acedb8bc00c98cc6f2fc58322
Colin Barrett <colin@springsandstruts.com>
2005-03-01 15:37:42 +0000

    AIPlasticPlusButtons now show a little triangle when they have a menu, like the one in Accounts does.

--- Frameworks/AIUtilities Framework/Source/AIPlasticPlusButton.m
+++ Frameworks/AIUtilities Framework/Source/AIPlasticPlusButton.m
@@ -8,13 +8,49 @@
 #import "AIPlasticPlusButton.h"
 #import "ESImageAdditions.h"
 
+@interface AIPlasticPlusButton(PRIVATE)
+- (NSBezierPath *)popUpArrowPath;
+@end
+
 @implementation AIPlasticPlusButton
 
 - (id)initWithFrame:(NSRect)frameRect
 {
     [super initWithFrame:frameRect];
     [self setImage:[NSImage imageNamed:@"plus" forClass:[self class]]];
+	arrowPath = nil;
     return(self);    
+}
+
+
+//Drawing --------------------------------------------------------------------------------------------------------------
+#pragma mark Drawing
+- (void)drawRect:(NSRect)rect
+{
+	//Let super do its thing
+	[super drawRect:rect];
+	
+	//Draw the arrow, if needed
+	if([self menu]){
+		[[[NSColor blackColor] colorWithAlphaComponent:0.75] set];
+		[[self popUpArrowPath] fill];
+	}
+}
+
+//Path for the little popup arrow (Cached)
+- (NSBezierPath *)popUpArrowPath
+{
+	if(!arrowPath){
+		NSRect	frame = [self frame];
+		
+		arrowPath = [[NSBezierPath bezierPath] retain];
+		[arrowPath moveToPoint:NSMakePoint(NSWidth(frame)-8, NSHeight(frame)-6)];
+		[arrowPath relativeLineToPoint:NSMakePoint( 6, 0)];
+		[arrowPath relativeLineToPoint:NSMakePoint(-3, 3)];
+		[arrowPath closePath];
+	}
+
+	return(arrowPath);
 }
 
 @end
