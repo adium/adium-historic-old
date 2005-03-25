9008b1c3ff49c1e420b69f0490551d30102b45bf
dchoby98 <dchoby98@localhost>
2005-03-25 22:13:19 +0000

    Quick fix to get things compiling.

--- Frameworks/Adium Framework/AIEditStateWindowController.h
+++ Frameworks/Adium Framework/AIEditStateWindowController.h
@@ -60,6 +60,8 @@
 	
 	BOOL		isOnPantherOrBetter;
 	BOOL		showSaveCheckbox;
+	
+	BOOL		allowSave;
 }
 
 + (id)editCustomState:(AIStatus *)inStatusState forType:(AIStatusType)inStatusType andAccount:(AIAccount *)inAccount withSaveOption:(BOOL)allowSave onWindow:(id)parentWindow notifyingTarget:(id)inTarget;
