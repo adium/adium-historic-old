bd7d3d7608887298fc3bc6705abd1dc4ee962131
Evan Schoenberg <Evan Schoenberg@localhost>
2005-03-28 23:55:31 +0000

    Fixed issues with the password prompt not coming to the front when a password is needed and with it not having the password field selected and ready for input in some situations.

--- Source/ESAccountPasswordPromptController.m
+++ Source/ESAccountPasswordPromptController.m
@@ -39,6 +39,7 @@
 	
     //bring the window front
     [controller showWindow:nil];
+	[[controller window] makeKeyAndOrderFront:nil];
 }
 
 - (id)initWithWindowNibName:(NSString *)windowNibName forAccount:(AIAccount *)inAccount notifyingTarget:(id)inTarget selector:(SEL)inSelector context:(id)inContext
