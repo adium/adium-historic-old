09ce962b3e61bc04288b2711648b618518447bb4
toby <toby@localhost>
2005-04-04 04:53:53 +0000

    Fix warning.

--- Frameworks/Adium Framework/AIContentObject.m
+++ Frameworks/Adium Framework/AIContentObject.m
@@ -14,6 +14,7 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#import "AIAccount.h"
 #import "AIChat.h"
 #import "AIContentController.h"
 #import "AIContentObject.h"
