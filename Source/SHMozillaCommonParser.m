22d931bddfd9d363c845ba6fab504c5b62224251
toby <toby@localhost>
2005-04-08 04:07:30 +0000

    Remove DeclareString/InitString stuff, fix whitespace.

--- Source/SHMozillaCommonParser.m
+++ Source/SHMozillaCommonParser.m
@@ -18,222 +18,202 @@
 #import "SHMozillaCommonParser.h"
 #import <AIHyperlinks/SHMarkedHyperlink.h>
 
-@interface SHMozillaCommonParser(PRIVATE)
-+(SHMarkedHyperlink *)hyperlinkForTitle:(NSString *)inString URL:(NSString *)inURLString;
-+(NSDictionary *)menuDictWithTitle:(NSString *)inTitle menuItems:(NSArray *)inMenuItems;
+#define gtSign				@">"
+#define Hclose				@"</H"
+#define Hopen				@"H3 "
+#define Aopen				@"A "
+#define hrefStr				@"HREF="
+#define closeQuote			@"\""
+#define closeLink			@"\">"
+#define Aclose				@"</A"
+#define DLOpen				@"<DL>"
+#define DLclose				@"/DL>"
+#define ltSign				@"<"
+    
+#define bSemicolon			@";"
+#define bTagCharStartString	@"&"
+#define bAmpersand			@"&"
+#define bAmpersandHTML		@"AMP"
+#define bGreaterThan		@">"
+#define bGreaterThanHTML	@"GT"
+#define bLessThan			@"<"
+#define bLessThanHTML		@"LT"
+#define bQuote				@"\""
+#define bQuoteHTML			@"QUOT"
+#define bSpace				@" "
+#define bSpaceHTML			@"NBSP"
+#define bApostrophe			@"'"
+#define bApostropheHTML		@"APOS"
+#define bMdash				@"-"
+#define bMdashHTML			@"MDASH"
+
+@interface SHMozillaCommonParser (PRIVATE)
++ (SHMarkedHyperlink *)hyperlinkForTitle:(NSString *)inString URL:(NSString *)inURLString;
++ (NSDictionary *)menuDictWithTitle:(NSString *)inTitle menuItems:(NSArray *)inMenuItems;
 @end
 
 @implementation SHMozillaCommonParser
 
-DeclareString(gtSign)
-DeclareString(Hclose)
-DeclareString(Hopen)
-DeclareString(Aopen)
-DeclareString(hrefStr)
-DeclareString(closeQuote)
-DeclareString(closeLink)
-DeclareString(Aclose)
-DeclareString(DLOpen)
-DeclareString(DLclose)
-DeclareString(ltSign)
-
-DeclareString(bSemicolon)
-DeclareString(bTagCharStartString)
-DeclareString(bAmpersand)
-DeclareString(bAmpersandHTML)
-DeclareString(bGreaterThan)
-DeclareString(bGreaterThanHTML)
-DeclareString(bLessThan)
-DeclareString(bLessThanHTML)
-DeclareString(bQuote)
-DeclareString(bQuoteHTML)
-DeclareString(bSpace)
-DeclareString(bSpaceHTML)
-DeclareString(bApostrophe)
-DeclareString(bApostropheHTML)
-DeclareString(bMdash)
-DeclareString(bMdashHTML)
-
-//DeclareString(untitledString)
-DeclareString(bookmarkDictTitle)
-DeclareString(bookmarkDictContent)
-
-+ (void)initialize
-{
-    InitString(gtSign,@">")
-    InitString(Hclose,@"</H")
-    InitString(Hopen,@"H3 ")
-    InitString(Aopen,@"A ")
-    InitString(hrefStr,@"HREF=")
-    InitString(closeQuote,@"\"")
-    InitString(closeLink,@"\">")
-    InitString(Aclose,@"</A")
-    InitString(DLOpen,@"<DL>")
-    InitString(DLclose,@"/DL>")
-    InitString(ltSign,@"<")
-    
-    InitString(bSemicolon,@";")
-    InitString(bTagCharStartString,@"&")
-    InitString(bAmpersand,@"&")
-    InitString(bAmpersandHTML,@"AMP")
-    InitString(bGreaterThan,@">")
-    InitString(bGreaterThanHTML,@"GT")
-    InitString(bLessThan,@"<")
-    InitString(bLessThanHTML,@"LT")
-    InitString(bQuote,@"\"")
-    InitString(bQuoteHTML,@"QUOT")
-    InitString(bSpace,@" ")
-    InitString(bSpaceHTML,@"NBSP")
-    InitString(bApostrophe,@"'")
-    InitString(bApostropheHTML,@"APOS")
-    InitString(bMdash,@"-");
-    InitString(bMdashHTML,@"MDASH");
-    
-//    InitString(untitledString,@"untitled")
-    InitString(bookmarkDictTitle,SH_BOOKMARK_DICT_TITLE)
-    InitString(bookmarkDictContent,SH_BOOKMARK_DICT_CONTENT)
-}
-
 + (NSArray *)parseBookmarksfromString:(NSString *)inString
 {
-    NSMutableArray      *bookmarksArray = [[NSMutableArray alloc] init];
-    NSMutableArray      *arrayStack = [NSMutableArray array];
-    NSScanner			*linkScanner = [NSScanner scannerWithString:inString];
-    NSString			*titleString = nil, *urlString = nil;
-    
-    unsigned int		stringLength = [inString length];
-    
-    
-    NSCharacterSet		*quotesSet = [NSCharacterSet characterSetWithCharactersInString:@"'\""];
-    
+	NSMutableArray		*bookmarksArray = [[NSMutableArray alloc] init];
+	NSMutableArray		*arrayStack = [NSMutableArray array];
+	NSScanner			*linkScanner = [NSScanner scannerWithString:inString];
+	NSString			*titleString = nil, *urlString = nil;
+
+	unsigned int		stringLength = [inString length];
+
+	NSCharacterSet		*quotesSet = [NSCharacterSet characterSetWithCharactersInString:@"'\""];
+
     while(![linkScanner isAtEnd]){
-	
-        if((stringLength - [linkScanner scanLocation]) < 4){
-            [linkScanner setScanLocation:[inString length]];
-			
-        }else if([[inString substringWithRange:NSMakeRange([linkScanner scanLocation],3)] caseInsensitiveCompare:Hopen] == NSOrderedSame){
-            if((stringLength - [linkScanner scanLocation]) > 3) [linkScanner setScanLocation:[linkScanner scanLocation] + 3];
-            [linkScanner scanUpToString:gtSign intoString:nil];
-            if((stringLength - [linkScanner scanLocation]) > 1) [linkScanner setScanLocation:[linkScanner scanLocation] + 1];
-            
+		if((stringLength - [linkScanner scanLocation]) < 4){
+			[linkScanner setScanLocation:[inString length]];
+		}else if([[inString substringWithRange:NSMakeRange([linkScanner scanLocation],3)] caseInsensitiveCompare:Hopen] == NSOrderedSame){
+			if((stringLength - [linkScanner scanLocation]) > 3){
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 3];
+			}
+
+			[linkScanner scanUpToString:gtSign intoString:nil];
+
+			if((stringLength - [linkScanner scanLocation]) > 1){
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 1];
+			}
+
 			titleString = nil;
-			
-            if([linkScanner scanUpToString:Hclose intoString:&titleString]){
-                // decode html stuff
-                titleString = [SHMozillaCommonParser simplyReplaceHTMLCodes:titleString];
-                [linkScanner setScanLocation:[linkScanner scanLocation] + 3];
-            }
-            [arrayStack addObject:bookmarksArray];
-            [bookmarksArray release]; bookmarksArray = [[NSMutableArray alloc] init];
-            [(NSMutableArray *)[arrayStack lastObject] addObject:[self menuDictWithTitle:titleString
+
+			if([linkScanner scanUpToString:Hclose intoString:&titleString]){
+				// decode html stuff
+				titleString = [SHMozillaCommonParser simplyReplaceHTMLCodes:titleString];
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 3];
+			}
+
+			[arrayStack addObject:bookmarksArray];
+			[bookmarksArray release];
+			bookmarksArray = [[NSMutableArray alloc] init];
+			[(NSMutableArray *)[arrayStack lastObject] addObject:[self menuDictWithTitle:titleString
 																			   menuItems:bookmarksArray]];
+		}else if([[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],2)] caseInsensitiveCompare:Aopen] == NSOrderedSame){
+			[linkScanner scanUpToString:hrefStr intoString:nil];
 
-        }else if([[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],2)] caseInsensitiveCompare:Aopen] == NSOrderedSame){
-
-            [linkScanner scanUpToString:hrefStr intoString:nil];
-            if((stringLength - [linkScanner scanLocation]) > 6) [linkScanner setScanLocation:[linkScanner scanLocation] + 6];
+			if((stringLength - [linkScanner scanLocation]) > 6){
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 6];
+			}
             
 			if([linkScanner scanUpToCharactersFromSet:quotesSet intoString:&urlString]){
-			    [linkScanner scanUpToString:gtSign intoString:nil];
-                if((stringLength - [linkScanner scanLocation]) > 1) [linkScanner setScanLocation:[linkScanner scanLocation] + 1];
-                
-				titleString = nil;
-				
-                if([linkScanner scanUpToString:Aclose intoString:&titleString]){
-			        // decode html stuff
-                    titleString = [SHMozillaCommonParser simplyReplaceHTMLCodes:titleString];
-                }
-            
-                [bookmarksArray addObject:[self hyperlinkForTitle:titleString URL:urlString]];
-            }
-        }else if([[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],4)] caseInsensitiveCompare:DLclose] == NSOrderedSame){
-            if((stringLength - [linkScanner scanLocation]) > 4) [linkScanner setScanLocation:[linkScanner scanLocation] + 4];
+				[linkScanner scanUpToString:gtSign intoString:nil];
 
-            if([arrayStack count]){
+				if((stringLength - [linkScanner scanLocation]) > 1){
+					[linkScanner setScanLocation:[linkScanner scanLocation] + 1];
+				}
+
+				titleString = nil;
+
+				if([linkScanner scanUpToString:Aclose intoString:&titleString]){
+					// decode html stuff
+					titleString = [SHMozillaCommonParser simplyReplaceHTMLCodes:titleString];
+				}
+
+				[bookmarksArray addObject:[self hyperlinkForTitle:titleString URL:urlString]];
+			}
+		}else if([[[linkScanner string] substringWithRange:NSMakeRange([linkScanner scanLocation],4)] caseInsensitiveCompare:DLclose] == NSOrderedSame){
+			if((stringLength - [linkScanner scanLocation]) > 4){
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 4];
+			}
+
+			if([arrayStack count]){
 				//Set bookmarks array to the last array added to the arrayStack to avoid an extra alloc/init.
 				//However, if we somehow get here and bookmarksArray is already that object, we don't want to release
 				//and set as that would later lead to a double release.
 				NSMutableArray *previousBookmarksArray = [arrayStack lastObject];
+
 				if (bookmarksArray != previousBookmarksArray){
 					[bookmarksArray release]; bookmarksArray = [previousBookmarksArray retain];
 				}
 
-                [arrayStack removeLastObject];
-            }
-        }else{
-            [linkScanner scanUpToString:ltSign intoString:nil];
-            if((stringLength - [linkScanner scanLocation]) > 1) [linkScanner setScanLocation:[linkScanner scanLocation] + 1];
-        }
-    }
+				[arrayStack removeLastObject];
+			}
+		}else{
+			[linkScanner scanUpToString:ltSign intoString:nil];
 
-    return [bookmarksArray autorelease];
+			if((stringLength - [linkScanner scanLocation]) > 1){
+				[linkScanner setScanLocation:[linkScanner scanLocation] + 1];
+			}
+		}
+	}
+
+	return [bookmarksArray autorelease];
 }
 
-+(SHMarkedHyperlink *)hyperlinkForTitle:(NSString *)inString URL:(NSString *)inURLString
++ (SHMarkedHyperlink *)hyperlinkForTitle:(NSString *)inString URL:(NSString *)inURLString
 {
-    NSString    *title = inString? inString : @"untitled";
-    return([[[SHMarkedHyperlink alloc] initWithString:inURLString
-                                 withValidationStatus:SH_URL_VALID
-                                         parentString:title
-                                             andRange:NSMakeRange(0,[title length])] autorelease]);
+	NSString	*title = inString ? inString : @"untitled";
+
+	return [[[SHMarkedHyperlink alloc] initWithString:inURLString
+								 withValidationStatus:SH_URL_VALID
+										 parentString:title
+											 andRange:NSMakeRange(0,[title length])] autorelease];
 }
 
-+(NSDictionary *)menuDictWithTitle:(NSString *)inTitle menuItems:(NSArray *)inMenuItems
++ (NSDictionary *)menuDictWithTitle:(NSString *)inTitle menuItems:(NSArray *)inMenuItems
 {
-    NSString    *titleString = inTitle? inTitle : @"untitled";
-    return [NSDictionary dictionaryWithObjectsAndKeys:titleString, bookmarkDictTitle, inMenuItems, bookmarkDictContent, nil];
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+		(inTitle ? inTitle : @"untitled"), SH_BOOKMARK_DICT_TITLE,
+		inMenuItems, SH_BOOKMARK_DICT_CONTENT,
+		nil];
 }
 
 #pragma mark HTML replacement        
+
 + (NSString *)simplyReplaceHTMLCodes:(NSString *)inString
 {
-    NSString        *tokenString,*blahString,*tagOpen;
-    NSScanner       *scanner;
-    NSCharacterSet  *tagCharStart,*charEnd;
-    NSMutableString *newString;
-    BOOL             validTag;
-    
-    tagCharStart = [NSCharacterSet characterSetWithCharactersInString:bTagCharStartString];
-    charEnd = [NSCharacterSet characterSetWithCharactersInString:bSemicolon];
-    scanner = [NSScanner scannerWithString:inString];
-    newString = [NSMutableString string];
-    [scanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@""]];
-    
-    while(![scanner isAtEnd]){
-        if([scanner scanUpToCharactersFromSet:tagCharStart intoString:&blahString]){
-            [newString appendString:blahString];
-        }
-        
-        if([scanner scanCharactersFromSet:tagCharStart intoString:&tagOpen]){
-            unsigned int scanLoc = [scanner scanLocation];
-            if(validTag = [scanner scanUpToCharactersFromSet:charEnd intoString:&tokenString]){
-                if(NSOrderedSame == [tokenString caseInsensitiveCompare:bAmpersandHTML]){
-                    [newString appendString:bAmpersand];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bGreaterThanHTML]){
-                    [newString appendString:bGreaterThan];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bLessThanHTML]){
-                    [newString appendString:bLessThan];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bQuoteHTML]){
-                    [newString appendString:bQuote];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bSpaceHTML]){
-                    [newString appendString:bSpace];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bApostropheHTML]){
-                    [newString appendString:bApostrophe];
-                }else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bMdashHTML]){
-                    [newString appendString:bMdash];
-                }
-                
-                if(validTag){
-                    [scanner scanCharactersFromSet:charEnd intoString:nil];
-                }else{
-                    [newString appendString:bAmpersand];
-                    [scanner setScanLocation:scanLoc];
-                }
-            }
-        }
-    }
+	NSString		*tokenString,*blahString,*tagOpen;
+	NSScanner		*scanner;
+	NSCharacterSet	*tagCharStart,*charEnd;
+	NSMutableString	*newString;
+	BOOL			validTag;
 	
-    return(newString);
+	tagCharStart = [NSCharacterSet characterSetWithCharactersInString:bTagCharStartString];
+	charEnd = [NSCharacterSet characterSetWithCharactersInString:bSemicolon];
+	scanner = [NSScanner scannerWithString:inString];
+	newString = [NSMutableString string];
+	[scanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@""]];
+	
+	while(![scanner isAtEnd]){
+		if([scanner scanUpToCharactersFromSet:tagCharStart intoString:&blahString]){
+			[newString appendString:blahString];
+		}
+		
+		if([scanner scanCharactersFromSet:tagCharStart intoString:&tagOpen]){
+			unsigned int scanLoc = [scanner scanLocation];
+
+			if(validTag = [scanner scanUpToCharactersFromSet:charEnd intoString:&tokenString]){
+				if(NSOrderedSame == [tokenString caseInsensitiveCompare:bAmpersandHTML]){
+					[newString appendString:bAmpersand];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bGreaterThanHTML]){
+					[newString appendString:bGreaterThan];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bLessThanHTML]){
+					[newString appendString:bLessThan];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bQuoteHTML]){
+					[newString appendString:bQuote];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bSpaceHTML]){
+					[newString appendString:bSpace];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bApostropheHTML]){
+					[newString appendString:bApostrophe];
+				}else if(NSOrderedSame == [tokenString caseInsensitiveCompare:bMdashHTML]){
+					[newString appendString:bMdash];
+				}
+				
+				if(validTag){
+					[scanner scanCharactersFromSet:charEnd intoString:nil];
+				}else{
+					[newString appendString:bAmpersand];
+					[scanner setScanLocation:scanLoc];
+				}
+			}
+		}
+	}
+
+	return newString;
 }
 
 @end
