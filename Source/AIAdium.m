be6f16f17d7af1f977c605833f511b07fbc22b26
toby <toby@localhost>
2005-04-09 23:07:21 +0000

    Fix warning about -initController

--- Source/AIAdium.m
+++ Source/AIAdium.m
@@ -15,6 +15,7 @@
  */
 
 #import "AIAccountController.h"
+#import "AIBookmarksImporterController.h"
 #import "AIContactController.h"
 #import "AIContentController.h"
 #import "AICoreComponentLoader.h"
