3638223238e7596e52a944c6ea8aa0257ed8d49b
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-04-10 22:44:47 +0000

    - Set KFTypeSelectTableView as our NSTableView base class when intializing the interface controller rather than in a category within Adium.framework.  This has two advantages:     1) The environemnt during +(void)load is not guaranteed; while there are no reports of problems due to this poseAsClass, where +(void)load can be avoided it is probably wise to do so     2) Previously, one had to know to look in External Sources in Adium.framework to have any indication that we were using KFTypeSelectTableView throughout Adium.  Now, the interface controller's initialize method performs the poseAsClass, so it is much clearer what's happening.
    - Added some documentation to the interface controller.

--- Source/AIInterfaceController.m
+++ Source/AIInterfaceController.m
@@ -35,6 +35,7 @@
 #import <Adium/AIService.h>
 #import <Adium/AIServiceIcons.h>
 #import <Adium/AISortController.h>
+#import <Adium/KFTypeSelectTableView.h>
 
 #define CLOSE_CHAT_MENU_TITLE			AILocalizedString(@"Close Chat","Title for the close chat menu item")
 #define CLOSE_MENU_TITLE				AILocalizedString(@"Close","Title for the close menu item")
@@ -68,11 +69,40 @@
 - (void)contactOrderChanged:(NSNotification *)notification;
 @end
 
+/*
+ * @class AIInterfaceController
+ * @brief Interface controller
+ *
+ * Chat window related requests, such as opening and closing chats, are routed through the interface controller
+ * to the appropriate component. The interface controller keeps track of the most recently active chat, handles chat
+ * cycling (switching between chats), chat sorting, and so on.  The interface controller also handles switching to
+ * an appropriate window or chat when the dock icon is clicked for a 'reopen' event.
+ *
+ * Contact list window requests, such as toggling window visibilty are routed to the contact list controller component.
+ *
+ * Error messages are routed through the interface controller.
+ *
+ * Tooltips, such as seen on hover in the contact list are generated and displayed here.  Tooltip display components and
+ * plugins register with the interface controller to be queried for contact information when a tooltip is displayed.
+ *
+ * When displays in Adium flash, such as in the dock or the contact list for unviewed content, the interface controller
+ * manages keeping the flashing synchronized.
+ *
+ * Finally, the interface controller manages many menu items, providing better menu item validation and target routing
+ * than the responder chain alone would do.
+ */
 @implementation AIInterfaceController
 
-//init
+/*
+ * @brief Initialize the controller
+ */
 - (void)initController
-{     
+{
+	/* Use KFTypeSelectTableView as our NSTableView base class to allow type-select searching of all
+	 * table and outline views throughout Adium.
+	 */
+	[[KFTypeSelectTableView class] poseAsClass:[NSTableView class]];
+	
     contactListViewArray = [[NSMutableArray alloc] init];
     messageViewArray = [[NSMutableArray alloc] init];
     contactListTooltipEntryArray = [[NSMutableArray alloc] init];
