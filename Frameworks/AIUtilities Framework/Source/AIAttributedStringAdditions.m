faca5cf3d6af9f1498ab2c6d084aaacad4bade57
Evan Schoenberg <Evan Schoenberg@localhost>
2005-04-13 04:53:47 +0000

    Added a (deprecated) safeString method which calls attributedStringByConvertingAttachmentsToStrings... the SQL Logger plugin needs to be recompiled, and I figure a transitionary period is reasonable.  It'll log angrily to the console with every invocation.

--- Frameworks/AIUtilities Framework/Source/AIAttributedStringAdditions.m
+++ Frameworks/AIUtilities Framework/Source/AIAttributedStringAdditions.m
@@ -434,6 +434,13 @@
     }
 }
 
+/* Deprecated */
+- (NSAttributedString *)safeString
+{
+	NSLog(@"**** You are using an out of date external Adium plugin [most likely the SQL Logger]. Please recompile and reinstall the plugin. This will crash in a future release. ****");
+	return [self attributedStringByConvertingAttachmentsToStrings];
+}
+
 - (NSAttributedString *)attributedStringByConvertingLinksToStrings
 {
 	NSMutableAttributedString	*newAttributedString = [[self mutableCopy] autorelease];	
