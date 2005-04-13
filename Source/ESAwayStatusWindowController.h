a6b47a91e20e302bc116087a4923d5e420279019
Evan Schoenberg <Evan Schoenberg@localhost>
2005-04-13 00:48:25 +0000

    Added ESAwayStatusWindowPlugin and ESAwayStatusWindowController. The window controller currently does nothing besides log when the away status window would become [or remain] visible or hidden.

---
+++ Source/ESAwayStatusWindowController.h
@@ -0,0 +1,17 @@
+//
+//  ESAwayStatusWindowController.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 4/12/05.
+//  Copyright 2005 The Adium Team. All rights reserved.
+//
+
+#import <Adium/AIWindowController.h>
+
+@interface ESAwayStatusWindowController : AIWindowController {
+
+}
+
++ (void)setStatusWindowVisible:(BOOL)shouldBeVisibile;
+
+@end
