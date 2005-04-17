22f2fa9b432ad43b0098c3eb39668241f88755f6
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-04-17 19:36:27 +0000

    Implemented the away status floating window.

--- Source/ESAwayStatusWindowController.m
+++ Source/ESAwayStatusWindowController.m
@@ -7,13 +7,309 @@
 //
 
 #import "ESAwayStatusWindowController.h"
+#import "AIAccountController.h"
+#import "AIStatusController.h"
+#import <Adium/AIAccount.h>
+#import <Adium/AIStatus.h>
+#import <Adium/AIStatusIcons.h>
+#import <Adium/AIServiceIcons.h>
+#import <AIUtilities/AIArrayAdditions.h>
+#import <AIUtilities/AIAttributedStringAdditions.h>
+#import <AIUtilities/AIImageTextCell.h>
+#import <AIUtilities/AITableViewAdditions.h>
+#import <AIUtilities/CBApplicationAdditions.h>
 
 
+#define AWAY_STATUS_WINDOW_NIB					@"AwayStatusWindow"
+#define	KEY_AWAY_STATUS_WINDOW_FRAME			@"Away Status Window Frame"
+
+@interface ESAwayStatusWindowController (PRIVATE)
+- (void)configureStatusWindow;
+- (NSAttributedString *)attributedStatusTitleForStatus:(AIStatus *)statusState withIcon:(NSImage *)statusIcon;
+- (NSArray *)awayAccounts;
+- (void)setupMultistatusTable;
+@end
+
+/*
+ * @class ESAwayStatusWindowController
+ * @brief Window controller for the status window which optionally shows when one or more accounts are away or invisible
+ */
 @implementation ESAwayStatusWindowController
 
-+ (void)setStatusWindowVisible:(BOOL)shouldBeVisible
+static ESAwayStatusWindowController	*sharedInstance = nil;
+
+/*
+ * @brief Update the visibility of the status window
+ *
+ * Opens or closes the window if necessary.
+ *
+ * If shouldBeVisibile is YES and the window is already visible, updates its contents to reflect the current status.
+ * If shouldBeVisible is NO and the window is already not visibile, no action is taken.
+ */
++ (void)updateStatusWindowWithVisibility:(BOOL)shouldBeVisible
 {
-	NSLog(@"The status window should now be %@",(shouldBeVisible ? @"visible" : @"hidden"));
+	if(shouldBeVisible){
+		if(sharedInstance){
+			//Update the window's configuration
+			[sharedInstance configureStatusWindow];
+		}else{
+			//Create a new shared instance, which will be configured automatically once the window loads
+			sharedInstance = [[self alloc] initWithWindowNibName:AWAY_STATUS_WINDOW_NIB];
+			[sharedInstance showWindow:nil];
+		}
+	
+	}else{
+		if(sharedInstance){
+			//If the window is current visible, close it
+			[sharedInstance closeWindow:nil];
+		}
+	}
+}
+
+/*
+ * @brief Window size and position autosave name
+ */
+- (NSString *)adiumFrameAutosaveName
+{
+	return(KEY_AWAY_STATUS_WINDOW_FRAME);
+}
+
+/*
+ * @brief Window loaded
+ */
+- (void)windowDidLoad
+{
+	//Call super first so we get our placement before performing autosizing
+	[super windowDidLoad];
+	
+	//Setup the textviews
+    [textView_singleStatus setHorizontallyResizable:NO];
+    [textView_singleStatus setVerticallyResizable:YES];
+    [textView_singleStatus setDrawsBackground:NO];
+	[textView_singleStatus setMinSize:NSZeroSize];
+    [scrollView_singleStatus setDrawsBackground:NO];
+	
+	[self setupMultistatusTable];
+	
+	[self configureStatusWindow];
+}
+
+/*
+ * @brief Window will close
+ *
+ * Release and clear the reference to our shared instance
+ */
+- (void)windowWillClose:(id)sender
+{
+	[super windowWillClose:sender];
+
+    //Clean up and release the shared instance
+    [sharedInstance autorelease]; sharedInstance = nil;
+}
+
+/*
+ * @brief Deallocate
+ */
+- (void)dealloc
+{
+	[_awayAccounts release];
+	
+	[super dealloc];
+}
+
+/*
+ * @brief Configure status window for the current account status(es)
+ */
+ - (void)configureStatusWindow
+{
+	NSWindow		*window = [self window];
+	BOOL			allOnlineAccountsAreUnvailable;
+	AIStatusType	activeUnvailableStatusType;
+	NSString		*activeUnvailableStatusName = nil;
+	NSSet			*relevantStatuses;
+	NSRect			frame = [window frame];
+	int				newHeight;
+	
+	[window setTitle:AILocalizedString(@"Current Status",nil)];
+	[_awayAccounts release]; _awayAccounts = nil;
+
+	relevantStatuses = [[adium statusController] activeUnavailableStatusesAndType:&activeUnvailableStatusType 
+																		 withName:&activeUnvailableStatusName
+												   allOnlineAccountsAreUnvailable:&allOnlineAccountsAreUnvailable];
+	
+	if(allOnlineAccountsAreUnvailable && ([relevantStatuses count] == 1)){
+		//Show the single status tab if all online accounts are unavailable and they are all in the same status state
+		NSImage				*statusIcon;
+		NSAttributedString	*statusTitle;
+
+		statusIcon = [AIStatusIcons statusIconForStatusName:activeUnvailableStatusName
+												  statusType:activeUnvailableStatusType
+													iconType:AIStatusIconTab
+												  direction:AIIconNormal];
+		statusTitle = [self attributedStatusTitleForStatus:[relevantStatuses anyObject]
+												  withIcon:statusIcon];
+		
+		[[textView_singleStatus textStorage] setAttributedString:statusTitle];
+
+		newHeight = [statusTitle heightWithWidth:[textView_singleStatus frame].size.width] + 65;
+		frame.origin.y -= (newHeight - frame.size.height);
+		frame.size.height = newHeight;
+			
+		//Select the right tab view item
+		[tabView_configuration selectTabViewItemWithIdentifier:@"singlestatus"];
+		
+	}else{
+		/* Show the multistatus tableview tab if accounts are in different states, which includes the case of only one
+		 * away state being in use but not all online accounts currently making use of it.
+		 */
+		NSTabViewItem	*selectedTabViewItem = [tabView_configuration selectedTabViewItem];
+		
+		_awayAccounts = [[self awayAccounts] retain];
+		
+		[tableView_multiStatus reloadData];
+
+		/* Only need to resize if we are currently in the single status tab... if we are already in the multistatus,
+		 * we shouldn't change whatever size the user has set. */
+		if(![[selectedTabViewItem identifier] isEqualToString:@"multistatus"]){
+			int requiredHeight = (([tableView_multiStatus rowHeight] + [tableView_multiStatus intercellSpacing].height) *
+								  [_awayAccounts count]);
+
+			newHeight = requiredHeight + 65;
+			frame.origin.y -= (newHeight - frame.size.height);
+			frame.size.height = newHeight;
+
+			/* Multiple statuses */
+			[tabView_configuration selectTabViewItemWithIdentifier:@"multistatus"];
+		}
+	}
+	
+	//Perform the window resizing as needed
+	if ([NSApp isOnPantherOrBetter]){
+		[window setFrame:frame display:YES animate:YES];
+	}else{
+		[window setFrame:frame display:YES]; //animate:YES can crash in 10.2
+	}
+}
+
+- (NSAttributedString *)attributedStatusTitleForStatus:(AIStatus *)statusState withIcon:(NSImage *)statusIcon
+{
+	NSMutableAttributedString	*statusTitle;
+	NSTextAttachment			*attachment;
+	NSTextAttachmentCell		*cell;
+	NSDictionary				*attributesDict;
+
+	cell = [[NSTextAttachmentCell alloc] init];
+	[cell setImage:statusIcon];
+	
+	attachment = [[NSTextAttachment alloc] init];
+	[attachment setAttachmentCell:cell];
+	[cell release];
+	
+	attributesDict = [NSDictionary dictionaryWithObject:[NSFont systemFontOfSize:0]
+												 forKey:NSFontAttributeName];
+	statusTitle = [[[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@" %@",[statusState title]]
+														  attributes:attributesDict] autorelease];
+
+	//Insert the image at the beginning
+	[statusTitle insertAttributedString:[NSAttributedString attributedStringWithAttachment:attachment]
+								atIndex:0];
+
+	return statusTitle;
+}
+
+- (NSArray *)awayAccounts
+{	
+	NSMutableArray	*awayAccounts = [NSMutableArray array];	
+	NSEnumerator	*enumerator = [[[adium accountController] accountArray] objectEnumerator];
+	AIAccount		*account;
+	
+	while(account = [enumerator nextObject]){
+		if([account online] || [account integerStatusObjectForKey:@"Connecting"]){
+			AIStatus	*statusState = [account statusState];
+			if([statusState statusType] != AIAvailableStatusType){
+				[awayAccounts addObject:account];
+			}
+		}
+	}
+
+	return awayAccounts;
+}
+
+/*
+ * @brief Return from away
+ */
+- (IBAction)returnFromAway:(id)sender
+{
+	NSTabViewItem	*selectedTabViewItem = [tabView_configuration selectedTabViewItem];
+	AIStatus		*availableStatusState = [[adium statusController] defaultInitialStatusState];
+	
+	if([[selectedTabViewItem identifier] isEqualToString:@"singlestatus"]){
+		//Put all accounts in the Available status state
+		[[adium statusController] setActiveStatusState:availableStatusState];
+
+	}else{
+		//Multistatus
+		NSArray	*selectedAccounts;
+		
+		selectedAccounts = [[tableView_multiStatus arrayOfSelectedItemsUsingSourceArray:_awayAccounts] copy];
+		
+		if([selectedAccounts count]){
+			//Apply the available status state to only the selected accounts
+			[[adium statusController] applyState:availableStatusState
+									  toAccounts:selectedAccounts];
+		}else{
+			//No selection: Put all accounts in the Available status state
+			[[adium statusController] setActiveStatusState:availableStatusState];			
+		}
+
+		[selectedAccounts release];
+	}
+}
+
+- (void)setupMultistatusTable
+{
+	AIImageTextCell	*imageTextCell;
+	
+	imageTextCell = [[AIImageTextCell alloc] init];
+	[imageTextCell setDrawsGradientHighlight:YES];
+	[[tableView_multiStatus tableColumnWithIdentifier:@"status"] setDataCell:imageTextCell];
+	[imageTextCell release];	
+}
+
+#pragma mark Multiservice table view datasource
+/*!
+* @brief Number of rows in the table
+ */
+- (int)numberOfRowsInTableView:(NSTableView *)tableView
+{
+	return([_awayAccounts count]);
+}
+
+/*!
+ * @brief Table values
+ *
+ * Object value is the account's formatted UID
+ */
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(int)row
+{
+	AIAccount	*account = [_awayAccounts objectAtIndex:row];
+
+	return([account formattedUID]);
+}
+
+/*!
+ * @brief Will display a cell
+ *
+ * Set the image (status icon) and substring (status title) before display.  Cell is an AIImageTextCell.
+ */
+- (void)tableView:(NSTableView *)tableView willDisplayCell:(id)cell forTableColumn:(NSTableColumn *)tableColumn row:(int)row
+{
+	AIAccount	*account = [_awayAccounts objectAtIndex:row];
+
+	[cell setImage:[AIStatusIcons statusIconForListObject:account
+													 type:AIServiceIconSmall
+												direction:AIIconNormal]];
+	[cell setSubString:[[account statusState] title]];
 }
 
 @end
