eb7e7015086c9d2c61c891759b37acc848668d23
Peter Hosey <git@boredzo.org>
2005-04-18 10:26:57 +0000

    Made stringBy{En,De}codingURLEscapes cooler by making them use translation tables. Also clarified and unified the variable names, switched to -UTF8String from -cString, and switched to strlen from [self length] (because the number of UTF-8 or $(CSTRINGENCODING) characters may not match the number of UTF-16 characters).

--- Frameworks/AIUtilities Framework/Source/AIStringAdditions.m
+++ Frameworks/AIUtilities Framework/Source/AIStringAdditions.m
@@ -343,9 +343,45 @@
 	const char			*UTF8 = [self UTF8String];
 	char				*destPtr;
 	NSMutableData		*destData;
-	unsigned			 sourceIndex = 0;
+	register unsigned	 sourceIndex = 0;
 	unsigned			 sourceLength = strlen(UTF8);
 	register unsigned	 destIndex = 0;
+
+	//this table translates plusses to spaces, and flags all characters that need hex-encoding with 0x00.
+	static const char translationTable[256] = {
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		 ' ',  '!',  '"',  '#',   '$',  '%',  '&', '\'',
+		 '(',  ')',  '*',  ' ',   ',',  '-',  '.',  '/',
+		 '0',  '1',  '2',  '3',   '4',  '5',  '6',  '7',
+		 '8',  '9',  ':',  ';',   '<',  '=',  '>',  '?',
+		 '@',  'A',  'B',  'C',   'D',  'E',  'F',  'G',
+		 'H',  'I',  'J',  'K',   'L',  'M',  'N',  'O',
+		 'P',  'Q',  'R',  'S',   'T',  'U',  'V',  'W',
+		 'X',  'Y',  'Z',  '[',  '\\',  ']',  '^',  '_',
+		 '`',  'a',  'b',  'c',   'd',  'e',  'f',  'g',
+		 'h',  'i',  'j',  'k',   'l',  'm',  'n',  'o',
+		 'p',  'q',  'r',  's',   't',  'u',  'v',  'w',
+		 'x',  'y',  'z',  '{',   '|',  '}',  '~', 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00
+	};
 
 	//Worst case scenario is 3 times the original length (every character escaped)
 	destData = [NSMutableData dataWithLength:(sourceLength * 3)];
@@ -353,28 +389,19 @@
 
 	while(sourceIndex < sourceLength) {
 		char	ch = UTF8[sourceIndex];
+		destPtr[destIndex++] = translationTable[ch];
 
-		if((ch >= 'a'  &&  ch <= 'z')
-		|| (ch >= 'A'  &&  ch <= 'Z')
-		|| (ch >= '0'  &&  ch <= '9')
-		|| (ch == '_') || (ch == '-'))
-		{
-			//keep it raw.
-			destPtr[destIndex++] = ch;
-		} else if(ch == ' ') {
-			//change space to +.
-			destPtr[destIndex++] = '+';
-		} else {
+		if(!translationTable[ch]) {
 			//hex-encode.
-			destPtr[destIndex++] = '%';
-			destPtr[destIndex++] = intToHex(ch / 16);
-			destPtr[destIndex++] = intToHex(ch % 16);
+			destPtr[destIndex-1] = '%';
+			destPtr[destIndex++] = intToHex(ch / 0x10);
+			destPtr[destIndex++] = intToHex(ch % 0x10);
 		}
 
 		sourceIndex++;
 	}
 
-	return [[[NSString alloc] initWithBytes:cDest length:d encoding:NSASCIIStringEncoding] autorelease];
+	return [[[NSString alloc] initWithBytes:destPtr length:destIndex encoding:NSASCIIStringEncoding] autorelease];
 }
 
 //stringByDecodingURLEscapes
@@ -385,34 +412,64 @@
 	const char			*UTF8 = [self UTF8String];
 	char				*destPtr;
 	NSMutableData		*destData;
-	unsigned			 sourceIndex = 0;
+	register unsigned	 sourceIndex = 0;
 	unsigned			 sourceLength = strlen(UTF8);
 	register unsigned	 destIndex = 0;
+
+	//this table translates spaces to plusses.
+	static const char translationTable[256] = {
+		0x00, 0x01, 0x02, 0x03,  0x04, 0x05, 0x06, 0x07,
+		0x08, 0x09, 0x0a, 0x0b,  0x0c, 0x0d, 0x0e, 0x0f,
+		0x10, 0x11, 0x12, 0x13,  0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1a, 0x1b,  0x1c, 0x1d, 0x1e, 0x1f,
+		 '+',  '!',  '"',  '#',   '$',  '%',  '&', '\'',
+		 '(',  ')',  '*',  '+',   ',',  '-',  '.',  '/',
+		 '0',  '1',  '2',  '3',   '4',  '5',  '6',  '7',
+		 '8',  '9',  ':',  ';',   '<',  '=',  '>',  '?',
+		 '@',  'A',  'B',  'C',   'D',  'E',  'F',  'G',
+		 'H',  'I',  'J',  'K',   'L',  'M',  'N',  'O',
+		 'P',  'Q',  'R',  'S',   'T',  'U',  'V',  'W',
+		 'X',  'Y',  'Z',  '[',  '\\',  ']',  '^',  '_',
+		 '`',  'a',  'b',  'c',   'd',  'e',  'f',  'g',
+		 'h',  'i',  'j',  'k',   'l',  'm',  'n',  'o',
+		 'p',  'q',  'r',  's',   't',  'u',  'v',  'w',
+		 'x',  'y',  'z',  '{',   '|',  '}',  '~', 0x7f,
+		0x80, 0x81, 0x82, 0x83,  0x84, 0x85, 0x86, 0x87,
+		0x88, 0x89, 0x8a, 0x8b,  0x8c, 0x8d, 0x8e, 0x8f,
+		0x90, 0x91, 0x92, 0x93,  0x94, 0x95, 0x96, 0x97,
+		0x98, 0x99, 0x9a, 0x9b,  0x9c, 0x9d, 0x9e, 0x9f,
+		0xa0, 0xa1, 0xa2, 0xa3,  0xa4, 0xa5, 0xa6, 0xa7,
+		0xa8, 0xa9, 0xaa, 0xab,  0xac, 0xad, 0xae, 0xaf,
+		0xb0, 0xb1, 0xb2, 0xb3,  0xb4, 0xb5, 0xb6, 0xb7,
+		0xb8, 0xb9, 0xba, 0xbb,  0xbc, 0xbd, 0xbe, 0xbf,
+		0xc0, 0xc1, 0xc2, 0xc3,  0xc4, 0xc5, 0xc6, 0xc7,
+		0xc8, 0xc9, 0xca, 0xcb,  0xcc, 0xcd, 0xce, 0xcf,
+		0xd0, 0xd1, 0xd2, 0xd3,  0xd4, 0xd5, 0xd6, 0xd7,
+		0xd8, 0xd9, 0xda, 0xdb,  0xdc, 0xdd, 0xde, 0xdf,
+		0xe0, 0xe1, 0xe2, 0xe3,  0xe4, 0xe5, 0xe6, 0xe7,
+		0xe8, 0xe9, 0xea, 0xeb,  0xec, 0xed, 0xee, 0xef,
+		0xf0, 0xf1, 0xf2, 0xf3,  0xf4, 0xf5, 0xf6, 0xf7,
+		0xf8, 0xf9, 0xfa, 0xfb,  0xfc, 0xfd, 0xfe, 0xff
+	};
+
+	//Best case scenario is 1/3 the original length (every character escaped); worst should be the same length
+	destData = [NSMutableData dataWithLength:sourceLength];
+	destPtr = [destData mutableBytes];
 	
-    //Best case scenario is 1/3 the original length (every character escaped); worst should be the same length
-    destData = [NSMutableData dataWithLength:sourceLength];
-    cDest = [destData mutableBytes];
-    
-    while(s < sourceLength){
-        char	ch = cSource[s];
-		
-        if(ch == '%'){
-            cDest[d] = ( hexToInt(cSource[s+1]) * 16 ) + hexToInt(cSource[s+2]);
-            s += 3;
-			
-        }else if(ch == '+'){
-            cDest[d] = ' ';
-            s++;
-			
-        }else{
-            cDest[d] = ch;
-            s++;
-        }
-		
-        d++;
-    }
-	
-    return [[[NSString alloc] initWithBytes:cDest length:d encoding:NSASCIIStringEncoding] autorelease];
+	while(sourceIndex < sourceLength){
+		char	ch = UTF8[sourceIndex++];
+
+		if(ch == '%') {
+			destPtr[destIndex] = ( hexToInt(UTF8[sourceIndex]) * 0x10 ) + hexToInt(UTF8[sourceIndex+1]);
+			sourceIndex += 2;
+		} else {
+			destPtr[destIndex] = translationTable[ch];
+		}
+
+		destIndex++;
+	}
+
+	return [[[NSString alloc] initWithBytes:destPtr length:destIndex encoding:NSASCIIStringEncoding] autorelease];
 }
 
 - (NSString *)string
