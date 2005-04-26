f8a6301fb726e4d109a3bde770666d389e299c1b
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-04-26 02:04:04 +0000

    Updated Bonjour to use -[AIStatusController localizedDescriptionForCoreStatusName:]

--- Plugins/Bonjour/AWBonjourService.m
+++ Plugins/Bonjour/AWBonjourService.m
@@ -63,12 +63,12 @@
 }
 - (void)registerStatuses{
 	[[adium statusController] registerStatus:STATUS_NAME_AVAILABLE
-							 withDescription:STATUS_DESCRIPTION_AVAILABLE
+							 withDescription:[[adium statusController] localizedDescriptionForCoreStatusName:STATUS_NAME_AVAILABLE]
 									  ofType:AIAvailableStatusType
 								  forService:self];
 	
 	[[adium statusController] registerStatus:STATUS_NAME_AWAY
-							 withDescription:STATUS_DESCRIPTION_AWAY
+							 withDescription:[[adium statusController] localizedDescriptionForCoreStatusName:STATUS_NAME_AWAY]
 									  ofType:AIAwayStatusType
 								  forService:self];
 }
