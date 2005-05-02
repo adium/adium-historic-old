2bb575a9084fb8cebd97fe41f674df8516873725
Peter Hosey <Peter Hosey@localhost>
2005-05-02 12:37:40 +0000

    Moving the source code and resource files to the new place. The AIBookmarksImporterController in the framework is what used to be (and still is, kind of) AIBookmarksImporterPlugin in the plug-in. If you want a peek at how I plan to implement this, look at Password Assistant in both Panther and Tiger. Since there are no source files left in the plug-in except for AIBookmarksImporterPlugin, removed the plug-in from the list of dependencies in Adium.app. This is temporary, to be fixed when the framework works well enough for the plug-in to use it.

---
+++ Frameworks/AIBookmarksImporter Framework/Source/AIBookmarksImporterController.h
@@ -0,0 +1,46 @@
+/*
+ * Adium is the legal property of its developers, whose names are listed in the copyright file included
+ * with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#define ADIUM_BOOKMARK_DICT_TITLE		@"Title"
+#define ADIUM_BOOKMARK_DICT_CONTENT		@"Content"
+
+@class AIBookmarksImporter;
+
+@interface AIBookmarksImporterController: NSObject
+{
+	//for both of these, the menu is set by -buildBookmarksMenu, and is cleared upon retrieval by -menuNeedsUpdate.
+	NSMenuItem				*bookmarkRootMenuItem;
+	NSMenu					*bookmarksMainSubmenu;
+	NSMenuItem				*bookmarkRootContextualMenuItem;
+	NSMenu					*bookmarksContextualSubmenu;
+	NSLock					*menuLock;
+
+	NSToolbarItem			*toolbarItem;
+	NSMutableArray			*toolbarItemArray;
+
+	//instances of AIBookmarksImporter.
+	NSMutableArray			*importers;
+	NSTimer					*menuUpdateTimer;
+
+	BOOL					updatingMenu;
+	BOOL					menuNeedsUpdate;
+}
+
++ (AIBookmarksImporterController *)sharedInstance;
+- (void)addImporter:(AIBookmarksImporter *)importerToAdd;
+- (void)removeImporter:(AIBookmarksImporter *)importerToRemove;
+
+@end
