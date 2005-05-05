f45b1665e6d0168a269c79ece29a014a3c300bc4
Peter Hosey <git@boredzo.org>
2005-05-05 10:52:12 +0000

    We don't need to expand tildes in a path that doesn't contain them.

--- Source/AIExceptionController.m
+++ Source/AIExceptionController.m
@@ -123,7 +123,7 @@ static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
 		}
 			   
 		if(shouldLaunchCrashReporter){
-			NSString	*bundlePath = [[[NSBundle mainBundle] bundlePath] stringByExpandingTildeInPath];
+			NSString	*bundlePath = [[NSBundle mainBundle] bundlePath];
 			NSString	*crashReporterPath = [bundlePath stringByAppendingPathComponent:RELATIVE_PATH_TO_CRASH_REPORTER];
 			NSString	*versionString = [[NSProcessInfo processInfo] operatingSystemVersionString];
 
