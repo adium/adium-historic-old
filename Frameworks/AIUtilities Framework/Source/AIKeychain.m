8e9b0b5ebb119db1d2ec1aab190463e71c723276
Peter Hosey <Peter Hosey@localhost>
2005-05-11 19:52:15 +0000

    More error-reporting messages.

--- Frameworks/AIUtilities Framework/Source/AIKeychain.m
+++ Frameworks/AIUtilities Framework/Source/AIKeychain.m
@@ -64,7 +64,7 @@ OSStatus GetPasswordKeychain(const char *service,const char *account,void **pass
 	ret = GetPasswordKeychain([service UTF8String],[account UTF8String],(void **)&passwordBytes,&passwordLength,NULL);
 	if(ret != noErr) {
 		//XXX localize me!
-		NSLog(@"could not get password for account %@ on service %@: GetPasswordKeychain returned %li", account, service, (long)err);
+		NSLog(@"could not get password from keychain for account %@ on service %@: GetPasswordKeychain returned %li", account, service, (long)err);
 	} else {
 		NSData	*passwordData = [AIWiredData dataWithBytes:passwordBytes length:passwordLength];
 		passwordString = [[[AIWiredString alloc] initWithData:passwordData
@@ -105,8 +105,11 @@ OSStatus GetPasswordKeychain(const char *service,const char *account,void **pass
 											  NULL						// the item reference - NULL if unneedeed
 											  );
 		
-		if(ret == noErr) success = YES;
-		
+		success = (ret == noErr);
+		if(!success) {
+			//XXX localize me!
+			NSLog(@"could not add password in keychain for account %@ on service %@: SecKeychainAddInternetPassword returned %li", account, service, (long)err);
+		}
 	}else if (ret == noErr){
 		//Item already present, so change it to the new password
 		ret = SecKeychainItemModifyAttributesAndData (itemRef,					// the item reference
@@ -115,7 +118,11 @@ OSStatus GetPasswordKeychain(const char *service,const char *account,void **pass
 													  [passwordData bytes]		// pointer to password data
 													  );
 		
-		if(ret == noErr) success = YES;
+		success = (ret == noErr);
+		if(!success) {
+			//XXX localize me!
+			NSLog(@"could not change password in keychain for account %@ on service %@: SecKeychainItemModifyAttributesAndData returned %li", account, service, (long)err);
+		}
 	}
 
 	//Cleanup
