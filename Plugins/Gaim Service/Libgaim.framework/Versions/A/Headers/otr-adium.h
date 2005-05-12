fc4f449a5e684b6b3a7a13465505003fbe9728e2
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-05-12 21:04:08 +0000

    Libgaim.framework to revision 131.  Meanwhile available/away messages are now properly updated; fixes #105

---
+++ Plugins/Gaim Service/Libgaim.framework/Versions/A/Headers/otr-adium.h
@@ -0,0 +1,12 @@
+/*
+ *  otr-adium.h
+ *  Libgaim
+ *
+ *  Created by Evan Schoenberg on 5/11/05.
+ *  Copyright 2005 The Adium Team. All rights reserved.
+ *
+ */
+
+#include <libotr/userstate.h>
+
+OtrlUserState otrg_get_userstate(void);
