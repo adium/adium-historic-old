ac4a25e415df35bc8260e712f885ed2a93ce2612
Peter Hosey <Peter Hosey@localhost>
2005-05-12 20:36:15 +0000

    Mmm, columns. (From [11898].)

--- Source/AICrashController.m
+++ Source/AICrashController.m
@@ -44,13 +44,13 @@ static AICrashController *sharedCrashController = nil;
 		[[NSFileManager defaultManager] trashFileAtPath:CRASHES_PATH];
 
 		//Install custom handlers which properly terminate this application if one is received
-		signal(SIGILL, CrashHandler_Signal);	/* 4:   illegal instruction (not reset when caught) */
+		signal(SIGILL,  CrashHandler_Signal);	/* 4:   illegal instruction (not reset when caught) */
 		signal(SIGTRAP, CrashHandler_Signal);	/* 5:   trace trap (not reset when caught) */
-		signal(SIGEMT, CrashHandler_Signal);	/* 7:   EMT instruction */
-		signal(SIGFPE, CrashHandler_Signal);	/* 8:   floating point exception */
-		signal(SIGBUS, CrashHandler_Signal);	/* 10:  bus error */
+		signal(SIGEMT,  CrashHandler_Signal);	/* 7:   EMT instruction */
+		signal(SIGFPE,  CrashHandler_Signal);	/* 8:   floating point exception */
+		signal(SIGBUS,  CrashHandler_Signal);	/* 10:  bus error */
 		signal(SIGSEGV, CrashHandler_Signal);	/* 11:  segmentation violation */
-		signal(SIGSYS, CrashHandler_Signal);	/* 12:  bad argument to system call */
+		signal(SIGSYS,  CrashHandler_Signal);	/* 12:  bad argument to system call */
 		signal(SIGXCPU, CrashHandler_Signal);	/* 24:  exceeded CPU time limit */
 		signal(SIGXFSZ, CrashHandler_Signal);	/* 25:  exceeded file size limit */
 
