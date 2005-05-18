2bed1eb4149519c0d1988878caa1c1b88abd641d
Evan Schoenberg <Evan Schoenberg@localhost>
2005-05-18 07:31:47 +0000

    Added a file for the trunk which will serve as a resting place for 'dead' strings.. that is strings, which have been removed from trunk but not from 0.8x

---
+++ Source/DeadStrings.h
@@ -0,0 +1,16 @@
+/* These strings are no longer used in HEAD but are still needed in 0.8x.  If you delete a string from HEAD,
+ * but not from 0.8x, move it here so genstrings continues to put it in our Localizable.strings.
+ *
+ * This file goes when 0.90 is released.
+ */
+
+AILocalizedString(@"Sorry, but Adium Message Styles are not supported in OS X 10.2 (Jaguar).", nil)
+AILocalizedString(@"Sort tabs with the current sort options",nil)
+AILocalizedString(@"Remove %@",nil)
+AILocalizedString(@"Order the contact list:",nil)
+AILocalizedString(@"Hide while Adium is in the background",nil)
+AILocalizedString(@"Add to Accounts:",nil)
+AILocalizedString(@"Group Name:",nil)
+AILocalizedString(@"Messages send on:",nil)
+AILocalizedString(@"Send on Enter",nil)
+AILocalizedString(@"Send on Return",nil)
