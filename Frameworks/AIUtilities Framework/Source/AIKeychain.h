a7f2190d3e06012f149529c606d5699e66717b5c
Peter Hosey <Peter Hosey@localhost>
2005-05-20 18:32:29 +0000

    {{{AIKeychain}}} is now basically a direct wrapper on Keychain Services. The old methods are still in place for now, as I change over the source code that uses them to the new ones.

--- Frameworks/AIUtilities Framework/Source/AIKeychain.h
+++ Frameworks/AIUtilities Framework/Source/AIKeychain.h
@@ -13,15 +13,174 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
+#include <Security/Security.h>
+
+/*NOTE: it is **STRONGLY** recommended that you use AIWiredString, rather than
+ *	the Apple-supplied NSString or CFString, to supply passwords to AIKeychain.
+ *AIKeychain uses AIWiredString itself for all handling of passwords.
+ */
+@class AIWiredString;
+
+#pragma mark AIKeychain errors
+
+/*all AIKeychain methods return by reference an NSError object when the
+ *	Keychain Services function that backs that method returns an OSStatus
+ *	other than noErr.
+ *the domain of the error is AIKEYCHAIN_ERROR_DOMAIN.
+ *the error code is the OSStatus returned by Keychain Services.
+ *you may pass NULL for the error argument, in which case the error will be
+ *	silently dropped.
+ */
+
+#define AIKEYCHAIN_ERROR_DOMAIN @"AIKeychainError"
+
+//the function that returned the error
+#define AIKEYCHAIN_ERROR_USERINFO_SECURITYFUNCTION @"SecurityFrameworkFunction"
+#define AIKEYCHAIN_ERROR_USERINFO_SECURITYFUNCTIONNAME @"SecurityFrameworkFunctionName"
+//description of the error (from SecErrorMessages.strings)
+#define AIKEYCHAIN_ERROR_USERINFO_ERRORDESCRIPTION @"SecurityFrameworkErrorDescription"
+//the AIKeychain that was involved
+#define AIKEYCHAIN_ERROR_USERINFO_KEYCHAIN @"Keychain"
+//for +allowsUserInteraction, +setAllowsUserInteraction
+#define AIKEYCHAIN_ERROR_USERINFO_USERINTERACTIONALLOWEDSTATE @"UserInteractionAllowed"
+//for -getSettings:, -setSettings:
+#define AIKEYCHAIN_ERROR_USERINFO_SETTINGSPOINTER @"PointerToSettingsStructure"
+//for -{add,find}{Internet,Generic}Password:...
+#define AIKEYCHAIN_ERROR_USERINFO_SERVICE  @"GenericPasswordService"
+#define AIKEYCHAIN_ERROR_USERINFO_SERVER   @"InternetPasswordServer"
+#define AIKEYCHAIN_ERROR_USERINFO_DOMAIN   @"InternetPasswordSecurityDomain"
+#define AIKEYCHAIN_ERROR_USERINFO_ACCOUNT  @"PasswordAccount"
+#define AIKEYCHAIN_ERROR_USERINFO_PROTOCOL @"PasswordProtocol"
+#define AIKEYCHAIN_ERROR_USERINFO_AUTHTYPE @"PasswordAuthenticationType"
+
 /*!
  * @class AIKeychain
- * @brief Cocoa wrapper for accessing the system keychain
+ * @brief Cocoa wrapper for accessing keychains
  *
- * Cocoa wrapper which offers class methods for accessing the system keychain
+ * Cocoa wrapper which offers class methods for accessing the default keychain and others, and instance methods for accessing data from the keychains.
  */
-@interface AIKeychain : NSObject {
-
+@interface AIKeychain: NSObject {
+	SecKeychainRef keychainRef;
 }
+
+//SecKeychainLockAll
++ (void)lockAllKeychains_error:(out NSError **)outError;
+//SecKeychainLock (with keychain=NULL)
++ (void)lockDefaultKeychain_error:(out NSError **)outError;
+//SecKeychainUnlock (with keychain=NULL)
++ (void)unlockDefaultKeychain_error:(out NSError **)outError;
++ (void)unlockDefaultKeychainWithPassword:(NSString *)password error:(out NSError **)outError;
+
+//SecKeychainGetUserInteractionAllowed
+//returns YES if an error occurs.
++ (BOOL)allowsUserInteraction_error:(out NSError **)outError;
+//SecKeychainSetUserInteractionAllowed
++ (void)setAllowsUserInteraction:(BOOL)flag error:(out NSError **)outError;
+
+//SecKeychainGetVersion
++ (u_int32_t)keychainServicesVersion_error:(out NSError **)outError;
+
+#pragma mark -
+
+//SecKeychainCopyDefault
++ (AIKeychain *)defaultKeychain_error:(out NSError **)outError;
+//SecKeychainSetDefault
++ (void)setDefaultKeychain:(AIKeychain *)newDefaultKeychain error:(out NSError **)outError;
+
+//SecKeychainOpen
++ (AIKeychain *)keychainWithContentsOfFile:(NSString *)path error:(out NSError **)outError;
+- (id)initWithContentsOfFile:(NSString *)path error:(out NSError **)outError;
+
+//SecKeychainCreate
++ (AIKeychain *)keychainWithPath:(NSString *)path password:(NSString *)password promptUser:(BOOL)prompt initialAccess:(SecAccessRef)initialAccess error:(out NSError **)outError;
+- (id)initWithPath:(NSString *)path
+		  password:(NSString *)password //can be nil if promptUser is true
+		promptUser:(BOOL)prompt
+	 initialAccess:(SecAccessRef)initialAccess //can be NULL
+			 error:(out NSError **)outError;
+
++ (AIKeychain *)keychainWithKeychainRef:(SecKeychainRef)newKeychainRef;
+- (id)initWithKeychainRef:(SecKeychainRef)newKeychainRef;
+
+#pragma mark -
+
+//SecKeychainCopySettings
+- (void)getSettings:(out struct SecKeychainSettings *)outSettings error:(out NSError **)outError;
+//SecKeychainSetSettings
+- (void)setSettings:( in struct SecKeychainSettings *)newSettings error:(out NSError **)outError;
+
+//SecKeychainGetStatus
+- (SecKeychainStatus)status_error:(out NSError **)outError;
+//SecKeychainGetPath
+- (char *)getPathFileSystemRepresentation:(out char *)outBuf length:(inout u_int32_t *)outLength error:(out NSError **)outError;
+- (NSString *)path;
+
+#pragma mark -
+
+//SecKeychainLock
+- (void)lockKeychain_error:(out NSError **)outError;
+//SecKeychainUnlock
+- (void)unlockKeychain_error:(out NSError **)outError;
+- (void)unlockKeychainWithPassword:(NSString *)password error:(out NSError **)outError;
+
+#pragma mark -
+
+//SecKeychainDelete
+- (void)deleteKeychain_error:(out NSError **)outError;
+
+#pragma mark -
+
+//keychain items returned by these methods must be released.
+
+//SecKeychainAddInternetPassword
+- (void)addInternetPassword:(NSString *)password
+				  forServer:(NSString *)server
+			 securityDomain:(NSString *)domain //can pass nil
+					account:(NSString *)account
+					   path:(NSString *)path
+					   port:(u_int16_t)port //can pass 0
+				   protocol:(SecProtocolType)protocol
+		 authenticationType:(SecAuthenticationType)authType
+			   keychainItem:(out SecKeychainItemRef *)outKeychainItem
+					  error:(out NSError **)outError;
+
+//SecKeychainFindInternetPassword
+- (NSString *)findInternetPasswordForServer:(NSString *)server
+							 securityDomain:(NSString *)domain //can pass nil
+									account:(NSString *)account
+									   path:(NSString *)path
+									   port:(u_int16_t)port //can pass 0
+								   protocol:(SecProtocolType)protocol
+						 authenticationType:(SecAuthenticationType)authType
+							   keychainItem:(out SecKeychainItemRef *)outKeychainItem
+									  error:(out NSError **)outError;
+
+//convenience version: domain = path = nil; port = 0; authType = default; does not return keychain item
+- (NSString *)internetPasswordForServer:(NSString *)server account:(NSString *)account protocol:(SecProtocolType)protocol error:(out NSError **)outError;
+
+#pragma mark -
+
+//SecKeychainAddGenericPassword
+- (void)addGenericPassword:(NSString *)password
+				forService:(NSString *)service
+				   account:(NSString *)account
+			  keychainItem:(out SecKeychainItemRef *)outKeychainItem
+					 error:(out NSError **)outError;
+
+//SecKeychainFindGenericPassword
+- (NSString *)findGenericPasswordForService:(NSString *)service
+									account:(NSString *)account
+							   keychainItem:(out SecKeychainItemRef *)outKeychainItem
+									  error:(out NSError **)outError;
+
+- (NSDictionary *)dictionaryFromKeychainForServer:(NSString *)server error:(out NSError **)outError;
+
+#pragma mark -
+
+//returns the Keychain Services object that backs this object.
+- (SecKeychainRef)keychainRef;
+
+#pragma mark -
 
 /*!
  * @brief Retrieve a password from the keychain for a specified service/account combination
