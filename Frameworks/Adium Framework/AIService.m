6796c89e0e75e965239ec97775d7d79465f8654b
adamiser <adamiser@localhost>
2005-05-27 15:56:36 +0000

    Lets have the service filter its UID when creating an account, rather than passing the UID to the account and having the account immediately pass it back to be filtered.

--- Frameworks/Adium Framework/AIService.m
+++ Frameworks/Adium Framework/AIService.m
@@ -55,7 +55,9 @@
  */
 - (id)accountWithUID:(NSString *)inUID internalObjectID:(NSString *)inInternalObjectID
 {
-	return([[[[self accountClass] alloc] initWithUID:inUID internalObjectID:inInternalObjectID service:self] autorelease]);
+	return([[[[self accountClass] alloc] initWithUID:[self filterUID:inUID removeIgnoredCharacters:YES]
+									internalObjectID:inInternalObjectID
+											 service:self] autorelease]);
 }
 
 /*!
