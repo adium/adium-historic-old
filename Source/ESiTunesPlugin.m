8d22198228ce9cc87be4eb775fe332540da60515
Evan Schoenberg <Evan Schoenberg@localhost>
2005-06-12 09:33:54 +0000

    The iTunes plugin is now functional.  When the iTunes track changes, postNotificationName:Adium_RequestImmediateDynamicContentUpdate is posted.  It supports album, artist, composer, genre status, track, itunes music store url, and /music, as well as a new /currentITunesTrack which will be used internally for a default status state I believe.  It still needs to have a menu and probably a toolbar button.

--- Source/ESiTunesPlugin.m
+++ Source/ESiTunesPlugin.m
@@ -4,35 +4,33 @@
 //
 //  Created by Evan Schoenberg on 6/11/05.
 
-
-/*
- Some code copyright (c) The Growl Project, 2004
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- 
- 
- 1. Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- 3. Neither the name of Growl nor the names of its contributors
- may be used to endorse or promote products derived from this software
- without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* Thanks to GrowlTunes from the Growl project for demonstrating how to receive notifications when 
+ * the iTunes track changes.
  */
 
 #import "ESiTunesPlugin.h"
 #import "AIContentController.h"
+#import <Adium/AIAccount.h>
 #import <AIUtilities/AIAttributedStringAdditions.h>
 
 #define ITUNES_MINIMUM_VERSION 4.6f
 
+#define	KEY_PLAYING			@"Playing"
+#define	KEY_STOPPED			@"Stopped"
+
+#define ALBUM_TRIGGER		AILocalizedString(@"%_album","Trigger for the album of the currently playing iTunes song")
+#define ARTIST_TRIGGER		AILocalizedString(@"%_artist","Trigger for the artist of the currently playing iTunes song")
+#define COMPOSER_TRIGGER	AILocalizedString(@"%_composer","Trigger for the composer of the currently playing iTunes song")
+#define GENRE_TRIGGER		AILocalizedString(@"%_genre","Trigger for the genre of the currently playing iTunes song")
+#define STATUS_TRIGGER		AILocalizedString(@"%_status","Trigger for the genre of the currently playing iTunes song")
+#define TRACK_TRIGGER		AILocalizedString(@"%_track","Trigger for the name of the currently playing iTunes song")
+#define	STORE_URL_TRIGGER	AILocalizedString(@"%_iTMS","Trigger for an iTunes Music Store link to the currently playing iTunes song")
+#define MUSIC_TRIGGER		AILocalizedString(@"/music","Command which triggers *is listening to %_track by %_artist*")
+
 @implementation ESiTunesPlugin
 
 static	NSDictionary	*substitutionDict = nil;
+static	NSDictionary	*phraseSubstitutionDict = nil;
 
 /*!
 * @brief Install
@@ -40,7 +38,7 @@ static	NSDictionary	*substitutionDict = nil;
 - (void)installPlugin
 {
 	NSString *itunesPath = [[NSWorkspace sharedWorkspace] fullPathForApplication:@"iTunes"];
-	if ([[[NSBundle bundleWithPath:itunesPath] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] floatValue] > 4.6f) {
+	if ([[[NSBundle bundleWithPath:itunesPath] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] floatValue] > ITUNES_MINIMUM_VERSION) {
 		//Perform substitutions on outgoing content
 		[[adium contentController] registerContentFilter:self 
 												  ofType:AIFilterContent
@@ -48,23 +46,50 @@ static	NSDictionary	*substitutionDict = nil;
 												threaded:NO];
 
 		[[NSDistributedNotificationCenter defaultCenter] addObserver:self
-															selector:@selector(songChanged:)
+															selector:@selector(iTunesUpdate:)
 																name:@"com.apple.iTunes.playerInfo"
 															  object:nil];
 		
 		substitutionDict = [[NSDictionary alloc] initWithObjectsAndKeys:
-			@"Album", @"%_album",
-			@"Artist", @"%_artist",
-			@"Comment", @"%_comment", /* ?? */
-			@"Composer", @"%_composer",
-			@"Genre", @"%_genre",
-			@"Play Count", @"%_playcount", /* ?? */
-			@"Rating", @"%_rating", /* ?? */
-			@"Player State", @"%_status",
-			@"Name", @"%_track",
-			@"Year", @"%_year",
+			@"Album", ALBUM_TRIGGER,
+			@"Artist", ARTIST_TRIGGER,
+//			@"Comment", @"%_comment", /* ?? */
+			@"Composer", COMPOSER_TRIGGER,
+			@"Genre", GENRE_TRIGGER,
+//			@"Play Count", @"%_playcount", /* ?? */
+//			@"Rating", @"%_rating", /* ?? */
+			@"Player State", STATUS_TRIGGER,
+			@"Name", TRACK_TRIGGER,
+			@"Store URL", STORE_URL_TRIGGER,
+/*			@"Year", @"%_year", */
 			nil];
-			/* missing iTMS, playcount, position, /music */
+		
+		NSDictionary	*slashMusicDict;
+		NSDictionary	*conditionalArtistTrackDict;
+		
+		slashMusicDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+			AILocalizedString(@"*is listening to %_track by %_artist*","Phrase sent in response to /music.  %_track and %_artist MUST match their localized forms for this to work properly."),
+			KEY_PLAYING,
+			AILocalizedString(@"*is listening to nothing*","Phrase sent in response to /music when nothing is playing."),
+			KEY_STOPPED,
+			nil];
+		
+		conditionalArtistTrackDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+			AILocalizedString(@"%_track - %_artist","Phrase for the Current iTunes Track status. %_track and %_artist MUST match their localized forms for this to work properly."),
+			KEY_PLAYING,
+			@"",
+			KEY_STOPPED,
+			nil];
+		
+		phraseSubstitutionDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+			slashMusicDict,
+			MUSIC_TRIGGER,
+			conditionalArtistTrackDict,
+			@"/currentITunesTrack", /* not localized since only used internally */
+			nil];
+		
+		[slashMusicDict release];
+		[conditionalArtistTrackDict release];
 	}
 }
 
@@ -84,32 +109,66 @@ static	NSDictionary	*substitutionDict = nil;
 	NSString					*stringMessage;
 	
 	if ((stringMessage = [inAttributedString string])) {
-		NSEnumerator	*triggerEnumerator;
+		NSEnumerator	*enumerator;
 		NSString		*trigger;
 		
-		//
-		
-		//Substitute simple triggers as appropriate
-		triggerEnumerator = [substitutionDict keyEnumerator];
-		while (trigger = [triggerEnumerator nextObject]) {
+		// Perform phrase substitutions, which change based on the current playstate
+		enumerator = [phraseSubstitutionDict keyEnumerator];
+		while ((trigger = [enumerator nextObject])) {
 			if (([stringMessage rangeOfString:trigger options:NSCaseInsensitiveSearch].location != NSNotFound)) {
-				NSString	*iTunesReplacement;
+				NSDictionary	*replacementDict;
+				NSString		*replacement;
+				NSString		*playerState;
 				
-				if (!(iTunesReplacement = [iTunesCurrentInfo objectForKey:trigger])) {
-					iTunesReplacement = @"";
+				replacementDict = [phraseSubstitutionDict objectForKey:trigger];
+				playerState = [iTunesCurrentInfo objectForKey:@"Player State"];
+				
+				//Determine the appropriate replacement based on the player state
+				if ([playerState isEqualToString:@"Playing"] || [playerState isEqualToString:@"Paused"]) {
+					replacement = [replacementDict objectForKey:KEY_PLAYING];
+				} else {
+					replacement = [replacementDict objectForKey:KEY_STOPPED];					
 				}
 				
 				if (!filteredMessage) filteredMessage = [[inAttributedString mutableCopy] autorelease];
 				
+				//Perform the replacement
 				[filteredMessage replaceOccurrencesOfString:trigger
-												 withString:iTunesReplacement
+												 withString:replacement
+													options:NSLiteralSearch
+													  range:NSMakeRange(0, [filteredMessage length])];
+				
+				//Update our string for the simple trigger replacement process
+				stringMessage = [filteredMessage string];
+			}
+		}
+		
+		//Substitute simple triggers as appropriate
+		enumerator = [substitutionDict keyEnumerator];
+		while ((trigger = [enumerator nextObject])) {
+			if (([stringMessage rangeOfString:trigger options:NSCaseInsensitiveSearch].location != NSNotFound)) {
+				NSString	*replacement;
+				
+				/* Look up the appropriate key for this trigger, then use that key to find the replacement in the
+				 * current iTunes info.
+				 */
+				if (!(replacement = [iTunesCurrentInfo objectForKey:[substitutionDict objectForKey:trigger]])) {
+					//If no replacement is found, replace the trigger with an empty string
+					replacement = @"";
+				}
+				
+				if (!filteredMessage) filteredMessage = [[inAttributedString mutableCopy] autorelease];
+				
+				//Perform the replacement
+				[filteredMessage replaceOccurrencesOfString:trigger
+												 withString:replacement
 													options:NSLiteralSearch
 													  range:NSMakeRange(0, [filteredMessage length])];
 			}
 		}
 	}
-	
-	return filteredMessage;
+
+	return (filteredMessage ? filteredMessage : inAttributedString);
 }
 
 /*!
@@ -122,89 +181,19 @@ static	NSDictionary	*substitutionDict = nil;
 	return HIGH_FILTER_PRIORITY;
 }
 
-- (void)songChanged:(NSNotification *)aNotification
+- (void)iTunesUpdate:(NSNotification *)aNotification
 {
 	NSDictionary	*newInfo = [aNotification userInfo];
 	
 	if (newInfo != iTunesCurrentInfo) {
 		[iTunesCurrentInfo release];
 		iTunesCurrentInfo = [[aNotification userInfo] retain];
+		
+		AILog(@"iTunesUpdate: %@",iTunesCurrentInfo);
+
+		[[adium notificationCenter] postNotificationName:Adium_RequestImmediateDynamicContentUpdate
+												  object:nil];
 	}
 }
-
-#if 0
-	NSString     *playerState = nil;
-	iTunesState   newState    = itUNKNOWN;
-	NSString     *newTrackURL = nil;
-	NSDictionary *userInfo    = [aNotification userInfo];
-	
-	playerState = [[aNotification userInfo] objectForKey:@"Player State"];
-	if ([playerState isEqualToString:@"Paused"]) {
-		newState = itPAUSED;
-	} else if ([playerState isEqualToString:@"Stopped"]) {
-		newState = itSTOPPED;
-	} else if ([playerState isEqualToString:@"Playing"]){
-		newState = itPLAYING;
-		/*For radios and files, the ID is the location.
-		*For iTMS purchases, it's the Store URL.
-		*For Rendezvous shares, we'll hash a compilation of a bunch of info.
-		*/
-		if ([userInfo objectForKey:@"Location"]) {
-			newTrackURL = [userInfo objectForKey:@"Location"];
-		} else if ([userInfo objectForKey:@"Store URL"]) {
-			newTrackURL = [userInfo objectForKey:@"Store URL"];
-		} else {
-			/*Get all the info we can, in such a way that the empty fields are
-			*	blank rather than (null).
-			*Then we hash it and turn that into our identifier string.
-			*That way a track name of "file://foo" won't confuse our code later on.
-			*/
-			NSArray *args = [userInfo objectsForKeys:
-				[NSArray arrayWithObjects:@"Name", @"Artist", @"Album", @"Composer", @"Genre",
-					@"Year",@"Track Number", @"Track Count", @"Disc Number", @"Disc Count",
-					@"Total Time", nil]
-									  notFoundMarker:@""];
-			newTrackURL = [args componentsJoinedByString:@"|"];
-			newTrackURL = [[NSNumber numberWithUnsignedLong:[newTrackURL hash]] stringValue];
-		}
-	}
-	
-	if (newTrackURL && ![newTrackURL isEqualToString:trackURL]) { // this is different from previous notification
-		NSString		*track         = nil;
-		NSString		*length        = nil;
-		NSString		*artist        = @"";
-		NSString		*album         = @"";
-		BOOL			compilation    = NO;
-		NSNumber		*rating        = nil;
-		NSString		*ratingString  = nil;
-		NSImage			*artwork       = nil;
-		NSDictionary	*error         = nil;
-		NSString		*displayString;
-		
-		if ([userInfo objectForKey:@"Artist"])
-			artist = [userInfo objectForKey:@"Artist"];
-		if ([userInfo objectForKey:@"Album"])
-			album = [userInfo objectForKey:@"Album"];
-		track = [userInfo objectForKey:@"Name"];
-		
-		length  = [userInfo objectForKey:@"Total Time"];
-		// need to format a bit the length as it is returned in ms
-		int sec  = [length intValue] / 1000;
-		int hr  = sec/3600;
-		sec -= 3600*hr;
-		int min = sec/60;
-		sec -= 60*min;
-		if (hr > 0)
-			length = [NSString stringWithFormat:@"%d:%02d:%02d", hr, min, sec];
-		else
-			length = [NSString stringWithFormat:@"%d:%02d", min, sec];
-		
-		compilation = ([userInfo objectForKey:@"Compilation"] != nil);
-
-		rating = [userInfo objectForKey:@"Rating"];
-	}
-}
-
-#endif
 
 @end
