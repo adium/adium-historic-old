aa19e8036d431bb65589db21919082c870dce581
Evan Schoenberg <Evan Schoenberg@localhost>
2005-06-27 09:24:11 +0000

    Finished implementing the improved AIHostReachabilityMonitor.  If CFHost can't resolve the node name to an IP address, the local IP list dynamic store is monitored for changes, checking any unconfigured hosts when it changes.  This also utilizes CFHost to immediately look up reachability of hosts when they are added to AIHostReachabilityMonitor and succesfully resolved to an IP. Fixes #597

--- Frameworks/AIUtilities Framework/Source/AIHostReachabilityMonitor.h
+++ Frameworks/AIUtilities Framework/Source/AIHostReachabilityMonitor.h
@@ -18,33 +18,23 @@
 
 @interface AIHostReachabilityMonitor: NSObject
 {
-	NSMutableArray	*hosts;
-	NSMutableArray	*observers;
-	NSMutableArray	*reachabilities;
+	NSMutableArray		*hosts;
+	NSMutableArray		*observers;
+	NSMutableArray		*reachabilities;
 	
-	NSMutableSet	*unconfiguredHostsAndObservers;
+	NSMutableSet		*unconfiguredHostsAndObservers;
 	
-	NSLock			*hostAndObserverListLock;
+	NSLock				*hostAndObserverListLock;
+	
+	CFRunLoopSourceRef	ipChangesRunLoopSourceRef;
 }
 
-/*!
- *	@brief	Returns a shared instance, usable for most purposes.
- *	@return A shared <tt>AIHostReachabilityMonitor</tt> instance.
- */
+
 + (id)defaultMonitor;
 
 #pragma mark -
 
-/*!
- *	@brief	Begins observing a host's reachability for an object.
- */
 - (void)addObserver:(id <AIHostReachabilityObserver>)newObserver forHost:(NSString *)host;
-/*!
- *	@brief	Stops an object's observation of a host's reachability.
- *
- *	When <tt>host</tt> is non-nil, stops observing that host's reachability for the given observer.
- *	When <tt>host</tt> is nil, stops observing all hosts' reachability for the given observer.
- */
 - (void)removeObserver:(id <AIHostReachabilityObserver>)observer forHost:(NSString *)host;
 
 @end
