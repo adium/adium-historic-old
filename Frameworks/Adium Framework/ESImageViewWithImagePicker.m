d595f391a9081422c6b656e090b581b07736e537
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-07-08 01:36:04 +0000

    Need these for compatibility

---
+++ Frameworks/Adium Framework/ESImageViewWithImagePicker.m
@@ -0,0 +1,14 @@
+//
+//  ESImageViewWithImagePicker.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 7/7/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "ESImageViewWithImagePicker.h"
+
+
+@implementation ESImageViewWithImagePicker
+
+@end
