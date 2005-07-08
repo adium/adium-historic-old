998f7afdda185d68815420bfabc2924eee0a3e94
Evan Schoenberg <Evan Schoenberg@localhost>
2005-07-08 01:36:04 +0000

    Need these for compatibility

---
+++ Frameworks/Adium Framework/ESImageViewWithImagePicker.h
@@ -0,0 +1,15 @@
+//
+//  ESImageViewWithImagePicker.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 7/7/05.
+//
+
+#import <AIUtilities/AIImageViewWithImagePicker.h>
+
+//Used in WebKit message view preferences, which are localizable
+@interface ESImageViewWithImagePicker : AIImageViewWithImagePicker {
+
+}
+
+@end
