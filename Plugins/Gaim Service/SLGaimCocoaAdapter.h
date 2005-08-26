8273422891dfbe5dfc8fc4b43bb06da25ac65742
David Smith <catfish.man@gmail.com>
2005-08-26 03:00:02 +0000

    Removing unneeded oneways

--- Plugins/Gaim Service/SLGaimCocoaAdapter.h
+++ Plugins/Gaim Service/SLGaimCocoaAdapter.h
@@ -43,50 +43,50 @@
 					inChat:(AIChat *)chat
 				 withFlags:(int)flags;
 
-- (oneway void)sendTyping:(AITypingState)typingState inChat:(AIChat *)chat;
+- (void)sendTyping:(AITypingState)typingState inChat:(AIChat *)chat;
 
-- (oneway void)addUID:(NSString *)objectUID onAccount:(id)adiumAccount toGroup:(NSString *)groupName;
-- (oneway void)removeUID:(NSString *)objectUID onAccount:(id)adiumAccount fromGroup:(NSString *)groupName;
-- (oneway void)moveUID:(NSString *)objectUID onAccount:(id)adiumAccount toGroup:(NSString *)groupName;
-- (oneway void)renameGroup:(NSString *)oldGroupName onAccount:(id)adiumAccount to:(NSString *)newGroupName;
-- (oneway void)deleteGroup:(NSString *)groupName onAccount:(id)adiumAccount;
+- (void)addUID:(NSString *)objectUID onAccount:(id)adiumAccount toGroup:(NSString *)groupName;
+- (void)removeUID:(NSString *)objectUID onAccount:(id)adiumAccount fromGroup:(NSString *)groupName;
+- (void)moveUID:(NSString *)objectUID onAccount:(id)adiumAccount toGroup:(NSString *)groupName;
+- (void)renameGroup:(NSString *)oldGroupName onAccount:(id)adiumAccount to:(NSString *)newGroupName;
+- (void)deleteGroup:(NSString *)groupName onAccount:(id)adiumAccount;
 
-- (oneway void)setAlias:(NSString *)alias forUID:(NSString *)UID onAccount:(id)adiumAccount;
+- (void)setAlias:(NSString *)alias forUID:(NSString *)UID onAccount:(id)adiumAccount;
 
-- (oneway void)openChat:(AIChat *)chat onAccount:(id)adiumAccount;
-- (oneway void)inviteContact:(AIListContact *)contact toChat:(AIChat *)chat withMessage:(NSString *)inviteMessage;
+- (void)openChat:(AIChat *)chat onAccount:(id)adiumAccount;
+- (void)inviteContact:(AIListContact *)contact toChat:(AIChat *)chat withMessage:(NSString *)inviteMessage;
 
-- (oneway void)closeChat:(AIChat *)chat;
+- (void)closeChat:(AIChat *)chat;
 - (void)connectAccount:(id)adiumAccount;
 - (void)disconnectAccount:(id)adiumAccount;
 - (void)registerAccount:(id)adiumAccount;
-- (oneway void)xferRequest:(GaimXfer *)xfer;
-- (oneway void)xferRequestAccepted:(GaimXfer *)xfer withFileName:(NSString *)xferFileName;
-- (oneway void)xferRequestRejected:(GaimXfer *)xfer;
-- (oneway void)xferCancel:(GaimXfer *)xfer;
-- (oneway void)getInfoFor:(NSString *)inUID onAccount:(id)adiumAccount;
+- (void)xferRequest:(GaimXfer *)xfer;
+- (void)xferRequestAccepted:(GaimXfer *)xfer withFileName:(NSString *)xferFileName;
+- (void)xferRequestRejected:(GaimXfer *)xfer;
+- (void)xferCancel:(GaimXfer *)xfer;
+- (void)getInfoFor:(NSString *)inUID onAccount:(id)adiumAccount;
 
-- (oneway void)setGaimStatusType:(const char *)gaimStatusType withMessage:(NSString *)message onAccount:(id)adiumAccount;
-- (oneway void)setInvisible:(BOOL)isInvisible onAccount:(id)adiumAccount;
-- (oneway void)setInfo:(NSString *)profileHTML onAccount:(id)adiumAccount;
-- (oneway void)setBuddyIcon:(NSString *)buddyImageFilename onAccount:(id)adiumAccount;
-- (oneway void)setIdleSinceTo:(NSDate *)idleSince onAccount:(id)adiumAccount;
+- (void)setGaimStatusType:(const char *)gaimStatusType withMessage:(NSString *)message onAccount:(id)adiumAccount;
+- (void)setInvisible:(BOOL)isInvisible onAccount:(id)adiumAccount;
+- (void)setInfo:(NSString *)profileHTML onAccount:(id)adiumAccount;
+- (void)setBuddyIcon:(NSString *)buddyImageFilename onAccount:(id)adiumAccount;
+- (void)setIdleSinceTo:(NSDate *)idleSince onAccount:(id)adiumAccount;
 
-- (oneway void)setCheckMail:(NSNumber *)checkMail forAccount:(id)adiumAccount;
-- (oneway void)setDefaultPermitDenyForAccount:(id)adiumAccount;
+- (void)setCheckMail:(NSNumber *)checkMail forAccount:(id)adiumAccount;
+- (void)setDefaultPermitDenyForAccount:(id)adiumAccount;
 
-- (oneway void)OSCAREditComment:(NSString *)comment forUID:(NSString *)inUID onAccount:(id)adiumAccount;
-- (oneway void)OSCARSetFormatTo:(NSString *)inFormattedUID onAccount:(id)adiumAccount;
-- (oneway void)OSCARSetAvailableMessageTo:(NSString *)availablePlaintext onAccount:(id)adiumAccount;
+- (void)OSCAREditComment:(NSString *)comment forUID:(NSString *)inUID onAccount:(id)adiumAccount;
+- (void)OSCARSetFormatTo:(NSString *)inFormattedUID onAccount:(id)adiumAccount;
+- (void)OSCARSetAvailableMessageTo:(NSString *)availablePlaintext onAccount:(id)adiumAccount;
 
 - (void)displayFileSendError;
 - (void *)handleNotifyMessageOfType:(GaimNotifyType)type withTitle:(const char *)title primary:(const char *)primary secondary:(const char *)secondary;
 - (void *)handleNotifyFormattedWithTitle:(const char *)title primary:(const char *)primary secondary:(const char *)secondary text:(const char *)text;
 
-- (oneway void)performContactMenuActionFromDict:(NSDictionary *)dict;
-- (oneway void)performAccountMenuActionFromDict:(NSDictionary *)dict;
+- (void)performContactMenuActionFromDict:(NSDictionary *)dict;
+- (void)performAccountMenuActionFromDict:(NSDictionary *)dict;
 
-- (oneway void)requestSecureMessaging:(BOOL)inSecureMessaging
+- (void)requestSecureMessaging:(BOOL)inSecureMessaging
 							   inChat:(AIChat *)inChat;
 - (void)gaimConversation:(GaimConversation *)conv setSecurityDetails:(NSDictionary *)securityDetailsDict;
 - (void)refreshedSecurityOfGaimConversation:(GaimConversation *)conv;
