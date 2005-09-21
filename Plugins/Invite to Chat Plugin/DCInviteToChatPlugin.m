ab0e92d34d0dad51a98b31790ea7980b4464863c
David Smith <catfish.man@gmail.com>
2005-09-21 23:49:04 +0000

    Gah! Stop hiding from me! Yet more updates to use -isGroupChat. Refs #1582

--- Plugins/Invite to Chat Plugin/DCInviteToChatPlugin.m
+++ Plugins/Invite to Chat Plugin/DCInviteToChatPlugin.m
@@ -139,7 +139,7 @@
 				if ( [[[[chat account] service] serviceClass] isEqualToString:serviceClass] ) {
 					
 					// Is this a group chat?
-					if ( [chat name] ) {
+					if ([ chat isGroupChat]) {
 						if (!menu_chatMenu) {
 							menu_chatMenu = [[[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:@""] autorelease];
 						}
