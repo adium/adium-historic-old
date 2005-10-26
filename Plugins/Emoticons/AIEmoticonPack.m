796e3e9d1eb936acdaf11e4f4413da1ccb18837d
David Smith <David Smith@localhost>
2005-10-26 02:46:48 +0000

    I fail at Objective-C >.<

--- Plugins/Emoticons/AIEmoticonPack.m
+++ Plugins/Emoticons/AIEmoticonPack.m
@@ -61,7 +61,7 @@
 		path = [inPath retain];
 		name = [[[inPath lastPathComponent] stringByDeletingPathExtension] retain];
 		NSBundle * xtraBundle = [NSBundle bundleWithPath:path];
-		if (xtraBundle && ([[xtraBundle objectForInfoDictionaryKey:@"XtraBundleVersion] intValue == 1)) {//This checks for a new-style xtra
+		if (xtraBundle && ([[xtraBundle objectForInfoDictionaryKey:@"XtraBundleVersion"] intValue] == 1)) {//This checks for a new-style xtra
 			path = [xtraBundle resourcePath]; //new style xtras store the same info, but it's in Contents/Resources/ so that we can have an info.plist file and use NSBundle
 		}
 		emoticonArray = nil;
