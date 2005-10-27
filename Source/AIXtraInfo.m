c5aaf6e1fbcff8e4904f3c585d1dfe61d7070c09
David Smith <catfish.man@gmail.com>
2005-10-27 05:47:44 +0000

    Adding a default description for xtras. This should probably be tweaked

--- Source/AIXtraInfo.m
+++ Source/AIXtraInfo.m
@@ -34,19 +34,20 @@
 	return [[[self alloc] initWithURL:url] autorelease];
 }
 
-- (id) initWithURL:(NSURL *)url//url to a plist with info
+- (id) initWithURL:(NSURL *)url
 {
 	if((self = [super init]))
 	{
-		/*NSDictionary * info = [NSDictionary dictionaryWithContentsOfURL:
-		name = [info objectForKey:@"Name"];*/
 		path = [[url path] retain];
-		name = [[[path lastPathComponent] stringByDeletingPathExtension]retain];
 		NSBundle * xtraBundle = [[NSBundle alloc] initWithPath:path];
-		if(xtraBundle && ([[xtraBundle objectForInfoDictionaryKey:@"XtraBundleVersion"] intValue] == 1))//This checks for a new-style xtra
-		{
+		if (xtraBundle && ([[xtraBundle objectForInfoDictionaryKey:@"XtraBundleVersion"] intValue] == 1)) { //This checks for a new-style xtra
+			name = [xtraBundle objectForInfoDictionaryKey:(NSString *)kCFBundleNameKey];
 			readMePath = [[xtraBundle pathForResource:@"ReadMe" ofType:@"rtf"]retain];
 			icon = [[NSImage alloc] initByReferencingFile:[xtraBundle pathForResource:@"Icon" ofType:@"icns"]];
+		}
+		else {
+			name = [[[path lastPathComponent] stringByDeletingPathExtension]retain];
+			readMePath = [[[NSBundle mainBundle] pathForResource:@"DefaultXtraReadme" ofType:@"rtf"]retain];	
 		}
 		if(!icon)
 			icon = [[[NSWorkspace sharedWorkspace] iconForFile:path]retain];
