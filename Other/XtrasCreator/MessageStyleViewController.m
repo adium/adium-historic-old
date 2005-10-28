b1084cf1ab90b8b11aee377cdbbb356b3b6c1c3b
David Smith <catfish.man@gmail.com>
2005-10-28 06:57:03 +0000

    XtrasCreator! Yay! (Reaaaaally in progress)

---
+++ Other/XtrasCreator/MessageStyleViewController.m
@@ -0,0 +1,45 @@
+//
+//  MessageStyleViewController.m
+//  XtrasCreator
+//
+//  Created by David Smith on 10/27/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "MessageStyleViewController.h"
+
+
+@implementation MessageStyleViewController
+
++ (id)controller
+{
+	MessageStyleViewController * c = [[MessageStyleViewController alloc] init];
+	[NSBundle loadNibNamed:@"MessageStyleView" owner:c];
+	return [c autorelease];
+}
+
+- (NSView *)view
+{
+	return view;
+}
+
+- (id) init
+{
+	if((self = [super init]))
+	{
+		
+	}
+	return self;
+}
+
+- (void) writeCustomFilesToPath:(NSString *)path
+{
+	path = [[path stringByDeletingLastPathComponent] stringByAppendingPathComponent:@"Info.plist"];
+	NSMutableDictionary * info = [NSMutableDictionary dictionaryWithContentsOfFile:path];
+	[info setObject:[DisplayNameForNoVariantField stringValue] forKey:@"DisplayNameForNoVariant"];
+	[info setObject:[NSNumber numberWithInt:[[DefaultFontSizeField stringValue]intValue]] forKey:@"DefaultFontSize"];
+	[info setObject:[DefaultFontSizeField stringValue] forKey:@"DefaultFontSize"];
+	[info writeToFile:path atomically:YES];
+}
+
+@end
---
+++ Other/XtrasCreator/MyDocument.m
@@ -0,0 +1,118 @@
+//
+//  MyDocument.m
+//  XtrasCreator
+//
+//  Created by David Smith on 10/27/05.
+//  Copyright __MyCompanyName__ 2005 . All rights reserved.
+//
+
+#import "MyDocument.h"
+#import "MessageStyleViewController.h"
+
+@implementation MyDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+    
+        // Add your subclass-specific initialization here.
+        // If an error occurs here, send a [self release] message and return nil.
+    
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController
+{
+    [super windowControllerDidLoadNib:aController];
+	[typePopup setAutoenablesItems:NO];
+	NSMenuItem * item = [[[NSMenuItem alloc] initWithTitle:@"Message View Style" 
+													action:@selector(setXtraType:) 
+											 keyEquivalent:@""]autorelease];
+	[item setEnabled:YES];
+    [[typePopup menu] addItem:item];
+	item = [[[NSMenuItem alloc] initWithTitle:@"Status Icons" 
+									   action:@selector(setXtraType:) 
+								keyEquivalent:@""]autorelease];
+	[item setEnabled:YES];
+	[[typePopup menu] addItem:item];
+}
+
+- (BOOL)writeToFile:(NSString *)fileName ofType:(NSString *)docType
+{
+	NSString * path = fileName;
+	NSFileManager * manager = [NSFileManager defaultManager];
+	NSString * name = [nameField stringValue];
+	if(![manager fileExistsAtPath:path])
+	{
+		[manager createDirectoryAtPath:path attributes:[NSDictionary dictionary]];
+		path = [path stringByAppendingPathComponent:@"Contents"];
+		[manager createDirectoryAtPath:path attributes:[NSDictionary dictionary]];
+		[[NSDictionary dictionaryWithObjectsAndKeys:
+			@"English", kCFBundleDevelopmentRegionKey,
+			name, kCFBundleNameKey,
+			@"AdIM", @"CFBundlePackageType",
+			[@"com.adiumx." stringByAppendingString:name], kCFBundleIdentifierKey,
+			[NSNumber numberWithInt:1], @"XtraBundleVersion",
+			@"1.0", kCFBundleInfoDictionaryVersionKey,
+			[versionField stringValue], @"XtraVersion",
+			[authorField stringValue], @"XtraAuthors",
+			xtraType, @"XtraType",
+			nil] writeToFile:[path stringByAppendingPathComponent:@"Info.plist"] atomically:YES];
+		path = [path stringByAppendingPathComponent:@"Resources"];
+		[manager createDirectoryAtPath:path attributes:[NSDictionary dictionary]];
+		NSEnumerator * resourceEnu = [resources objectEnumerator];
+		NSString * resourcePath;
+		while ((resourcePath = [resourceEnu nextObject]))
+		{
+			[manager copyPath:resourcePath 
+					   toPath:[path stringByAppendingPathComponent:[resourcePath lastPathComponent]]												  handler:nil];
+		}
+		[controller writeCustomFilesToPath:path];
+		return YES;
+	}	
+	else
+		return NO;
+}
+
+- (BOOL) readFromFile:(NSString *)path ofType:(NSString *)type
+{
+	
+    return YES;
+}
+
+- (IBAction) addFiles:(id)sender
+{
+	if(!resources)
+		resources = [[NSMutableSet alloc] init];
+	NSOpenPanel * p = [NSOpenPanel openPanel];
+	[p setAllowsMultipleSelection:YES];
+	[p runModal];
+	[resources addObjectsFromArray:[p filenames]];
+	[fileView setString:[[resources allObjects] componentsJoinedByString:@"\n"]];
+}
+
+- (IBAction) setXtraType:(id)sender
+{
+	if([tabs numberOfTabViewItems] > 2)
+		[tabs removeTabViewItem:[tabs tabViewItemAtIndex:2]];
+	xtraType = [sender title];
+	[typePopup selectItemWithTitle:xtraType];
+	NSTabViewItem * item = [[NSTabViewItem alloc] initWithIdentifier:xtraType];
+	[item setLabel:xtraType];
+	if([xtraType isEqualToString:@"Message View Style"]) {
+		controller = [[MessageStyleViewController controller]retain];
+		[item setView:[controller view]];
+	}
+	[tabs addTabViewItem:[item autorelease]];
+}
+
+@end
