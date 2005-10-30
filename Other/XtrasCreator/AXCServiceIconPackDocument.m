ea2712471978da93cae655bb0975f0377d6ccdcd
Peter Hosey <Peter Hosey@localhost>
2005-10-30 14:17:20 +0000

    Added the document class for service icon packs. Rearranged the source files in the Compile Sources stage.

---
+++ Other/XtrasCreator/AXCServiceIconPackDocument.m
@@ -0,0 +1,24 @@
+//
+//  AXCServiceIconPackDocument.m
+//  XtrasCreator
+//
+//  Created by Mac-arena the Bored Zo on 2005-10-30.
+//  Copyright 2005 Adium Team. All rights reserved.
+//
+
+#import "AXCServiceIconPackDocument.h"
+
+
+@implementation AXCServiceIconPackDocument
+
+- (NSString *) OSType {
+	return @"AISr";
+}
+- (NSString *) pathExtension {
+	return @"AdiumServiceIcons";
+}
+- (NSString *) uniformTypeIdentifier {
+	return @"com.adiumx.serviceicons";
+}
+
+@end
