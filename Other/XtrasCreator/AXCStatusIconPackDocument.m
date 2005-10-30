82626e8dc370a1d1b3d82a1e74b0657a9f831cc7
Peter Hosey <git@boredzo.org>
2005-10-30 14:12:39 +0000

    New classes: AXCFileCell, AXCStatusIconPackDocument. In AXCAbstractXtraDocument: Removed {{{-setXtraType:}}}; added abstract methods; shinier {{{-init}}}; shiny new {{{-dealloc}}}; {{{resources}}} is now an NSArray; tabs. Replaced the text view with a table view. It uses AXCFileCell for spiffy display of the file items. To-do: sort by filename. Rearranged the ordering of the document types in Info.plist, so that we create a status icon pack on start-up instead of a message view style. Should be slightly easier to debug. To-do: a 'Starting Points' window.

---
+++ Other/XtrasCreator/AXCStatusIconPackDocument.m
@@ -0,0 +1,24 @@
+//
+//  AXCStatusIconPackDocument.m
+//  XtrasCreator
+//
+//  Created by Mac-arena the Bored Zo on 2005-10-30.
+//  Copyright 2005 Adium Team. All rights reserved.
+//
+
+#import "AXCStatusIconPackDocument.h"
+
+
+@implementation AXCStatusIconPackDocument
+
+- (NSString *) OSType {
+	return @"AISt";
+}
+- (NSString *) pathExtension {
+	return @"AdiumStatusIcons";
+}
+- (NSString *) uniformTypeIdentifier {
+	return @"com.adiumx.statusicons";
+}
+
+@end
