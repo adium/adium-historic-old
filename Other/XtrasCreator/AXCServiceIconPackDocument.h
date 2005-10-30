d39a6174808a0f8558665ff1e195fd3418f68896
Peter Hosey <git@boredzo.org>
2005-10-30 14:17:20 +0000

    Added the document class for service icon packs. Rearranged the source files in the Compile Sources stage.

---
+++ Other/XtrasCreator/AXCServiceIconPackDocument.h
@@ -0,0 +1,15 @@
+//
+//  AXCServiceIconPackDocument.h
+//  XtrasCreator
+//
+//  Created by Mac-arena the Bored Zo on 2005-10-30.
+//  Copyright 2005 Adium Team. All rights reserved.
+//
+
+#import "AXCAbstractXtraDocument.h"
+
+@interface AXCServiceIconPackDocument : AXCAbstractXtraDocument {
+
+}
+
+@end
