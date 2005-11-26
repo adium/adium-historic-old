def703cb1e75ca3b402ef4129b2d92ae99297237
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-11-26 22:41:19 +0000

    Adding a class for adding/editing status groups

---
+++ Frameworks/Adium Framework/ESEditStatusGroupWindowController.h
@@ -0,0 +1,27 @@
+//
+//  ESEditStatusGroupWindowController.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 11/25/05.
+
+#import <Adium/AIWindowController.h>
+
+@class AIStatusGroup;
+
+@interface ESEditStatusGroupWindowController : AIWindowController {
+	IBOutlet	NSTextField			*label_title;
+	IBOutlet	NSTextField			*textField_title;
+
+	IBOutlet	NSTextField			*label_groupWith;
+	IBOutlet	NSPopUpButton		*popUp_groupWith;
+	
+	AIStatusGroup					*statusGroup;
+	id								target;
+}
+
++ (void)editStatusGroup:(AIStatusGroup *)inStatusGroup onWindow:(id)parentWindow notifyingTarget:(id)inTarget;
+
+- (IBAction)okay:(id)sender;
+- (IBAction)cancel:(id)sender;
+
+@end
