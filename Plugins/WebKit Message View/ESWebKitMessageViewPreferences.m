4bd60eaed17ae50ed315e8d50ee6eb4952058733
Evan Schoenberg <Evan Schoenberg@localhost>
2005-11-26 09:45:21 +0000

    Don't set any messages as outgoing for the preview chat; this fixes an oddity in font/color display in the preview chat

--- Plugins/WebKit Message View/ESWebKitMessageViewPreferences.m
+++ Plugins/WebKit Message View/ESWebKitMessageViewPreferences.m
@@ -581,7 +581,7 @@ int menuTitleSort(id objectA, id objectB, void *context)
 			
 			//AIContentMessage won't know whether the message is outgoing unless we tell it since neither our source
 			//nor our destination are AIAccount objects.
-			[content _setIsOutgoing:outgoing];
+			//[content _setIsOutgoing:outgoing];
 
 		} else if ([msgType isEqualToString:CONTENT_STATUS_TYPE]) {
 			//Create status content object
