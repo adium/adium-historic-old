fd9115b4ca89a3b6d5ce0063a070fd8ac519dd6a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-12-06 00:54:23 +0000

    Outline and the most important functionality (account setup) of the setup assistant.
    Feel free to write good text for the three existing panes and to add some sort of basic preferences configuration pane for it :)
    
    There's a #define in AIAdium.m {{{ALWAYS_RUN_SETUP_WIZARD}}} which can be changed to TRUE to see it with an existing configuration.  Otherwise, it runs when no accounts are configured at startup.
    
    The !AdiumyButler image is by Wes Underwood, of course.
    
    Refs #9.

---
+++ Source/AdiumSetupWizard.h
@@ -0,0 +1,45 @@
+//
+//  AdiumSetupWizard.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/4/05.
+
+#import <Adium/AIWindowController.h>
+
+@class SetupWizardBackgroundView;
+
+@interface AdiumSetupWizard : AIWindowController {
+	IBOutlet SetupWizardBackgroundView	*backgroundView;
+
+	IBOutlet NSButton	*button_continue;
+	IBOutlet NSButton	*button_goBack;
+	IBOutlet NSButton	*button_alternate;
+	
+	IBOutlet NSTabView	*tabView;
+
+	//Welcome
+	IBOutlet NSTextField *textField_welcome;
+	IBOutlet NSTextView	 *textView_welcomeMessage;
+	
+	//Account Setup
+	IBOutlet NSTextField	*textField_addAccount;
+	IBOutlet NSTextView		*textView_addAccountMessage;
+	IBOutlet NSPopUpButton	*popUp_services;
+	IBOutlet NSTextField	*textField_serviceLabel;
+	IBOutlet NSTextField	*textField_usernameLabel;
+	IBOutlet NSTextField	*textField_username;
+	IBOutlet NSTextField	*textField_passwordLabel;
+	IBOutlet NSTextField	*textField_password;
+	BOOL					setupAccountTabViewItem;
+	
+	//All Done
+	IBOutlet NSTextField	*textField_done;
+	IBOutlet NSTextView		*textView_doneMessage;
+}
+
++ (void)runWizard;
+- (IBAction)nextTab:(id)sender;
+- (IBAction)previousTab:(id)sender;
+- (IBAction)pressedAlternateButton:(id)sender;
+
+@end
