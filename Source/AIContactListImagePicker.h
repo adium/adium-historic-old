c7e6419ff08af7155d54667ef8dd3b76036cb4c8
David Smith <catfish.man@gmail.com>
2005-12-17 01:02:20 +0000

    Fixing this mysteriously consistent typo, and removing MyCompanyName

---
+++ Source/AIContactListImagePicker.h
@@ -0,0 +1,15 @@
+//
+//  AIContactListImagePicker.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/16/05.
+//  Copyright 2005 the Adium Team. All rights reserved.
+//
+
+#import <AIUtilities/AIImageViewWithImagePicker.h>
+
+@interface AIContactListImagePicker : AIImageViewWithImagePicker {
+
+}
+
+@end
---
+++ Source/AIContactListImagePicker.m
@@ -0,0 +1,14 @@
+//
+//  AIContactListImagePicker.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/16/05.
+//  Copyright 2005 the Adium Team. All rights reserved.
+//
+
+#import "AIContactListImagePicker.h"
+
+
+@implementation AIContactListImagePicker
+
+@end
