ac5be0b138b32d2512d6d2c1328a46c0836562a4
durin42 <durin42@localhost>
2005-12-17 05:17:05 +0000

    warnings--

--- Frameworks/Adium Framework/AIListGroupMockieCell.m
+++ Frameworks/Adium Framework/AIListGroupMockieCell.m
@@ -18,6 +18,7 @@
 #import "AIListOutlineView.h"
 #import <AIUtilities/AIBezierPathAdditions.h>
 #import <AIUtilities/AIGradient.h>
+#import <AIUtilities/AIColorAdditions.h>
 
 @implementation AIListGroupMockieCell
 
