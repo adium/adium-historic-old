66d9606edf72343e38cdbc1b0eaae40b3d581449
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-12-24 06:56:29 +0000

    Placeholder class for now for the contact list name

---
+++ Source/AIContactListNameView.h
@@ -0,0 +1,15 @@
+//
+//  AIContactListNameView.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/23/05.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AIContactListNameView : NSTextField {
+
+}
+
+@end
---
+++ Source/AIContactListNameView.m
@@ -0,0 +1,14 @@
+//
+//  AIContactListNameView.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/23/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIContactListNameView.h"
+
+
+@implementation AIContactListNameView
+
+@end
