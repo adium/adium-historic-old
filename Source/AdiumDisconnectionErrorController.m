4b64446f67aec2a34a41556215cd6b1eaecca03f
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-12-24 18:34:08 +0000

    Empty files to keep the project happy

---
+++ Source/AdiumDisconnectionErrorController.m
@@ -0,0 +1,14 @@
+//
+//  AdiumDisconnectionErrorController.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/23/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "AdiumDisconnectionErrorController.h"
+
+
+@implementation AdiumDisconnectionErrorController
+
+@end
