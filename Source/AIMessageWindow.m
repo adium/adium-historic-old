667396ccfddfc65814f36b0872959035d5d82dab
Evan Schoenberg <Evan Schoenberg@localhost>
2005-12-27 08:29:02 +0000

    Added AIMessageWindow, which will be the window class for message windows and which vends {{{AIClickThroughThemeDocumentButton}}} objects for the document icon

---
+++ Source/AIMessageWindow.m
@@ -0,0 +1,37 @@
+//
+//  AIMessageWindow.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/26/05.
+//
+
+#import "AIMessageWindow.h"
+#import "AIClickThroughThemeDocumentButton.h"
+
+/*
+ * @class AIMessageWindow
+ * @brief This AIDockingWindow subclass serves message windows.
+ *
+ * It overrides the standardWindowButton:forStyleMask: class method to provide
+ * AIClickThroughThemeDocumentButton objects for NSWindowDocumentIconButton requests.
+ */
+@implementation AIMessageWindow
+
+/*
+ * @brief Return the standard window button for a mask
+ *
+ * We return AIClickThroughThemeDocumentButton instead of NSThemeDocumentButton to provide
+ * click-through dragging behavior.
+ */
++ (NSButton *)standardWindowButton:(NSWindowButton)button forStyleMask:(unsigned int)styleMask
+{
+	NSButton *standardWindowButton = [super standardWindowButton:button forStyleMask:styleMask];
+
+	if (button == NSWindowDocumentIconButton) {
+		standardWindowButton = [[[AIClickThroughThemeDocumentButton alloc] initWithFrame:[standardWindowButton frame]] autorelease];
+	}
+
+	return standardWindowButton;
+}
+
+@end
