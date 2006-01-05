fcbbca99b7f69bcdb607fa98225842095f97a697
David Smith <David Smith@localhost>
2006-01-05 13:31:47 +0000

    Remembering to save first would be great :/

--- Plugins/Bonjour/AWBonjourService.h
+++ Plugins/Bonjour/AWBonjourService.h
@@ -3,7 +3,7 @@
 //  Adium
 //
 //  Created by Adam Iser on 8/26/04.
-//  Copyright (c) 2004-2005 The Adium Team. All rights reserved.
+//  Copyright (c) 2004-2006 The Adium Team. All rights reserved.
 //
 
 #import <Adium/AIService.h>
