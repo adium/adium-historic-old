f1ba972daedf128d7404c8222efd195cebb5a303
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-01-21 16:37:21 +0000

    Log message for [15023]: The AIM profile view in the Accounts Personal prefs now shows the global profile as greyed-out text if no per-account profile is present.

--- Plugins/Gaim Service/AIGaimOscarAccountViewController.m
+++ Plugins/Gaim Service/AIGaimOscarAccountViewController.m
@@ -64,7 +64,7 @@
 	} else {
 		[textView_textProfile setString:@""];
 	}
-	
+
 	if ([textView_textProfile isKindOfClass:[AITextViewWithPlaceholder class]]) {
 		NSData				*globalProfileData = [[adium preferenceController] preferenceForKey:@"TextProfile" group:GROUP_ACCOUNT_STATUS];
 		NSAttributedString	*globalProfile = (globalProfileData ? [NSAttributedString stringWithData:globalProfileData] : nil);
