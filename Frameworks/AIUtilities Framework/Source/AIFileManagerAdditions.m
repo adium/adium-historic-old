cf647c94057ce0268ca17555750b80fd113b5e2a
Evan Schoenberg <Evan Schoenberg@localhost>
2006-02-14 05:15:53 +0000

    Moved uniquePathForPath: to AIFileManagerAdditions

--- Frameworks/AIUtilities Framework/Source/AIFileManagerAdditions.m
+++ Frameworks/AIUtilities Framework/Source/AIFileManagerAdditions.m
@@ -122,4 +122,55 @@
 	return path;
 }
 
+/*!
+ * @brief Generate a unique path given a path
+ *
+ * If nothing exists at the path, the path is returned.
+ * If a file or folder with the passed name already exists, a hyphen and a number is added, the number being the 
+ * smallest necessary for it to be unique.
+ *
+ * For example, if ~/Desktop/pr0n.jpg already exists, ~/Desktop/pr0n-1.jpg will be returned, if that file does not
+ * exist.  If ~/Desktop/pr0n-1.jpg exists, ~/Desktop/pr0n-2.jpg will be returned, and so on.
+ *
+ * @result The full unique path
+ */
+- (NSString *)uniquePathForPath:(NSString *)inPath
+{
+	NSFileManager	*defaultManager = [NSFileManager defaultManager];
+	NSString		*uniquePath = inPath;
+	NSString		*basePath = nil, *fileName = nil, *extension = nil;
+	BOOL			generatedParts = NO;
+	unsigned		uniqueNameCounter = 0;
+	
+	//Get a unique name if necessary. This could happen if we are sending this folder multiple times.
+	while ([defaultManager fileExistsAtPath:uniquePath]) {
+		NSString	*uniqueFilename;
+		
+		if (!generatedParts) {
+			basePath = [inPath stringByDeletingLastPathComponent];
+			fileName = [[inPath lastPathComponent] stringByDeletingPathExtension];
+			extension = [inPath pathExtension];
+			
+			//If there is no extension, -[NSString pathExtension] returns @""
+			if (![extension length]) extension = nil;
+			
+			generatedParts = YES;
+		}
+		
+		//Get a unique file name
+		uniqueFilename = [NSString stringWithFormat:@"%@-%i",fileName,++uniqueNameCounter];
+		
+		//Put it at the proper path
+		uniquePath = [basePath stringByAppendingPathComponent:uniqueFilename];
+		
+		//Append the extension if there is one
+		if (extension) {
+			uniquePath = [uniquePath stringByAppendingPathExtension:extension];
+		}
+	}
+	
+	return uniquePath;
+}
+
+
 @end
--- Source/ESFileTransferController.m
+++ Source/ESFileTransferController.m
@@ -24,6 +24,7 @@
 #import "ESFileTransferProgressWindowController.h"
 #import "ESFileTransferRequestPromptController.h"
 #import <AIUtilities/AIDictionaryAdditions.h>
+#import <AIUtilities/AIFileManagerAdditions.h>
 #import <AIUtilities/AIMenuAdditions.h>
 #import <AIUtilities/AIStringAdditions.h>
 #import <AIUtilities/AIToolbarUtilities.h>
@@ -57,8 +58,6 @@ static ESFileTransferPreferences *preferences;
 
 - (BOOL)shouldOpenCompleteFileTransfer:(ESFileTransfer *)fileTransfer;
 - (void)_removeFileTransfer:(ESFileTransfer *)fileTransfer;
-
-- (NSString *)uniquePathForPath:(NSString *)inPath;
 @end
 
 @implementation ESFileTransferController
@@ -192,7 +191,7 @@ static ESFileTransferPreferences *preferences;
 		localFilename = [preferredDownloadFolder stringByAppendingPathComponent:remoteFilename];
 		
 		[self _finishReceiveRequestForFileTransfer:fileTransfer
-									 localFilename:[self uniquePathForPath:localFilename]];
+									 localFilename:[[NSFileManager defaultManager] uniquePathForPath:localFilename]];
 			
 	} else {
 		//Prompt to accept/deny
@@ -272,7 +271,7 @@ static ESFileTransferPreferences *preferences;
 			//Ensure our temporary directory exists [it never will the first time this method is called]
 			[defaultManager createDirectoryAtPath:tmpDir attributes:nil];
 
-			pathToArchive = [self uniquePathForPath:[tmpDir stringByAppendingPathComponent:[folderName stringByAppendingPathExtension:@"zip"]]];
+			pathToArchive = [[NSFileManager defaultManager] uniquePathForPath:[tmpDir stringByAppendingPathComponent:[folderName stringByAppendingPathExtension:@"zip"]]];
 
 			arguments = [NSArray arrayWithObjects:
 				@"-r", //we'll want to store recursively
@@ -654,59 +653,6 @@ static ESFileTransferPreferences *preferences;
 	static NSImage	*eventImage = nil;
 	if (!eventImage) eventImage = [[NSImage imageNamed:@"pref-ft" forClass:[self class]] retain];
 	return eventImage;
-}
-
-#pragma mark Unique naming
-/*!
- * @brief Generate a unique path given a path
- *
- * If nothing exists at the path, the path is returned.
- * If a file or folder with the passed name already exists, a hyphen and a number is added, the number being the 
- * smallest necessary for it to be unique.
- *
- * For example, if ~/Desktop/pr0n.jpg already exists, ~/Desktop/pr0n-1.jpg will be returned, if that file does not
- * exist.  If ~/Desktop/pr0n-1.jpg exists, ~/Desktop/pr0n-2.jpg will be returned, and so on.
- *
- * @result The full unique path
- */
-- (NSString *)uniquePathForPath:(NSString *)inPath
-{
-	NSFileManager	*defaultManager = [NSFileManager defaultManager];
-	NSString		*uniquePath = inPath;
-	NSString		*basePath = nil, *fileName = nil, *extension = nil;
-	BOOL			generatedParts = NO;
-	unsigned		uniqueNameCounter = 0;
-	
-	fileName = [fileName stringByDeletingPathExtension];
-
-	//Get a unique name if necessary. This could happen if we are sending this folder multiple times.
-	while ([defaultManager fileExistsAtPath:uniquePath]) {
-		NSString	*uniqueFilename;
-		
-		if (!generatedParts) {
-			basePath = [inPath stringByDeletingLastPathComponent];
-			fileName = [[inPath lastPathComponent] stringByDeletingPathExtension];
-			extension = [inPath pathExtension];
-
-			//If there is no extension, -[NSString pathExtension] returns @""
-			if (![extension length]) extension = nil;
-
-			generatedParts = YES;
-		}
-		
-		//Get a unique file name
-		uniqueFilename = [NSString stringWithFormat:@"%@-%i",fileName,++uniqueNameCounter];
-		
-		//Put it at the proper path
-		uniquePath = [basePath stringByAppendingPathComponent:uniqueFilename];
-		
-		//Append the extension if there is one
-		if (extension) {
-			uniquePath = [uniquePath stringByAppendingPathExtension:extension];
-		}
-	}
-	
-	return uniquePath;
 }
 
 #pragma mark Strings for sizes
