aceb0805604cb5af8812167258e7e4c407469869
David Smith <David Smith@localhost>
2006-02-23 03:36:58 +0000

    warnings--

--- Plugins/WebKit Message View/AIWebkitMessageViewStyle.h
+++ Plugins/WebKit Message View/AIWebkitMessageViewStyle.h
@@ -122,7 +122,6 @@ typedef enum {
 + (NSString *)defaultVariantForBundle:(NSBundle *)inBundle;
 
 //Keyword substitution
-- (void) replaceKeyword:(NSString *)word inString:(NSMutableString *)string withString:(NSString *)newWord;
 - (NSMutableString *)fillKeywords:(NSMutableString *)inString forContent:(AIContentObject *)content;
 - (NSMutableString *)fillKeywordsForBaseTemplate:(NSMutableString *)inString chat:(AIChat *)chat;
 @end
