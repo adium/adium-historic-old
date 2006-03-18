9434e2437ac0c04ac9fc515a875115e1ce964a62
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-03-18 16:36:18 +0000

    * Removed appearance preferences code  * The icon/status/name selector now uses AIWindowDraggingView so that anywhere a subview isn't present a drag makes the window move.  This is a better solution than resizing its items because resizing makes it unable to ellipsize text if it is too large for the window width.

--- Plugins/Contact List/AISCLViewPlugin.m
+++ Plugins/Contact List/AISCLViewPlugin.m
@@ -22,16 +22,8 @@
 #import "AIStandardListWindowController.h"
 #import "ESContactListAdvancedPreferences.h"
 #import <AIUtilities/AIDictionaryAdditions.h>
-#import <AIUtilities/AIStringAdditions.h>
-#import "AIXtrasManager.h"
 
 #define PREF_GROUP_APPEARANCE		@"Appearance"
-
-int availableSetSort(NSDictionary *objectA, NSDictionary *objectB, void *context);
-
-@interface AISCLViewPlugin (PRIVATE)
-- (void)preferencesChanged:(NSNotification *)notification;
-@end
 
 /*!
  * @class AISCLViewPlugin
@@ -51,9 +43,6 @@ int availableSetSort(NSDictionary *objectA, NSDictionary *objectB, void *context
 - (void)installPlugin
 {
     [[adium interfaceController] registerContactListController:self];
-
-	[adium createResourcePathForName:LIST_LAYOUT_FOLDER];
-	[adium createResourcePathForName:LIST_THEME_FOLDER];
 
     //Install our preference views
 	advancedPreferences = [[ESContactListAdvancedPreferences preferencePane] retain];
@@ -135,42 +124,24 @@ int availableSetSort(NSDictionary *objectA, NSDictionary *objectB, void *context
 - (void)preferencesChangedForGroup:(NSString *)group key:(NSString *)key
 							object:(AIListObject *)object preferenceDict:(NSDictionary *)prefDict firstTime:(BOOL)firstTime
 {
-	if (firstTime || [group isEqualToString:PREF_GROUP_APPEARANCE]) {
-		//Theme
-		if (firstTime || !key || [key isEqualToString:KEY_LIST_THEME_NAME]) {
-			[AISCLViewPlugin applySetWithName:[prefDict objectForKey:KEY_LIST_THEME_NAME]
-									extension:LIST_THEME_EXTENSION
-									 inFolder:LIST_THEME_FOLDER
-							toPreferenceGroup:PREF_GROUP_LIST_THEME];
-		}
+	if (firstTime || !key || [key isEqualToString:KEY_LIST_LAYOUT_WINDOW_STYLE]) {
+		int	newWindowStyle = [[prefDict objectForKey:KEY_LIST_LAYOUT_WINDOW_STYLE] intValue];
 		
-		//Layout
-		if (firstTime || !key || [key isEqualToString:KEY_LIST_LAYOUT_NAME]) {
-			[AISCLViewPlugin applySetWithName:[prefDict objectForKey:KEY_LIST_LAYOUT_NAME]
-									extension:LIST_LAYOUT_EXTENSION
-									 inFolder:LIST_LAYOUT_FOLDER
-							toPreferenceGroup:PREF_GROUP_LIST_LAYOUT];
-		}
-
-		if (firstTime || !key || [key isEqualToString:KEY_LIST_LAYOUT_WINDOW_STYLE]) {
-			int	newWindowStyle = [[prefDict objectForKey:KEY_LIST_LAYOUT_WINDOW_STYLE] intValue];
-
-			if (newWindowStyle != windowStyle) {
-				windowStyle = newWindowStyle;
-				
-				//If a contact list is visible and the window style has changed, update for the new window style
-				if (contactListWindowController) {
-					//XXX - Evan: I really do not like this at all.  What to do?
-					//We can't close and reopen the contact list from within a preferencesChanged call, as the
-					//contact list itself is a preferences observer and will modify the array for its group as it
-					//closes... and you can't modify an array while enuemrating it, which the preferencesController is
-					//currently doing.  This isn't pretty, but it's the most efficient fix I could come up with.
-					//It has the obnoxious side effect of the contact list changing its view prefs and THEN closing and
-					//reopening with the right windowStyle.
-					[self performSelector:@selector(closeAndReopencontactList)
-							   withObject:nil
-							   afterDelay:0.00001];
-				}
+		if (newWindowStyle != windowStyle) {
+			windowStyle = newWindowStyle;
+			
+			//If a contact list is visible and the window style has changed, update for the new window style
+			if (contactListWindowController) {
+				//XXX - Evan: I really do not like this at all.  What to do?
+				//We can't close and reopen the contact list from within a preferencesChanged call, as the
+				//contact list itself is a preferences observer and will modify the array for its group as it
+				//closes... and you can't modify an array while enuemrating it, which the preferencesController is
+				//currently doing.  This isn't pretty, but it's the most efficient fix I could come up with.
+				//It has the obnoxious side effect of the contact list changing its view prefs and THEN closing and
+				//reopening with the right windowStyle.
+				[self performSelector:@selector(closeAndReopencontactList)
+						   withObject:nil
+						   afterDelay:0.00001];
 			}
 		}
 	}
@@ -180,167 +151,6 @@ int availableSetSort(NSDictionary *objectA, NSDictionary *objectB, void *context
 {
 	[self closeContactList];
 	[self showContactListAndBringToFront:NO];
-}
-
-#pragma mark ListLayout and ListTheme preference management
-//Apply a set of preferences
-+ (void)applySetWithName:(NSString *)setName extension:(NSString *)extension inFolder:(NSString *)folder toPreferenceGroup:(NSString *)preferenceGroup
-{
-	AIAdium			*adiumInstance = [AIObject sharedAdiumInstance];
-	NSFileManager	*defaultManager = [NSFileManager defaultManager];
-	NSEnumerator	*enumerator;
-	NSString		*fileName, *resourcePath;
-	NSDictionary	*setDictionary = nil;
-
-	//Look in each resource location until we find it
-	fileName = [setName stringByAppendingPathExtension:extension];
-	
-	enumerator = [[adiumInstance resourcePathsForName:folder] objectEnumerator];
-	while ((resourcePath = [enumerator nextObject]) && !setDictionary) {
-		NSString		*filePath = [resourcePath stringByAppendingPathComponent:fileName];
-		
-		if ([defaultManager fileExistsAtPath:filePath]) {
-			setDictionary = [NSDictionary dictionaryWithContentsOfFile:filePath];
-		}
-	}
-	
-	//Apply its values
-	[[adiumInstance preferenceController] setPreferences:setDictionary
-												 inGroup:preferenceGroup];
-}
-
-//Create a layout or theme set
-+ (BOOL)createSetFromPreferenceGroup:(NSString *)preferenceGroup withName:(NSString *)setName extension:(NSString *)extension inFolder:(NSString *)folder
-{
-	NSString		*path;
-	NSString		*fileName = [[setName safeFilenameString] stringByAppendingPathExtension:extension];
-	AIAdium			*sharedAdiumInstance = [AIObject sharedAdiumInstance];
-
-	//If we don't find one, create a path to a bundle in the application support directory
-	path = [[[[AIObject sharedAdiumInstance] applicationSupportDirectory] stringByAppendingPathComponent:folder] stringByAppendingPathComponent:fileName];
-	if ([AIXtrasManager createXtraBundleAtPath:path])
-		path = [path stringByAppendingPathComponent:@"Contents/Resources/Data.plist"];
-	
-	if ([[[sharedAdiumInstance preferenceController] preferencesForGroup:preferenceGroup] writeToFile:path atomically:NO]) {
-		
-		[[sharedAdiumInstance notificationCenter] postNotificationName:Adium_Xtras_Changed object:extension];
-
-		return YES;
-	} else {
-		NSRunAlertPanel(AILocalizedString(@"Error Saving Theme",nil),
-						AILocalizedString(@"Unable to write file %@ to %@",nil),
-						AILocalizedString(@"OK",nil),
-						nil,
-						nil,
-						fileName,
-						path);
-		return NO;
-	}
-}
-
-//Delete a layout or theme set
-+ (BOOL)deleteSetWithName:(NSString *)setName extension:(NSString *)extension inFolder:(NSString *)folder
-{
-	BOOL		success;
-	
-	success = [[NSFileManager defaultManager] removeFileAtPath:[[AIObject sharedAdiumInstance] pathOfPackWithName:setName
-																										extension:extension
-																							   resourceFolderName:folder]
-													   handler:nil];
-	
-	//The availability of an xtras just changed, since we deleted it... post a notification so we can update
-	[[[AIObject sharedAdiumInstance] notificationCenter] postNotificationName:Adium_Xtras_Changed object:extension];
-	
-	return success;
-}
-
-//
-+ (BOOL)renameSetWithName:(NSString *)setName extension:(NSString *)extension inFolder:(NSString *)folder toName:(NSString *)newName
-{
-	BOOL		success;
-	
-	NSString	*destFolder = [[[AIObject sharedAdiumInstance] applicationSupportDirectory] stringByAppendingPathComponent:folder];
-	NSString	*newFileName = [newName stringByAppendingPathExtension:extension];
-	
-	success = [[NSFileManager defaultManager] movePath:[[AIObject sharedAdiumInstance] pathOfPackWithName:setName
-																								extension:extension
-																					   resourceFolderName:folder]
-												toPath:[destFolder stringByAppendingPathComponent:newFileName]
-											   handler:nil];
-
-	//The availability of an xtras just changed, since we deleted it... post a notification so we can update
-	[[[AIObject sharedAdiumInstance] notificationCenter] postNotificationName:Adium_Xtras_Changed object:extension];
-	
-	return success;
-}
-
-//
-+ (BOOL)duplicateSetWithName:(NSString *)setName extension:(NSString *)extension inFolder:(NSString *)folder newName:(NSString *)newName
-{
-	BOOL		success;
-	
-	//Duplicate the set
-	NSString	*destFolder = [[[AIObject sharedAdiumInstance] applicationSupportDirectory] stringByAppendingPathComponent:folder];
-	NSString	*newFileName = [newName stringByAppendingPathExtension:extension];
-	
-	success = [[NSFileManager defaultManager] copyPath:[[AIObject sharedAdiumInstance] pathOfPackWithName:setName
-																								extension:extension
-																					   resourceFolderName:folder]
-												toPath:[destFolder stringByAppendingPathComponent:newFileName]
-											   handler:nil];
-	
-	//The availability of an xtras just changed, since we deleted it... post a notification so we can update
-	[[[AIObject sharedAdiumInstance] notificationCenter] postNotificationName:Adium_Xtras_Changed object:extension];
-
-	return success;
-}
-
-+ (NSArray *)availableLayoutSets
-{
-	return [AISCLViewPlugin availableSetsWithExtension:LIST_LAYOUT_EXTENSION 
-											fromFolder:LIST_LAYOUT_FOLDER];
-}
-+ (NSArray *)availableThemeSets
-{
-	return [AISCLViewPlugin availableSetsWithExtension:LIST_THEME_EXTENSION fromFolder:LIST_THEME_FOLDER];
-}
-
-//
-+ (NSArray *)availableSetsWithExtension:(NSString *)extension fromFolder:(NSString *)folder
-{
-	NSMutableArray	*setArray = [NSMutableArray array];
-	NSEnumerator	*enumerator = [[[AIObject sharedAdiumInstance] allResourcesForName:folder withExtensions:extension] objectEnumerator];
-	NSMutableArray	*alreadyAddedArray = [NSMutableArray array];
-	NSString		*filePath, *name;
-	NSBundle		*xtraBundle;
-	
-    while ((filePath = [enumerator nextObject])) {
-		name = [[filePath lastPathComponent] stringByDeletingPathExtension];
-		xtraBundle = [NSBundle bundleWithPath:filePath];
-		if(xtraBundle && ([[xtraBundle objectForInfoDictionaryKey:@"XtraBundleVersion"] intValue] == 1))
-			filePath = [[xtraBundle resourcePath] stringByAppendingPathComponent:@"Data.plist"];
-		NSDictionary 	*themeDict = [NSDictionary dictionaryWithContentsOfFile:filePath];
-		
-		if (themeDict) {			
-			//The Adium resource path is last in our resourcePaths array; by only adding sets we haven't
-			//already added, we allow precedence to occur rather than conflict.
-			if ([alreadyAddedArray indexOfObject:name] == NSNotFound) {
-				[setArray addObject:[NSDictionary dictionaryWithObjectsAndKeys:
-					name, @"name",
-					filePath, @"path",
-					themeDict, @"preferences",
-					nil]];
-				[alreadyAddedArray addObject:name];
-			}
-		}
-	}
-	
-	return [setArray sortedArrayUsingFunction:availableSetSort context:nil];
-}
-
-//Sort sets
-int availableSetSort(NSDictionary *objectA, NSDictionary *objectB, void *context) {
-	return [[objectA objectForKey:@"name"] caseInsensitiveCompare:[objectB objectForKey:@"name"]];
 }
 
 @end
