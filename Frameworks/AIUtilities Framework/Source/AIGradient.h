471c9bc36f8993ac9cf58b682d684bc44f36e20f
Peter Hosey <Peter Hosey@localhost>
2006-04-22 07:35:25 +0000

    [wiki:Doxygenation Documentation].

--- Frameworks/AIUtilities Framework/Source/AIGradient.h
+++ Frameworks/AIUtilities Framework/Source/AIGradient.h
@@ -13,17 +13,24 @@
  | write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  \------------------------------------------------------------------------------------------------------ */
 
+//This documentation comment doesn't show up. I don't know why. --boredzo
 /*!	@enum AIDirection
- *	A gradient direction.
+ *	@brief A gradient direction.
+ *	Can be left-to-right or bottom-to-top.
  */
 enum AIDirection {
-	AIHorizontal = 0, //Horizontal
-	AIVertical //Vertical
+	/*!	@brief Left-to-right.
+	 *	The far left point in the gradient will be the first color; the far right point will be the second color.
+	 */
+	AIHorizontal,
+	/*!	@brief Bottom-to-top.
+	 *	The bottom point in the gradient will be the first color; the top point will be the second color.
+	 */
+	AIVertical
 };
 
-/*!
- * @class AIGradient
- * @brief Cocoa wrapper around lower level gradient drawing functions.  Draws simple gradients.
+/*!	@class AIGradient
+ *	@brief Cocoa wrapper around lower level (CoreGraphics) gradient drawing functions, implementing two-color linear gradients.
  */
 @interface AIGradient : NSObject {
 	enum AIDirection	 direction;
@@ -36,8 +43,8 @@ enum AIDirection {
  *
  * @param inColor1 The starting NSColor
  * @param inColor2 The ending NSColor
- * @param inDirection The <tt>AIDirection</tt> for the gradient
- * @result An autoreleased <tt>AIGadient</tt>
+ * @param inDirection The \c AIDirection for the gradient
+ * @return An autoreleased \c AIGradient
  */
 + (AIGradient*)gradientWithFirstColor:(NSColor*)inColor1
 						  secondColor:(NSColor*)inColor2
@@ -49,66 +56,65 @@ enum AIDirection {
  * Use the system selectedControl color to create a gradient in the specified direction. This gradient is appropriate
  * for a Tiger-style selected highlight.
  *
- * @param inDirection The <tt>AIDirection</tt> for the gradient
- * @result An autoreleased <tt>AIGradient</tt> for a selected control
+ * @param inDirection The \c AIDirection for the gradient
+ * @return An autoreleased \c AIGradient for a selected control
  */
 + (AIGradient*)selectedControlGradientWithDirection:(enum AIDirection)inDirection;
 
 /*!
- * @brief Set the first (left or top) color
+ * @brief Set the first (left or bottom) color.
  *
- * @param inColor The first <tt>NSColor</tt>
+ * @param inColor The first \c NSColor.
  */
 - (void)setFirstColor:(NSColor*)inColor;
 
 /*!
- * @brief Return the first (left or top) color
+ * @brief Return the first (left or bottom) color.
  *
  * @result The first color.
  */
 - (NSColor*)firstColor;
 
 /*!
- * @brief Set the second (right or bottom) color
+ * @brief Set the second (right or top) color.
  *
- * @param inColor The second <tt>NSColor</tt>
+ * @param inColor The second \c NSColor.
  */
 - (void)setSecondColor:(NSColor*)inColor;
 
 /*!
- * @brief Return the second (right or bottom) color
+ * @brief Return the second (right or top) color.
  *
- * @result The second color
+ * @result The second color.
  */
 - (NSColor*)secondColor;
 
 /*!
- * @brief Set the direction for the gradient
+ * @brief Set the direction for the gradient.
  *
- * @param inDirection The <tt>AIDirection</tt> for the gradient
+ * @param inDirection The \c AIDirection for the gradient.
  */
 - (void)setDirection:(enum AIDirection)inDirection;
 
 /*!
- * @brief Return the direction for the gradient
+ * @brief Return the direction for the gradient.
  *
- * @result The <tt>AIDirection</tt> for the gradient
+ * @result The \c AIDirection for the gradient.
  */
 - (enum AIDirection)direction;
 
 /*!
- * @brief Draw the gradient in an <tt>NSRect</tt>
+ * @brief Draw the gradient in an \c NSRect.
  *
- * @param rect The <tt>NSRect</tt> in which to render the gradient
+ * @param rect The \c NSRect in which to render the gradient.
  */
 - (void)drawInRect:(NSRect)rect;
 
-/*!
- * @brief Draw the gradient in an <tt>NSBezierPath</tt>
+/*!	@brief Draw the gradient in an \c NSBezierPath.
  *
- * The gradient will fill the specified path according to its winding rules, transformations, etc.
+ *	The gradient will fill the specified path according to the path's winding rule, transformation matrix, and so on.
  *
- * @param inPath The <tt>NSBezierPath</tt> in which to render to gradient
+ *	@param inPath The \c NSBezierPath in which to render to gradient.
  */
 - (void)drawInBezierPath:(NSBezierPath *)inPath;
 
