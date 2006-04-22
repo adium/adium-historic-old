fd63c839af413bf4dde5e5302e592f217baf1b14
Peter Hosey <Peter Hosey@localhost>
2006-04-22 06:39:51 +0000

    Documented these, pursuant to [wiki:Doxygenation Doxygenation].

--- Frameworks/AIUtilities Framework/Source/AIWiredData.h
+++ Frameworks/AIUtilities Framework/Source/AIWiredData.h
@@ -21,6 +21,20 @@
 
 #import <Cocoa/Cocoa.h>
 
+/*!	@class AIWiredData AIWiredData.h <AIUtilities/AIWiredData.h>
+ *	@brief A concrete, immutable subclass of NSData that uses wired memory for its backing.
+ *
+ *	@par
+ *	When storing passwords, you do not want the password to be paged out to disk, where it could be obtained using data-recovery techniques (e.g. DriveSavers).
+ *	Memory that has been "wired" cannot be paged out to disk. Therefore wired memory should be used for short-term storage of passwords.
+ *	This class stores bytes in a wired backing, so that they will not be paged to disk.
+ *	The AIWiredData instance itself may still be paged out, but this is of no value after the machine has been shut down (e.g. to remove the HDD to search it), because the secret information will no longer be in memory.
+ *
+ *	@par
+ *	Note: Does not yet conform to \c NSCopying. Use \c -subdataWithRange: instead for now.
+ *
+ *	@see AIWiredString
+ */
 
 @interface AIWiredData: NSData
 {
