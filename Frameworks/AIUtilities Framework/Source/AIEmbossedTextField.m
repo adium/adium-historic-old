51b41102e7a2d4e1ba1691da1d04b31661f80f31
Peter Hosey <Peter Hosey@localhost>
2006-05-01 03:59:18 +0000

    Reindented.

--- Frameworks/AIUtilities Framework/Source/AIEmbossedTextField.m
+++ Frameworks/AIUtilities Framework/Source/AIEmbossedTextField.m
@@ -12,33 +12,33 @@
 
 - (void)drawRect:(NSRect)inRect
 {
-    if ([[self window] isTextured]) {
-	NSFont			*font = [NSFont boldSystemFontOfSize:[[self font] pointSize]];
-	NSRect			bounds = [self bounds];
-	NSDictionary		*attributes;
-	NSColor			*textColor;
+	if ([[self window] isTextured]) {
+		NSFont			*font = [NSFont boldSystemFontOfSize:[[self font] pointSize]];
+		NSRect			 bounds = [self bounds];
+		NSDictionary	*attributes;
+		NSColor			*textColor;
 
-	//Disable sub-pixel rendering.  It looks horrible with embossed text
-	CGContextSetShouldSmoothFonts([[NSGraphicsContext currentContext] graphicsPort], 0);
+		//Disable sub-pixel rendering.  It looks horrible with embossed text
+		CGContextSetShouldSmoothFonts([[NSGraphicsContext currentContext] graphicsPort], 0);
     
-	//
-	textColor = [NSColor colorWithCalibratedWhite:1.0 alpha:0.4];
-	attributes = [NSDictionary dictionaryWithObjectsAndKeys:
-		textColor, NSForegroundColorAttributeName,
-		font, NSFontAttributeName, nil];
-		
-	[[self stringValue] drawInRect:NSOffsetRect(bounds, +2, +1) withAttributes:attributes];
-    
-	
-	textColor = [NSColor colorWithCalibratedWhite:0.16 alpha:1.0];
-	attributes = [NSDictionary dictionaryWithObjectsAndKeys:
-	    textColor, NSForegroundColorAttributeName,
-	    font, NSFontAttributeName, nil];
-    
-	[[self stringValue] drawInRect:NSOffsetRect(bounds, +2, 0) withAttributes:attributes];
-    } else {
-	[super drawRect:inRect];
-    }
+		//
+		textColor = [NSColor colorWithCalibratedWhite:1.0 alpha:0.4];
+		attributes = [NSDictionary dictionaryWithObjectsAndKeys:
+			textColor, NSForegroundColorAttributeName,
+			font, NSFontAttributeName, nil];
+
+		[[self stringValue] drawInRect:NSOffsetRect(bounds, +2, +1) withAttributes:attributes];
+
+		textColor = [NSColor colorWithCalibratedWhite:0.16 alpha:1.0];
+		attributes = [NSDictionary dictionaryWithObjectsAndKeys:
+			textColor, NSForegroundColorAttributeName,
+			font, NSFontAttributeName,
+			nil];
+
+		[[self stringValue] drawInRect:NSOffsetRect(bounds, +2, 0) withAttributes:attributes];
+	} else {
+		[super drawRect:inRect];
+	}
 }
 
 @end
