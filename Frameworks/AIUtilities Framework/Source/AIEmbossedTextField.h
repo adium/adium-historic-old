0dda9ceceb534643bff23f6b77a5d47dc3142a31
Peter Hosey <Peter Hosey@localhost>
2006-05-01 03:59:10 +0000

    Documented purpose of class.

--- Frameworks/AIUtilities Framework/Source/AIEmbossedTextField.h
+++ Frameworks/AIUtilities Framework/Source/AIEmbossedTextField.h
@@ -6,6 +6,11 @@
 //  Copyright (c) 2003-2005 The Adium Team. All rights reserved.
 //
 
+/*!	@class AIEmbossedTextField <AIEmbossedTextField.h> <AIUtilities/AIEmbossedTextField.h>
+ *	@brief Draws embossed text.
+ *
+ *	Draws its text with an embossed appearance.
+ */
 @interface AIEmbossedTextField : NSTextField 
 {
 
