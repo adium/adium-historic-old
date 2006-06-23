dc07584b7ae9cd0b778ac9f2b8fa78efa474a6f5
Andreas Monitzer <Andreas Monitzer@localhost>
2006-06-23 01:47:40 +0000

    Fixed error plugin, now following the specs.

--- andreas_monitzer/Plugins/Smack Service/SmackXMPPErrorMessagePlugin.m
+++ andreas_monitzer/Plugins/Smack Service/SmackXMPPErrorMessagePlugin.m
@@ -12,6 +12,31 @@
 
 #import <AIUtilities/AIStringUtilities.h>
 
+static struct
+{
+    int code;
+    NSString *description;
+} mapping[] =
+{
+    { 302, @"Redirect" },
+    { 400, @"Bad Request" },
+    { 401, @"Unauthorized" },
+    { 402, @"Payment Required" },
+    { 403, @"Forbidden" },
+    { 404, @"Not Found" },
+    { 405, @"Not Allowed" },
+    { 406, @"Not Acceptable" },
+    { 407, @"Registration Required" },
+    { 408, @"Request Timeout " },
+    { 409, @"Conflict" },
+    { 500, @"Internal Server Error" },
+    { 501, @"Not Implemented" },
+    { 502, @"Remote Server Error" },
+    { 503, @"Service Unavailable" },
+    { 504, @"Remote Server Timeout" },
+    { 000, nil }
+};
+
 @implementation SmackXMPPErrorMessagePlugin
 
 - (id)initWithAccount:(SmackXMPPAccount*)account
@@ -31,11 +56,32 @@
     SmackMessage *packet = [[n userInfo] objectForKey:SmackXMPPPacket];
     
     if([[[packet getType] toString] isEqualToString:@"error"])
-        [[NSAlert alertWithMessageText:[NSString stringWithFormat:AILocalizedString(@"XMPP Error From %@","XMPP Error From %@"),[packet getFrom]]
+    {
+        SmackXMPPError *error = [packet getError];
+        if(!error)
+            return; // invalid error packet, the error-info is required
+        NSString *message = [error getMessage];
+        int code = [error getCode];
+        int i;
+        NSString *errordesc = nil;
+        
+        for(i=0;mapping[i].code;i++)
+        {
+            if(mapping[i].code == code)
+            {
+                errordesc = mapping[i].description;
+                break;
+            }
+        }
+        if(!errordesc)
+            errordesc = [[NSNumber numberWithInt:code] stringValue];
+
+        [[NSAlert alertWithMessageText:[NSString stringWithFormat:AILocalizedString(@"XMPP Error %d \"%@\" From %@","XMPP Error %d \"%@\" From %@"), code, errordesc, [packet getFrom]]
                          defaultButton:AILocalizedString(@"OK","OK")
                        alternateButton:nil
                            otherButton:nil
-             informativeTextWithFormat:@"%@",[packet getBody]] runModal];
+             informativeTextWithFormat:@"%@",message?message:AILocalizedString(@"(no message provided)","(no message provided)")] runModal];
+    }
 }
 
 @end
