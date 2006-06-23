22559d63105aa9b266296a8a1f23f2470d9c8d16
kbotc <kbotc@localhost>
2006-06-23 08:22:56 +0000

    I suppose I should check something in. This is an updated window that does some of the snap to other window stuff that will be used later in my code when I have actual other contact lists made. This is probably the last commit that will work properly for the next week. BTW this code is about 50% done.

---
+++ nicholas_peshek/Plugins/Contact List/AISnappingWindow.h
@@ -0,0 +1,15 @@
+//
+//  AISnappingWindow.h
+//  AIUtilities.framework
+//
+//  Created by Nick Peshek on 6/12/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <AIUtilities/AIBorderlessWindow.h>
+
+@interface AISnappingWindow : AIBorderlessWindow {
+	
+}
+
+@end
