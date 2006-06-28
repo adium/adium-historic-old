2b32e3e15adc10f4c9c58c61aefe0545f8a9b1a8
Andreas Monitzer <github@monitzer.com>
2006-06-28 09:24:56 +0000

    Reverted JavaVM preloading.

--- andreas_monitzer/Plugins/Smack Service/SmackCocoaAdapter.m
+++ andreas_monitzer/Plugins/Smack Service/SmackCocoaAdapter.m
@@ -13,6 +13,10 @@
 
 #import <JavaVM/NSJavaVirtualMachine.h>
 
+#define SMACKBRIDGE_JAR @"SmackBridge"
+#define SMACK_JAR @"smack"
+#define SMACKX_JAR @"smackx"
+
 extern CFRunLoopRef CFRunLoopGetMain(void);
 /* java.lang.Object */
 @interface NSObject (JavaObjectAdditions)
@@ -21,6 +25,85 @@ extern CFRunLoopRef CFRunLoopGetMain(void);
 @end
 
 @implementation SmackCocoaAdapter
+
++ (void)initializeJavaVM
+{
+	[NSThread detachNewThreadSelector:@selector(prepareJavaVM)
+							 toTarget:self
+						   withObject:nil];
+}
+
+#pragma mark Java VM Preparation
++ (void)prepareJavaVM
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+	@synchronized(self) {
+		//Only one vm is needed for all accounts
+		static	NSJavaVirtualMachine	*vm = nil;
+		static BOOL		attachedVmToMainRunLoop = NO;
+		BOOL			onMainRunLoop = (CFRunLoopGetCurrent() == CFRunLoopGetMain());
+        
+        NSLog(@"VM Thread = %p, main thread = %p",CFRunLoopGetCurrent(),CFRunLoopGetMain());
+        
+		if (!vm) {
+			NSString	*smackJarPath, *smackxJarPath, *smackBridgeJarPath;
+			NSString	*classPath;
+            
+			smackBridgeJarPath = [[NSBundle bundleForClass:[self class]] pathForResource:SMACKBRIDGE_JAR
+                                                                                  ofType:@"jar"
+                                                                             inDirectory:@"Java"];
+			smackJarPath = [[NSBundle bundleForClass:[self class]] pathForResource:SMACK_JAR
+																			ofType:@"jar"
+																	   inDirectory:@"Java"];
+			smackxJarPath = [[NSBundle bundleForClass:[self class]] pathForResource:SMACKX_JAR
+																			 ofType:@"jar"
+																		inDirectory:@"Java"];
+			
+			classPath = [NSString stringWithFormat:@"%@:%@:%@:%@",
+				[NSJavaVirtualMachine defaultClassPath],
+				smackJarPath, smackxJarPath, smackBridgeJarPath];
+			
+			vm = [[NSJavaVirtualMachine alloc] initWithClassPath:classPath];
+			
+			AILog(@"-[%@ prepareJavaVM]: Java %@ ; Smack %@. Using classPath: %@",
+				  self,
+				  [NSClassFromString(@"java.lang.System") getProperty:@"java.version"],
+				  [NSClassFromString(@"org.jivesoftware.smack.SmackConfiguration") getVersion],
+				  classPath);
+			
+			if (onMainRunLoop) {
+				attachedVmToMainRunLoop = YES;
+			}
+            
+		} else {
+			if  (!attachedVmToMainRunLoop && onMainRunLoop) {
+				[vm attachCurrentThread];
+				attachedVmToMainRunLoop = YES;
+			} else
+                [vm attachCurrentThread];
+		}
+        
+		if (onMainRunLoop &&
+			!NSClassFromString(@"org.jivesoftware.smack.SmackConfiguration")) {
+			NSMutableString	*msg = [NSMutableString string];
+			
+			[msg appendFormat:@"Java version %@ could not load SmackConfiguration\n",[NSClassFromString(@"java.lang.System") getProperty:@"java.version"]];
+            
+			NSRunCriticalAlertPanel(@"Fatal Java error",
+									msg,
+									nil,nil,nil);
+		}
+		
+#ifdef DEBUG_BUILD
+		[[NSNotificationCenter defaultCenter] performSelector:@selector(postNotificationName:object:)
+												   withObject:@"AttachedJavaVM"
+												   withObject:nil];
+#endif
+	}
+    
+	[pool release];
+}
 
 #pragma mark utility functions
 
