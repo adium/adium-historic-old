204c2fc38dfb5d8c765e193f87d188af7b91a67c
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-07-04 20:02:10 +0000

    * Fixed indexing of XML logs -- specifically, they are now properly marked as dirty when changed so they will be indexed.  * Removed the old logging code. No going back!

--- Source/AILoggerPlugin.h
+++ Source/AILoggerPlugin.h
@@ -14,7 +14,6 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#import <Adium/AIHTMLDecoder.h>
 #import <Adium/AIPlugin.h>
 
 #define PATH_LOGS                       @"/Logs"
@@ -25,10 +24,7 @@
 
 #define PREF_KEYPATH_LOGGER_ENABLE		PREF_GROUP_LOGGING @"." KEY_LOGGER_ENABLE
 
-//Uncomment this to enable XML_LOGGING
-#define XML_LOGGING
-
-@class AIAccount, AIChat, AILoggerPreferences, AILoggerAdvancedPreferences;
+@class AIAccount, AIHTMLDecoder, AIChat, AILoggerPreferences;
 
 @interface AILoggerPlugin : AIPlugin {
     AILoggerPreferences                 *preferences;
@@ -37,13 +33,11 @@
     BOOL				observingContent;
     BOOL				logHTML;
 
-#ifdef XML_LOGGING
 	NSMutableDictionary					*activeAppenders;
 	NSMutableDictionary					*activeTimers;
 	
-	AIHTMLDecoder						*HTMLDecoder;
+	AIHTMLDecoder						*xhtmlDecoder;
 	NSDictionary						*statusTranslation;
-#endif
 	
     //Log viewer menu items
     NSMenuItem                          *logViewerMenuItem;
@@ -66,23 +60,19 @@
 	NSLock				*logAccessLock;
     
     //Array of dirty logs / Logs that need re-indexing.  (Locked access)
-    NSMutableArray                      *dirtyLogArray;
+    NSMutableArray		*dirtyLogArray;
     NSLock				*dirtyLogLock;
     
     //Indexing progress
-    int                                 logsToIndex;
-    int                                 logsIndexed;
+    int					logsToIndex;
+    int					logsIndexed;
     
 }
 
 //Paths
 + (NSString *)logBasePath;
 + (NSString *)relativePathForLogWithObject:(NSString *)object onAccount:(AIAccount *)account;
-#ifdef XML_LOGGING
 + (NSString *)fileNameForLogWithObject:(NSString *)object onDate:(NSDate *)date;
-#endif
-+ (NSString *)fileNameForLogWithObject:(NSString *)object onDate:(NSDate *)date plainText:(BOOL)plainText;
-+ (NSString *)fullPathOfLogAtRelativePath:(NSString *)relativePath;
 
 //Log viewer
 - (void)showLogViewerToSelectedContact:(id)sender;
