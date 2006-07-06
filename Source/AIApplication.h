d8d95d4e0e07a6dcf0e316bf07029cd100ef7401
Evan Schoenberg <Evan Schoenberg@localhost>
2006-07-06 21:59:12 +0000

    AIApplication is now used. It is a simple NSApplication subclass which overrides  {{{applicationIconImage}}} so that we can return an image without badges, labels, or animation states. Fixes #4477. Fixes #4498.

---
+++ Source/AIApplication.h
@@ -0,0 +1,14 @@
+//
+//  AIApplication.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 7/6/06.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AIApplication : NSApplication {
+
+}
+
+@end
---
+++ Source/AIApplication.m
@@ -0,0 +1,25 @@
+//
+//  AIApplication.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 7/6/06.
+//
+
+#import "AIApplication.h"
+#import "AIObject.h"
+#import "AIDockController.h"
+
+@implementation AIApplication
+/*!
+ * @brief Intercept applicationIconImage so we can return a base application icon
+ *
+ * The base application icon doesn't have any badges, labels, or animation states.
+ */
+- (NSImage *)applicationIconImage
+{
+	NSImage *applicationIconImage = [[[AIObject sharedAdiumInstance] dockController] baseApplicationIconImage];
+	
+	return (applicationIconImage ? applicationIconImage : [super applicationIconImage]);
+}
+
+@end
