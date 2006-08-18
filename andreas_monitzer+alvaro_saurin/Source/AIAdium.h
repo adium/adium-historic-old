3d6eeb15fb7959c8a2c7850ddaa9992819aafbc7
alvaro <alvaro@localhost>
2006-08-18 11:03:56 +0000

    Added the AIVideoConfController again, using the protocol.

--- andreas_monitzer+alvaro_saurin/Source/AIAdium.h
+++ andreas_monitzer+alvaro_saurin/Source/AIAdium.h
@@ -9,7 +9,7 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program; if not,
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
@@ -22,9 +22,7 @@
 			AIPreferenceController, AIMenuController, AIApplescriptabilityController, AIStatusController,
 			AIContentController, AIToolbarController, AISoundController, AIDockController,
 			AIFileTransferController, AILoginController, AIInterfaceController, AIContactController,
-			AIEmoticonController, AIJavaController;
-
-@class AIVideoConfController;
+			AIEmoticonController, AIJavaController, AIVideoConfController;
 
 @interface AIAdium : NSObject {
     IBOutlet	NSObject <AIMenuController>			*menuController;
@@ -45,11 +43,12 @@
 	NSObject <AIContactAlertsController>*contactAlertsController;
 	NSObject <AIFileTransferController>	*fileTransferController;
     NSObject <AIJavaController>         *javaController;
-    AIVideoConfController               *vcController;
+	NSObject <AIVideoConfController>	*vcController;
 
 	NSObject <AIApplescriptabilityController>	*applescriptabilityController;
 	NSObject <AIDebugController>				*debugController;
 
+	
 	AICoreComponentLoader			*componentLoader;
 	AICorePluginLoader				*pluginLoader;
     
