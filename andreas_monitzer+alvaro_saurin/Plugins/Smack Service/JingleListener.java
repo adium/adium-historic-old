e13e3e984a1764a63491c5ebf8c4b6e3ac30cf28
Andreas Monitzer <github@monitzer.com>
2006-08-20 14:43:14 +0000

    Moved Jingle to its own jar-file and fixed a bug in the phone plugin (the class loader wasn't retained).

--- andreas_monitzer+alvaro_saurin/Plugins/Smack Service/JingleListener.java
+++ andreas_monitzer+alvaro_saurin/Plugins/Smack Service/JingleListener.java
@@ -13,25 +13,39 @@ import org.jivesoftware.smackx.jingle.*;
 import org.jivesoftware.smackx.nat.STUNResolver;
 import com.apple.cocoa.foundation.NSObject;
 
-public class JingleListener implements org.jivesoftware.smackx.jingle.JingleListener.SessionRequest {
-    NSObject delegate;
-    JingleManager manager;
+import java.lang.reflect.*;
+
+public class JingleListener {
+    final NSObject delegate;
+    Object manager;
+    ClassLoader jingleClassLoader;
     
-    public JingleListener(XMPPConnection conn, NSObject delegate) {
-        STUNResolver resolver = new STUNResolver();
-        manager = new JingleManager(conn,resolver);
-        manager.addJingleSessionRequestListener(this);
+    public JingleListener(XMPPConnection conn, final NSObject delegate, ClassLoader jingleClassLoader) throws ClassNotFoundException, InstantiationException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Object resolver = jingleClassLoader.loadClass("org.jivesoftware.smackx.nat.STUNResolver").newInstance();
+        Class jingleManagerClass = jingleClassLoader.loadClass("org.jivesoftware.smackx.jingle.JingleManager");
+        Class sessionRequestListener = jingleClassLoader.loadClass("org.jivesoftware.smackx.jingle.JingleListener$SessionRequest");
+        
+        manager = jingleManagerClass.getConstructor(new Class[] { XMPPConnection.class, jingleClassLoader.loadClass("org.jivesoftware.smackx.nat.TransportResolver") }).newInstance(new Object[] { conn, resolver });
+        
+        jingleManagerClass.getMethod("addJingleSessionRequestListener",new Class[] { sessionRequestListener }).invoke(manager,
+          Proxy.newProxyInstance(jingleClassLoader, new Class[] { sessionRequestListener }, new InvocationHandler() {
+              public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                  if(method.getName().equals("sessionRequested")) {
+                      delegate.takeValueForKey(args[0], "sessionRequested");
+                  }
+                  return null;
+              }
+          }));
+        
+        this.jingleClassLoader = jingleClassLoader;
+        this.delegate = delegate;
     }
     
-    public static JingleListener getInstance(XMPPConnection conn, NSObject delegate) {
-        return new JingleListener(conn,delegate);
+    public static JingleListener getInstance(XMPPConnection conn, NSObject delegate, ClassLoader jingleClassLoader) throws ClassNotFoundException, InstantiationException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        return new JingleListener(conn,delegate,jingleClassLoader);
     }
     
-    public void sessionRequested(JingleSessionRequest request) {
-        delegate.takeValueForKey(request,"jingleSessionRequest");
-    }
-    
-    public JingleManager getManager() {
+    public Object getManager() {
         return manager;
     }
 }
