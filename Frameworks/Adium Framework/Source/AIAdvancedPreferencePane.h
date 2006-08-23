81691929e4c942e74b718fabec7cc06525509ff1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-08-23 05:58:22 +0000

    {{{AIAdvancedPreferencePane}}} added, a subclass of {{{AIPreferencePane}}} for advanced 'uns

---
+++ Frameworks/Adium Framework/Source/AIAdvancedPreferencePane.h
@@ -0,0 +1,15 @@
+//
+//  AIAdvancedPreferencePane.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 8/23/06.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIPreferencePane.h>
+
+@interface AIAdvancedPreferencePane : AIPreferencePane {
+
+}
+
+@end
