988bce5ab70aa964caa3e6564e3cc25db0a5db84
Evan Schoenberg <Evan Schoenberg@localhost>
2006-08-23 05:58:22 +0000

    {{{AIAdvancedPreferencePane}}} added, a subclass of {{{AIPreferencePane}}} for advanced 'uns

---
+++ Frameworks/Adium Framework/Source/AIAdvancedPreferencePane.m
@@ -0,0 +1,18 @@
+//
+//  AIAdvancedPreferencePane.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 8/23/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIAdvancedPreferencePane.h"
+
+
+@implementation AIAdvancedPreferencePane
+//Resizable
+- (BOOL)resizable
+{
+	return YES;
+}
+@end
