b94c4282ebf6cf23e1ba4c461630d981d79ddd24
Evan Schoenberg <Evan Schoenberg@localhost>
2006-09-10 16:53:09 +0000

    Added a content filter which removes NULs from all incoming and outgoing messages. Fixes #5093.

---
+++ Source/AINulRemovalPlugin.h
@@ -0,0 +1,17 @@
+//
+//  AINulRemovalPlugin.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/10/06.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIPlugin.h>
+
+@protocol AIContentFilter;
+
+@interface AINulRemovalPlugin : AIPlugin <AIContentFilter> {
+
+}
+
+@end
