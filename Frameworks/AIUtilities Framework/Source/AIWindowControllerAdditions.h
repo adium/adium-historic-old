49128e2d1e81d9b719bfc73c82888e17c49a56e9
David Smith <David Smith@localhost>
2006-09-16 18:36:29 +0000

    .m files need a .h to go with them...

---
+++ Frameworks/AIUtilities Framework/Source/AIWindowControllerAdditions.h
@@ -0,0 +1,16 @@
+//
+//  AIWindowControllerAdditions.h
+//  AIUtilities.framework
+//
+//  Created by David Smith on 9/16/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSWindowController (AIWindowControllerAdditions) 
+
+- (BOOL) canCustomizeToolbar;
+
+@end
