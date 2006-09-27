e1056688a7142223fc2c42d5370d4404247c3e2a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-09-27 21:25:44 +0000

    oops, forgot these two debugging files, which I added to let us figure out where nil is being passed to NSScanner. Debug builds will now crash with an assertion in that situation instead of just logging to console

---
+++ Source/AIScannerDebug.m
@@ -0,0 +1,28 @@
+//
+//  AIScannerDebug.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/27/06.
+//
+
+#import "AIScannerDebug.h"
+
+@implementation AIScannerDebug
+
+#ifdef DEBUG_BUILD
+
++ (void)load
+{
+	[self poseAsClass:[NSScanner class]];
+}
+
+- (id)initWithString:(NSString *)aString
+{
+	NSParameterAssert(aString != nil);
+	
+	return [super initWithString:aString];
+}
+
+#endif
+
+@end
