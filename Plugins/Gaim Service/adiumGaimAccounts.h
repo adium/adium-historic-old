e493b32c2fcdbc46cb580ae2fd02b870c1ae50b3
David Smith <catfish.man@gmail.com>
2006-12-04 21:09:48 +0000

    merging libgaim changes from trunk to libgaim-oscar branch

---
+++ Plugins/Gaim Service/adiumGaimAccounts.h
@@ -0,0 +1,10 @@
+//
+//  adiumGaimAccounts.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 12/3/06.
+
+
+#import "SLGaimCocoaAdapter.h"
+
+GaimAccountUiOps *adium_gaim_accounts_get_ui_ops(void);
