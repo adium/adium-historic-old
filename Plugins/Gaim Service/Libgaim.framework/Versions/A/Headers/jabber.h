8a6cc91acf97a19f6dae9e9d5c2f92cf4df3c1da
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2006-12-26 03:32:00 +0000

    Fixed initial retrieval of the Jabber contact list for non-gtalk accounts. Fixes #6037

--- Plugins/Gaim Service/Libgaim.framework/Versions/A/Headers/jabber.h
+++ Plugins/Gaim Service/Libgaim.framework/Versions/A/Headers/jabber.h
@@ -134,8 +134,6 @@ typedef struct _JabberStream
 	char *gmail_last_time;
 	char *gmail_last_tid;
 
-	gchar* last_mail_alert_time;
-
 	/* OK, this stays at the end of the struct, so plugins can depend
 	 * on the rest of the stuff being in the right place
 	 */
