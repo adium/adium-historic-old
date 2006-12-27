f6980e20db31477c073dba92d73ec72a95352c3a
Colin Barrett <colin@springsandstruts.com>
2006-12-27 09:05:47 +0000

    Adding set/get methods for the background color of the scrollbar view. Also, some debugging code. Also, preparing to pull trunk in.

--- Source/AIListScroller.m
+++ Source/AIListScroller.m
@@ -9,6 +9,11 @@
 #import "AIListScroller.h"
 
 
+@interface NSScroller(lolihaetgcc)
+- (NSRect)_drawingRectForPart:(NSScrollerPart)aPart;
+@end
+
+
 @implementation AIListScroller
 
 - (id)init
@@ -16,25 +21,61 @@
 	if ((self = [super init])) {
 		//Let the super know we really don't want those arrows there, ever.
 		[super setArrowsPosition:NSScrollerArrowsNone];
+		
+		backgroundColor = [[NSColor whiteColor] retain];
 	}
 	
 	return self;
 }
-/*
-- (NSRect)rectForPart:(NSScrollerPart)aPart
+
+- (void)dealloc
 {
-	NSRect superRect = [super rectForPart:aPart];
+	[backgroundColor release];
+	
+	[super dealloc];
+}
+
+- (NSColor *)backgroundColor
+{
+	return backgroundColor;
+}
+
+- (void)setBackgroundColor:(NSColor *)newColor
+{
+
+	if (backgroundColor != newColor) {
+		[backgroundColor release];
+		backgroundColor = [newColor retain];
+	}
+	
+	[self setNeedsDisplay:YES];
+
+}
+
+- (NSRect)_drawingRectForPart:(NSScrollerPart)aPart
+{
+	NSRect superRect = [super _drawingRectForPart:aPart];
+	
+	NSLog(@"part %d, rect %@", aPart, NSStringFromRect(superRect));
+	
+	
 	switch (aPart) {
+		//case NSScrollerKnob:
+		case NSScrollerKnobSlot:
+			return [self bounds];
+			
+		case NSScrollerNoPart:
 		case NSScrollerDecrementLine:
 		case NSScrollerIncrementLine:
 			return NSZeroRect;
-		case NSScrollerKnobSlot:
-			return [self frame];
+			
 		default:
 			return superRect;
 	}
+	
+	return superRect;
 }
-*/
+
 - (void)setArrowsPosition:(NSScrollArrowPosition)location
 {
 	//stub. we don't want this to change.
@@ -48,13 +89,13 @@
 
 - (BOOL)isOpqaue
 {
-	return NO;
+	return [backgroundColor alphaComponent] == 1.0;
 }
 
 - (void)drawKnobSlotInRect:(NSRect)rect highlight:(BOOL)highlight
 {
 	//We hates knob slots
-	[[NSColor clearColor] set];
+	[backgroundColor set];
 	NSRectFill(rect);
 }
 
@@ -62,7 +103,7 @@
 - (void)drawArrow:(NSScrollerArrow)arrow highlightPart:(int)flag
 {
 	//We also hates arrows
-	[[NSColor redColor] set];
+	[backgroundColor set];
 	NSRectFill([self rectForPart:(arrow == NSScrollerIncrementArrow ? NSScrollerIncrementLine : NSScrollerDecrementLine)]);
 }
 
@@ -70,7 +111,14 @@
 // This method, while in the documentation and the header, never seems to get called. At least in 10.4. Instead, drawArrow:highlightPart: (an undocumented method) is called. So in case this method was used in previous versions of the OS, I'm forwarding calls from this (seemingly useless) method to the one that actually does things.
 - (void)drawArrow:(NSScrollerArrow)arrow highlight:(BOOL)highlight
 {
+	NSLog(@"bool highlight is called");
 	[self drawArrow:arrow highlightPart:highlight ? 0 : -1];
+}
+
+//This method also doesn't seem to get called. Forwarding messages to drawingRectForPart, which does.
+- (NSRect)rectForPart:(NSScrollerPart)partCode
+{
+	return [self _drawingRectForPart:partCode];
 }
 
 @end
