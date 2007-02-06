0f9c2b1974991790abc727a05cfe85367573b1fc
Evan Schoenberg <Evan Schoenberg@localhost>
2007-02-06 23:09:35 +0000

    merged [18859]: Learning from and improving on the joscar debug window, added live filtering to the Adium Debug window

--- Source/ESDebugWindowController.h
+++ Source/ESDebugWindowController.h
@@ -22,8 +22,11 @@
 	IBOutlet	NSTextView			*textView_debug;
 	IBOutlet	AIAutoScrollView	*scrollView_debug;
 	NSMutableString					*mutableDebugString;
+	NSMutableArray					*fullDebugLogArray;
 	NSMutableParagraphStyle			*debugParagraphStyle;
 	IBOutlet	NSButton			*checkBox_logWriting;
+	
+	NSString						*filter;
 }
 
 #ifdef DEBUG_BUILD
@@ -33,6 +36,8 @@
 + (void)addedDebugMessage:(NSString *)message;
 - (IBAction)toggleLogWriting:(id)sender;
 - (IBAction)clearLog:(id)sender;
+
+- (void)setFilter:(NSString *)inFilter;
 #endif
 
 
