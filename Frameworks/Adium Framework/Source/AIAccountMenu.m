78c30b3cb32d4920e56448e4dc9b4a6b1570b37f
David Smith <David Smith@localhost>
2007-02-14 23:35:52 +0000

    Merging [18991]

--- Frameworks/Adium Framework/Source/AIAccountMenu.m
+++ Frameworks/Adium Framework/Source/AIAccountMenu.m
@@ -312,7 +312,7 @@
 - (NSSet *)updateListObject:(AIListObject *)inObject keys:(NSSet *)inModifiedKeys silent:(BOOL)silent
 {
 	if ([inObject isKindOfClass:[AIAccount class]]) {
-		NSMenuItem	*menuItem = [[self menuItemForAccount:(AIAccount *)inObject] retain];
+		NSMenuItem	*menuItem = [self menuItemForAccount:(AIAccount *)inObject];
 		BOOL		rebuilt = NO;
 		
 		if ([inModifiedKeys containsObject:@"Enabled"]) {
