a30f36369af87ddb69d26d85cd8212000cd95bff
Evan Schoenberg <Evan Schoenberg@localhost>
2007-04-08 06:05:10 +0000

    Patch from Michael Dippery to add support for a subset of the XMPP links specified in [http://www.xmpp.org/extensions/xep-0147.html XEP-0147]. Good work :) Closes #6541.

--- Frameworks/AIUtilities Framework/Source/AIURLAdditions.h
+++ Frameworks/AIUtilities Framework/Source/AIURLAdditions.h
@@ -6,11 +6,29 @@
 //  Copyright (c) 2004-2005 The Adium Team. All rights reserved.
 //
 
+/*!
+ * Provides some additional functionality when working with \c NSURL objects.
+ */
 @interface NSURL (AIURLAdditions)
 
+/**
+ * @brief Gets the length of the URL.
+ *
+ * @return The length (number of characters) of the URL.
+ */
 - (unsigned int)length;
 
-//Search is case sensitive, and you're responsible for removing any percent escapes (and +'s too)
+/*!
+ * @brief Returns the argument for the specified key in the query string component of
+ * the URL.
+ *
+ * The search is case-sensitive, and the caller is responsible for removing any
+ * percent escapes, as well as "+" escapes, too.
+ *
+ * @param key The key whose value should be located and returned.
+ * @return The argument for the specified key, or \c nil if the key could not
+ *   be found in the query string.
+ */
 - (NSString *)queryArgumentForKey:(NSString *)key;
 
 @end
