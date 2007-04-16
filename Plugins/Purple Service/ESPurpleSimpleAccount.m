a3bd14e5b1da36686150d754398ec1a7dea22e21
Evan Schoenberg <Evan Schoenberg@localhost>
2007-04-16 12:30:09 +0000

    Added SIP/SIMPLE proxy, authorization user, authorization domain, and connect port settings using the new bindings-based AIAccountViewController.  Look how little glue code!
    Fixes #6741

--- Plugins/Purple Service/ESPurpleSimpleAccount.m
+++ Plugins/Purple Service/ESPurpleSimpleAccount.m
@@ -18,11 +18,18 @@
 {
 	[super configurePurpleAccount];
 	
-	BOOL useUDP = [[self preferenceForKey:KEY_SIMPLE_USE_UDP group:GROUP_ACCOUNT_STATUS] boolValue];
-	purple_account_set_bool(account, "udp", useUDP);
+	purple_account_set_bool(account, "udp", [[self preferenceForKey:KEY_SIMPLE_USE_UDP group:GROUP_ACCOUNT_STATUS] boolValue]);
 	
-	BOOL publishStatus = [[self preferenceForKey:KEY_SIMPLE_PUBLISH_STATUS group:GROUP_ACCOUNT_STATUS] boolValue];
-	purple_account_set_bool(account, "dopublish", publishStatus);
+	purple_account_set_bool(account, "dopublish", [[self preferenceForKey:KEY_SIMPLE_PUBLISH_STATUS group:GROUP_ACCOUNT_STATUS] boolValue]);
+	
+	purple_account_set_bool(account, "useproxy", [[self preferenceForKey:KEY_SIMPLE_USE_SIP_PROXY
+																   group:GROUP_ACCOUNT_STATUS] boolValue]);
+	purple_account_set_string(account, "proxy", [[self preferenceForKey:KEY_SIMPLE_SIP_PROXY
+																  group:GROUP_ACCOUNT_STATUS] UTF8String]);
+	purple_account_set_string(account, "authuser", [[self preferenceForKey:KEY_SIMPLE_AUTH_USER
+																	 group:GROUP_ACCOUNT_STATUS] UTF8String]);
+	purple_account_set_string(account, "authdomain", [[self preferenceForKey:KEY_SIMPLE_AUTH_DOMAIN
+																	   group:GROUP_ACCOUNT_STATUS] UTF8String]);
 }
 
 - (const char *)purpleAccountName
--- Plugins/Purple Service/ESPurpleSimpleAccountViewController.m
+++ Plugins/Purple Service/ESPurpleSimpleAccountViewController.m
@@ -10,32 +10,45 @@
 #import "ESPurpleSimpleAccount.h"
 
 @implementation ESPurpleSimpleAccountViewController
++ (void)initialize {
+	if (self == [ESPurpleSimpleAccountViewController class]) {
+		NSArray *bindings = [NSArray arrayWithObjects:
+			@"publishStatus",
+			@"useUDP",
+			@"useSIPProxy",
+			@"sipProxy",	
+			@"authUser",		
+			@"authDomain",
+			nil];
+		
+		NSEnumerator *enumerator = [bindings objectEnumerator];
+		NSString	 *binding;
+		while ((binding = [enumerator nextObject])) {
+			[self exposeBinding:binding];
+
+			//Notify for all our exposed bindings when the account changes
+			[self setKeys:[NSArray arrayWithObject:@"account"] triggerChangeNotificationsForDependentKey:binding];
+		}
+	}
+}
+
 - (NSString *)nibName{
     return @"ESPurpleSimpleAccountView";
 }
 
-//Configure controls
-- (void)configureForAccount:(AIAccount *)inAccount
+- (NSDictionary *)keyToKeyDict
 {
-    [super configureForAccount:inAccount];
-
-	[checkBox_publishStatus setState:[[account preferenceForKey:KEY_SIMPLE_PUBLISH_STATUS 
-															  group:GROUP_ACCOUNT_STATUS] boolValue]];
-	[checkBox_useUDP setState:[[account preferenceForKey:KEY_SIMPLE_USE_UDP 
-												   group:GROUP_ACCOUNT_STATUS] boolValue]];
-
-}
-
-//Save controls
-- (void)saveConfiguration
-{
-	[account setPreference:[NSNumber numberWithBool:[checkBox_publishStatus state]] 
-					forKey:KEY_SIMPLE_PUBLISH_STATUS group:GROUP_ACCOUNT_STATUS];
-
-	[account setPreference:[NSNumber numberWithBool:[checkBox_useUDP state]] 
-					forKey:KEY_SIMPLE_USE_UDP group:GROUP_ACCOUNT_STATUS];
-
-	[super saveConfiguration];
+	NSMutableDictionary *keyToKeyDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+		KEY_SIMPLE_PUBLISH_STATUS, @"publishStatus",
+		KEY_SIMPLE_USE_UDP, @"useUDP",
+		KEY_SIMPLE_USE_SIP_PROXY, @"useSIPProxy",
+		KEY_SIMPLE_SIP_PROXY, @"sipProxy",
+		KEY_SIMPLE_AUTH_USER, @"authUser",
+		KEY_SIMPLE_AUTH_DOMAIN, @"authDomain",
+		nil];
+	[keyToKeyDict addEntriesFromDictionary:[super keyToKeyDict]];
+	
+	return keyToKeyDict;
 }
 
 @end
