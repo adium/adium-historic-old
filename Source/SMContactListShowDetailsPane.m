ce638aec88b955c49bc9324e2c262081f54bc7f8
Evan Schoenberg <Evan Schoenberg@localhost>
2007-04-24 20:03:39 +0000

    Fixed lack of localization of the OK/Cancel/Alert: text in the event editing sheet

--- Source/SMContactListShowDetailsPane.m
+++ Source/SMContactListShowDetailsPane.m
@@ -36,6 +36,17 @@
 }
 
 /*!
+* @brief View did load
+ */
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	
+	[label_secondsToShow setLocalizedString:AILocalizedString(@"Seconds To Show:", "Label before a slider which sets the number of seconds to show the contact list when an action is triggerred")];
+	[label_note setLocalizedString:AILocalizedString(@"Note: This behavior is only available if the contact list is set to hide.", "Explanation of when the 'show contact list' action is available for use")];
+}
+
+/*!
 * @brief Load the state of our controls
  */
 - (void)configureForActionDetails:(NSDictionary *)inDetails listObject:(AIListObject *)inObject
