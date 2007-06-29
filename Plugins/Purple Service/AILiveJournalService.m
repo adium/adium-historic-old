e01b33c04f3f875fb19b5950884f72a812464c9a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-06-29 15:46:55 +0000

    Show the correct placeholder for Live Journal accounts. Fixes #7209

--- Plugins/Purple Service/AILiveJournalService.m
+++ Plugins/Purple Service/AILiveJournalService.m
@@ -64,7 +64,7 @@
  */
 - (NSString *)UIDPlaceholder
 {
-	return AILocalizedString(@"username@livejournal.com","Sample name and server for new livejournal accounts");
+	return AILocalizedString(@"username","Sample name and server for new livejournal accounts");
 }
 
 - (NSString *)userNameLabel{
