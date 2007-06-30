a624f19f5dd59aec64f49aece9ee6e2606d0b853
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-06-30 13:55:57 +0000

    Clear the undo stack when an AIMessageEntryTextView deallocates

--- Frameworks/Adium Framework/Source/AIMessageEntryTextView.m
+++ Frameworks/Adium Framework/Source/AIMessageEntryTextView.m
@@ -138,7 +138,9 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 	[[adium preferenceController] unregisterPreferenceObserver:self];
 	[[[AIObject sharedAdiumInstance] notificationCenter] removeObserver:self];
-	
+
+	[[self undoManager] removeAllActions];
+
     [chat release];
     [associatedView release];
     [historyArray release]; historyArray = nil;
