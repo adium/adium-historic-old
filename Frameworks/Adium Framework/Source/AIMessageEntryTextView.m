cbbb80feb1b685211ce71eef802bf57f96269548
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-07-02 20:25:43 +0000

    Fixed drag & drop of text clippings to properly insert their RTF contents. Improved determination of whether to accept a drop as text or an image, fixing a problem when doing a drag & drop from Microsoft Word and other applications which offer to give the text as an image. Fixes #6929.
    [18705] is the revision responsible for causes #6929, though that revision fixed other problems in the process.

--- Frameworks/Adium Framework/Source/AIMessageEntryTextView.m
+++ Frameworks/Adium Framework/Source/AIMessageEntryTextView.m
@@ -982,6 +982,8 @@
 	NSString 		*type = [pasteboard availableTypeFromArray:FILES_AND_IMAGES_TYPES];
 	NSString		*superclassType = [pasteboard availableTypeFromArray:PASS_TO_SUPERCLASS_DRAG_TYPE_ARRAY];
 	
+	
+	
 	if (!type || superclassType) {
 		[super concludeDragOperation:sender];
 	}
@@ -1010,14 +1012,16 @@
 - (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
 {
 	NSPasteboard	*pasteboard = [sender draggingPasteboard];
-	NSString 		*type = [pasteboard availableTypeFromArray:FILES_AND_IMAGES_TYPES];
-	//NSString		*superclassType = [pasteboard availableTypeFromArray:PASS_TO_SUPERCLASS_DRAG_TYPE_ARRAY]; Unused after Drag-Image fix.
+	BOOL			success = NO;
 
-	BOOL	success = NO;
-	if (type) {
+	NSString *myType = [[pasteboard types] firstObjectCommonWithArray:FILES_AND_IMAGES_TYPES];
+	NSString *superclassType = [[pasteboard types] firstObjectCommonWithArray:PASS_TO_SUPERCLASS_DRAG_TYPE_ARRAY];
+	
+	if (myType &&
+		(!superclassType || ([[pasteboard types] indexOfObject:myType] < [[pasteboard types] indexOfObject:superclassType]))) {
 		[self addAttachmentsFromPasteboard:pasteboard];
-
-		success = YES;
+		
+		success = YES;		
 	} else {
 		success = [super performDragOperation:sender];
 		
@@ -1064,15 +1068,38 @@
  */
 - (void)addAttachmentOfPath:(NSString *)inPath
 {
-	AITextAttachmentExtension   *attachment = [[AITextAttachmentExtension alloc] init];
-	[attachment setPath:inPath];
-	[attachment setString:[inPath lastPathComponent]];
-	[attachment setShouldSaveImageForLogging:YES];
+	if ([[inPath pathExtension] caseInsensitiveCompare:@"textClipping"] == NSOrderedSame) {
+		inPath = [inPath stringByAppendingString:@"/..namedfork/rsrc"];
 
-	//Insert an attributed string into the text at the current insertion point
-	[self insertText:[self attributedStringWithTextAttachmentExtension:attachment]];
-	
-	[attachment release];
+		NSData *data = [NSData dataWithContentsOfFile:inPath];
+		if (data) {
+			data = [data subdataWithRange:NSMakeRange(260, [data length] - 260)];
+			
+			NSAttributedString *clipping = [[[NSAttributedString alloc] initWithRTF:data documentAttributes:nil] autorelease];
+			if (clipping) {
+				NSDictionary	*attributes = [[self typingAttributes] copy];
+				
+				[self insertText:clipping];
+
+				if (attributes) {
+					[self setTypingAttributes:attributes];
+				}
+				
+				[attributes release];
+			}
+		}
+
+	} else {
+		AITextAttachmentExtension   *attachment = [[AITextAttachmentExtension alloc] init];
+		[attachment setPath:inPath];
+		[attachment setString:[inPath lastPathComponent]];
+		[attachment setShouldSaveImageForLogging:YES];
+		
+		//Insert an attributed string into the text at the current insertion point
+		[self insertText:[self attributedStringWithTextAttachmentExtension:attachment]];
+		
+		[attachment release];
+	}
 }
 
 /*!
