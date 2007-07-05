11acc8f8107bbcf7217a8dffc6812e8298959ad1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-07-05 19:19:37 +0000

    Merged [20116]: Sparkle.framework [87], which will allow us to have multiple items in the appcast with differing system requirements (Adium will utilize the latest version which the current operating system can support)

--- Frameworks/Sparkle.framework/Versions/A/Headers/SUUtilities.h
+++ Frameworks/Sparkle.framework/Versions/A/Headers/SUUtilities.h
@@ -14,6 +14,7 @@ NSString *SUHostAppName();
 NSString *SUHostAppDisplayName();
 NSString *SUHostAppVersion();
 NSString *SUHostAppVersionString();
+NSString *SUCurrentSystemVersionString();
 
 NSComparisonResult SUStandardVersionComparison(NSString * versionA, NSString * versionB);
 
