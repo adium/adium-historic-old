b6207f5fa49ef21e1d008b1f3e4e54c3bb54a729
Evan Schoenberg <Evan Schoenberg@localhost>
2007-07-10 00:46:54 +0000

    Removed AIGradientCell; AIAlternatingRowOutlineView and AIAlternatingRowTableView handle gradient selection correctly, and AIGradientCell just messes things up.  Fixes #7272

--- Plugins/Contact List/Preferences/AIListThemePreviewCell.h
+++ Plugins/Contact List/Preferences/AIListThemePreviewCell.h
@@ -14,9 +14,9 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#import <AIUtilities/AIGradientCell.h>
+#import <AppKit/NSCell.h>
 
-@interface AIListThemePreviewCell : AIGradientCell {
+@interface AIListThemePreviewCell : NSCell {
 	NSArray			*colorKeyArray;
 	NSDictionary	*themeDict;
 }
