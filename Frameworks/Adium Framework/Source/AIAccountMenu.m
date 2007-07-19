5e318b3958e6919a512146ac87baf05151880e33
Andreas Monitzer <github@monitzer.com>
2007-07-19 22:49:28 +0000

    An NSPopUpButton uses font size 13 for itself and font size 14 for the menu items in the menu it displays on click. Since this can't be reproduced using attributed strings in the menu items (wtf? AppKit bug), I simply set all menu items to be attributed strings. I hope that this doesn't disturb someone, since the text on the popup buttons are now slightly larger. Fixes #7364

--- Frameworks/Adium Framework/Source/AIAccountMenu.m
+++ Frameworks/Adium Framework/Source/AIAccountMenu.m
@@ -298,6 +298,13 @@
 		[menuItem setTitle:[self _titleForAccount:account]];
 		[menuItem setImage:[self imageForListObject:account]];
 		[account accountMenuDidUpdate:menuItem];
+		if(![menuItem attributedTitle]) {
+			NSAttributedString *title = [[NSAttributedString alloc] initWithString:[menuItem title] attributes:[NSDictionary dictionaryWithObjectsAndKeys:
+				[NSFont menuFontOfSize:14.0f], NSFontAttributeName, // for some reason, the default font size seems to be slightly smaller than the real font, seems to be an AppKit bug
+				nil]];
+			[menuItem setAttributedTitle:title];
+			[title release];
+		}
 		[[menuItem menu] setMenuChangedMessagesEnabled:YES];
 	}
 }
