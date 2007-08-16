038ad573bd0223585f4d3afe0707575120953400
Evan Schoenberg <Evan Schoenberg@localhost>
2007-08-16 00:13:54 +0000

    * Removed the fading contact list transition - it's fairly slow and not particularly attractive. Look at all that code that gets to go the way of the dodo!  * Replaced it with a preference "Animate Changes", on by default, in the advanced contact list preferences.  This preference controls whether sorting changes are animated.

--- Source/AIAnimatingListOutlineView.h
+++ Source/AIAnimatingListOutlineView.h
@@ -9,11 +9,16 @@
 #import <Adium/AIListOutlineView.h>
 
 @interface AIAnimatingListOutlineView : AIListOutlineView {
+	BOOL	enableAnimation;
+	
 	NSMutableDictionary *allAnimatingItemsDict;
 	int animations;
 	NSSize animationHedgeFactor;
 	
 	BOOL disableExpansionAnimation;
 }
+
+- (void)setEnableAnimation:(BOOL)shouldEnable;
+- (BOOL)enableAnimation;
 
 @end
