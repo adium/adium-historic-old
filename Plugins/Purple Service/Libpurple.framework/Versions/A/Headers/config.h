7eb9c0ca5d80b8dc3000e3cb3eca8290eac64845
Evan Schoenberg <Evan Schoenberg@localhost>
2007-08-26 20:03:12 +0000

    Libpurple.framework [465], which has the msn debug logging for the ack_cmd() crash

--- Plugins/Purple Service/Libpurple.framework/Versions/A/Headers/config.h
+++ Plugins/Purple Service/Libpurple.framework/Versions/A/Headers/config.h
@@ -2,10 +2,10 @@
 #include <AvailabilityMacros.h>
 
 /* Version number of package */
-#define VERSION "2.1.0"
+#define VERSION "2.1.1"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libpurple 2.1.0"
+#define PACKAGE_STRING "libpurple 2.1.1"
 
 #define DATADIR ""
 
