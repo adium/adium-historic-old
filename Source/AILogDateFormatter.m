04bdf269d1370dbd9730545793a2b9129a5d5285
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-02 06:37:18 +0000

    Fixed AILogDateFormatter for 10.5.  NSThisDayDesignations and NSPriorDayDesignations might return nil on 10.5.  It's possible I consider this a very poor implementation of 'deprecated'.

--- Source/AILogDateFormatter.m
+++ Source/AILogDateFormatter.m
@@ -7,48 +7,63 @@
 
 #import "AILogDateFormatter.h"
 #import "AICalendarDate.h"
+#import <AIUtilities/AIApplicationAdditions.h>
 
 @implementation AILogDateFormatter
 
+static BOOL isOnLeopardOrBetter = NO;
+
++ (void)load
+{
+	if (self == [AILogDateFormatter class]) {
+		isOnLeopardOrBetter = [[NSApplication sharedApplication] isOnLeopardOrBetter];
+	}
+}
+
 - (NSString *)stringForObjectValue:(NSDate *)date
 {
+	NSString *returnValue = nil;
+
 	if ([self respondsToSelector:@selector(timeStyle)] && [date isKindOfClass:[AICalendarDate class]]) {
 		int today = [[NSCalendarDate calendarDate] dayOfCommonEra];
 		int dateDay = [(AICalendarDate *)date dayOfCommonEra];
 		NSDateFormatterStyle timeStyle = [self timeStyle];
 
 		if ((dateDay == today) || (dateDay == (today - 1))) {
-			NSString			*dayString = [[[[NSUserDefaults standardUserDefaults] stringArrayForKey:((dateDay == today) ? NSThisDayDesignations : NSPriorDayDesignations)] objectAtIndex:0] capitalizedString];
-			NSMutableString		*returnValue = [dayString mutableCopy];
+			NSString			*dayString = (isOnLeopardOrBetter ?
+							((dateDay == today) ? AILocalizedString(@"Today", "Day designation for the current day") : AILocalizedString(@"Yesterday", "Day designation for the previous day")) :
+							[[[[NSUserDefaults standardUserDefaults] stringArrayForKey:((dateDay == today) ? NSThisDayDesignations : NSPriorDayDesignations)] objectAtIndex:0] capitalizedString]);
 
 			if ((timeStyle != NSDateFormatterNoStyle) &&
 				([(AICalendarDate *)date granularity] == AISecondGranularity)) {
 				//Supposed to show time, and the date has sufficient granularity to show it
 				NSDateFormatterStyle dateStyle = [self dateStyle];
+				NSMutableString *mutableString = [dayString mutableCopy];
+
 				[self setDateStyle:NSDateFormatterNoStyle];
-				[returnValue appendString:@" "];
-				[returnValue appendString:[super stringForObjectValue:date]];
+				[mutableString appendString:@" "];
+				[mutableString appendString:[super stringForObjectValue:date]];
 				[self setDateStyle:dateStyle];
+	
+				returnValue = [mutableString autorelease];
 			}
-			
-			return [returnValue autorelease];
 
 		} else {
 			if ((timeStyle != NSDateFormatterNoStyle) &&
 				([(AICalendarDate *)date granularity] == AIDayGranularity)) {
 				//Currently supposed to show time, but the date does not have that level of granularity
-				NSString	*returnValue;
 				
 				[self setTimeStyle:NSDateFormatterNoStyle];
 				returnValue = [super stringForObjectValue:date];
 				[self setTimeStyle:timeStyle];
-				
-				return returnValue;
 			}
 		}
 	}
 
-	return [super stringForObjectValue:date];
+	if (![returnValue length]) returnValue = [super stringForObjectValue:date];
+	if (![returnValue length]) returnValue = [date description];
+
+	return returnValue;
 }
 
 @end
