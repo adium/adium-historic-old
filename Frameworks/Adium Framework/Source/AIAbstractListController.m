61c3cfdcbe81a92aa7b4b9bb5f2d0f422348f330
Zachary West <zacw@adiumx.com>
2007-09-11 18:22:33 +0000

    Remove the spacing and gradient around group headers. Refs #7870.

--- Frameworks/Adium Framework/Source/AIAbstractListController.m
+++ Frameworks/Adium Framework/Source/AIAbstractListController.m
@@ -328,7 +328,7 @@
 
 	//Standard special cases.  Add an extra line of padding to the bottom of the standard window.
 	if (windowStyle == AIContactListWindowStyleStandard) {
-		[contactListView setDesiredHeightPadding:3];   //1 pixel border at the top and bottom + extra line at bottom
+		[contactListView setDesiredHeightPadding:2];   //1 pixel border at the top and bottom + extra line at bottom
 	} else {
 		[contactListView setDesiredHeightPadding:2];   //Accounts for the 1 pixel border at the top and bottom
 	}
@@ -361,10 +361,10 @@
 		[groupCell setTopSpacing:[[prefDict objectForKey:KEY_LIST_LAYOUT_GROUP_TOP_SPACING] intValue]];
 	} else if (windowStyle == AIContactListWindowStyleStandard) {
 		//Force some spacing and draw a bordeer around our groups (This doesn't look good in borderless)
-		[groupCell setTopSpacing:1];
-		[groupCell setLeftSpacing:1];
-		[groupCell setRightSpacing:1];
-		[groupCell setDrawsGradientEdges:YES];
+		//[groupCell setTopSpacing:1];
+		//[groupCell setLeftSpacing:1];
+		//[groupCell setRightSpacing:1];
+		//[groupCell setDrawsGradientEdges:YES];
 	}
 	
 	//Disable square row highlighting for bubble lists - the bubble cells handle this on their own
