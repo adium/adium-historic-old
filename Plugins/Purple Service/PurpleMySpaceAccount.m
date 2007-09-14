0df7c622af010b3c71ec6863e2cb2754ea8e1ae1
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-14 20:17:56 +0000

    Merged [20921], [20922], [20969], [20971]: Added MySpace service and account for the new libpurple 2.2.0 myspace support. No reason to keep this out of the 1.1.x series of releases.

---
+++ Plugins/Purple Service/PurpleMySpaceAccount.m
@@ -0,0 +1,56 @@
+//
+//  PurpleMyspaceAccount.m
+//  Adium
+//
+//  Created by Evan Schoenberg on 9/11/07.
+//
+
+#import "PurpleMyspaceAccount.h"
+#import <Adium/AIHTMLDecoder.h>
+
+@implementation PurpleMyspaceAccount
+
+- (const char*)protocolPlugin
+{
+    return "prpl-myspace";
+}
+
+- (NSString *)connectionStringForStep:(int)step
+{
+	switch (step) {
+	case 0:
+		return AILocalizedString(@"Connecting",nil);
+		break;
+	case 1:
+		return AILocalizedString(@"Reading challenge",nil);
+		break;
+	case 2:
+		return AILocalizedString(@"Logging in","Connection step");
+		break;			
+	case 3:
+		return AILocalizedString(@"Connected","Connection step");
+		break;
+	}
+
+	return nil;
+}
+	
+- (NSString *)encodedAttributedString:(NSAttributedString *)inAttributedString forListObject:(AIListObject *)inListObject
+{
+	return [AIHTMLDecoder encodeHTML:inAttributedString
+							 headers:YES
+							fontTags:YES
+				  includingColorTags:YES
+					   closeFontTags:YES
+						   styleTags:YES
+		  closeStyleTagsOnFontChange:YES
+					  encodeNonASCII:YES
+						encodeSpaces:NO
+						  imagesPath:nil
+				   attachmentsAsText:YES
+		   onlyIncludeOutgoingImages:NO
+					  simpleTagsOnly:NO
+					  bodyBackground:NO];
+}
+
+@end
