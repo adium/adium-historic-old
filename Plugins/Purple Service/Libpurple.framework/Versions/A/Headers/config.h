7e0188298788dfbc2c2d8bdcac5d11448b5b0b14
Evan Schoenberg <Evan Schoenberg@localhost>
2007-10-13 16:19:10 +0000

    Libpurple.framework [501], which is im.pidgin.pidgin @ 5e03fe2d3a1f4d81543aeffc399cf6d05210e16b (as we are tracking HEAD in trunk to help get libpurple 2.3.0 out) and also links against libsasl2 and enables cyrus-sasl authentication, which may fix authentication issues with some xmpp servers

--- Plugins/Purple Service/Libpurple.framework/Versions/A/Headers/config.h
+++ Plugins/Purple Service/Libpurple.framework/Versions/A/Headers/config.h
@@ -106,7 +106,7 @@ first (like Motorola and SPARC, unlike Intel and VAX). */
 #define HAVE_CONNECT 1
 
 /* Define to 1 if Cyrus SASL is present */
-//#define HAVE_CYRUS_SASL 1
+#define HAVE_CYRUS_SASL 1
 
 /* Define if you have the external 'daylight' variable. */
 #define HAVE_DAYLIGHT 1
