66b5d844694c763ddcbba6ffc20f22ed25e16435
Peter Hosey <Peter Hosey@localhost>
2007-10-30 19:04:45 +0000

    Indented preprocessor directives in AITigerCompatibility, for easier reading.

--- Frameworks/AIUtilities Framework/Source/AITigerCompatibility.h
+++ Frameworks/AIUtilities Framework/Source/AITigerCompatibility.h
@@ -8,34 +8,34 @@
  */
 #ifndef AITigerCompatibility
 
-#define AITigerCompatibility
+#	define AITigerCompatibility
 
-#import <AvailabilityMacros.h>
+#	import <AvailabilityMacros.h>
 
-#ifndef MAC_OS_X_VERSION_10_5
+#	ifndef MAC_OS_X_VERSION_10_5
 
-#define NS_REQUIRES_NIL_TERMINATION
+#		define NS_REQUIRES_NIL_TERMINATION
 
-#if __LP64__ || NS_BUILD_32_LIKE_64
+#		if __LP64__ || NS_BUILD_32_LIKE_64
 typedef long NSInteger;
 typedef unsigned long NSUInteger;
-#else
+#		else
 typedef int NSInteger;
 typedef unsigned int NSUInteger;
-#endif
+#		endif
 
-#define NSIntegerMax    LONG_MAX
-#define NSIntegerMin    LONG_MIN
-#define NSUIntegerMax   ULONG_MAX
+#		define NSIntegerMax    LONG_MAX
+#		define NSIntegerMin    LONG_MIN
+#		define NSUIntegerMax   ULONG_MAX
 
-#define NSINTEGER_DEFINED 1
+#		define NSINTEGER_DEFINED 1
 
-#else
+#	else
 
-#if !defined(NS_REQUIRES_NIL_TERMINATION)
-#define NS_REQUIRES_NIL_TERMINATION __attribute__((sentinel))
-#endif
+#		if !defined(NS_REQUIRES_NIL_TERMINATION)
+#			define NS_REQUIRES_NIL_TERMINATION __attribute__((sentinel))
+#		endif
 
-#endif //MAC_OS_X_VERSION_10_5
+#	endif //MAC_OS_X_VERSION_10_5
 
 #endif //AITigerCompatibility
