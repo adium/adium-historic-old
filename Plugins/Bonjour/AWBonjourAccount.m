bd1ee1aeb89abad1626fdc7194eb016586b8e637
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-10-30 13:04:33 +0000

    merged [21412]: Fixed use of NSLock in initialization of the threads for Bonjour. As of 10.5, the fact that a standard NSLock should be used only on one thread is enforced. NSConditionLock is what we really wanted.

--- Plugins/Bonjour/AWBonjourAccount.m
+++ Plugins/Bonjour/AWBonjourAccount.m
@@ -41,10 +41,16 @@
 #import <AIUtilities/AIMutableOwnerArray.h>
 #import <AIUtilities/AIObjectAdditions.h>
 
-static	NSLock				*threadPreparednessLock = nil;
-static	NDRunLoopMessenger	*bonjourThreadMessenger = nil;
-static	AWEzv				*_libezvThreadProxy = nil;
-static	NSAutoreleasePool	*currentAutoreleasePool = nil;
+
+static	NSConditionLock     *threadPreparednessLock = nil;
+static	NDRunLoopMessenger  *bonjourThreadMessenger = nil;
+static	AWEzv               *_libezvThreadProxy = nil;
+static	NSAutoreleasePool   *currentAutoreleasePool = nil;
+
+typedef enum {
+	AIThreadPreparing = 0,
+	AIThreadReady
+};
 
 #define	AUTORELEASE_POOL_REFRESH	5.0
 
@@ -94,8 +100,7 @@ static	NSAutoreleasePool	*currentAutoreleasePool = nil;
 {
 	if (!_libezvThreadProxy) {
 		//Obtain the lock
-		threadPreparednessLock = [[NSLock alloc] init];
-		[threadPreparednessLock lock];
+		threadPreparednessLock = [[NSConditionLock alloc] initWithCondition:AIThreadPreparing];
 		
 		//Detach the thread, which will unlock threadPreparednessLock when it is ready
 		[NSThread detachNewThreadSelector:@selector(prepareBonjourThread)
@@ -103,7 +108,8 @@ static	NSAutoreleasePool	*currentAutoreleasePool = nil;
 							   withObject:nil];
 		
 		//Obtain the lock - this will spinlock until the thread is ready
-		[threadPreparednessLock lock];
+		[threadPreparednessLock lockWhenCondition:AIThreadReady];
+		[threadPreparednessLock unlock];
 		[threadPreparednessLock release]; threadPreparednessLock = nil;
 	}
 
@@ -566,6 +572,7 @@ static	NSAutoreleasePool	*currentAutoreleasePool = nil;
 	NSTimer				*autoreleaseTimer;
 
 	currentAutoreleasePool = [[NSAutoreleasePool alloc] init];
+	[threadPreparednessLock lock];
 
 	bonjourThreadMessenger = [[NDRunLoopMessenger runLoopMessengerForCurrentRunLoop] retain];
 	_libezvThreadProxy = [[bonjourThreadMessenger target:libezv] retain];
@@ -583,7 +590,7 @@ static	NSAutoreleasePool	*currentAutoreleasePool = nil;
 											   object:[NSThread currentThread]];
 
 	//We're good to go; release that lock
-	[threadPreparednessLock unlock];
+	[threadPreparednessLock unlockWithCondition:AIThreadReady];
 	CFRunLoopRun();
 
 	[self clearBonjourThreadInfo];
