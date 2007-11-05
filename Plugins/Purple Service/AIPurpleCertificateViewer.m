f5427e54bf7ea7a05bd6536ec06f430d0ef549d6
Andreas Monitzer <github@monitzer.com>
2007-11-05 12:27:29 +0000

    Maybe this helps compiling on/for Tiger

--- Plugins/Purple Service/AIPurpleCertificateViewer.m
+++ Plugins/Purple Service/AIPurpleCertificateViewer.m
@@ -9,6 +9,18 @@
 #import "AIPurpleCertificateViewer.h"
 #import <SecurityInterface/SFCertificateView.h>
 #import <AIUtilities/AITigerCompatibility.h>
+#import <AvailabilityMacros.h>
+#include <Security/cssmapi.h>
+#include <Security/oidscert.h>
+#include <Security/oidsattr.h>
+
+#if MAC_OS_X_VERSION_10_5 > MAC_OS_X_VERSION_MAX_ALLOWED
+@interface SFCertificateView (LeopardKnowsItAll)
+
+- (void)setDetailsDisclosed:(BOOL)perhaps;
+
+@end
+#endif
 
 @interface AIPurpleCertificateViewer (privateMethods)
 
@@ -59,11 +71,79 @@
 	return CFArrayGetCount(certificatechain);
 }
 
+#if MAC_OS_X_VERSION_10_5 > MAC_OS_X_VERSION_MAX_ALLOWED
+/* compare two OIDs, return CSSM_TRUE if identical */
+static CSSM_BOOL compareOids(
+							 const CSSM_OID *oid1,
+							 const CSSM_OID *oid2)
+{
+	if((oid1 == NULL) || (oid2 == NULL)) {
+		return CSSM_FALSE;
+	}	
+	if(oid1->Length != oid2->Length) {
+		return CSSM_FALSE;
+	}
+	if(memcmp(oid1->Data, oid2->Data, oid1->Length)) {
+		return CSSM_FALSE;
+	}
+	else {
+		return CSSM_TRUE;
+	}
+}
+#endif
+
 - (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
-	NSString *commonname;
+	NSString *commonname = nil;
 	SecCertificateRef cert = (SecCertificateRef)CFArrayGetValueAtIndex(certificatechain, row);
 	
+	// seriously WTF? I want my Leopard!
+#if MAC_OS_X_VERSION_10_5 > MAC_OS_X_VERSION_MAX_ALLOWED
+	CSSM_CL_HANDLE handle;
+	CSSM_DATA certdata;
+	uint32 numFields;
+	CSSM_HANDLE resultsHandle;
+	CSSM_DATA_PTR subject;
+	CSSM_X509_NAME_PTR x509Name;
+	OSStatus err = SecCertificateGetCLHandle(cert, &handle);
+	if(err == noErr) {
+		err = SecCertificateGetData(cert, &certdata);
+		
+		if(err == noErr) {
+			CSSM_RETURN ret = CSSM_CL_CertGetFirstFieldValue(handle, &certdata, &CSSMOID_X509V1SubjectNameCStruct, &resultsHandle, &numFields, (CSSM_DATA_PTR*)&subject);
+			unsigned rdnDex;
+			
+			if(ret == CSSM_OK) {
+				x509Name = (CSSM_X509_NAME_PTR)subject->Data;
+				
+				for(rdnDex=0; rdnDex<x509Name->numberOfRDNs; rdnDex++) {
+					CSSM_X509_RDN_PTR rdnp = &x509Name->RelativeDistinguishedName[rdnDex];
+					unsigned pairDex;
+					
+					for(pairDex=0; pairDex<rdnp->numberOfPairs; pairDex++) {
+						CSSM_X509_TYPE_VALUE_PAIR *ptvp = &rdnp->AttributeTypeAndValue[pairDex];
+						if(compareOids(&ptvp->type, &CSSMOID_CommonName)) {
+							CSSM_DATA_PTR cn = &ptvp->value;
+							NSData *data = [NSData dataWithBytes:cn->Data length:cn->Length];
+							
+							commonname = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+							break;
+						}
+					}
+					if(commonname)
+						break;
+				}
+			}
+		}
+	}
+
+	if(!commonname)
+		err = -1;
+	
+	CSSM_CL_FreeFieldValue(handle, &CSSMOID_X509V1SubjectNameCStruct, subject);
+	
+#else
 	OSStatus err = SecCertificateCopyCommonName(cert, (CFStringRef*)&commonname);
+#endif
 	
 	if(err == noErr)
 		return [commonname autorelease];
@@ -76,7 +156,8 @@
 	if(selectedRow != NSNotFound) {
 		SecCertificateRef cert = (SecCertificateRef)CFArrayGetValueAtIndex(certificatechain, selectedRow);
 		[certificateview setCertificate:cert];
-		[certificateview setDetailsDisclosed:YES]; // we want details!
+		if([certificateview respondsToSelector:@selector(setDetailsDisclosed:)])
+			[certificateview setDetailsDisclosed:YES]; // we want details!
 	} else
 		[certificateview setCertificate:NULL];
 }
