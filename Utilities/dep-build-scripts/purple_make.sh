1c22a5255df67754a92931e3fae1d75ac6ac346a
Evan Schoenberg <Evan Schoenberg@localhost>
2007-11-09 18:09:07 +0000

    Added the two patches for new sametime servers to fix contact presence and file transfers from mikael in sf bug #1626349

--- Utilities/dep-build-scripts/purple_make.sh
+++ Utilities/dep-build-scripts/purple_make.sh
@@ -21,6 +21,12 @@ export PATH_PPC="$TARGET_DIR_PPC/bin:$PATH"
 export PATH_I386="$TARGET_DIR_I386/bin:$PATH"
 
 # Meanwhile
+# Apply patches
+pushd ../$MEANWHILE
+patch --forward -p1 < ../meanwhile_ft_newservers_fix_1626349.diff
+patch --forward -p1 < ../meanwhile_prescence_newservers_fix_1626349.diff
+popd
+
 for ARCH in ppc i386 ; do
     export CFLAGS="$BASE_CFLAGS -arch $ARCH"
 	export LDFLAGS="$BASE_LDFLAGS -arch $ARCH"
@@ -68,7 +74,7 @@ done
 # Cyrus-SASL
 # Apply our patch
 pushd ../$SASL
-patch -p1 < ../$SASL.patch
+patch --forward -p1 < ../$SASL.patch
 popd
 
 for ARCH in ppc i386 ; do
@@ -84,8 +90,13 @@ for ARCH in ppc i386 ; do
     esac
     # In my experience, --enable-static horks things. Not sure why. Be careful
     # if you turn it back on. Check things with a completely clean build.
-    ../../$SASL/configure --prefix=$TARGET_DIR --disable-static \
-      --enable-shared --disable-macos-framework --host=$HOST
+    ../../$SASL/configure --prefix=$TARGET_DIR \
+        --disable-static --enable-shared \
+#        --with-staticsasl --disable-shared \
+        --disable-macos-framework \
+#        --with-openssl=/Developer/SDKs/MacOSX10.4u/sdk/usr/lib \
+#        --disable-digest --enable-krb4 \
+        --host=$HOST
     # EVIL HACK ALERT: http://www.theronge.com/2006/04/15/how-to-compile-cyrus-sasl-as-universal/
     # We edit libtool before we run make. This is evil and makes me sad.
     cat libtool | sed 's%archive_cmds="\\\$CC%archive_cmds="\\\$CC -mmacosx-version-min=10.4 -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -arch '$ARCH'%' > libtool.tmp
