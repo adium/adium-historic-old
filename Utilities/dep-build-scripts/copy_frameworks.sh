1197bc4bee2511ad4903cca789304e7ed915fb27
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-11-22 04:01:50 +0000

    Clear out the copied libXXX frameworks when copying frameworks, too

--- Utilities/dep-build-scripts/copy_frameworks.sh
+++ Utilities/dep-build-scripts/copy_frameworks.sh
@@ -5,4 +5,5 @@ cp -r "`dirname $0`"/build/Frameworks/*.framework "$ADIUM/Frameworks/"
 
 pushd "$ADIUM/build"
 rm -rf */AdiumLibpurple.framework 
+rm -rf */*/Adium.app/Contents/Frameworks/lib*
 popd
