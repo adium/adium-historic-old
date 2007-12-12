e24d0b647189a338995d7cdffcce96716fc1592a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-12-12 03:43:51 +0000

    Updated make_po_files to work

--- Utilities/dep-build-scripts/make_po_files.sh
+++ Utilities/dep-build-scripts/make_po_files.sh
@@ -9,6 +9,19 @@ for patch in "$PATCHDIR/libpurple-restrict-potfiles-to-libpurple.diff" ; do
 	cat $patch | patch --forward -p0
 done
 
-pushd po
-make update-po && make all && make install
 popd
+
+pushd $PATCHDIR/build/libpurple-i386/po
+#make update-po && make all && make install
+make all && make install
+popd
+
+pushd $PATCHDIR/build/root-i386/share/locale
+mkdir $PATCHDIR/build/Frameworks/libpurple.framework/Resources || true
+cp -v -r * $PATCHDIR/build/Frameworks/libpurple.framework/Resources
+popd
+
+pushd $PATCHDIR/build/Frameworks/libpurple.framework/Resources
+find . \( -name gettext-runtime.mo -or -name gettext-tools.mo -or -name glib20.mo \) -type f -delete
+popd
+
