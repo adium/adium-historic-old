3f75339de63d5c2f3b613c64563c200ddffd01fc
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-12-26 11:12:01 +0000

    Allow specification of a custom QQ server and port. We don't specify a default so that if the user doesn't enter a custom server/port the libpurple defaults - a rotating set of servers - will be used. Fixes #8661

--- Plugins/Purple Service/ESPurpleQQAccount.m
+++ Plugins/Purple Service/ESPurpleQQAccount.m
@@ -22,16 +22,6 @@
 	purple_account_set_bool(account, "use_tcp", [[self preferenceForKey:KEY_QQ_USE_TCP group:GROUP_ACCOUNT_STATUS] boolValue]);
 }
 
-- (NSString *)host
-{
-/* This is not technically right, since the qq plugin randomly chooses one of many different servers at connect time.
- * "sz.tencent.com" or "sz#.tencent.com" for UDP
- * "tcpconn.tencent.com" or "tcpconn#.tencent.com" where (# <= 6) for TCP.
- * Specifying the host is important for network reachability checking, though, and generally all hosts should be up if one is reachable.
- */
-	return ([[self preferenceForKey:KEY_QQ_USE_TCP group:GROUP_ACCOUNT_STATUS] boolValue] ? @"tcpconn.tencent.com" : @"sz.tencent.com");
-}
-
 /*!
  * @brief The server name to be passed to libpurple
  * QQ prpl will choose a server randomly for load balancing if we don't pass one, so do that.  -[self host] returns the first server
@@ -39,7 +29,22 @@
  */
 - (NSString *)hostForPurple
 {
-	return NULL;
+	NSString *specifiedHost = [self preferenceForKey:KEY_CONNECT_HOST group:GROUP_ACCOUNT_STATUS];
+	return (specifiedHost ? specifiedHost : nil);			
+}
+
+- (NSString *)host
+{
+/* This is not technically right, since the qq plugin randomly chooses one of many different servers at connect time.
+ * "sz.tencent.com" or "sz#.tencent.com" for UDP
+ * "tcpconn.tencent.com" or "tcpconn#.tencent.com" where (# <= 6) for TCP.
+ * Specifying the host is important for network reachability checking, though, and generally all hosts should be up if one is reachable.
+ */
+	NSString *host = [self hostForPurple];
+	if (!host)
+		host = ([[self preferenceForKey:KEY_QQ_USE_TCP group:GROUP_ACCOUNT_STATUS] boolValue] ? @"tcpconn.tencent.com" : @"sz.tencent.com");
+	
+	return host;
 }
 
 #pragma mark Account Action Menu Items
