321fca40b3de66168d0e9925863745606368d5ec
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-01-08 04:20:20 +0000

    libpurple 2.3.1 with patches as of [22189] which includes im.pidgin.pidgin 6227c43549bf66022512f18bb36d70b7c57c4430 to fix jabber cert checking to use the domain of the JID. Fixes #8787

--- Frameworks/libpurple.framework/Versions/0.3.1/Headers/jabber.h
+++ Frameworks/libpurple.framework/Versions/0.3.1/Headers/jabber.h
@@ -191,6 +191,8 @@ struct _JabberStream
 	char *old_uri;
 	int old_length;
 	char *old_track;
+	
+	char *certificate_CN;
 };
 
 typedef gboolean (JabberFeatureEnabled)(JabberStream *js, const gchar *shortname, const gchar *namespace);
