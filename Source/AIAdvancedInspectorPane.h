f75b8c095be08dda01c2925bea778fa743992e3f
Elliott Harris <elliot@adium.im>
2008-02-11 18:23:22 +0000

    Forgot to include source, hopefully this resolves everything.

--- Source/AIAdvancedInspectorPane.h
+++ Source/AIAdvancedInspectorPane.h
@@ -8,10 +8,53 @@
 
 #import <Cocoa/Cocoa.h>
 #import <Adium/AIListObject.h>
+#import <Adium/AIListContact.h>
+#import <Adium/AIChat.h>
+#import <Adium/AIContactControllerProtocol.h>
+#import <Adium/AIContentControllerProtocol.h>
+#import <AIUtilities/AIMenuAdditions.h>
+#import <AIUtilities/AIPopUpButtonAdditions.h>
+#import <AIUtilities/AIStringFormatter.h>
+#import <AIContactInfoContentController.h>
 
-@interface AIAdvancedInspectorPane : AIObject {
+//imports from old accounts pane
+#import <Adium/AIAccountControllerProtocol.h>
+#import <AIUtilities/AIAlternatingRowTableView.h>
+#import <AIUtilities/AIArrayAdditions.h>
+#import <Adium/AIAccount.h>
+#import <Adium/AIListContact.h>
+#import <Adium/AIListObject.h>
+#import <Adium/AIListGroup.h>
+#import <Adium/AILocalizationTextField.h>
+#import <Adium/AIMetaContact.h>
 
+@interface AIAdvancedInspectorPane : AIObject <AIContentInspectorPane> {
+	IBOutlet	NSView							*inspectorContentView;
+	
+	IBOutlet	AIAlternatingRowTableView		*accountsTableView;
+	IBOutlet	NSTableColumn					*contactsColumn;
+	
+	IBOutlet	NSTextField						*accountsLabel;
+	IBOutlet	NSPopUpButton					*accountsButton;
+	
+	IBOutlet	NSTextField						*encryptionField;
+	IBOutlet	NSPopUpButton					*encryptionButton;
+	
+	IBOutlet	NSTextField						*visibilityField;
+	IBOutlet	NSButton						*visibilityButton;
+	
+				AIListObject					*displayedObject;
+				NSArray							*accounts;
+				NSArray							*contacts;
+				BOOL							contactsColumnIsInAccountsTableView;
 }
+
 -(NSString *)nibName;
+-(NSView *)inspectorContentView;
 -(void)updateForListObject:(AIListObject *)inObject;
+
+-(IBAction)selectAccount:(id)sender;
+- (IBAction)selectedEncryptionPreference:(id)sender;
+- (IBAction)setVisible:(id)sender;
+
 @end
--- Source/AIEventsInspectorPane.h
+++ Source/AIEventsInspectorPane.h
@@ -8,10 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 #import <Adium/AIListObject.h>
+#import <Adium/AIListContact.h>
+#import <AIContactInfoContentController.h>
+#import <Adium/AIContactControllerProtocol.h>
+#import <Adium/ESContactAlertsViewController.h>
 
-@interface AIEventsInspectorPane : AIObject {
-
+@interface AIEventsInspectorPane : AIObject <AIContentInspectorPane> {
+			 AIListObject						*displayedObject;
+	
+	IBOutlet NSView								*inspectorContentView;
+	IBOutlet ESContactAlertsViewController		*alertsController;
+	//Other IBOutlets here
 }
+
 -(NSString *)nibName;
+-(NSView *)inspectorContentView;
 -(void)updateForListObject:(AIListObject *)inObject;
+
 @end
