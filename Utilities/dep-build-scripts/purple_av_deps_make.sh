bb16a6fc0605028db947da2db308043d6d84c3a1
durin42 <durin42@localhost>
2008-03-13 02:58:15 +0000

    Welcome to Adium AV. Step one of the build script done.

---
+++ Utilities/dep-build-scripts/purple_av_deps_make.sh
@@ -0,0 +1,44 @@
+LIBOGG=libogg-1.1.3
+
+mkdir -p build/universal
+cd build
+
+SDK_ROOT="/Developer/SDKs/MacOSX10.4u.sdk"
+BASE_CFLAGS="-mmacosx-version-min=10.4 -isysroot $SDK_ROOT"
+BASE_LDFLAGS="-mmacosx-version-min=10.4 -headerpad_max_install_names -Wl,-syslibroot,$SDK_ROOT"
+
+NUMBER_OF_CORES=`sysctl -n hw.activecpu`
+
+TARGET_DIR_PPC="$PWD/root-ppc"
+TARGET_DIR_I386="$PWD/root-i386"
+TARGET_DIR_BASE="$PWD/root"
+export PATH_USER="$PATH"
+export PATH_PPC="$TARGET_DIR_PPC/bin:$PATH"
+export PATH_I386="$TARGET_DIR_I386/bin:$PATH"
+
+#libogg
+for ARCH in ppc i386 ; do
+    export CFLAGS="$BASE_CFLAGS -arch $ARCH"
+	export LDFLAGS="$BASE_LDFLAGS -arch $ARCH"
+    mkdir -p libogg-$ARCH
+    cd libogg-$ARCH
+    export ARCH
+	case $ARCH in
+		ppc) TARGET_DIR="$TARGET_DIR_PPC"
+			 export PATH="$PATH_PPC";;
+		i386) TARGET_DIR="$TARGET_DIR_I386"
+			  export PATH="$PATH_I386";;
+	esac
+    ../../$LIBOGG/configure --prefix=$TARGET_DIR 
+	# We edit libtool before we run make. This is evil and makes me sad.
+    cat libtool | sed 's%archive_cmds="\\\$CC%archive_cmds="\\\$CC -mmacosx-version-min=10.4 -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -arch '$ARCH'%' > libtool.tmp
+    mv libtool.tmp libtool
+    make -j $NUMBER_OF_CORES && make install
+    cd ..
+done
+
+
+#libvorbis
+#libspeex
+#libtheora
+#taglib
