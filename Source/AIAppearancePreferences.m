fcb2feda5df2a8fc6e04e9bbed8c3141927971cd
Evan Schoenberg <Evan Schoenberg@localhost>
2008-03-21 01:08:00 +0000

    Patch from '''hortont424''' which fixes a very longstanding problem which forced us to limit minimum transparency of the contact list to 5% opaque.  Turns out we can tell windows not to ignore mouse events!  I modified the patch slightly to maintain the old behavior for the "Bubbles (To Fit)" window style.
    Put the minimum opacity at 0% where it belongs. Hooray!
    
    Fixes #5104.

--- Source/AIAppearancePreferences.m
+++ Source/AIAppearancePreferences.m
@@ -87,7 +87,10 @@ typedef enum {
  * @brief Configure the preference view
  */
 - (void)viewDidLoad
-{	
+{
+	[slider_windowOpacity setMinValue:0.0];
+	[slider_windowOpacity setMaxValue:100.0];
+
 	//Other list options
 	[popUp_windowStyle setMenu:[self _windowStyleMenu]];
 		
