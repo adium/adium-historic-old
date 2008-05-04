9a5ce2d586b092848c6d2673fa765224522452c2
Zachary West <zacwest@gmail.com>
2008-05-04 21:39:49 +0000

    Give the Attach and Detach their own context menu location.

--- Frameworks/Adium Framework/Source/AIMenuControllerProtocol.h
+++ Frameworks/Adium Framework/Source/AIMenuControllerProtocol.h
@@ -32,7 +32,7 @@ typedef enum {
 } AIMenuLocation;
 
 typedef enum {
-    Context_Group_Manage,Context_Contact_Manage, Context_Contact_Action, Context_Contact_NegativeAction,
+    Context_Group_AttachDetach,Context_Group_Manage,Context_Contact_Manage, Context_Contact_Action, Context_Contact_NegativeAction,
     Context_Contact_Message, Context_Contact_Additions, Context_Contact_ChatAction, Context_Contact_Stranger_ChatAction, Context_Contact_ListAction,
 	Context_Contact_GroupChatAction,
 	Context_Tab_Action,
