c0be93585a9bbd31e7f093f8d3f7a3709b0cfd8e
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-05-08 04:57:05 +0000

    When an object's address book preferences change, don't let its preferences become those of the global plugin. This fixes loss of AB data after changing a per-contact address book setting such as associating a contact with an address book card. Fixes #9303. Also, don't set the phonetic name if it's the same as the display name when adding to the address book.

--- Source/ESAddressBookIntegrationPlugin.m
+++ Source/ESAddressBookIntegrationPlugin.m
@@ -476,6 +476,8 @@ NSString* serviceIDForJabberUID(NSString *UID);
 - (void)preferencesChangedForGroup:(NSString *)group key:(NSString *)key
 							object:(AIListObject *)object preferenceDict:(NSDictionary *)prefDict firstTime:(BOOL)firstTime
 {
+	if (object) return;
+
     if ([group isEqualToString:PREF_GROUP_ADDRESSBOOK]) {
 		BOOL			oldCreateMetaContacts = createMetaContacts;
 		
@@ -1122,7 +1124,6 @@ NSString* serviceIDForJabberUID(NSString *UID)
 			NSString *key;
 			
 			dict = [addressBookDict objectForKey:serviceID];
-			
 			keysEnumerator = [[dict allKeysForObject:uniqueID] objectEnumerator];
 			
 			//The same person may have multiple accounts from the same service; we should remove them all.
@@ -1180,8 +1181,9 @@ NSString* serviceIDForJabberUID(NSString *UID)
 		
 		//Set the name
 		[person setValue:[contact displayName] forKey:kABFirstNameProperty];
-		[person setValue:[contact phoneticName] forKey:kABFirstNamePhoneticProperty];
-		
+		if (![[contact phoneticName] isEqualToString:[contact displayName]])
+			[person setValue:[contact phoneticName] forKey:kABFirstNamePhoneticProperty];
+
 		NSString				*UID = [contact formattedUID];
 	
 		NSEnumerator * containedContactEnu = [contact isKindOfClass:[AIMetaContact class]] ? [[(AIMetaContact *)contact listContacts] objectEnumerator] : [[NSArray arrayWithObject:contact] objectEnumerator];
