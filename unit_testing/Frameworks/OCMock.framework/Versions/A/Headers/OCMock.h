bc7a1082270ee6aa9fc7b6b5a0bc315bf54b22e1
bjhomer <bjhomer@localhost>
2008-05-13 17:02:49 +0000

    Removed old OCMock framework in preparation to upgrade to new version.

--- unit_testing/Frameworks/OCMock.framework/Versions/A/Headers/OCMock.h
+++
@@ -1,7 +0,0 @@
-//---------------------------------------------------------------------------------------
-//  $Id: OCMock.h 2 2004-08-24 17:00:05Z erik $
-//  Copyright (c) 2004 by Mulle Kybernetik. See License file for details.
-//---------------------------------------------------------------------------------------
-
-#import "OCMockObject.h"
-#import "OCMockRecorder.h"
