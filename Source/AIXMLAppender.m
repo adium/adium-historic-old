3f884e836e167e2daaa21f43f7aa4bd4e39952ea
Peter Hosey <git@boredzo.org>
2008-05-17 02:22:50 +0000

    Reverting r17828, which contaminated AIXMLAppender with GPL code. It appears to now be GPL-free once again.

--- Source/AIXMLAppender.m
+++ Source/AIXMLAppender.m
@@ -39,7 +39,6 @@
 
 #import "AIXMLAppender.h"
 #define BSD_LICENSE_ONLY 1
-#import <AIUtilities/AIFileManagerAdditions.h>
 #import <AIUtilities/AIStringAdditions.h>
 #import <sys/stat.h>
 #include <unistd.h>
@@ -91,9 +90,55 @@ enum {
 		rootElementName = nil;
 		filePath = [path copy];
 		initialized = NO;
-		fsyncAfterEachAppend = YES;
 
 		[self prepareFileHandle];
+		
+		//Check if the file already exists
+		if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+			//Get the root element name and set initialized
+			rootElementName = [[self rootElementNameForFileAtPath:filePath] retain];
+			initialized = (rootElementName != nil);				
+		//We may need to create the directory structure, so call this just in case
+		} else {
+			NSFileManager *mgr = [NSFileManager defaultManager];
+
+			//Save the current working directory, so we can change back to it.
+			NSString *savedWorkingDirectory = [mgr currentDirectoryPath];
+			//Change to the root.
+			[mgr changeCurrentDirectoryPath:@"/"];
+
+			/*Create each component of the path, then change into it.
+			 *E.g. /foo/bar/baz:
+			 *	cd /
+			 *	mkdir foo
+			 *	cd foo
+			 *	mkdir bar
+			 *	cd bar
+			 *	mkdir baz
+			 *	cd baz
+			 *	cd $savedWorkingDirectory
+			 */
+			NSArray *pathComponents = [[filePath stringByDeletingLastPathComponent] pathComponents];
+			NSEnumerator *pathComponentsEnum = [pathComponents objectEnumerator];
+			NSString *component;
+			while ((component = [pathComponentsEnum nextObject])) {
+				[mgr createDirectoryAtPath:component attributes:nil];
+				[mgr changeCurrentDirectoryPath:component];
+			}
+
+			[mgr changeCurrentDirectoryPath:savedWorkingDirectory];
+		}
+		
+		//Open our file handle and seek if necessary
+		const char *pathCString = [filePath fileSystemRepresentation];
+		int fd = open(pathCString, O_CREAT | O_WRONLY, 0644);
+		file = [[NSFileHandle alloc] initWithFileDescriptor:fd closeOnDealloc:YES];
+		if (initialized) {
+			struct stat sb;
+			fstat(fd, &sb);
+			int closingTagLength = [rootElementName length] + 4; //</rootElementName>
+			[file seekToFileOffset:sb.st_size - closingTagLength];
+		}
 	}
 
 	return self;
@@ -157,8 +202,34 @@ enum {
 		rootElementName = [[self rootElementNameForFileAtPath:filePath] retain];
 		initialized = (rootElementName != nil);				
 		//We may need to create the directory structure, so call this just in case
-	} else {
-		[manager createDirectoriesForPath:[filePath stringByDeletingLastPathComponent]];
+	} else { 
+		NSFileManager *mgr = [NSFileManager defaultManager]; 
+
+		//Save the current working directory, so we can change back to it. 
+		NSString *savedWorkingDirectory = [mgr currentDirectoryPath]; 
+		//Change to the root. 
+		[mgr changeCurrentDirectoryPath:@"/"]; 
+
+		/*Create each component of the path, then change into it. 
+		 *E.g. /foo/bar/baz: 
+		 *	  cd / 
+		 *	  mkdir foo 
+		 *	  cd foo 
+		 *	  mkdir bar 
+		 *	  cd bar 
+		 *	  mkdir baz 
+		 *	  cd baz 
+		 *	  cd $savedWorkingDirectory 
+		 */ 
+		NSArray *pathComponents = [[filePath stringByDeletingLastPathComponent] pathComponents]; 
+		NSEnumerator *pathComponentsEnum = [pathComponents objectEnumerator]; 
+		NSString *component; 
+		while ((component = [pathComponentsEnum nextObject])) { 
+				[mgr createDirectoryAtPath:component attributes:nil]; 
+				[mgr changeCurrentDirectoryPath:component]; 
+		} 
+
+		[mgr changeCurrentDirectoryPath:savedWorkingDirectory]; 
 		initialized = NO;
 	}
 	
