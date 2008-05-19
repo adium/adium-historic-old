2b7efa1c16afe6453e4dfc59b2854df4d2dff31c
Peter Hosey <Peter Hosey@localhost>
2008-05-19 15:57:41 +0000

    Added a CLI tool to run the stress-test, in order to make it Sharkable/Xrayable.

---
+++ Frameworks/AutoHyperlinks Framework/Source/StressTestMain.m
@@ -0,0 +1,35 @@
+#import <Cocoa/Cocoa.h>
+#import <SenTestingKit/SenTestingKit.h>
+#import "StressTest.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sysexits.h>
+
+int main(int argc, char **argv) {
+	NSUInteger numIterations = 1U;
+	if (*++argv) {
+		//We have at least one argument. Interpret it as the number of times to run the test.
+		numIterations = strtoul(*argv, /*next*/ NULL, /*radix*/ 0);
+		if (numIterations == 0U) {
+			fprintf(stderr, "Could not interpret number of iterations: %s\n", *argv);
+			return EX_USAGE;
+		}
+	}
+
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+	StressTest *test = [[StressTest alloc] initWithSelector:@selector(testStress)];
+	SenTestRun *run = [[SenTestRun alloc] initWithTest:test];
+
+	[run start];
+	[test performTest:run];
+	[run stop];
+
+	BOOL success = [run hasSucceeded];
+	NSLog(@"Test %@ in %f seconds", success ? @"succeeded" : @"failed", [[run stopDate] timeIntervalSinceDate:[run startDate]]);
+
+	[pool drain]; //Glug glug glug
+
+	return success ? EXIT_SUCCESS : EXIT_FAILURE;
+}
