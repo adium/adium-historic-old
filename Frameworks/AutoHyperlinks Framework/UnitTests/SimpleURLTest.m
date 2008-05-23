550457d680bc62515020e71e81fbe5b7a741b735
Peter Hosey <Peter Hosey@localhost>
2008-05-23 14:49:47 +0000

    Corrected uID that I intended to be 0, not 02.

--- Frameworks/AutoHyperlinks Framework/UnitTests/SimpleURLTest.m
+++ Frameworks/AutoHyperlinks Framework/UnitTests/SimpleURLTest.m
@@ -444,7 +444,7 @@
 	testHyperlink(@"gtalk:call?jid=example@gmail.com&from_jid=example2@gmail.com");
 	testHyperlink(@"gtalk:gtalk?jid=example@gmail.com");
 	testHyperlink(@"myim:addContact?uID=0&cID=42");
-	testHyperlink(@"myim:addContact?uID=02&cID=&auto=true");
+	testHyperlink(@"myim:addContact?uID=0&cID=&auto=true");
 	testHyperlink(@"myim:addContact?uID=42&cID=42");
 	testHyperlink(@"myim:addContact?uID=42&cID=&auto=true");
 	testHyperlink(@"myim:sendIM?uID=0&cID=42");
