5d03b93a03b31ced1d5780bbcdb3c325745a123f
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-05-28 00:10:24 +0000

    Learned about the 'logged in from another location' error number from pidgin-facebookchat

--- Plugins/Facebook Service/AIFacebookBuddyListManager.m
+++ Plugins/Facebook Service/AIFacebookBuddyListManager.m
@@ -78,6 +78,7 @@
 		if ([status isKindOfClass:[NSNull class]]) status = nil;
 
 		[listContact setFormattedUID:name notify:NotifyLater];
+	
 		[listContact setStatusMessage:(status ? 
 									   [[[NSAttributedString alloc] initWithString:status] autorelease] :
 									   nil)
@@ -199,9 +200,13 @@
 	NSDictionary *buddyListJSONDict = [receivedString JSONValue];
 
 	AILogWithSignature(@"%@", buddyListJSONDict);
-	if ([[buddyListJSONDict objectForKey:@"errorSummary"] length]) {
-		if ([[buddyListJSONDict objectForKey:@"errorSummary"] isEqualToString:@"Not Logged In"]) {
-			[account reconnect];
+	if ([[buddyListJSONDict objectForKey:@"error"] length]) {
+		if ([[buddyListJSONDict objectForKey:@"errorSummary"] length] &&
+			[[buddyListJSONDict objectForKey:@"errorSummary"] isEqualToString:@"Not Logged In"]) {
+				[account reconnect];		
+		} else if ([[buddyListJSONDict objectForKey:@"error"] isEqualToString:@"1357001"]) {
+			[account setLastDisconnectionError:AILocalizedString(@"Logged in from another location", nil)];
+			[account disconnect];
 		}
 	}
 
