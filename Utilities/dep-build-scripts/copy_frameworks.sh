8075a822a002834b608f8e989130dcdace3863e2
Evan Schoenberg <Evan Schoenberg@localhost>
2008-06-07 18:01:07 +0000

    Commented out a stray cp I left in from testing

--- Utilities/dep-build-scripts/copy_frameworks.sh
+++ Utilities/dep-build-scripts/copy_frameworks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 ADIUM="`dirname $0`/../.."
 
-cp build/universal/libmsn-pecan.so build/Frameworks/libpurple.framework/Resources
+# cp build/universal/libmsn-pecan.so build/Frameworks/libpurple.framework/Resources
 
 cp -r "`dirname $0`"/build/Frameworks/*.framework "$ADIUM/Frameworks/"
 
