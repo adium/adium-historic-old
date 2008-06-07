8e9e9a8d631d2894e7e83bf31af331f1739ace5f
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-06-07 17:59:02 +0000

    Patch from '''tomgr''' which cleans up the dependencies build scripts. Closes #9982. Thanks :)

--- Utilities/dep-build-scripts/copy_frameworks.sh
+++ Utilities/dep-build-scripts/copy_frameworks.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 ADIUM="`dirname $0`/../.."
 
+cp build/universal/libmsn-pecan.so build/Frameworks/libpurple.framework/Resources
+
 cp -r "`dirname $0`"/build/Frameworks/*.framework "$ADIUM/Frameworks/"
 
 pushd "$ADIUM/build"
-rm -rf */AdiumLibpurple.framework 
-rm -rf */*/Adium.app/Contents/Frameworks/lib*
+	rm -rf */AdiumLibpurple.framework 
+	rm -rf */*/Adium.app/Contents/Frameworks/lib*
 popd
--- Utilities/dep-build-scripts/universalize.sh
+++ Utilities/dep-build-scripts/universalize.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-#create universal libraries for AdiumDeps.
+
+source common.sh
+setupDirStructure
+cd "$BUILDDIR"
+
+# create universal libraries for AdiumDeps.
 # "top-level" deps
 LIBINTL=libintl.8
 LIBGLIB=libglib-2.0.0
@@ -17,19 +22,10 @@ PURPLE_VERSION=0.5.0
 LIBPURPLE=libpurple.$PURPLE_VERSION
 PURPLE_FOLDER=libpurple-$PURPLE_VERSION
 
-SCRIPT_DIR=$PWD
+SCRIPT_DIR=$BASEDIR
 
-mkdir build || true
-cd build
-
-TARGET_DIR_PPC="$PWD/root-ppc"
-TARGET_DIR_I386="$PWD/root-i386"
-TARGET_DIR_BASE="$PWD/root"
-
-mkdir universal || true
-UNIVERSAL_DIR="$PWD/universal"
-
-# Copy the headers to the universal dir so that we can put them in the frameworks once they are built. We stick the required headers for each framework into it's own folder
+# Copy the headers to the universal dir so that we can put them in the frameworks 
+# once they are built. We stick the required headers for each framework into its own folder
 # named after the project to keep the frameworkize script library independent.
 
 mkdir $UNIVERSAL_DIR/include || true
@@ -70,7 +66,7 @@ cd $UNIVERSAL_DIR
 for lib in $LIBINTL $LIBGLIB $LIBGOBJECT $LIBGTHREAD $LIBGMODULE $MEANWHILE \
            $GADU $LIBPURPLE; do
 	echo "Making $lib universal..."
-	python  $SCRIPT_DIR/framework_maker/universalize.py \
+	python $SCRIPT_DIR/framework_maker/universalize.py \
 	  i386:$TARGET_DIR_I386/lib/$lib.dylib \
 	  ppc:$TARGET_DIR_PPC/lib/$lib.dylib \
 	  $UNIVERSAL_DIR/$lib.dylib \
@@ -78,14 +74,29 @@ for lib in $LIBINTL $LIBGLIB $LIBGOBJECT $LIBGTHREAD $LIBGMODULE $MEANWHILE \
       $TARGET_DIR_I386/lib:$UNIVERSAL_DIR
 done
 
+#	echo "Making libmsn-pecan.so universal..."
+#	python  $SCRIPT_DIR/framework_maker/universalize.py \
+#	  i386:$TARGET_DIR_I386/lib/purple-2/libmsn-pecan.so \
+#	  ppc:$TARGET_DIR_PPC/lib/purple-2/libmsn-pecan.so \
+#	  $UNIVERSAL_DIR/libmsn-pecan.so \
+#	  $TARGET_DIR_PPC/lib:$UNIVERSAL_DIR \
+#      $TARGET_DIR_I386/lib:$UNIVERSAL_DIR
+
 cd ..
 
 export PATH="$PATH:$SCRIPT_DIR/rtool_trunk"
 echo "Making a framework for $PURPLE_FOLDER and all dependencies..."
 python $SCRIPT_DIR/framework_maker/frameworkize.py $UNIVERSAL_DIR/$LIBPURPLE.dylib $PWD/Frameworks
 
+#msn pecan
+install_name_tool -change $UNIVERSAL_DIR/libmsn-pecan.so @executable_path/../Frameworks/Resources/libmsn-pecan.so $UNIVERSAL_DIR/libmsn-pecan.so
+install_name_tool -change $UNIVERSAL_DIR/libpurple.0.dylib @executable_path/../Frameworks/libpurple.framework/Versions/Current/libpurple $UNIVERSAL_DIR/libmsn-pecan.so
+install_name_tool -change $UNIVERSAL_DIR/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib.framework/Versions/Current/libglib $UNIVERSAL_DIR/libmsn-pecan.so
+install_name_tool -change $UNIVERSAL_DIR/libgobject-2.0.0.dylib @executable_path/../Frameworks/libgobject.framework/Versions/Current/libgobject $UNIVERSAL_DIR/libmsn-pecan.so
+
 echo "Adding the Adium framework header."
-cp $SCRIPT_DIR/libpurple-full.h \
-   $PWD/Frameworks/libpurple.framework/Headers/libpurple.h
+cp $SCRIPT_DIR/libpurple-full.h $PWD/Frameworks/libpurple.framework/Headers/libpurple.h
 
 cp $SCRIPT_DIR/Libpurple-Info.plist $PWD/Frameworks/libpurple.framework/Resources/Info.plist
+
+echo "Done - now run ./make_po_files.sh (if necessary) then ./copy_frameworks.sh"
