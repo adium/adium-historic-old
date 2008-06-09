6fa634efb1b4e555621ed8d3a49963d0456b2024
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-06-09 22:22:50 +0000

    Avoid setting a status message for a Facebook contact with no status which is actually an NSAttributedString with no backing string. This should resolve sporadic Console warnings seen when Facebook is connected.

--- Plugins/Facebook Service/AIFacebookBuddyListManager.m
+++ Plugins/Facebook Service/AIFacebookBuddyListManager.m
@@ -79,7 +79,7 @@
 
 		[listContact setFormattedUID:name notify:NotifyLater];
 	
-		[listContact setStatusMessage:(![status isEqualToString:@""] ? 
+		[listContact setStatusMessage:((status && ![status isEqualToString:@""]) ? 
 									   [[[NSAttributedString alloc] initWithString:status] autorelease] :
 									   nil)
 							   notify:NotifyLater];
