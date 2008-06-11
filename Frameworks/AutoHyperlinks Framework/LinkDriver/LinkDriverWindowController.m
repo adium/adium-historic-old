102e26528503dd62415bafc51b88d175cc94c91d
Stephen Holt <Stephen Holt@localhost>
2008-06-11 17:14:54 +0000

    Make the threaded stress test use more threads (8), and make both that and the LinkDriver share the same AHHyperlinkScanner instance between threads

--- Frameworks/AutoHyperlinks Framework/LinkDriver/LinkDriverWindowController.m
+++ Frameworks/AutoHyperlinks Framework/LinkDriver/LinkDriverWindowController.m
@@ -8,18 +8,26 @@
 
 #import "LinkDriverWindowController.h"
 
+#define	SCANNER_KEY @"linkScanner"
+#define VIEW_KEY @"linkView"
 
 @implementation LinkDriverWindowController
 -(IBAction) linkifyTextView:(id)sender {
-	[NSThread	detachNewThreadSelector:@selector(performLinkification:) toTarget:self withObject:linkifyView];
-	[NSThread	detachNewThreadSelector:@selector(performLinkification:) toTarget:self withObject:otherView];
+	AHHyperlinkScanner	*scanner = [[AHHyperlinkScanner alloc] initWithStrictChecking:NO];
+	[NSThread	detachNewThreadSelector:@selector(performLinkification:)
+							   toTarget:self
+							 withObject:[NSDictionary dictionaryWithObjectsAndKeys:scanner,SCANNER_KEY,linkifyView,VIEW_KEY,nil]];
+	[NSThread	detachNewThreadSelector:@selector(performLinkification:)
+							   toTarget:self
+							 withObject:[NSDictionary dictionaryWithObjectsAndKeys:scanner,SCANNER_KEY,otherView,VIEW_KEY,nil]];
 }
 
--(void) performLinkification:(NSTextView *)inView
+-(void) performLinkification:(NSDictionary *)inDict
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	AHHyperlinkScanner	*scanner = [[AHHyperlinkScanner alloc] initWithStrictChecking:NO];
-	[scanner linkifyTextView:[inView retain]];
+	AHHyperlinkScanner	*scanner = [inDict objectForKey:SCANNER_KEY];
+	NSTextView	*myView = [inDict objectForKey:VIEW_KEY];
+	[scanner linkifyTextView:[myView retain]];
 	[pool release];
 }
 @end
