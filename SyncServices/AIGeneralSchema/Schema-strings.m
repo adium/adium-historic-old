8a19e80a061ab719df0cdecffea74b4d4f7955d0
Stephen Holt <sholt@steveholt.org>
2008-06-13 16:48:20 +0000

    copied sycnServices dir and recreated Xcode targets

---
+++ SyncServices/AIGeneralSchema/Schema-strings.m
@@ -0,0 +1,18 @@
+//
+//  Schema-strings.m
+//  AIGeneralSchema
+//
+//  Created by Brian Ganninger on 9/6/06.
+//  Copyright __MyCompanyName__ 2006. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// --- Data Classes ---
+NSString * const ISyncDataClass_Com_MyCompany_MySchema_Dataclass = @"com.mycompany.myschema.dataclass";
+
+// +++ Entity Types +++
+NSString * const ISyncEntity_Com_MyCompany_MySchema_Firstnames = @"com.mycompany.myschema.firstnames";
+NSString * const ISyncAttribute_Com_MyCompany_MySchema_Firstnames_FirstNameAttribute = @"firstNameAttribute";
+NSString * const ISyncRelationship_Com_MyCompany_MySchema_Firstnames_Friends = @"friends";
+
