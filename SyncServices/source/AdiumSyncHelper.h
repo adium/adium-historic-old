589bff7e87abcebdd30a55a996d7a177e29bcb0a
Stephen Holt <Stephen Holt@localhost>
2008-06-13 16:48:20 +0000

    copied sycnServices dir and recreated Xcode targets

---
+++ SyncServices/source/AdiumSyncHelper.h
@@ -0,0 +1,16 @@
+//
+//  AdiumSyncHelper.h
+//  Adium
+//
+//  Created by Stephen Holt on 6/13/08.
+//  Copyright 2008 Adium. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AdiumSyncHelper : NSObject {
+
+}
+
+@end
---
+++ SyncServices/source/AdiumSyncHelper.m
@@ -0,0 +1,14 @@
+//
+//  AdiumSyncHelper.m
+//  Adium
+//
+//  Created by Stephen Holt on 6/13/08.
+//  Copyright 2008 Adium. All rights reserved.
+//
+
+#import "AdiumSyncHelper.h"
+
+
+@implementation AdiumSyncHelper
+
+@end
