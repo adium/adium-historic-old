369c50b3d229fe995a38b2af16b836415b526335
Zachary West <zacwest@gmail.com>
2008-06-14 17:38:42 +0000

    For the log viewer, display the formatted UID along with the alias if the contact has an alias set. I find it very hard to follow meta contacts without this (and I don't want to see UIDs in the contact list).

--- Source/AIAbstractLogViewerWindowController.m
+++ Source/AIAbstractLogViewerWindowController.m
@@ -1549,7 +1549,11 @@ NSArray *pathComponentsForDocument(SKDocumentRef inDocument)
 																																		UID:[theLog to]]];
 			if (listObject) {
 				//Use the longDisplayName, following the user's contact list preferences as this is presumably how she wants to view contacts' names.
-				value = [listObject longDisplayName];
+				if (![[listObject displayName] isEqualToString:[listObject UID]]) {
+					value = [NSString stringWithFormat:@"%@ (%@)", [listObject displayName], [listObject UID]];
+				} else {
+					value = [listObject formattedUID];
+				}
 
 			} else {
 				//No username available
